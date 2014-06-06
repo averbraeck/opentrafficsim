289c121cbc39298ce8cc17379e242ccf6476c093
OTSim <P.Knoppers@tudelft.nl>
2014-06-06 08:42:42 +0000

    Added MassUnitTests

---
+++ ots-core/src/test/java/org/opentrafficsim/core/unit/MassUnitTests.java
@@ -0,0 +1,92 @@
+package org.opentrafficsim.core.unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Locale;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opentrafficsim.core.locale.DefaultLocale;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2014 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved.
+ * <p>
+ * See for project information <a href="http://www.simulation.tudelft.nl/"> www.simulation.tudelft.nl</a>.
+ * <p>
+ * The DSOL project is distributed under the following BSD-style license:<br>
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ * <ul>
+ * <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.</li>
+ * <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+ * <li>Neither the name of Delft University of Technology, nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.</li>
+ * </ul>
+ * This software is provided by the copyright holders and contributors "as is" and any express or implied warranties,
+ * including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental,
+ * special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or
+ * services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability,
+ * whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use
+ * of this software, even if advised of the possibility of such damage.
+ * @version Jun 6, 2014 <br>
+ * @author <a href="http://tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class MassUnitTests extends AbstractUnitTest<MassUnit>
+{
+    /**
+     * Set the locale to "en" so we know what texts should be retrieved from the resources
+     */
+    @SuppressWarnings("static-method")
+    @Before
+    public void setup()
+    {
+        DefaultLocale.setLocale(new Locale("en"));
+    }
+
+    /**
+     * Verify the result of some get*Key methods
+     */
+    @Test
+    public void massKeys()
+    {
+        checkKeys(MassUnit.KILOGRAM, "MassUnit.kilogram", "MassUnit.kg");
+    }
+
+    /**
+     * Verify conversion factors, English names and abbreviations
+     */
+    @Test
+    public void conversions()
+    {
+        checkUnitRatioNameAndAbbreviation(MassUnit.KILOGRAM, 1, 0.00000001, "kilogram", "kg");
+        checkUnitRatioNameAndAbbreviation(MassUnit.GRAM, 0.001, 0.000000001, "gram", "g");
+        checkUnitRatioNameAndAbbreviation(MassUnit.POUND, 0.453592, 0.000001, "pound", "lb");
+        // Check two conversions between non-standard units
+        assertEquals("one POUND is 16 OUNCE", 16, getMultiplicationFactorTo(MassUnit.POUND, MassUnit.OUNCE), 0.00001);
+        assertEquals("one OUNCE is 0.0625 POUND", 0.0625, getMultiplicationFactorTo(MassUnit.OUNCE, MassUnit.POUND),
+                0.000001);
+        // Check conversion factor to standard unit for all remaining time units
+        checkUnitRatioNameAndAbbreviation(MassUnit.OUNCE, 0.0283495, 0.0000001, "ounce", "oz");
+        checkUnitRatioNameAndAbbreviation(MassUnit.TON_LONG, 1016.046906, 0.00001, "long ton", "long tn");
+        checkUnitRatioNameAndAbbreviation(MassUnit.TON_SHORT, 907.18474, 0.00001, "short ton", "sh tn");
+        checkUnitRatioNameAndAbbreviation(MassUnit.TON_METRIC, 1000, 0.001, "metric tonne", "t");
+    }
+
+    /**
+     * Verify that we can create our own mass unit
+     */
+    @Test
+    public void createMassUnit()
+    {
+        MassUnit myMU = new MassUnit("MassUnit.Person", "MassUnit.pn", 80);
+        assertTrue("Can create a new MassUnit", null != myMU);
+        checkUnitRatioNameAndAbbreviation(myMU, 80, 1, "!Person!", "!pn!");
+    }
+
+}
