a43e5141802c271086419ffc05b3a9a6ce6c808b
OTSim <P.Knoppers@tudelft.nl>
2014-06-17 11:08:00 +0000

    Created package-info.java file for unit

---
+++ ots-core/src/main/java/org/opentrafficsim/core/unit/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes that make up the unit system.
+ * <br /> The unit system ensures that quantities (lengths, temperatures, etc) have a suitable unit and cannot be confused with quantities in other units
+ */
+ package org.opentrafficsim.core.unit;
