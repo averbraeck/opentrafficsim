11dc20bda1196097ee49c31afa62a275cb7591f9
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2014-06-18 23:23:56 +0000

    Documentation class diagrams for values. Unnecessary imports removed.

--- ots-core/src/test/java/org/opentrafficsim/core/unit/SICoefficientsTest.java
+++ ots-core/src/test/java/org/opentrafficsim/core/unit/SICoefficientsTest.java
@@ -1,6 +1,8 @@
 package org.opentrafficsim.core.unit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
