81cabe800d445c59e3c5dfea527fec28bf8af76d
OTSim <P.Knoppers@tudelft.nl>
2014-06-19 09:42:52 +0000

    Added a test for ElectrocalPotentialUnit that takes a PowerUnit and an ElectricalCurrentUnit

--- ots-core/src/test/java/org/opentrafficsim/core/unit/ElectricalPotentialUnitTest.java
+++ ots-core/src/test/java/org/opentrafficsim/core/unit/ElectricalPotentialUnitTest.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import org.junit.Before;
 import org.junit.Test;
 import org.opentrafficsim.core.locale.DefaultLocale;
+import org.opentrafficsim.core.unit.unitsystem.UnitSystem;
 
 /**
  * <p>
@@ -72,7 +73,7 @@ public class ElectricalPotentialUnitTest extends AbstractUnitTest<ElectricalPote
         assertEquals("one KILOVOLT is 1000000 MILLIVOLT", 1000000,
                 getMultiplicationFactorTo(ElectricalPotentialUnit.KILOVOLT, ElectricalPotentialUnit.MILLIVOLT), 0.0001);
     }
-    
+
     /**
      * Verify that we can create our own electrical potential unit
      */
@@ -80,10 +81,20 @@ public class ElectricalPotentialUnitTest extends AbstractUnitTest<ElectricalPote
     public void createElectricalPotentialUnit()
     {
         ElectricalPotentialUnit myEPU =
-                new ElectricalPotentialUnit(UnitLocalizationsTest.doNotCheckPrefix + "ElectricalPotentialUnit.NanoVolt",
-                        UnitLocalizationsTest.doNotCheckPrefix + "ElectricalPotentialUnit.NanoV", SI_DERIVED, ElectricalPotentialUnit.VOLT, 1e-9);
+                new ElectricalPotentialUnit(
+                        UnitLocalizationsTest.doNotCheckPrefix + "ElectricalPotentialUnit.NanoVolt",
+                        UnitLocalizationsTest.doNotCheckPrefix + "ElectricalPotentialUnit.NanoV", SI_DERIVED,
+                        ElectricalPotentialUnit.VOLT, 1e-9);
         assertTrue("Can create a new ElectricalPotentialUnit", null != myEPU);
         checkUnitRatioNameAndAbbreviation(myEPU, 1e-9, 0.1, "!NanoVolt!", "!NanoV!");
+
+        myEPU =
+                new ElectricalPotentialUnit(PowerUnit.FOOT_POUND_FORCE_PER_HOUR, ElectricalCurrentUnit.MICROAMPERE,
+                        UnitLocalizationsTest.doNotCheckPrefix + "ElectricalPotentialUnit.fpfph/microA",
+                        UnitLocalizationsTest.doNotCheckPrefix + "ElectricalPotentialUnit.fpfph/uA",
+                        UnitSystem.IMPERIAL);
+        assertTrue("Can create a new ElectricalPotentialUnit", null != myEPU);
+        checkUnitRatioNameAndAbbreviation(myEPU, 376.6, 0.1, "!fpfph/microA!", "!fpfph/uA!");
     }
 
 }
