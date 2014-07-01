0ff70f05cbc92e34f32db9425f4a93103b903eb3
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2014-07-01 14:10:01 +0000

    Removed unused imports

--- trunk/ots-core/src/test/java/org/opentrafficsim/core/unit/UnitTest.java
+++ trunk/ots-core/src/test/java/org/opentrafficsim/core/unit/UnitTest.java
@@ -1,9 +1,9 @@
 package org.opentrafficsim.core.unit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.opentrafficsim.core.unit.unitsystem.UnitSystem;
 
 /**
  * <p>
