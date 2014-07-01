1c011db23f6496d499271f3c105d2ff5851c1636
OTSim <P.Knoppers@tudelft.nl>
2014-07-01 14:08:24 +0000

    Removed unnecessary imports

--- ots-core/src/test/java/org/opentrafficsim/core/value/vfloat/vector/FloatVectorTest.java
+++ ots-core/src/test/java/org/opentrafficsim/core/value/vfloat/vector/FloatVectorTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.Locale;
 import java.util.Set;
 
 import org.junit.Test;
