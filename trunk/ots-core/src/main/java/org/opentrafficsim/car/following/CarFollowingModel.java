12b3627db10f80c3c2bb65e0c21daed384a79889
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2014-07-08 14:36:08 +0000

    Initial attempt at implementing IDMplus

---
+++ trunk/ots-core/src/main/java/org/opentrafficsim/car/following/CarFollowingModel.java
@@ -0,0 +1,48 @@
+package org.opentrafficsim.car.following;
+
+import java.util.Set;
+
+import org.opentrafficsim.car.Car;
+import org.opentrafficsim.core.unit.AccelerationUnit;
+import org.opentrafficsim.core.unit.SpeedUnit;
+import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalarAbs;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2014 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights
+ * reserved.
+ * <p>
+ * See for project information <a href="http://www.simulation.tudelft.nl/"> www.simulation.tudelft.nl</a>.
+ * <p>
+ * The OpenTrafficSim project is distributed under the following BSD-style license:<br>
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ * <ul>
+ * <li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.</li>
+ * <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.</li>
+ * <li>Neither the name of Delft University of Technology, nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.</li>
+ * </ul>
+ * This software is provided by the copyright holders and contributors "as is" and any express or implied warranties,
+ * including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental,
+ * special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or
+ * services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability,
+ * whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use
+ * of this software, even if advised of the possibility of such damage.
+ * @version Jul 2, 2014 <br>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public abstract interface CarFollowingModel
+{
+    /**
+     * Compute the acceleration that would be used to follow a specified leader
+     * @param car Car; the Car for which acceleration is computed
+     * @param leaders Set&lt;Car&gt;; the set of leaders to take into consideration
+     * @param speedLimit DoubleScalarAbs&lt;SpeedUnit&gt;; the local speed limit
+     * @return DoubleScalarAbs&lt;AccelerationUnit&gt;; the acceleration for following the leader
+     */
+    public abstract DoubleScalarAbs<AccelerationUnit> computeAcceleration(final Car car, final Set<Car> leaders, final DoubleScalarAbs<SpeedUnit> speedLimit);
+}
