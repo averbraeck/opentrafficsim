ef5ee548d7a6831a36d62eec72d13a58bc8b3bc5
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2014-09-21 21:11:13 +0000

    Network classes extended and improved. Graph calculations added. NTM project now uses these abstract definitions.

--- ots-core/src/main/java/org/opentrafficsim/core/network/AbstractNode.java
+++ ots-core/src/main/java/org/opentrafficsim/core/network/AbstractNode.java
@@ -2,7 +2,10 @@ package org.opentrafficsim.core.network;
 
 import java.io.Serializable;
 
+import nl.tudelft.simulation.dsol.animation.LocatableInterface;
+
 /**
+ * The Node is a point with an id. It is used in the network to connect Links.
  * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
@@ -11,45 +14,30 @@ import java.io.Serializable;
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
- * @param <ID>
+ * @param <ID> the ID type.
+ * @param <P> the point type, e.g., com.vividsolutions.jts.geom.Point, DirectedPoint. 
  */
-public class Node<ID> implements Serializable
+public abstract class AbstractNode<ID, P> implements LocatableInterface, Serializable
 {
     /** */
     private static final long serialVersionUID = 20140920L;
 
     /** the node id. */
-    private final ID id;
+    private ID id;
 
-    /** the node name. */
-    private final String name;
+    /** the point. */
+    private P point;
 
     /**
      * Construction of a Node.
      * @param id the id of the Node.
-     * @param name the name of the Node as a String.
+     * @param point the point with usually an x and y setting.
      */
 
-    public Node(final ID id, final String name)
+    public AbstractNode(final ID id, final P point)
     {
         this.id = id;
-        this.name = name;
-    }
-
-    /**
-     * @param id the id of the Node.
-     */
-    public Node(final ID id)
-    {
-        this(id, "");
-    }
-
-    /**
-     * @return node name.
-     */
-    public final String getName()
-    {
-        return this.name;
+        this.point = point;
     }
 
     /**
@@ -58,6 +46,14 @@ public class Node<ID> implements Serializable
     public final ID getId()
     {
         return this.id;
+    }
+
+    /**
+     * @return point.
+     */
+    public final P getPoint()
+    {
+        return this.point;
     }
 
 }
