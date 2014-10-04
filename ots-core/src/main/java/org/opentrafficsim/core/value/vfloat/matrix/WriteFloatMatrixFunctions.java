0a967c06136f3c7eda0ce32b225a1dc0436af4f5
OTSim <P.Knoppers@tudelft.nl>
2014-10-04 09:03:25 +0000

    Minor edits

--- ots-core/src/main/java/org/opentrafficsim/core/value/vfloat/matrix/WriteFloatMatrixFunctions.java
+++ ots-core/src/main/java/org/opentrafficsim/core/value/vfloat/matrix/WriteFloatMatrixFunctions.java
@@ -5,7 +5,7 @@ import org.opentrafficsim.core.value.ValueException;
 import org.opentrafficsim.core.value.vfloat.scalar.FloatScalar;
 
 /**
- * Methods that modify the data stored in a matrix.
+ * Methods that modify the data stored in a FloatMatrix.
  * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
@@ -17,36 +17,39 @@ import org.opentrafficsim.core.value.vfloat.scalar.FloatScalar;
 public interface WriteFloatMatrixFunctions<U extends Unit<U>>
 {
     /**
-     * Store a value in a cell of this matrix.
-     * @param row integer; row of the entry where the value must be stored
-     * @param column integer; column of the entry where the value must be store
-     * @param valueSI the value to store in the cell
-     * @throws ValueException if row or column is invalid.
+     * Replace the value at row, column by the supplied value which is expressed in the standard SI unit.
+     * @param row int; row of the value to replace
+     * @param column int; column of the value to replace
+     * @param valueSI float; the value to store (expressed in the standard SI unit)
+     * @throws ValueException when row or column out of range (row &lt; 0 or row &gt;= rows() or column &lt; 0 or column
+     *             &gt;= columns())
      */
     void setSI(int row, int column, float valueSI) throws ValueException;
 
     /**
-     * Store a value in a cell of this matrix.
-     * @param row integer; row of the entry where the value must be stored
-     * @param column integer; column of the entry where the value must be store
-     * @param value FloatScalar; the value to store in the cell
-     * @throws ValueException if row or column is invalid.
+     * Replace the value at row, column by the supplied value which is in a compatible unit.
+     * @param row int; row of the value to replace
+     * @param column int; column of the value to replace
+     * @param value FloatScalar&lt;U&gt;; the strongly typed value to store
+     * @throws ValueException when row or column out of range (row &lt; 0 or row &gt;= rows() or column &lt; 0 or column
+     *             &gt;= columns())
      */
     void set(int row, int column, FloatScalar<U> value) throws ValueException;
 
     /**
-     * Store a value in a cell of this matrix.
-     * @param row integer; row of the entry where the value must be stored
-     * @param column integer; column of the entry where the value must be store
-     * @param value float; the value to store in the cell
-     * @param valueUnit U; the unit of the provided value
-     * @throws ValueException if row or column is invalid.
+     * Replace the value at row, column by the supplied value which is expressed in a supplied (compatible) unit.
+     * @param row int; row of the value to replace
+     * @param column int; column of the value to replace
+     * @param value float; the value to store (which is expressed in valueUnit)
+     * @param valueUnit U; unit of the supplied value
+     * @throws ValueException when row or column out of range (row &lt; 0 or row &gt;= rows() or column &lt; 0 or column
+     *             &gt;= columns())
      */
     void setInUnit(int row, int column, float value, U valueUnit) throws ValueException;
 
     /**
-     * normalize the matrix, i.e. make the sum of all elements equal to 1.
-     * @throws ValueException if the sum of the values is zero, and normalization is not possible
+     * Normalize the matrix, i.e. scale the values to make the sum equal to 1.
+     * @throws ValueException when the sum of the values is zero and normalization is not possible
      */
     void normalize() throws ValueException;
 
