1540fec4d309d4e7a524ba3fe996798de16c6183
OTSim <P.Knoppers@tudelft.nl>
2014-11-04 14:40:21 +0000

    Lane change model egoistic looks OK (but is totally untested).

--- ots-core/src/main/java/org/opentrafficsim/car/lanechanging/Egoistic.java
+++ ots-core/src/main/java/org/opentrafficsim/car/lanechanging/Egoistic.java
@@ -1,21 +1,20 @@
 package org.opentrafficsim.car.lanechanging;
 
-import java.util.Set;
-
-import org.opentrafficsim.core.gtu.GTU;
 import org.opentrafficsim.core.unit.AccelerationUnit;
-import org.opentrafficsim.core.unit.SpeedUnit;
-import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
+import org.opentrafficsim.core.value.ValueException;
+import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar.Abs;
+import org.opentrafficsim.core.value.vdouble.vector.DoubleVector;
 
 /**
  * <p>
- * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
+ * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
  * @version Sep 19, 2014 <br>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
-public final class Egoistic
+public final class Egoistic extends AbstractLaneChangeModel
 {
     /**
      * This class should never be instantiated.
@@ -25,68 +24,19 @@ public final class Egoistic
         // This class should never be instantiated.
     }
 
-    /**
-     * Compute the acceleration of this car and the new follower car after a considered lane change. The lowers of the two
-     * computed accelerations is returned. If changing lane is not possible because it would result in dangerous deceleration or
-     * collision, the returned value is Double.NEGATIVE_INFINITY.
-     * @param referenceCar Car; the car that considers changing lane
-     * @param otherCars Set&lt;Car&gt;; the cars in the target lane
-     * @param maximumDeceleration DoubleScalar.Abs&lt;AccelerationUnit&gt;; the maximum (deemed safe) deceleration. This must be
-     *            a positive value, about 2 m/s/s.
-     * @param speedLimit DoubleScalar.Abs&lt;SpeedUnit&gt;; the speed limit
-     * @return DoubleScalar.Abs&lt;AccelerationUnit&gt;; the lowest acceleration (highest deceleration) incurred if the lane
-     *         change is carried out
-     */
-    public static DoubleScalar.Abs<AccelerationUnit> acceleration(final GTU<?> referenceCar, final Set<GTU<?>> otherCars,
-            final DoubleScalar.Rel<AccelerationUnit> maximumDeceleration, final DoubleScalar.Abs<SpeedUnit> speedLimit)
+    /** {@inheritDoc} */
+    @Override
+    public Abs<AccelerationUnit> applyDriverPersonality(final DoubleVector.Abs.Dense<AccelerationUnit> accelerations)
     {
-        /*-
-        DoubleScalar.Abs<TimeUnit> when = referenceCar.getSimulator().getSimulatorTime().get();
-        GTU<?> leader = null;
-        DoubleScalar.Rel<LengthUnit> leaderHeadway = null;
-        GTU<?> follower = null;
-        DoubleScalar.Rel<LengthUnit> followerHeadway = null;
-        DoubleScalar.Abs<LengthUnit> referenceCarPosition = referenceCar.getPosition(when);
-        // TODO: if otherCars are sorted in some way, scanning the entire list like this is not needed.
-
-        // TODO: the car following model already deals with a Set of leaders; can't we do something similar with the set
-        // of followers?
-        for (GTU<?> c : otherCars)
+        try
         {
-            DoubleScalar.Rel<LengthUnit> headway =
-                    MutableDoubleScalar.minus(c.getPosition(when), referenceCarPosition).immutable();
-            if (headway.getSI() < 0)
-            {
-                if (null == follower || followerHeadway.getSI() > headway.getSI())
-                {
-                    follower = c;
-                    followerHeadway = headway;
-                }
-            }
-            else
-            {
-                if (null == leader || leaderHeadway.getSI() < headway.getSI())
-                {
-                    leader = c;
-                    leaderHeadway = headway;
-                }
-            }
+            // The egoistic driver only looks at the effects on him-/herself.
+            return accelerations.get(0);
         }
-        GTUFollowingModel carFollowingModel = referenceCar.getGTUFollowingModel();
-        DoubleScalar.Abs<AccelerationUnit> followerAcceleration =
-                FollowAcceleration.acceleration(follower, referenceCar, when, carFollowingModel, speedLimit);
-        if (followerAcceleration.getSI() < -maximumDeceleration.getSI())
+        catch (ValueException exception)
         {
-            return new DoubleScalar.Abs<AccelerationUnit>(Double.NEGATIVE_INFINITY, AccelerationUnit.METER_PER_SECOND_2);
+            throw new Error("Cannot happen");
         }
-        DoubleScalar.Abs<AccelerationUnit> referenceAcceleration =
-                FollowAcceleration.acceleration(referenceCar, leader, when, carFollowingModel, speedLimit);
-        if (followerAcceleration.getSI() < referenceAcceleration.getSI())
-        {
-            return followerAcceleration;
-        }
-        return referenceAcceleration;
-         */
-        return new DoubleScalar.Abs<AccelerationUnit>(0.0, AccelerationUnit.METER_PER_SECOND_2);
     }
+
 }
--- ots-core/src/main/java/org/opentrafficsim/core/gtu/following/FollowAcceleration.java
+++ ots-core/src/main/java/org/opentrafficsim/core/gtu/following/FollowAcceleration.java
@@ -1,6 +1,7 @@
 package org.opentrafficsim.core.gtu.following;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,8 +13,10 @@ import org.opentrafficsim.core.unit.AccelerationUnit;
 import org.opentrafficsim.core.unit.LengthUnit;
 import org.opentrafficsim.core.unit.SpeedUnit;
 import org.opentrafficsim.core.unit.TimeUnit;
+import org.opentrafficsim.core.value.ValueException;
 import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
 import org.opentrafficsim.core.value.vdouble.scalar.MutableDoubleScalar;
+import org.opentrafficsim.core.value.vdouble.vector.DoubleVector;
 
 /**
  * Determine acceleration (deceleration) for a GTU that follows another GTU.
@@ -79,7 +82,7 @@ public final class FollowAcceleration
             }
             else
             {
-                // Not on same lane. Find a Link that is shared by both
+                // Not on the same lane. Find a Link that is shared by both
                 Set<CrossSectionLink<?, ?>> links = new HashSet<CrossSectionLink<?, ?>>();
                 for (Lane lane : leader.getLongitudinalPositions().keySet())
                 {
@@ -126,7 +129,7 @@ public final class FollowAcceleration
                 leader.positionOfRear(leaderLane, when);
                 double leaderRatio = leader.positionOfRear(when).getFractionalLongitudinalPosition();
                 double followerRatio = follower.positionOfFront(when).getFractionalLongitudinalPosition();
-                double ratioDifference = leaderRatio - followerRatio; // TODO prove that this is really correct
+                double ratioDifference = leaderRatio - followerRatio; // TODO prove that this is always correct
                 // Scale that difference by the length of the lane of the follower to obtain the headway
                 DoubleScalar.Rel<LengthUnit> headway =
                         new DoubleScalar.Rel<LengthUnit>(followerLane.getLength().getSI() * ratioDifference,
@@ -136,6 +139,85 @@ public final class FollowAcceleration
             }
         }
         return gtuFollowingModel.computeAcceleration(follower, leader, speedLimit).getAcceleration();
+    }
+
+    /**
+     * Compute the acceleration of this car and the (new) follower car in the current lane, or after a considered lane
+     * change. Both values are returned; the acceleration for the reference car at index 0, the acceleration of the
+     * (new) follower car at index 1. If changing lane is not possible because it would result in dangerous deceleration
+     * or collision, the returned value in both fields is Double.NEGATIVE_INFINITY.
+     * @param referenceGTU AbstractLaneBasedGTU&lt;?&gt;; the car that considers changing lane and whose
+     *            gtuFollowingModel will be used
+     * @param otherCars Set&lt;Car&gt;; the cars in the target lane
+     * @param speedLimit DoubleScalar.Abs&lt;SpeedUnit&gt;; the speed limit
+     * @return DoubleScalar.Abs&lt;AccelerationUnit&gt;; the lowest acceleration (highest deceleration) incurred if the
+     *         lane change is carried out
+     * @throws RemoteException on communication failure
+     * @throws NetworkException when the network is inconsistent
+     */
+    @SuppressWarnings("unchecked")
+    public static DoubleVector.Abs.Dense<AccelerationUnit> acceleration(final AbstractLaneBasedGTU<?> referenceGTU,
+            final Collection<AbstractLaneBasedGTU<?>> otherCars, final DoubleScalar.Abs<SpeedUnit> speedLimit)
+            throws RemoteException, NetworkException
+    {
+        final DoubleScalar.Abs<AccelerationUnit> maximumDeceleration =
+                referenceGTU.getGTUFollowingModel().maximumSafeDeceleration();
+        DoubleScalar.Abs<TimeUnit> when = referenceGTU.getSimulator().getSimulatorTime().get();
+        AbstractLaneBasedGTU<?> leader = null;
+        DoubleScalar.Rel<LengthUnit> leaderHeadway = null;
+        AbstractLaneBasedGTU<?> follower = null;
+        DoubleScalar.Rel<LengthUnit> followerHeadway = null;
+        Lane referenceLane = referenceGTU.getLongitudinalPositions().keySet().iterator().next();
+        DoubleScalar.Rel<LengthUnit> referenceCarPosition = referenceGTU.positionOfFront(referenceLane, when);
+        // Find the nearest leader and the nearest follower.
+        for (AbstractLaneBasedGTU<?> c : otherCars)
+        {
+            if (c == referenceGTU)
+            {
+                continue;
+            }
+            Lane otherLane = c.getLongitudinalPositions().keySet().iterator().next();
+            DoubleScalar.Rel<LengthUnit> headway =
+                    DoubleScalar.minus(c.positionOfFront(otherLane, when), referenceCarPosition).immutable();
+            if (headway.getSI() < 0)
+            {
+                if (null == follower || followerHeadway.getSI() > headway.getSI())
+                {
+                    follower = c;
+                    followerHeadway = headway;
+                }
+            }
+            else
+            {
+                if (null == leader || leaderHeadway.getSI() < headway.getSI())
+                {
+                    leader = c;
+                    leaderHeadway = headway;
+                }
+            }
+        }
+        GTUFollowingModel gtuFollowingModel = referenceGTU.getGTUFollowingModel();
+        DoubleScalar.Abs<AccelerationUnit> followerAcceleration =
+                FollowAcceleration.acceleration(follower, referenceGTU, when, gtuFollowingModel, speedLimit);
+        try
+        {
+            if (followerAcceleration.getSI() >= -maximumDeceleration.getSI())
+            {
+                DoubleScalar.Abs<AccelerationUnit> referenceAcceleration =
+                        FollowAcceleration.acceleration(referenceGTU, leader, when, gtuFollowingModel, speedLimit);
+                if (referenceAcceleration.getSI() >= -maximumDeceleration.getSI())
+                {
+                    return new DoubleVector.Abs.Dense<AccelerationUnit>(new DoubleScalar.Abs[]{referenceAcceleration,
+                            followerAcceleration});
+                }
+            }
+            return new DoubleVector.Abs.Dense<AccelerationUnit>(new double[]{Double.NEGATIVE_INFINITY,
+                    Double.NEGATIVE_INFINITY}, AccelerationUnit.METER_PER_SECOND_2);
+        }
+        catch (ValueException exception)
+        {
+            throw new Error("Cannot happen");
+        }
     }
 
 }
