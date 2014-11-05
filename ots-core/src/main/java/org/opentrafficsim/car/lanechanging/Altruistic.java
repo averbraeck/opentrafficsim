7918734a23a014a288296ec81568c077512c4f29
OTSim <P.Knoppers@tudelft.nl>
2014-11-05 14:27:47 +0000

    Harmonized the lane change code. This is still untested!

--- ots-core/src/main/java/org/opentrafficsim/car/lanechanging/Altruistic.java
+++ ots-core/src/main/java/org/opentrafficsim/car/lanechanging/Altruistic.java
@@ -2,29 +2,31 @@ package org.opentrafficsim.car.lanechanging;
 
 import org.opentrafficsim.core.unit.AccelerationUnit;
 import org.opentrafficsim.core.value.ValueException;
-import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar.Abs;
+import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
 import org.opentrafficsim.core.value.vdouble.vector.DoubleVector;
 
 /**
- * The egoistic drive changes lane when this yields is personal advantage (totally ignoring any disadvantage to others).
+ * The altruistic driver changes lane when that is beneficial for all drivers.
  * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
  * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
- * @version Sep 19, 2014 <br>
+ * @version 5 nov. 2014 <br>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
-public final class Egoistic extends AbstractLaneChangeModel
+public class Altruistic extends AbstractLaneChangeModel
 {
+
     /** {@inheritDoc} */
     @Override
-    public Abs<AccelerationUnit> applyDriverPersonality(final DoubleVector.Abs.Dense<AccelerationUnit> accelerations)
+    public final DoubleScalar.Abs<AccelerationUnit> applyDriverPersonality(
+            final DoubleVector.Abs.Dense<AccelerationUnit> accelerations)
     {
         try
         {
-            // The egoistic driver only looks at the effects on him-/herself.
-            return accelerations.get(0);
+            return new DoubleScalar.Abs<AccelerationUnit>(accelerations.getInUnit(0) + accelerations.getInUnit(1),
+                    accelerations.getUnit());
         }
         catch (ValueException exception)
         {
