bd1d8f0fb8b1ccf073307b769017d53c386c2e4c
OTSim <P.Knoppers@tudelft.nl>
2014-11-13 08:45:16 +0000

    Added java doc

--- ots-demo/src/main/java/org/opentrafficsim/demo/IDMPlus/swing/ContourPlots.java
+++ ots-demo/src/main/java/org/opentrafficsim/demo/IDMPlus/swing/ContourPlots.java
@@ -45,6 +45,7 @@ import org.opentrafficsim.simulationengine.SimpleSimulator;
 import com.vividsolutions.jts.geom.Coordinate;
 
 /**
+ * Simplest contour plots demonstration.
  * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
  * reserved. <br>
