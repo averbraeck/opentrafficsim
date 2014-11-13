86f65e5a282b3b66e900e2754093769c1f24361f
OTSim <P.Knoppers@tudelft.nl>
2014-11-13 08:43:02 +0000

    Created simple simulator class that makes it really easy to create a simulation

---
+++ ots-core/src/main/java/org/opentrafficsim/simulationengine/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <p>
+ * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version 12 nov. 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://Hansvanlint.weblog.tudelft.nl">Hans van Lint</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
+ * @author <a href="http://www.citg.tudelft.nl">Yufei Yuan</a>
+ */
+package org.opentrafficsim.simulationengine;
