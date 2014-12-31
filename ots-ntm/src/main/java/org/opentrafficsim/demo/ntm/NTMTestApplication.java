904adc89469f463f16398846795275898a07235c
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2014-12-31 00:02:47 +0000

    Changed TODO: into TODO to highlight TODOs as a task.

--- ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/NTMTestApplication.java
+++ ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/NTMTestApplication.java
@@ -80,7 +80,7 @@ public class NTMTestApplication extends DSOLApplication
         panel.getTabbedPane().addTab(0, "animation", animationPanel);
 
         // tell the animation panel to update its statistics
-        // TODO: should be done automatically in DSOL!
+        // TODO should be done automatically in DSOL!
         animationPanel.notify(new Event(SimulatorInterface.START_REPLICATION_EVENT, simulator, null));
 
         new NTMTestApplication("Network Transmission Model", panel);
