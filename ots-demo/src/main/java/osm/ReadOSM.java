1f92d613c3691a07ebe9f0b14d493fd1b315ebc3
OTSim <P.Knoppers@tudelft.nl>
2015-01-20 12:30:24 +0000

    Fixed javadoc.

--- ots-demo/src/main/java/osm/ReadOSM.java
+++ ots-demo/src/main/java/osm/ReadOSM.java
@@ -10,7 +10,8 @@ import org.opentrafficsim.importexport.osm.Tag;
 import org.opentrafficsim.importexport.osm.input.ReadOSMFile;
 
 /**
- *  * <p>
+ * Import an OpenStreetMap file.
+ * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>.
