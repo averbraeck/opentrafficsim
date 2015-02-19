531ef77202ca83d752e90f9824c4ae07d7897b39
OTSim <P.Knoppers@tudelft.nl>
2015-02-19 09:44:16 +0000

    FollowAcceleration class is to be removed.

--- ots-core/src/main/java/org/opentrafficsim/core/gtu/following/FollowAcceleration.java
+++ ots-core/src/main/java/org/opentrafficsim/core/gtu/following/FollowAcceleration.java
@@ -9,7 +9,6 @@ import org.opentrafficsim.core.unit.AccelerationUnit;
 import org.opentrafficsim.core.unit.LengthUnit;
 import org.opentrafficsim.core.unit.SpeedUnit;
 import org.opentrafficsim.core.unit.TimeUnit;
-import org.opentrafficsim.core.value.conversions.Calc;
 import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
 
 /**
@@ -42,61 +41,11 @@ public final class FollowAcceleration
     }
 
     /**
-     * Compute the acceleration (or deceleration) for a GTU following another GTU.
-     * @param follower LaneBaseGTU; the GTU that is following
-     * @param leader LaneBaseGTU; the GTU that is leading
-     * @param when DoubleScalar.Abs&lt;TimeUnit&gt;; the current time
-     * @param gtuFollowingModel GTUFollowingModel; the GTU following model that is used to compute the result
-     * @param speedLimit DoubleScalar.Abs&lt;SpeedUnit&gt;; the speed limit
-     * @return DoubleScalar.Abs&lt;AccelerationUnit&gt;; the acceleration (deceleration) for the following GTU in order
-     *         to not collide with the leader GTU
-     * @throws RemoteException on communication failure
-     * @throws NetworkException if follower and leader do not have a common CrossSectionLink
-     */
-    /*
-     * public static DoubleScalar.Abs<AccelerationUnit> acceleration(final LaneBasedGTU<?> follower, final
-     * LaneBasedGTU<?> leader, final DoubleScalar.Abs<TimeUnit> when, final GTUFollowingModel gtuFollowingModel, final
-     * DoubleScalar.Abs<SpeedUnit> speedLimit) throws RemoteException, NetworkException { if (null != leader && null !=
-     * follower) { // find a lane where follower and leader are jointly // OOPS DONT DO IT THIS WAY: Set<Lane> lanes =
-     * leader.getLongitudinalPositions().keySet(); // Create a NEW Set (so we don't modify the HashSet of the leader).
-     * Set<Lane> lanes = new HashSet<Lane>(leader.positions(leader.getFront()).keySet());
-     * lanes.retainAll(follower.positions(follower.getFront()).keySet()); // TODO expand to lanes for next links as
-     * well, to a certain distance (which is...?) if (lanes.size() > 0) { Lane lane = lanes.iterator().next(); // Use
-     * the first one try { MutableDoubleScalar.Rel<LengthUnit> headway = DoubleScalar.minus(leader.position(lane,
-     * leader.getRear(), when), follower.position(lane, follower.getFront(), when)); if (headway.getSI() <= 0) { //
-     * Immediate collision; return a prohibitive negative value return new
-     * DoubleScalar.Abs<AccelerationUnit>(Double.NEGATIVE_INFINITY, AccelerationUnit.METER_PER_SECOND_2); } } catch
-     * (NetworkException ne) { throw new Error("Cannot happen -- both vehicles are on these lanes"); } } else { // Not
-     * on the same lane. Find a Link that is shared by both Set<CrossSectionLink<?, ?>> links = new
-     * HashSet<CrossSectionLink<?, ?>>(); for (Lane lane : leader.positions(leader.getFront()).keySet()) {
-     * links.add(lane.getParentLink()); } if (links.size() == 0) { throw new
-     * NetworkException("Leader is not on any link"); } Set<CrossSectionLink<?, ?>> followerLinks = new
-     * HashSet<CrossSectionLink<?, ?>>(); for (Lane lane : follower.positions(follower.getFront()).keySet()) {
-     * followerLinks.add(lane.getParentLink()); } links.retainAll(followerLinks); if (links.size() == 0) { throw new
-     * NetworkException("Leader and follower do not have a common CrossSectionLink"); } CrossSectionLink<?, ?>
-     * commonLink = links.iterator().next(); // Use the first one Lane leaderLane = null; for (Lane lane :
-     * leader.positions(leader.getFront()).keySet()) { if (lane.getParentLink() == commonLink) { leaderLane = lane;
-     * break; } } if (null == leaderLane) { throw new Error("Cannot happen -- Cannot find leaderLane"); } Lane
-     * followerLane = null; for (Lane lane : follower.positions(RelativePosition.REFERENCE_POSITION).keySet()) { if
-     * (lane.getParentLink() == commonLink) { followerLane = lane; break; } } if (null == followerLane) { throw new
-     * Error("Cannot happen -- Cannot find leaderLane"); } // Get the difference of the projections of both lanes onto
-     * the design line of the link // leader.positionOfRear(leaderLane, when); double leaderRatio =
-     * leader.fractionalPosition(leaderLane, leader.getRear(), when); double followerRatio =
-     * follower.fractionalPosition(followerLane, follower.getFront(), when); double ratioDifference = leaderRatio -
-     * followerRatio; // TODO prove that this is always correct // Scale that difference by the length of the lane of
-     * the follower to obtain the headway DoubleScalar.Rel<LengthUnit> headway = new
-     * DoubleScalar.Rel<LengthUnit>(followerLane.getLength().getSI() * ratioDifference, LengthUnit.METER); return
-     * gtuFollowingModel.computeAcceleration(follower, leader.getLongitudinalVelocity(when), headway,
-     * speedLimit).getAcceleration(); } } // follower == null || leader == null return
-     * gtuFollowingModel.computeAcceleration(follower, leader, speedLimit).getAcceleration(); }
-     */
-
-    /**
      * Compute the acceleration of this GTU and the (new) follower GTU in the current lane, or after a considered lane
      * change. Both values are returned; the acceleration for the reference car at index 0, the acceleration of the
      * (new) follower GTU at index 1. If changing lane is not possible because it would result in dangerous deceleration
      * or collision, the returned value in both fields is Double.NEGATIVE_INFINITY.
-     * @param referenceGTU AbstractLaneBasedGTU&lt;?&gt;; the car that considers changing lane and whose
+     * @param referenceGTU LaneBasedGTU&lt;?&gt;; the car that considers changing lane and whose
      *            gtuFollowingModel will be used for all estimations of the resulting accelerations and decelerations
      * @param otherGTUs Set&lt;HeadwayGTU&gt;; the GTUs in the target lane and their headways from the referenceGTU
      * @param speedLimit DoubleScalar.Abs&lt;SpeedUnit&gt;; the speed limit
