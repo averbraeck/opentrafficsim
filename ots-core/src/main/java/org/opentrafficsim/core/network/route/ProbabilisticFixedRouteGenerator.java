3316af869b4254255907911e7de873d14c22c952
OTSim <P.Knoppers@tudelft.nl>
2015-03-20 15:48:15 +0000

    Wrote tests for the RouteGenerators and fixed some deficiencies that I discovered (FixedRouteGenerator needed to implement Comparable).

--- ots-core/src/main/java/org/opentrafficsim/core/network/route/ProbabilisticFixedRouteGenerator.java
+++ ots-core/src/main/java/org/opentrafficsim/core/network/route/ProbabilisticFixedRouteGenerator.java
@@ -6,6 +6,8 @@ import java.util.SortedMap;
 import nl.tudelft.simulation.jstats.distributions.DistUniform;
 import nl.tudelft.simulation.jstats.streams.MersenneTwister;
 
+import org.opentrafficsim.core.network.NetworkException;
+
 /**
  * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
@@ -33,9 +35,10 @@ public class ProbabilisticFixedRouteGenerator implements RouteGenerator
      * @param routeProbabilities SortedMap&lt;RouteGenerator, Double&gt;; the RouteGenerators with the probabilities for
      *            each one. In stead of probabilities, (observed) counts may be used; i.e. the provided values are
      *            internally scaled to add up to 1.0.
-     * @throws Exception when the probabilities or frequencies are invalid (negative, or all zero)
+     * @throws NetworkException when the probabilities or frequencies are invalid (negative, or all zero)
      */
-    public ProbabilisticFixedRouteGenerator(SortedMap<RouteGenerator, Double> routeProbabilities) throws Exception
+    public ProbabilisticFixedRouteGenerator(SortedMap<RouteGenerator, Double> routeProbabilities)
+            throws NetworkException
     {
         this(routeProbabilities, System.currentTimeMillis());
     }
@@ -46,10 +49,10 @@ public class ProbabilisticFixedRouteGenerator implements RouteGenerator
      *            each one. In stead of probabilities, (observed) counts may be used; i.e. the provided values are
      *            internally scaled to add up to 1.0.
      * @param seed long; the seed for the random number generator that will be used
-     * @throws Exception when the probabilities or frequencies are invalid (negative, or all zero)
+     * @throws NetworkException when the probabilities or frequencies are invalid (negative, or all zero)
      */
     public ProbabilisticFixedRouteGenerator(SortedMap<RouteGenerator, Double> routeProbabilities, long seed)
-            throws Exception
+            throws NetworkException
     {
         double sum = 0;
         for (RouteGenerator rg : routeProbabilities.keySet())
@@ -57,23 +60,23 @@ public class ProbabilisticFixedRouteGenerator implements RouteGenerator
             Double frequency = routeProbabilities.get(rg);
             if (frequency < 0)
             {
-                throw new Exception("Negative probability or frequency is not allowed (got " + frequency + ")");
+                throw new NetworkException("Negative probability or frequency is not allowed (got " + frequency + ")");
             }
             sum += frequency;
             this.routes.add(rg);
         }
         if (0 == sum)
         {
-            throw new Exception("Sum of probabilities or freqencies must be > 0");
+            throw new NetworkException("Sum of probabilities or freqencies must be > 0");
         }
         this.probabilities = new double[this.routes.size()];
         int index = 0;
         for (RouteGenerator rg : routeProbabilities.keySet())
         {
             Double frequency = routeProbabilities.get(rg);
-            this.probabilities[index] = frequency / sum;
+            this.probabilities[index++] = frequency / sum;
         }
-        this.random = new DistUniform(new MersenneTwister(), 0, 1);
+        this.random = new DistUniform(new MersenneTwister(seed), 0, 1);
     }
 
     /** {@inheritDoc} */
