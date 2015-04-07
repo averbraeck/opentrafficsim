a209b2f0678e269b2369ca25cdd2d3fae4e8feb8
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-04-07 14:53:34 +0000

    Simple DASHED stripe animation works. Must be augmented to do DOUBLE, LEFTONLY and RIGHTONLY.

--- trunk/ots-core/src/main/java/org/opentrafficsim/core/network/animation/StripeAnimation.java
+++ trunk/ots-core/src/main/java/org/opentrafficsim/core/network/animation/StripeAnimation.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import java.awt.geom.Path2D;
 import java.awt.image.ImageObserver;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import javax.naming.NamingException;
 
@@ -16,10 +17,15 @@ import org.opentrafficsim.core.network.lane.Stripe;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.linearref.LengthIndexedLine;
+import com.vividsolutions.jts.operation.buffer.BufferParameters;
 
 /**
  * <p>
- * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
+ * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
  * @version Oct 17, 2014 <br>
@@ -30,6 +36,75 @@ public class StripeAnimation extends Renderable2D
     /** the line type. */
     private final TYPE type;
 
+    /** The Coordinates that for the outline of the Stripe. */
+    private final Coordinate[] coordinates;
+
+    /** Precision of buffer operations. */
+    private static final int QUADRANTSEGMENTS = 8;
+    
+    /** Dummy coordinate that forces the drawing operation to start a new path. */
+    private static final Coordinate NEWPATH = new Coordinate(Double.NaN, Double.NaN);
+
+    /**
+     * Generate the coordinates needed to draw the stripe
+     * @param stripe Stripe; the stripe
+     * @param stripeType TYPE; the stripe type
+     * @return Coordinate[]; array of Coordinate
+     * @throws NamingException when <cite>type</cite> is not supported
+     */
+    private Coordinate[] makeCoordinates(final Stripe stripe, final TYPE stripeType) throws NamingException
+    {
+        switch (this.type)
+        {
+            case DASHED:
+            {
+                // Draw a 3-9 stripe pattern
+                double onLength = 3;
+                double width = 0.15;// m
+                LengthIndexedLine centerLine = new LengthIndexedLine(stripe.getCenterLine());
+                double period = 12;// meter
+                double position = 0;// TODO should be initialized from a field in the Stripe
+                double length = stripe.getLength().getSI();
+                ArrayList<Coordinate> coordList = new ArrayList<Coordinate>();
+                while (position < length)
+                {
+                    double endIndex = position + onLength;
+                    if (endIndex > length)
+                    {
+                        endIndex = length;
+                    }
+                    Geometry stripeCenterGeometry = centerLine.extractLine(position, endIndex);
+                    Coordinate[] bufferCoordinates =
+                            stripeCenterGeometry.buffer(width / 2, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT)
+                                    .getCoordinates();
+                    if (coordList.size() > 0)
+                    {
+                        coordList.add(NEWPATH);
+                    }
+                    for (int i = 0; i < bufferCoordinates.length; i++)
+                    {
+                        coordList.add(bufferCoordinates[i]);
+                    }
+                    position += period;
+                }
+                Coordinate[] result = new Coordinate[coordList.size()];
+                coordList.toArray(result);
+                return result;
+            }
+            case DOUBLE:
+                return stripe.getContour().getCoordinates();// FIXME STUB
+            case LEFTONLY:
+                return stripe.getContour().getCoordinates();// FIXME STUB
+            case RIGHTONLY:
+                return stripe.getContour().getCoordinates();// FIXME STUB
+            case SOLID:
+                return stripe.getContour().getCoordinates();
+            default:
+                throw new NamingException("Unsupported stripe type: " + stripeType);
+        }
+
+    }
+
     /**
      * @param source s
      * @param simulator s
@@ -38,10 +113,11 @@ public class StripeAnimation extends Renderable2D
      * @throws RemoteException re
      */
     public StripeAnimation(final Stripe source, final OTSSimulatorInterface simulator, final TYPE type)
-        throws NamingException, RemoteException
+            throws NamingException, RemoteException
     {
         super(source, simulator);
         this.type = type;
+        this.coordinates = makeCoordinates(source, type);
     }
 
     /** {@inheritDoc} */
@@ -51,15 +127,20 @@ public class StripeAnimation extends Renderable2D
         graphics.setColor(Color.WHITE);
         Stripe stripe = (Stripe) getSource();
         DirectedPoint p = stripe.getLocation();
-        Geometry g = stripe.getContour();
-        Coordinate[] coordinates = g.getCoordinates();
         Path2D.Double path = new Path2D.Double();
-        boolean start = false;
-        for (Coordinate c : coordinates)
+        boolean continuation = false;
+        for (Coordinate c : this.coordinates)
         {
-            if (!start)
+            if (c == NEWPATH)
             {
-                start = true;
+                path.closePath();
+                graphics.fill(path);
+                path = new Path2D.Double();
+                continuation = false;
+            }
+            else if (!continuation)
+            {
+                continuation = true;
                 path.moveTo(c.x - p.x, -c.y + p.y);
             }
             else
@@ -78,8 +159,7 @@ public class StripeAnimation extends Renderable2D
      * All rights reserved. <br>
      * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
      */
-    public static enum TYPE
-    {
+    public static enum TYPE {
         /** solid line. */
         SOLID,
 
