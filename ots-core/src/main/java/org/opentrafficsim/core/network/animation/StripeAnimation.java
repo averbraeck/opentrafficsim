6527ad696b69f723a361c5a85b4963210d017528
OTSim <P.Knoppers@tudelft.nl>
2015-04-08 11:37:44 +0000

    Implemented more road stripe styles. The offsetGeometry method in CrossSectionElement is now public (is used to create the offset lines for parallel stripes).

--- ots-core/src/main/java/org/opentrafficsim/core/network/animation/StripeAnimation.java
+++ ots-core/src/main/java/org/opentrafficsim/core/network/animation/StripeAnimation.java
@@ -6,6 +6,7 @@ import java.awt.geom.Path2D;
 import java.awt.image.ImageObserver;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.naming.NamingException;
 
@@ -13,12 +14,12 @@ import nl.tudelft.simulation.dsol.animation.D2.Renderable2D;
 import nl.tudelft.simulation.language.d3.DirectedPoint;
 
 import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.core.network.lane.CrossSectionElement;
 import org.opentrafficsim.core.network.lane.Stripe;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.linearref.LengthIndexedLine;
 import com.vividsolutions.jts.operation.buffer.BufferParameters;
 
@@ -41,64 +42,175 @@ public class StripeAnimation extends Renderable2D
 
     /** Precision of buffer operations. */
     private static final int QUADRANTSEGMENTS = 8;
-    
+
     /** Dummy coordinate that forces the drawing operation to start a new path. */
     private static final Coordinate NEWPATH = new Coordinate(Double.NaN, Double.NaN);
 
     /**
-     * Generate the coordinates needed to draw the stripe
+     * Generate the drawing commands for a dash pattern.
+     * @param center LengthIndexedLine; the design line of the striped pattern
+     * @param width double; width of the stripes in meters
+     * @param startOffset double; shift the starting point in the pattern by this length in meters
+     * @param onOffLengths double[]; one or more lengths of the dashes and the gaps between those dashes. If the number
+     *            of values in <cite>onOffLengths</cite> is odd, the pattern repeats inverted. The first value in
+     *            <cite>onOffLengths</cite> is the length of a dash.
+     * @return ArrayList&lt;Coordinate&gt;; the coordinates of the dashes separated and terminated by a
+     *         <cite>NEWPATH</cite> Coordinate
+     */
+    ArrayList<Coordinate> makeDashes(final LengthIndexedLine center, final double width, final double startOffset,
+            final double[] onOffLengths)
+    {
+        double period = 0;
+        for (double length : onOffLengths)
+        {
+            if (length < 0)
+            {
+                throw new Error("Bad pattern - on or off length is < 0");
+            }
+            period += length;
+        }
+        if (period <= 0)
+        {
+            throw new Error("Bad pattern - repeat period length is 0");
+        }
+        double length = center.getEndIndex();
+        double position = -startOffset;
+        int phase = 0;
+        ArrayList<Coordinate> result = new ArrayList<Coordinate>();
+        while (position < length)
+        {
+            double nextBoundary = position + onOffLengths[phase++ % onOffLengths.length];
+            if (nextBoundary > 0) // Skip this one; this entire dash lies within the startOffset
+            {
+                if (position < 0)
+                {
+                    position = 0; // Draw a partial dash, starting at 0 (begin of the center line)
+                }
+                double endPosition = nextBoundary;
+                if (endPosition > length)
+                {
+                    endPosition = length; // Draw a partial dash, ending at length (end of the center line)
+                }
+                Coordinate[] oneDash =
+                        center.extractLine(position, endPosition)
+                                .buffer(width / 2, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT).getCoordinates();
+                for (int i = 0; i < oneDash.length; i++)
+                {
+                    result.add(oneDash[i]);
+                }
+                result.add(NEWPATH);
+            }
+            position = nextBoundary + onOffLengths[phase++ % onOffLengths.length];
+        }
+        return result;
+    }
+    
+    /**
+     * Create the contour for 
+     * @param center
+     * @param width
+     * @return
+     */
+    ArrayList<Coordinate> makeSolid(final Geometry center, final double width)
+    {
+        return new ArrayList<Coordinate>(Arrays.asList(center.buffer(0.1, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT).getCoordinates()));
+    }
+
+    /**
+     * Generate the coordinates needed to draw the stripe pattern.
      * @param stripe Stripe; the stripe
      * @param stripeType TYPE; the stripe type
      * @return Coordinate[]; array of Coordinate
      * @throws NamingException when <cite>type</cite> is not supported
      */
-    private Coordinate[] makeCoordinates(final Stripe stripe, final TYPE stripeType) throws NamingException
+    private ArrayList<Coordinate> makeCoordinates(final Stripe stripe, final TYPE stripeType) throws NamingException
     {
         switch (this.type)
         {
-            case DASHED:
+            case DASHED:// : - Draw a 3-9 dash pattern on the center line
+                return makeDashes(new LengthIndexedLine(stripe.getCenterLine()), 0.2, 0, new double[]{3, 9});
+            case DOUBLE:// ||- Draw two solid lines
             {
-                // Draw a 3-9 stripe pattern
-                double onLength = 3;
-                double width = 0.15;// m
-                LengthIndexedLine centerLine = new LengthIndexedLine(stripe.getCenterLine());
-                double period = 12;// meter
-                double position = 0;// TODO should be initialized from a field in the Stripe
-                double length = stripe.getLength().getSI();
-                ArrayList<Coordinate> coordList = new ArrayList<Coordinate>();
-                while (position < length)
+                try
                 {
-                    double endIndex = position + onLength;
-                    if (endIndex > length)
+                    Geometry centerLine = stripe.getCenterLine();
+                    Coordinate[] leftLine =
+                            CrossSectionElement.offsetGeometry(centerLine, 0.2)
+                                    .buffer(0.1, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT).getCoordinates();
+                    Coordinate[] rightLine =
+                            CrossSectionElement.offsetGeometry(centerLine, -0.2)
+                                    .buffer(0.1, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT).getCoordinates();
+                    ArrayList<Coordinate> result = new ArrayList<Coordinate>(leftLine.length + rightLine.length);
+                    for (int i = 0; i < leftLine.length; i++)
                     {
-                        endIndex = length;
+                        result.add(leftLine[i]);
                     }
-                    Geometry stripeCenterGeometry = centerLine.extractLine(position, endIndex);
-                    Coordinate[] bufferCoordinates =
-                            stripeCenterGeometry.buffer(width / 2, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT)
-                                    .getCoordinates();
-                    if (coordList.size() > 0)
+                    for (int i = 0; i < rightLine.length; i++)
                     {
-                        coordList.add(NEWPATH);
+                        result.add(rightLine[i]);
                     }
-                    for (int i = 0; i < bufferCoordinates.length; i++)
-                    {
-                        coordList.add(bufferCoordinates[i]);
-                    }
-                    position += period;
+                    return result;
                 }
-                Coordinate[] result = new Coordinate[coordList.size()];
-                coordList.toArray(result);
-                return result;
+                catch (NetworkException exception)
+                {
+                    exception.printStackTrace();
+                }
+                return new ArrayList<Coordinate>();// Return an empty ArrayList after an error occurred
             }
-            case DOUBLE:
-                return stripe.getContour().getCoordinates();// FIXME STUB
-            case LEFTONLY:
-                return stripe.getContour().getCoordinates();// FIXME STUB
-            case RIGHTONLY:
-                return stripe.getContour().getCoordinates();// FIXME STUB
-            case SOLID:
-                return stripe.getContour().getCoordinates();
+            case LEFTONLY: // |: - Draw left solid, right 3-9 dashed
+            {
+                try
+                {
+                    Geometry centerLine = stripe.getCenterLine();
+                    Geometry rightDesignLine =
+                            CrossSectionElement.offsetGeometry(centerLine, -0.2);
+                    ArrayList<Coordinate> result =
+                            makeDashes(new LengthIndexedLine(rightDesignLine), 0.2, 0, new double[]{3, 9});
+                    Geometry leftDesignLine =
+                            CrossSectionElement.offsetGeometry(centerLine, 0.2).buffer(0.1, QUADRANTSEGMENTS,
+                                    BufferParameters.CAP_FLAT);
+                    Coordinate[] leftCoordinates =
+                            leftDesignLine.buffer(0.1, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT).getCoordinates();
+                    for (int i = 0; i < leftCoordinates.length; i++)
+                    {
+                        result.add(leftCoordinates[i]);
+                    }
+                    result.add(NEWPATH);
+                    return result;
+                }
+                catch (NetworkException exception)
+                {
+                    exception.printStackTrace();
+                }
+                return new ArrayList<Coordinate>();// Return an empty ArrayList after an error occurred
+            }
+            case RIGHTONLY: // :| - Draw left 3-9 dashed, right solid
+                try
+                {
+                    Geometry centerLine = stripe.getCenterLine();
+                    Geometry leftDesignLine =
+                            CrossSectionElement.offsetGeometry(centerLine, 0.2);
+                    ArrayList<Coordinate> result =
+                            makeDashes(new LengthIndexedLine(leftDesignLine), 0.2, 0, new double[]{3, 9});
+                    Geometry rightDesignLine =
+                            CrossSectionElement.offsetGeometry(centerLine, -0.2).buffer(0.1, QUADRANTSEGMENTS,
+                                    BufferParameters.CAP_FLAT);
+                    Coordinate[] rightCoordinates =
+                            rightDesignLine.buffer(0.1, QUADRANTSEGMENTS, BufferParameters.CAP_FLAT).getCoordinates();
+                    for (int i = 0; i < rightCoordinates.length; i++)
+                    {
+                        result.add(rightCoordinates[i]);
+                    }
+                    result.add(NEWPATH);
+                    return result;
+                }
+                catch (NetworkException exception)
+                {
+                    exception.printStackTrace();
+                }
+                return new ArrayList<Coordinate>();// Return an empty ArrayList after an error occurred
+            case SOLID:// | - Draw single solid line. This (regretfully) involves copying everything twice...
+                return new ArrayList<Coordinate>(Arrays.asList(stripe.getContour().getCoordinates()));
             default:
                 throw new NamingException("Unsupported stripe type: " + stripeType);
         }
@@ -117,7 +229,9 @@ public class StripeAnimation extends Renderable2D
     {
         super(source, simulator);
         this.type = type;
-        this.coordinates = makeCoordinates(source, type);
+        ArrayList<Coordinate> coordList = makeCoordinates(source, type);
+        this.coordinates = new Coordinate[coordList.size()];
+        coordList.toArray(this.coordinates);
     }
 
     /** {@inheritDoc} */
@@ -128,7 +242,7 @@ public class StripeAnimation extends Renderable2D
         Stripe stripe = (Stripe) getSource();
         DirectedPoint p = stripe.getLocation();
         Path2D.Double path = new Path2D.Double();
-        boolean continuation = false;
+        boolean withinPath = false;
         for (Coordinate c : this.coordinates)
         {
             if (c == NEWPATH)
@@ -136,11 +250,11 @@ public class StripeAnimation extends Renderable2D
                 path.closePath();
                 graphics.fill(path);
                 path = new Path2D.Double();
-                continuation = false;
+                withinPath = false;
             }
-            else if (!continuation)
+            else if (!withinPath)
             {
-                continuation = true;
+                withinPath = true;
                 path.moveTo(c.x - p.x, -c.y + p.y);
             }
             else
@@ -148,7 +262,10 @@ public class StripeAnimation extends Renderable2D
                 path.lineTo(c.x - p.x, -c.y + p.y);
             }
         }
-        path.closePath();
+        if (withinPath)
+        {
+            path.closePath();
+        }
         graphics.fill(path);
     }
 
@@ -160,19 +277,19 @@ public class StripeAnimation extends Renderable2D
      * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
      */
     public static enum TYPE {
-        /** solid line. */
+        /** Single solid line. */
         SOLID,
 
-        /** line |: allow to go to left, but not to right. */
+        /** Line |: allow to go to left, but not to right. */
         LEFTONLY,
 
-        /** line :| allow to go to right, but not to left. */
+        /** Line :| allow to go to right, but not to left. */
         RIGHTONLY,
 
-        /** line : allow to cross in both directions. */
+        /** Dashes : allow to cross in both directions. */
         DASHED,
 
-        /** double solid line ||, don't cross. */
+        /** Double solid line ||, don't cross. */
         DOUBLE
     }
 }
