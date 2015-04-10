6a041d9f4d011e0c67e95a70e4187b075b99627d
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-04-10 15:07:53 +0000

    Moved the (often duplicated) code that draws a filled polygon into a separate class file.

---
+++ trunk/ots-core/src/main/java/org/opentrafficsim/core/network/animation/PaintPolygons.java
@@ -0,0 +1,71 @@
+package org.opentrafficsim.core.network.animation;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
+
+import nl.tudelft.simulation.language.d3.DirectedPoint;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+/**
+ * Paint a (series of) filled polygon(s) defined as a Path2D.Double
+ * <p>
+ * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
+ * reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version 10 apr. 2015 <br>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class PaintPolygons
+{
+    /** Do not instantiate this class. */
+    private PaintPolygons()
+    {
+        // Cannot be instantiated.
+    }
+
+    /** Dummy coordinate that forces the drawing operation to start a new path. */
+    public static final Coordinate NEWPATH = new Coordinate(Double.NaN, Double.NaN);
+
+    /**
+     * Paint (fill) a polygon or a series of polygons.
+     * @param graphics Graphics2D; the graphics environment
+     * @param color Color; the color to use
+     * @param referencePoint DirectedPoint; the reference point
+     * @param coordinates Coordinate[]; array of Coordinate
+     */
+    public static void paintMultiPolygon(final Graphics2D graphics, final Color color,
+            final DirectedPoint referencePoint, final Coordinate[] coordinates)
+    {
+        graphics.setColor(color);
+        Path2D.Double path = new Path2D.Double();
+        boolean withinPath = false;
+        for (Coordinate c : coordinates)
+        {
+            if (c == NEWPATH)
+            {
+                path.closePath();
+                graphics.fill(path);
+                path = new Path2D.Double();
+                withinPath = false;
+            }
+            else if (!withinPath)
+            {
+                withinPath = true;
+                path.moveTo(c.x - referencePoint.x, -c.y + referencePoint.y);
+            }
+            else
+            {
+                path.lineTo(c.x - referencePoint.x, -c.y + referencePoint.y);
+            }
+        }
+        if (withinPath)
+        {
+            path.closePath();
+        }
+        graphics.fill(path);
+    }
+
+}
--- trunk/ots-core/src/main/java/org/opentrafficsim/core/network/animation/ShoulderAnimation.java
+++ trunk/ots-core/src/main/java/org/opentrafficsim/core/network/animation/ShoulderAnimation.java
@@ -2,24 +2,20 @@ package org.opentrafficsim.core.network.animation;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Path2D;
 import java.awt.image.ImageObserver;
 import java.rmi.RemoteException;
 
 import javax.naming.NamingException;
 
 import nl.tudelft.simulation.dsol.animation.D2.Renderable2D;
-import nl.tudelft.simulation.language.d3.DirectedPoint;
 
 import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
 import org.opentrafficsim.core.network.lane.Shoulder;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-
 /**
  * <p>
- * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
+ * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
  * @version Oct 17, 2014 <br>
@@ -34,7 +30,7 @@ public class ShoulderAnimation extends Renderable2D
      * @throws RemoteException re
      */
     public ShoulderAnimation(final Shoulder source, final OTSSimulatorInterface simulator) throws NamingException,
-        RemoteException
+            RemoteException
     {
         super(source, simulator);
     }
@@ -43,26 +39,8 @@ public class ShoulderAnimation extends Renderable2D
     @Override
     public final void paint(final Graphics2D graphics, final ImageObserver observer) throws RemoteException
     {
-        graphics.setColor(Color.GREEN);
         Shoulder shoulder = (Shoulder) getSource();
-        DirectedPoint p = shoulder.getLocation();
-        Geometry g = shoulder.getContour();
-        Coordinate[] coordinates = g.getCoordinates();
-        Path2D.Double path = new Path2D.Double();
-        boolean start = false;
-        for (Coordinate c : coordinates)
-        {
-            if (!start)
-            {
-                start = true;
-                path.moveTo(c.x - p.x, -c.y + p.y);
-            }
-            else
-            {
-                path.lineTo(c.x - p.x, -c.y + p.y);
-            }
-        }
-        path.closePath();
-        graphics.fill(path);
+        PaintPolygons.paintMultiPolygon(graphics, Color.GREEN, shoulder.getLocation(), shoulder.getContour()
+                .getCoordinates());
     }
 }
