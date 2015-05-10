1825736eaa0e54f16e75c5a93e0973cf3ef6dda9
OTSim <P.Knoppers@tudelft.nl>
2015-05-10 08:34:55 +0000

    Improved javadoc

--- ots-core/src/main/java/org/opentrafficsim/core/car/LaneBasedIndividualCar.java
+++ ots-core/src/main/java/org/opentrafficsim/core/car/LaneBasedIndividualCar.java
@@ -30,6 +30,7 @@ import org.opentrafficsim.core.unit.SpeedUnit;
 import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
 
 /**
+ * Augments the AbstractLaneBasedIncividualGTU with a LaneBasedIndividualCarBuilder and animation support
  * <p>
  * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
  * reserved. <br>
