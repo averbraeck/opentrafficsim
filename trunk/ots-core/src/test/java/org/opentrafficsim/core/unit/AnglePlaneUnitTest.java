e6ac5dfa54b76e2f4debec7d4bdd557ad9486bbd
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-07-06 12:54:57 +0000

    Wrote unit test for the normalize methods. Test coverage is back up at 100%!

--- trunk/ots-core/src/test/java/org/opentrafficsim/core/unit/AnglePlaneUnitTest.java
+++ trunk/ots-core/src/test/java/org/opentrafficsim/core/unit/AnglePlaneUnitTest.java
@@ -4,15 +4,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.opentrafficsim.core.unit.unitsystem.UnitSystem.OTHER;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.opentrafficsim.core.locale.DefaultLocale;
+import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
+import org.opentrafficsim.core.value.vfloat.scalar.FloatScalar;
 
 /**
  * <p>
- * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2013-2014 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights
+ * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
  * @version Jun 4, 2014 <br>
@@ -49,14 +53,15 @@ public class AnglePlaneUnitTest extends AbstractUnitTest<AnglePlaneUnit>
         checkUnitRatioNameAndAbbreviation(AnglePlaneUnit.ARCMINUTE, 2 * Math.PI / 360 / 60, 0.0001, "arcminute", "\'");
         checkUnitRatioNameAndAbbreviation(AnglePlaneUnit.GRAD, 2 * Math.PI / 400, 0.00001, "gradian", "grad");
         // Check two conversions between non-standard units
-        assertEquals("one GRAD is about 54 ARCMINUTE", 54, getMultiplicationFactorTo(AnglePlaneUnit.GRAD,
-            AnglePlaneUnit.ARCMINUTE), 0.5);
-        assertEquals("one ARCMINUTE is about 0.0185 GRAD", 0.0185, getMultiplicationFactorTo(AnglePlaneUnit.ARCMINUTE,
-            AnglePlaneUnit.GRAD), 0.0001);
+        assertEquals("one GRAD is about 54 ARCMINUTE", 54,
+                getMultiplicationFactorTo(AnglePlaneUnit.GRAD, AnglePlaneUnit.ARCMINUTE), 0.5);
+        assertEquals("one ARCMINUTE is about 0.0185 GRAD", 0.0185,
+                getMultiplicationFactorTo(AnglePlaneUnit.ARCMINUTE, AnglePlaneUnit.GRAD), 0.0001);
         // Check conversion factor to standard unit for all remaining time units
         checkUnitRatioNameAndAbbreviation(AnglePlaneUnit.CENTESIMAL_ARCMINUTE, 0.00015708, 0.0000001,
-            "centesimal arcminute", "\'");
-        checkUnitRatioNameAndAbbreviation(AnglePlaneUnit.CENTESIMAL_ARCSECOND, 1.57079e-6, 0.1, "centesimal arcsecond", "\"");
+                "centesimal arcminute", "\'");
+        checkUnitRatioNameAndAbbreviation(AnglePlaneUnit.CENTESIMAL_ARCSECOND, 1.57079e-6, 0.1, "centesimal arcsecond",
+                "\"");
     }
 
     /**
@@ -66,10 +71,76 @@ public class AnglePlaneUnitTest extends AbstractUnitTest<AnglePlaneUnit>
     public final void createAngleUnit()
     {
         AnglePlaneUnit myAPU =
-            new AnglePlaneUnit(UnitLocalizationsTest.DONOTCHECKPREFIX + "AnglePlaneUnit.point",
-                UnitLocalizationsTest.DONOTCHECKPREFIX + "AnglePlaneUnit.pt", OTHER, AnglePlaneUnit.RADIAN, 0.19634954085);
+                new AnglePlaneUnit(UnitLocalizationsTest.DONOTCHECKPREFIX + "AnglePlaneUnit.point",
+                        UnitLocalizationsTest.DONOTCHECKPREFIX + "AnglePlaneUnit.pt", OTHER, AnglePlaneUnit.RADIAN,
+                        0.19634954085);
         assertTrue("Can create a new AngleUnit", null != myAPU);
         checkUnitRatioNameAndAbbreviation(myAPU, 0.19634954085, 0.0000001, "!point!", "!pt!");
+    }
+
+    /**
+     * Check normalize for all data types.
+     * @param expected double; expected value after normalization
+     * @param input double; value to normalize
+     */
+    private void checkDoubleNormalize(double input)
+    {
+        double margin = 0.00000000001;
+        double expected = input;
+        while (expected > 0)
+        {
+            expected -= 2 * Math.PI;
+        }
+        while (expected < 0)
+        {
+            expected += 2 * Math.PI;
+        }
+        assertEquals("double normalize", expected, AnglePlaneUnit.normalize(input), margin);
+        DoubleScalar.Abs<AnglePlaneUnit> dsa = new DoubleScalar.Abs<AnglePlaneUnit>(input, AnglePlaneUnit.SI);
+        assertEquals("DoubleScalar.Abs normalize", expected, AnglePlaneUnit.normalize(dsa).getSI(), margin);
+        DoubleScalar.Rel<AnglePlaneUnit> dsr = new DoubleScalar.Rel<AnglePlaneUnit>(input, AnglePlaneUnit.SI);
+        assertEquals("DoubleScalar.Rel normalize", expected, AnglePlaneUnit.normalize(dsr).getSI(), margin);
+    }
+
+    /**
+     * Check normalize for all data types.
+     * @param expected double; expected value after normalization
+     * @param input double; value to normalize
+     */
+    private void checkFloatNormalize(float input)
+    {
+        double margin = 0.00001;
+        float expected = input;
+        while (expected > 0)
+        {
+            expected -= 2 * Math.PI;
+        }
+        while (expected < 0)
+        {
+            expected += 2 * Math.PI;
+        }
+        assertEquals("float normalize", expected, AnglePlaneUnit.normalize(input), margin);
+        FloatScalar.Abs<AnglePlaneUnit> fsa = new FloatScalar.Abs<AnglePlaneUnit>(input, AnglePlaneUnit.SI);
+        assertEquals("FloatScalar.Abs normalize", expected, AnglePlaneUnit.normalize(fsa).getSI(), margin);
+        FloatScalar.Rel<AnglePlaneUnit> fsr = new FloatScalar.Rel<AnglePlaneUnit>(input, AnglePlaneUnit.SI);
+        assertEquals("FloatScalar.Rek normalize", expected, AnglePlaneUnit.normalize(fsr).getSI(), margin);
+    }
+
+    /**
+     * Verify that the normalizations work as intended.
+     */
+    @Test
+    public final void normalizations()
+    {
+        for (int i = -100; i <= 100; i++)
+        {
+            double doubleValue = i * Math.PI / 10;
+            checkDoubleNormalize(doubleValue - i * Math.ulp(doubleValue));
+            checkDoubleNormalize(doubleValue + i * Math.ulp(doubleValue));
+            float floatValue = (float) (i * Math.PI / 10);
+            checkFloatNormalize(floatValue - i * Math.ulp(floatValue));
+            checkFloatNormalize(floatValue + i * Math.ulp(floatValue));
+        }
     }
 
 }
