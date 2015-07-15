a93a03328452187ca23b02a0266b50ddd639bc80
OTSim <P.Knoppers@tudelft.nl>
2015-07-15 09:24:45 +0000

    New javadoc comment in header of all class files with svn keyword substitution.

--- ots-build-tools/development/mvn-all-clean-install.bat
+++ ots-build-tools/development/mvn-all-clean-install.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM $HeadURL: https://svn.tbm.tudelft.nl/TBM-SK/DSOL-AOS/dsol-aos/development/mvn-all-clean-install.bat $
-REM $Id: mvn-all-clean-install.bat 797 2013-07-01 08:41:27Z dvankrevelen $
+REM $Id$
 
 REM Runs Maven commands 'clean' and 'install' for all Maven artifacts
 
--- ots-build-tools/src/main/java/code/generators/ValueTestsGenerator.java
+++ ots-build-tools/src/main/java/code/generators/ValueTestsGenerator.java
@@ -10,7 +10,7 @@ import java.util.Date;
  * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
- * @version 16 okt. 2014 <br>
+ * $LastChangedDate$, @version $Revision$, by $Author$, initial version16 okt. 2014 <br>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
 public class ValueTestsGenerator
