c3e888a4b3231f26e12acad6550227f21f28863d
OTSim <P.Knoppers@tudelft.nl>
2015-07-15 10:53:54 +0000

    Reformat after SVN keyword substitution.

--- ots-core/src/main/java/org/opentrafficsim/core/gtu/AbstractGTU.java
+++ ots-core/src/main/java/org/opentrafficsim/core/gtu/AbstractGTU.java
@@ -13,7 +13,8 @@ import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
  * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
- * @version $Revision$, $LastChangedDate$, by $Author$, initial version Oct 22, 2014 <br>
+ * @version $Revision$, $LastChangedDate$, by $Author: pknoppers
+ *          $, initial version Oct 22, 2014 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  * @param <ID> The type of ID, e.g., String or Integer
--- ots-core/src/main/java/org/opentrafficsim/core/gtu/GTU.java
+++ ots-core/src/main/java/org/opentrafficsim/core/gtu/GTU.java
@@ -20,7 +20,8 @@ import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
  * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
  * <p>
- * @version $Revision$, $LastChangedDate$, by $Author$, initial version May 15, 2014 <br>
+ * @version $Revision$, $LastChangedDate$, by $Author: pknoppers
+ *          $, initial version May 15, 2014 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
