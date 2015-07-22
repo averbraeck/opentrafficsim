80eb2037b1add3b39d74dbc329276b162c150af3
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-07-22 08:26:40 +0000

    Smooth animation patch.

--- trunk/ots-core/src/main/java/org/opentrafficsim/simulationengine/SimpleAnimator.java
+++ trunk/ots-core/src/main/java/org/opentrafficsim/simulationengine/SimpleAnimator.java
@@ -7,6 +7,8 @@ import javax.naming.NamingException;
 import nl.tudelft.simulation.dsol.SimRuntimeException;
 import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
 import nl.tudelft.simulation.dsol.formalisms.eventscheduling.SimEvent;
+import nl.tudelft.simulation.dsol.formalisms.eventscheduling.SimEventInterface;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
 
 import org.opentrafficsim.core.dsol.OTSDEVSRealTimeClock;
 import org.opentrafficsim.core.dsol.OTSModelInterface;
@@ -14,6 +16,7 @@ import org.opentrafficsim.core.dsol.OTSReplication;
 import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
 import org.opentrafficsim.core.unit.TimeUnit;
 import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar;
+import org.opentrafficsim.core.value.vdouble.scalar.DoubleScalar.Rel;
 
 /**
  * <p>
@@ -66,6 +69,124 @@ public class SimpleAnimator extends OTSDEVSRealTimeClock implements SimpleSimula
                         executionTime.getSI(), TimeUnit.SECOND)), priority, source, target, method, args);
         scheduleEvent(result);
         return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public void run()
+    {
+        AnimationThread animationThread = new AnimationThread(this);
+        animationThread.start();
+
+        long clockTime0 = System.currentTimeMillis(); // _________ current zero for the wall clock
+        OTSSimTimeDouble simTime0 = this.simulatorTime; // _______ current zero for the sim clock
+        double factor = getSpeedFactor(); // _____________________ local copy of speed factor to detect change
+        double msec1 = relativeMillis(1.0).doubleValue(); // _____ translation factor for 1 msec for sim clock
+        DoubleScalar.Rel<TimeUnit> r10 = this.relativeMillis(10.0 * factor); // sim clock change for 10 msec wall clock
+
+        while (this.isRunning() && !this.eventList.isEmpty()
+                && this.simulatorTime.le(this.replication.getTreatment().getEndTime()))
+        {
+            // check if speedFactor has changed. If yes: re-baseline.
+            if (factor != getSpeedFactor())
+            {
+                clockTime0 = System.currentTimeMillis();
+                simTime0 = this.simulatorTime;
+                factor = getSpeedFactor();
+                r10 = this.relativeMillis(10.0 * factor);
+            }
+
+            // peek at the first event and determine the time difference relative to RT speed.
+            SimEventInterface<OTSSimTimeDouble> event = this.eventList.first();
+            double simTimeDiffMillis =
+                    (event.getAbsoluteExecutionTime().minus(simTime0)).doubleValue() / (msec1 * factor);
+
+            /*
+             * simTimeDiff gives the number of milliseconds between the last event and this event. if speed == 1, this
+             * is the number of milliseconds we have to wait. if speed == 10, we have to wait 1/10 of that. If the speed
+             * == 0.1, we have to wait 10 times that amount. We might also be behind.
+             */
+            if (simTimeDiffMillis < (System.currentTimeMillis() - clockTime0))
+            {
+                // we are behind.
+                if (!isCatchup())
+                {
+                    // if no catch-up: re-baseline.
+                    clockTime0 = System.currentTimeMillis();
+                    simTime0 = this.simulatorTime;
+                }
+                else
+                {
+                    // if catch-up: indicate we were behind.
+                    this.fireTimedEvent(BACKLOG_EVENT, this.simulatorTime, null);
+                }
+            }
+            else
+            {
+                while (simTimeDiffMillis > System.currentTimeMillis() - clockTime0)
+                {
+                    try
+                    {
+                        Thread.sleep(10);
+
+                        // check if speedFactor has changed. If yes: break out of this loop and execute event.
+                        // this could cause a jump.
+                        if (factor != getSpeedFactor())
+                        {
+                            simTimeDiffMillis = 0.0;
+                        }
+
+                    }
+                    catch (InterruptedException ie)
+                    {
+                        // do nothing
+                        ie = null;
+                    }
+
+                    // make a small time step for the animation during wallclock waiting.
+                    // but never beyond the next event time.
+                    if (this.simulatorTime.plus(r10).lt(event.getAbsoluteExecutionTime()))
+                    {
+                        this.simulatorTime.add(r10);
+                        updateAnimation();
+                    }
+                }
+            }
+
+            synchronized (super.semaphore)
+            {
+                this.simulatorTime = event.getAbsoluteExecutionTime();
+                this.fireTimedEvent(SimulatorInterface.TIME_CHANGED_EVENT, this.simulatorTime, this.simulatorTime);
+
+                // carry out all events scheduled on this simulation time, as long as we are still running.
+                while (this.isRunning() && !this.eventList.isEmpty()
+                        && event.getAbsoluteExecutionTime().eq(this.simulatorTime))
+                {
+                    event = this.eventList.removeFirst();
+                    try
+                    {
+                        event.execute();
+                    }
+                    catch (Exception exception)
+                    {
+                        exception.printStackTrace();
+                        if (this.isPauseOnError())
+                        {
+                            this.stop();
+                        }
+                    }
+                    if (!this.eventList.isEmpty())
+                    {
+                        // peek at next event for while loop.
+                        event = this.eventList.first();
+                    }
+                }
+            }
+        }
+        this.fireTimedEvent(SimulatorInterface.TIME_CHANGED_EVENT, this.simulatorTime, this.simulatorTime);
+        updateAnimation();
+        animationThread.stopAnimation();
     }
 
 }
