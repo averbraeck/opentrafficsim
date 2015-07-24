e403689c93b52e725776294a56a40d91806cc923
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2015-07-24 06:54:40 +0000

    Major revision to version 0.02.03. - Geotools hierarchy changed - DOM parser instead of SAX parser - removed point2D implementation - own coordinate system introduced

--- ots-core/src/main/java/org/opentrafficsim/core/network/factory/xml/NodeTag.java
+++ ots-core/src/main/java/org/opentrafficsim/core/network/factory/xml/NodeTag.java
@@ -1,7 +1,12 @@
 package org.opentrafficsim.core.network.factory.xml;
 
+import java.rmi.RemoteException;
+
+import javax.naming.NamingException;
+
 import org.opentrafficsim.core.geometry.OTSPoint3D;
 import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.core.network.OTSNode;
 import org.opentrafficsim.core.network.factory.xml.units.AngleUnits;
 import org.opentrafficsim.core.network.factory.xml.units.Coordinates;
 import org.opentrafficsim.core.unit.AnglePlaneUnit;
@@ -43,9 +48,12 @@ class NodeTag
      * @param parser the parser with the lists of information
      * @throws SAXException when parsing of GTU tag fails
      * @throws NetworkException when parsing of GTU tag fails
+     * @throws NamingException
+     * @throws RemoteException
      */
     @SuppressWarnings("checkstyle:needbraces")
-    static void parseNodes(final NodeList nodeList, final XmlNetworkLaneParser parser) throws SAXException, NetworkException
+    static void parseNodes(final NodeList nodeList, final XmlNetworkLaneParser parser) throws SAXException,
+        NetworkException
     {
         for (Node node : XMLParser.getNodes(nodeList, "NODE"))
         {
@@ -66,8 +74,43 @@ class NodeTag
                 nodeTag.angle = AngleUnits.parseAngleAbs(attributes.getNamedItem("ANGLE").getNodeValue());
 
             // TODO slope for the Node.
-            
+
             parser.nodeTags.put(nodeTag.name, nodeTag);
+
+            if (nodeTag.coordinate != null)
+            {
+                // only make a node if we know the coordinate. Otherwise, wait till we can calculate it.
+                try
+                {
+                    makeNode(nodeTag, parser);
+                }
+                catch (RemoteException | NamingException exception)
+                {
+                    throw new NetworkException(exception);
+                }
+            }
         }
+    }
+
+    /**
+     * @param nodeTag the tag with the info for the node.
+     * @param parser the parser with the lists of information
+     * @return a constructed node
+     * @throws NetworkException when point cannot be instantiated
+     * @throws NamingException when animation context cannot be found.
+     * @throws RemoteException when communication error occurs when trying to find animation context.
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    static org.opentrafficsim.core.network.Node makeNode(final NodeTag nodeTag, final XmlNetworkLaneParser parser)
+        throws NetworkException, RemoteException, NamingException
+    {
+        Object id = XMLParser.makeId(parser.nodeIdClass, nodeTag.name);
+        DoubleScalar.Abs<AnglePlaneUnit> angle =
+            nodeTag.angle == null ? new DoubleScalar.Abs<AnglePlaneUnit>(0.0, AnglePlaneUnit.SI) : nodeTag.angle;
+        DoubleScalar.Abs<AngleSlopeUnit> slope =
+            nodeTag.slope == null ? new DoubleScalar.Abs<AngleSlopeUnit>(0.0, AngleSlopeUnit.SI) : nodeTag.slope;
+        org.opentrafficsim.core.network.Node node = new OTSNode(id, nodeTag.coordinate, angle, slope);
+        parser.nodes.put(node.getId().toString(), node);
+        return node;
     }
 }
