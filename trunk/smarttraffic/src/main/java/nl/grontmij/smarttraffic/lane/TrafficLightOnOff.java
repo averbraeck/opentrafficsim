db7e8542bb91acf93220ddc46a81172dda950da3
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-08-10 10:34:25 +0000

    Aanpassingen gemaakt t.a.v. de wijzigingen in de code van 9 augustus in ots-core: - addLane => enterLane; zorgt ook voor de registratie op de Lane - removeLane => leaveLane; zorgt ook voor de afmelding van de Lane - moveFrontToSubsequentLane is verdwenen; dat is nu ingebouwd in gtu.triggerSensors() - SensorLaneStart en SensorLaneEnd zijn verdwenen; code voor aan/afmelden van een strook is nu ingebouwd in gtu.triggerSensors()

--- trunk/smarttraffic/src/main/java/nl/grontmij/smarttraffic/lane/TrafficLightOnOff.java
+++ trunk/smarttraffic/src/main/java/nl/grontmij/smarttraffic/lane/TrafficLightOnOff.java
@@ -361,21 +361,14 @@ public class TrafficLightOnOff extends AbstractGTU<Integer> implements LaneBased
 
     /** {@inheritDoc} */
     @Override
-    public final void addFrontToSubsequentLane(Lane<?, ?> lane) throws RemoteException, NetworkException
+    public final void enterLane(final Lane<?, ?> lane, final DoubleScalar.Rel<LengthUnit> position) throws NetworkException
     {
         // do nothing
     }
 
     /** {@inheritDoc} */
     @Override
-    public final void addLane(final Lane<?, ?> lane, final DoubleScalar.Rel<LengthUnit> position) throws NetworkException
-    {
-        // do nothing
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public final void removeLane(final Lane<?, ?> lane)
+    public final void leaveLane(final Lane<?, ?> lane)
     {
         // do nothing
     }
