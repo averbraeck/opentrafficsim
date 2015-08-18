de15893bb137749906a1acccd683e9c0bc425ba4
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2015-08-18 00:06:54 +0000

    Created OTSRenderable2D in gui package to better deal with the effects of race conditions (null pointers, exceptions).

--- ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/animation/AreaAnimation.java
+++ ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/animation/AreaAnimation.java
@@ -10,11 +10,10 @@ import java.rmi.RemoteException;
 
 import javax.naming.NamingException;
 
-import nl.tudelft.simulation.dsol.animation.D2.Renderable2D;
-
 import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
 import org.opentrafficsim.core.unit.SpeedUnit;
 import org.opentrafficsim.demo.ntm.Area;
+import org.opentrafficsim.gui.OTSRenderable2D;
 
 /**
  * <p>
@@ -26,7 +25,7 @@ import org.opentrafficsim.demo.ntm.Area;
  * $, initial version Sep 13, 2014 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class AreaAnimation extends Renderable2D
+public class AreaAnimation extends OTSRenderable2D
 {
     private float width;
 
