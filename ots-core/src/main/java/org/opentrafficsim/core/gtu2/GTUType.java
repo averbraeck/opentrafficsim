2b770d5c17a0556779313da0097d88797f8e60a9
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2015-09-01 22:23:25 +0000

    OTS 0.03.02 New shorthand for relative and absolute scalars Dependent on DJUNIT

---
+++ ots-core/src/main/java/org/opentrafficsim/core/gtu2/GTUType.java
@@ -0,0 +1,75 @@
+package org.opentrafficsim.core.gtu2;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * A GTU type identifies the type of a GTU. <br>
+ * GTU types are used to check whether a particular GTU can travel over a particular part of infrastructure. E.g. a
+ * (LaneBased)GTU with GTUType CAR can travel over lanes that have a LaneType that has the GTUType CAR in the compatibility set.
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision: 1165 $, $LastChangedDate: 2015-07-28 17:11:47 +0200 (Tue, 28 Jul 2015) $, by $Author: averbraeck $,
+ *          initial version Dec 31, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public final class GTUType implements Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20141231L;
+
+    /** The id of the GTUType to make it identifiable. */
+    private final String id;
+
+    /** ALL GTUType to be used only for permeability and accessibility. */
+    public static final GTUType ALL = new GTUType("ALL");
+
+    /**
+     * @param id The id of the GTUType to make it identifiable.
+     */
+    private GTUType(final String id)
+    {
+        this.id = id;
+    }
+
+    /** The set of previously instantiated GTUTypes. */
+    private static final Map<Object, GTUType> INSTANTIATEDGTUTYPES = new LinkedHashMap<Object, GTUType>();
+
+    /**
+     * Construct a new GTUType or (if it already exists) return an existing GTUType.
+     * @param id ID; the id of the GTUType
+     * @return GTUType&lt;ID&gt;
+     */
+    public static GTUType makeGTUType(final String id)
+    {
+        synchronized (INSTANTIATEDGTUTYPES)
+        {
+            GTUType result = INSTANTIATEDGTUTYPES.get(id);
+            if (null == result)
+            {
+                result = new GTUType(id);
+                INSTANTIATEDGTUTYPES.put(id, result);
+            }
+            return result;
+        }
+    }
+
+    /**
+     * @return id.
+     */
+    public String getId()
+    {
+        return this.id;
+    }
+
+    /** {@inheritDoc} */
+    public String toString()
+    {
+        return "GTUType: " + this.id;
+    }
+
+}
