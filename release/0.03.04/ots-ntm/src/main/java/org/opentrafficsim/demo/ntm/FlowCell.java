9e295304afdf5a1039e4a58bfb868497bf103395
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-09-25 22:59:42 +0000

    Tagged version 0.03.04

---
+++ release/0.03.04/ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/FlowCell.java
@@ -0,0 +1,265 @@
+package org.opentrafficsim.demo.ntm;
+
+import java.awt.geom.Point2D;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.media.j3d.Bounds;
+
+import nl.tudelft.simulation.dsol.animation.LocatableInterface;
+import nl.tudelft.simulation.language.d3.DirectedPoint;
+
+import org.djunits.unit.FrequencyUnit;
+import org.djunits.unit.LengthUnit;
+import org.djunits.unit.LinearDensityUnit;
+import org.djunits.unit.SpeedUnit;
+import org.djunits.unit.TimeUnit;
+import org.djunits.value.vdouble.scalar.DoubleScalar;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Abs;
+import org.opentrafficsim.demo.ntm.Node.TrafficBehaviourType;
+import org.opentrafficsim.demo.ntm.fundamentaldiagrams.FundamentalDiagram;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version 14 Oct 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://Hansvanlint.weblog.tudelft.nl">Hans van Lint</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
+ * @author <a href="http://www.citg.tudelft.nl">Yufei Yuan</a>
+ */
+public class FlowCell implements LocatableInterface
+{
+    /** link length in a length unit. */
+    private DoubleScalar.Rel<LengthUnit> cellLength;
+
+    /** link capacity in vehicles per hour. This is a mutable property (e.g., blockage). */
+    private DoubleScalar.Abs<FrequencyUnit> maxCapacity;
+
+    /** lanes in a link */
+    private int numberOfLanes;
+
+    /** SPEEDAB class java.lang.Double 120.0. */
+    private DoubleScalar.Abs<SpeedUnit> currentSpeed;
+
+    /** SPEEDAB class java.lang.Double 120.0. */
+    private DoubleScalar.Abs<TimeUnit> currentTravelTime;
+
+    /** */
+    private CellBehaviourFlow cellBehaviour;
+
+    /**
+     * @param cellLength
+     * @param maxCapacity
+     * @param speed
+     * @param numberOfLanes
+     * @param behaviourType
+     */
+    public FlowCell(final DoubleScalar.Rel<LengthUnit> cellLength, final DoubleScalar.Abs<FrequencyUnit> maxCapacity,
+        DoubleScalar.Abs<SpeedUnit> speed, final int numberOfLanes, final TrafficBehaviourType behaviourType)
+    {
+        this.cellLength = cellLength;
+        this.maxCapacity = maxCapacity;
+        this.numberOfLanes = numberOfLanes;
+        ParametersFundamentalDiagram parametersFD = new ParametersFundamentalDiagram(speed, maxCapacity, numberOfLanes);
+        if (behaviourType == TrafficBehaviourType.FLOW)
+        {
+            this.cellBehaviour = new CellBehaviourFlow(null, parametersFD);
+        }
+    }
+
+    /**
+     * Retrieves car production from network fundamental diagram.
+     * @param accumulatedCarsPerLengthUnit number of cars in Cell
+     * @param maximumCapacity based on area information
+     * @param param parameters FD
+     * @return carProduction
+     */
+    public final Abs<FrequencyUnit> retrieveCurrentInflowCapacity(final double accumulatedCarsPerLengthUnit,
+        final Abs<FrequencyUnit> maximumCapacity, final ParametersFundamentalDiagram param)
+    {
+        Abs<FrequencyUnit> currentInflowCapacity;
+        if (accumulatedCarsPerLengthUnit > param.getAccCritical().get(0))
+        {
+            ArrayList<Point2D> xyPairs = new ArrayList<Point2D>();
+            Point2D p = new Point2D.Double();
+            p.setLocation(0, 0);
+            xyPairs.add(p);
+            p = new Point2D.Double();
+            p.setLocation(param.getAccCritical().get(0), maximumCapacity.getInUnit());
+            xyPairs.add(p);
+            p = new Point2D.Double();
+            p.setLocation(param.getAccCritical().get(1), 0);
+            xyPairs.add(p);
+            currentInflowCapacity = FundamentalDiagram.PieceWiseLinear(xyPairs, accumulatedCarsPerLengthUnit);
+        }
+        else
+        {
+            currentInflowCapacity = maximumCapacity;
+        }
+        return currentInflowCapacity;
+    }
+
+    /**
+     * @param accumulatedCarsPerLengthUnit
+     * @return actualSpeed.
+     */
+    public DoubleScalar.Abs<SpeedUnit> retrieveCurrentSpeed(final double accumulatedCarsPerLengthUnit)
+    {
+        double speedDouble;
+        Abs<FrequencyUnit> currentInflowCapacity =
+            retrieveCurrentInflowCapacity(accumulatedCarsPerLengthUnit, this.maxCapacity, this.cellBehaviour
+                .getParametersFundamentalDiagram());
+        Abs<LinearDensityUnit> density =
+            new DoubleScalar.Abs<LinearDensityUnit>(accumulatedCarsPerLengthUnit, LinearDensityUnit.PER_KILOMETER);
+        if (density.getInUnit(LinearDensityUnit.PER_KILOMETER) > this.cellBehaviour.getParametersFundamentalDiagram()
+            .getAccCritical().get(0))
+        {
+            speedDouble =
+                currentInflowCapacity.getInUnit(FrequencyUnit.PER_HOUR) / density.getInUnit(LinearDensityUnit.PER_KILOMETER);
+            // speedDouble =
+            // Math.max(speedDouble, this.getCellBehaviourFlow().getParametersFundamentalDiagram().getFreeSpeed()
+            // .getInUnit(SpeedUnit.KM_PER_HOUR));
+        }
+        else
+        {
+            speedDouble =
+                this.cellBehaviour.getParametersFundamentalDiagram().getCapacity().getInUnit(FrequencyUnit.PER_HOUR)
+                    / this.cellBehaviour.getParametersFundamentalDiagram().getAccCritical().get(0);
+        }
+        return this.setActualSpeed(new DoubleScalar.Abs<SpeedUnit>(speedDouble, SpeedUnit.KM_PER_HOUR));
+    }
+
+    /**
+     * @param accumulatedCars
+     * @return actualSpeed.
+     */
+    public DoubleScalar.Abs<TimeUnit> retrieveCurrentTravelTime()
+    {
+        double densityPerLengthUnit =
+            this.getCellBehaviourFlow().getAccumulatedCars() / this.cellLength.getInUnit(LengthUnit.KILOMETER);
+        double timeDouble =
+            this.cellLength.getInUnit(LengthUnit.KILOMETER)
+                / retrieveCurrentSpeed(densityPerLengthUnit).getInUnit(SpeedUnit.KM_PER_HOUR);
+        double UPPERBOUND_TRAVELTIME_HOUR = 99;
+        timeDouble = Math.min(UPPERBOUND_TRAVELTIME_HOUR, timeDouble);
+        return this.setCurrentTravelTime(new DoubleScalar.Abs(timeDouble, TimeUnit.HOUR));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public DirectedPoint getLocation() throws RemoteException
+    {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Bounds getBounds() throws RemoteException
+    {
+        return null;
+    }
+
+    /**
+     * @return cellLength.
+     */
+    public DoubleScalar<LengthUnit> getCellLength()
+    {
+        return this.cellLength;
+    }
+
+    /**
+     * @param cellLength set cellLength.
+     */
+    public void setCellLength(DoubleScalar.Rel<LengthUnit> cellLength)
+    {
+        this.cellLength = cellLength;
+    }
+
+    /**
+     * @return capacity.
+     */
+    public DoubleScalar.Abs<FrequencyUnit> getMaxCapacity()
+    {
+        return this.maxCapacity;
+    }
+
+    /**
+     * @param maxCapacity set capacity.
+     */
+    public void setMaxCapacity(DoubleScalar.Abs<FrequencyUnit> maxCapacity)
+    {
+        this.maxCapacity = maxCapacity;
+    }
+
+    /**
+     * @return cellBehaviour.
+     */
+    public CellBehaviourFlow getCellBehaviourFlow()
+    {
+        return this.cellBehaviour;
+    }
+
+    /**
+     * @param cellBehaviour set cellBehaviour.
+     */
+    public void setCellBehaviourFlow(CellBehaviourFlow cellBehaviour)
+    {
+        this.cellBehaviour = cellBehaviour;
+    }
+
+    /**
+     * @return numberOfLanes.
+     */
+    public int getNumberOfLanes()
+    {
+        return this.numberOfLanes;
+    }
+
+    /**
+     * @param numberOfLanes set numberOfLanes.
+     */
+    public void setNumberOfLanes(int numberOfLanes)
+    {
+        this.numberOfLanes = numberOfLanes;
+    }
+
+    /**
+     * @return actualSpeed.
+     */
+    public DoubleScalar.Abs<SpeedUnit> getCurrentSpeed()
+    {
+        return currentSpeed;
+    }
+
+    /**
+     * @param currentSpeed set actualSpeed.
+     */
+    public DoubleScalar.Abs<SpeedUnit> setActualSpeed(DoubleScalar.Abs<SpeedUnit> currentSpeed)
+    {
+        this.currentSpeed = currentSpeed;
+        return currentSpeed;
+    }
+
+    /**
+     * @return currentTravelTime.
+     */
+    public DoubleScalar.Abs<TimeUnit> getCurrentTravelTime()
+    {
+        return currentTravelTime;
+    }
+
+    /**
+     * @param currentTravelTime set currentTravelTime.
+     */
+    public DoubleScalar.Abs<TimeUnit> setCurrentTravelTime(DoubleScalar.Abs<TimeUnit> currentTravelTime)
+    {
+        this.currentTravelTime = currentTravelTime;
+        return currentTravelTime;
+    }
+
+}
---
+++ release/0.03.04/ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/NTMTestApplication.java
@@ -0,0 +1,138 @@
+package org.opentrafficsim.demo.ntm;
+
+import java.awt.Dimension;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import nl.tudelft.simulation.dsol.animation.D2.AnimationPanel;
+import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
+import nl.tudelft.simulation.dsol.gui.swing.DSOLApplication;
+import nl.tudelft.simulation.dsol.gui.swing.DSOLPanel;
+import nl.tudelft.simulation.dsol.gui.swing.HTMLPanel;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.event.Event;
+
+import org.djunits.unit.TimeUnit;
+import org.djunits.value.vdouble.scalar.DoubleScalar;
+import org.opentrafficsim.core.dsol.OTSDEVSAnimator;
+import org.opentrafficsim.core.dsol.OTSReplication;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
+import org.opentrafficsim.demo.ntm.IO.ProjectConfigurations;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Aug 15, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class NTMTestApplication extends DSOLApplication
+{
+    /**
+     * @param title
+     * @param panel
+     */
+    public NTMTestApplication(String title,
+        DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> panel)
+    {
+        super(title, panel);
+    }
+
+    /** */
+    private static final long serialVersionUID = 20140819L;
+
+    /** */
+    public static DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> panel;
+
+    public static JTextArea textArea;
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(final String[] args) throws Exception
+    {
+        NTMModel model = new NTMModel();
+        InputNTM inputNTM = new InputNTM();
+        model.setInputNTM(inputNTM);
+        String startMap = "D:/gtamminga/workspace/ots-ntm/src/main/resources/gis/TheHague/";
+        // String startMap = System.getProperty("user.dir");
+        ProjectConfigurations.readConfigurations(startMap, model);
+        if (!new File(model.getInputNTM().getInputMap()).canRead())
+        {
+            model.getInputNTM().setInputMap(System.getProperty("user.dir"));
+        }
+        OTSDEVSAnimator simulator = new OTSDEVSAnimator();
+        // model.getSettingsNTM().getStartTimeSinceMidnight().getInUnit(TimeUnit.SECOND)
+        OTSSimTimeDouble startTime = new OTSSimTimeDouble(new DoubleScalar.Abs<TimeUnit>(0.0, TimeUnit.SECOND));
+        OTSReplication replication =
+            new OTSReplication("rep1", startTime, new DoubleScalar.Rel<TimeUnit>(0.0, TimeUnit.SECOND),
+                new DoubleScalar.Rel<TimeUnit>(10800.0, TimeUnit.SECOND), model);
+        // simulator.initialize(replication, ReplicationMode.TERMINATING);
+
+        panel = new DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble>(model, simulator);
+        addInfoTab(panel);
+
+        Rectangle2D extent = new Rectangle2D.Double(65000.0, 440000.0, 55000.0, 30000.0);
+        Dimension size = new Dimension(1024, 768);
+        AnimationPanel animationPanel = new AnimationPanel(extent, size, simulator);
+        panel.getTabbedPane().addTab(0, "animation", animationPanel);
+        String content =
+            "The simulation starts with the import of alle data, and initializes the NTM model\n"
+                + "Wait untill this process has finished...\n" + " \n";
+        int index = panel.getTabbedPane().getSelectedIndex();
+        textArea = new JTextArea(content);
+        panel.getTabbedPane().setComponentAt(index, textArea);
+        // tell the animation panel to update its statistics
+        // TODO should be done automatically in DSOL!
+        animationPanel.notify(new Event(SimulatorInterface.START_REPLICATION_EVENT, simulator, null));
+        // infoBox("Start initialization", "NTM");
+        new NTMTestApplication("Network Transmission Model", panel);
+
+        simulator.initialize(replication, ReplicationMode.TERMINATING);
+        // infoBox("Ended initialization", "NTM");
+        textArea.append("Finished the initialization,\n" + "Push the Start button now! \n" + " \n");
+
+    }
+
+    public static void infoBox(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * @param panel
+     */
+    private static void addInfoTab(
+        final DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> panel)
+    {
+        // Let's find some content for our infoscreen and add it to our tabbedPane
+        String helpSource = "/" + NTMModel.class.getPackage().getName().replace('.', '/') + "/html/ntm.html";
+        URL page = NTMModel.class.getResource(helpSource);
+        if (page != null)
+        {
+            HTMLPanel htmlPanel = null;
+            try
+            {
+                htmlPanel = new HTMLPanel(page);
+            }
+            catch (IOException exception)
+            {
+                exception.printStackTrace();
+            }
+            panel.getTabbedPane().addTab("info", new JScrollPane(htmlPanel));
+        }
+        else
+        {
+            System.err.println("Information page " + helpSource + " not found.");
+        }
+    }
+}
