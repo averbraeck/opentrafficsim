757de150ccd8cfec236c2aaa8eae28f70f867862
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-11-04 14:45:57 +0000

    Working on sessions and php. Credentials of the test user are Username	demo Password	abc

---
+++ trunk/ots-edit/network/selectproject.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+include ('login.php');
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>OTS - Select Project</title>
+</head>
+<body>
+	<div id="main">
+		<h1>OTS - login</h1>
+		You are logged in.
+		<hr>
+			<a href="logout.php">logout</a>
+	
+	</div>
+</body>
+</html>
