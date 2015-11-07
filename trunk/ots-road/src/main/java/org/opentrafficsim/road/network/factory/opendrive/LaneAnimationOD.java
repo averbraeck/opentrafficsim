65c65a888e7b6a25609e04d87ed939c81a5d0d11
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-11-07 17:25:51 +0000

    Arrows on the road for OpenDrive to see directions.

--- trunk/ots-road/src/main/java/org/opentrafficsim/road/network/factory/opendrive/LaneAnimationOD.java
+++ trunk/ots-road/src/main/java/org/opentrafficsim/road/network/factory/opendrive/LaneAnimationOD.java
@@ -8,6 +8,7 @@ import java.awt.image.ImageObserver;
 import java.rmi.RemoteException;
 
 import javax.naming.NamingException;
+import javax.vecmath.Point2d;
 
 import nl.tudelft.simulation.dsol.animation.D2.Renderable2D;
 import nl.tudelft.simulation.language.d3.DirectedPoint;
@@ -15,6 +16,9 @@ import nl.tudelft.simulation.language.d3.DirectedPoint;
 import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
 import org.opentrafficsim.core.geometry.OTSLine3D;
 import org.opentrafficsim.core.geometry.OTSPoint3D;
+import org.opentrafficsim.core.gtu.GTUType;
+import org.opentrafficsim.core.network.LongitudinalDirectionality;
+import org.opentrafficsim.core.network.NetworkException;
 import org.opentrafficsim.core.network.animation.PaintPolygons;
 import org.opentrafficsim.road.network.lane.Lane;
 
@@ -67,7 +71,63 @@ public class LaneAnimationOD extends Renderable2D
         }
         graphics.draw(path);
     }
-    
+
+    private static void paintArrow(final Graphics2D graphics, final Color color, final DirectedPoint ref,
+        final OTSLine3D line, final double fraction, final LongitudinalDirectionality dir)
+    {
+        try
+        {
+            double len = 3.0;
+            double ar = 0.5;
+            DirectedPoint start = line.getLocationFraction(fraction);
+            graphics.setColor(color);
+            graphics.setStroke(new BasicStroke(0.2f));
+            Path2D.Double path = new Path2D.Double();
+            path.moveTo(start.x - ref.x, -start.y + ref.y);
+            Point2d end =
+                new Point2d(start.x + len * Math.cos(start.getRotZ()), start.y + len * Math.sin(start.getRotZ()));
+            path.lineTo(end.x - ref.x, -end.y + ref.y);
+            graphics.draw(path);
+
+            if (dir.equals(LongitudinalDirectionality.FORWARD) || dir.equals(LongitudinalDirectionality.BOTH))
+            {
+                path = new Path2D.Double();
+                path.moveTo(end.x - ref.x, -end.y + ref.y);
+                Point2d ar1 =
+                    new Point2d(end.x + ar * Math.cos(start.getRotZ() + 0.75 * Math.PI), end.y + ar
+                        * Math.sin(start.getRotZ() + 0.75 * Math.PI));
+                path.lineTo(ar1.x - ref.x, -ar1.y + ref.y);
+                path.moveTo(end.x - ref.x, -end.y + ref.y);
+                Point2d ar2 =
+                        new Point2d(end.x + ar * Math.cos(start.getRotZ() + 1.25 * Math.PI), end.y + ar
+                            * Math.sin(start.getRotZ() + 1.25 * Math.PI));
+                    path.lineTo(ar2.x - ref.x, -ar2.y + ref.y);
+                graphics.draw(path);
+            }
+            
+            if (dir.equals(LongitudinalDirectionality.BACKWARD) || dir.equals(LongitudinalDirectionality.BOTH))
+            {
+                path = new Path2D.Double();
+                path.moveTo(start.x - ref.x, -start.y + ref.y);
+                Point2d ar1 =
+                    new Point2d(start.x + ar * Math.cos(start.getRotZ() + 0.25 * Math.PI), start.y + ar
+                        * Math.sin(start.getRotZ() + 0.25 * Math.PI));
+                path.lineTo(ar1.x - ref.x, -ar1.y + ref.y);
+                path.moveTo(start.x - ref.x, -start.y + ref.y);
+                Point2d ar2 =
+                        new Point2d(start.x + ar * Math.cos(start.getRotZ() - 0.25 * Math.PI), start.y + ar
+                            * Math.sin(start.getRotZ() - 0.25 * Math.PI));
+                    path.lineTo(ar2.x - ref.x, -ar2.y + ref.y);
+                graphics.draw(path);
+            }
+
+        }
+        catch (NetworkException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
     /** {@inheritDoc} */
     @Override
     public final void paint(final Graphics2D graphics, final ImageObserver observer)
@@ -76,5 +136,11 @@ public class LaneAnimationOD extends Renderable2D
         PaintPolygons.paintMultiPolygon(graphics, this.color, lane.getLocation(), lane.getContour());
         // paintLine(graphics, Color.yellow, lane.getLocation(), lane.getCenterLine());
         paintLine(graphics, Color.white, lane.getLocation(), lane.getContour());
+        paintArrow(graphics, Color.yellow, lane.getLocation(), lane.getCenterLine(), 0.25, lane
+            .getDirectionality(GTUType.ALL));
+        paintArrow(graphics, Color.green, lane.getLocation(), lane.getCenterLine(), 0.50, lane
+            .getDirectionality(GTUType.ALL));
+        paintArrow(graphics, Color.blue, lane.getLocation(), lane.getCenterLine(), 0.75, lane
+            .getDirectionality(GTUType.ALL));
     }
 }
