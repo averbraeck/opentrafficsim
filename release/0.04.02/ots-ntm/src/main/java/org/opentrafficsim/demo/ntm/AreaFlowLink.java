b00797d77ab550fda1ceee64d726bc4116d97f6f
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-11-30 23:25:49 +0000

    Release 0.04.02 before MAJOR REVISION to move to 0.05.00.

---
+++ release/0.04.02/ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/AreaFlowLink.java
@@ -0,0 +1,93 @@
+package org.opentrafficsim.demo.ntm;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.unit.SpeedUnit;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Abs;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Rel;
+import org.opentrafficsim.demo.ntm.Node.TrafficBehaviourType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version 2 Mar 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://Hansvanlint.weblog.tudelft.nl">Hans van Lint</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
+ * @author <a href="http://www.citg.tudelft.nl">Yufei Yuan</a>
+ */
+public class AreaFlowLink extends Area
+{
+    /** */
+    private LinkCellTransmission flowLink;
+
+    /** */
+    private int indexCell;
+
+    /**
+     * @param geometry
+     * @param centroidNr
+     * @param name
+     * @param gemeente
+     * @param gebied
+     * @param regio
+     * @param dhb
+     * @param centroid
+     * @param trafficBehaviourType
+     * @param roadLength
+     * @param averageSpeed
+     * @param increaseDemandByFactor
+     * @param parametersNTM
+     * @param flowLink
+     * @param indexCell
+     */
+    public AreaFlowLink(Geometry geometry, String centroidNr, String name, String gemeente, String gebied, String regio,
+        double dhb, Coordinate centroid, TrafficBehaviourType trafficBehaviourType, Rel<LengthUnit> roadLength,
+        Abs<SpeedUnit> averageSpeed, double increaseDemandByFactor, ParametersNTM parametersNTM,
+        LinkCellTransmission flowLink, int indexCell)
+    {
+        super(geometry, centroidNr, name, gemeente, gebied, regio, dhb, centroid, trafficBehaviourType, roadLength,
+            averageSpeed, increaseDemandByFactor, parametersNTM);
+        this.flowLink = flowLink;
+        this.indexCell = indexCell;
+    }
+
+    /**
+     * @return flowLink.
+     */
+    public LinkCellTransmission getFlowLink()
+    {
+        return this.flowLink;
+    }
+
+    /**
+     * @param flowLink set flowLink.
+     */
+    public void setFlowLink(LinkCellTransmission flowLink)
+    {
+        this.flowLink = flowLink;
+    }
+
+    /**
+     * @return indexCell.
+     */
+    public int getIndexCell()
+    {
+        return indexCell;
+    }
+
+    /**
+     * @param indexCell set indexCell.
+     */
+    public void setIndexCell(int indexCell)
+    {
+        this.indexCell = indexCell;
+    }
+
+}
---
+++ release/0.04.02/ots-ntm/src/main/java/org/opentrafficsim/demo/ntm/ParametersNTM.java
@@ -0,0 +1,126 @@
+package org.opentrafficsim.demo.ntm;
+
+import java.util.ArrayList;
+
+import org.djunits.unit.FrequencyUnit;
+import org.djunits.unit.LengthUnit;
+import org.djunits.unit.SpeedUnit;
+import org.djunits.value.vdouble.scalar.DoubleScalar;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Abs;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Rel;
+import org.djunits.value.vdouble.scalar.Frequency;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version 26 Sep 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://Hansvanlint.weblog.tudelft.nl">Hans van Lint</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
+ * @author <a href="http://www.citg.tudelft.nl">Yufei Yuan</a>
+ */
+public class ParametersNTM extends ParametersFundamentalDiagram
+{
+    /**
+     * roadLength: aggregated road lengths in CELL.
+     */
+    private final DoubleScalar.Rel<LengthUnit> roadLength;
+
+    /**
+     * @param accCritical
+     * @param freeSpeed
+     * @param roadLength
+     */
+    public ParametersNTM(ArrayList<Double> accCritical, Abs<SpeedUnit> freeSpeed, Rel<LengthUnit> roadLength)
+    {
+        super();
+        this.setAccCritical(accCritical);
+        this.setFreeSpeed(freeSpeed);
+        this.roadLength = roadLength;
+        double capacity = this.getFreeSpeed().getInUnit(SpeedUnit.KM_PER_HOUR) * this.getAccCritical().get(0);
+        this.setCapacityPerUnit(new Frequency(capacity, FrequencyUnit.PER_HOUR));
+    }
+
+    /**
+     * @param accCritical
+     * @param capacityPerLaneLength
+     * @param roadLength
+     */
+    public ParametersNTM(ArrayList<Double> accCritical, double capacityPerLaneLength, Rel<LengthUnit> roadLength)
+    {
+        super();
+        // TODO parameters should depend on area characteristics
+        this.setAccCritical(accCritical);
+        this.setCapacityPerUnit(new Frequency(capacityPerLaneLength, FrequencyUnit.PER_HOUR));
+        double freeSpeed = this.getCapacity().getInUnit(FrequencyUnit.PER_HOUR) / this.getAccCritical().get(0);
+        this.setFreeSpeed(new DoubleScalar.Abs<SpeedUnit>(freeSpeed, SpeedUnit.KM_PER_HOUR));
+        this.roadLength = roadLength;
+    }
+
+    /**
+     * @param accCritical
+     * @param capacityPerLaneLength
+     * @param roadLength
+     */
+    public ParametersNTM(ArrayList<Double> accCritical)
+    {
+        super();
+        // TODO parameters should depend on area characteristics
+        this.setAccCritical(accCritical);
+        this.roadLength = new Rel<LengthUnit>(0, LengthUnit.KILOMETER);
+    }
+
+    /**
+     * @param accCritical
+     * @param freeSpeed
+     * @param roadLength
+     */
+    public ParametersNTM(Abs<SpeedUnit> freeSpeed, Rel<LengthUnit> roadLength)
+    {
+        super();
+        // TODO parameters should depend on area characteristics
+        ArrayList<Double> accCritical = new ArrayList<Double>();
+        accCritical.add(25.0);
+        accCritical.add(50.0);
+        accCritical.add(100.0);
+        this.setAccCritical(accCritical);
+        this.setFreeSpeed(freeSpeed);
+        this.roadLength = roadLength;
+        double capacity = this.getFreeSpeed().getInUnit(SpeedUnit.KM_PER_HOUR) * this.getAccCritical().get(0);
+        this.setCapacityPerUnit(new Frequency(capacity, FrequencyUnit.PER_HOUR));
+    }
+
+    /**
+     * @param accCritical
+     * @param freeSpeed
+     * @param roadLength
+     */
+    public ParametersNTM()
+    {
+        super();
+        Abs<SpeedUnit> freeSpeed = new Abs<SpeedUnit>(50, SpeedUnit.KM_PER_HOUR);
+        // TODO parameters should depend on area characteristics
+        ArrayList<Double> accCritical = new ArrayList<Double>();
+        accCritical.add(25.0);
+        accCritical.add(50.0);
+        accCritical.add(100.0);
+        this.setAccCritical(accCritical);
+        this.setFreeSpeed(freeSpeed);
+        this.roadLength = new Rel<LengthUnit>(0, LengthUnit.KILOMETER);
+        double capacity = this.getFreeSpeed().getInUnit(SpeedUnit.KM_PER_HOUR) * this.getAccCritical().get(0);
+        this.setCapacityPerUnit(new Frequency(capacity, FrequencyUnit.PER_HOUR));
+    }
+
+    /**
+     * @return roadLength.
+     */
+    public Rel<LengthUnit> getRoadLength()
+    {
+        return this.roadLength;
+    }
+
+}
