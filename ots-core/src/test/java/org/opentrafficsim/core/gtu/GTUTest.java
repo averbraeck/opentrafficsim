3745a5f00586dce25ba7b6b94d8368155038a73a
OTSim <P.Knoppers@tudelft.nl>
2015-12-17 10:51:18 +0000

    GTUTest.java runs again. Not sure if it really tests all that it is supposed to test.

--- ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUTest.java
+++ ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUTest.java
@@ -3,20 +3,29 @@ package org.opentrafficsim.core.gtu;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.rmi.RemoteException;
 import java.util.Map;
 
 import javax.media.j3d.Bounds;
+import javax.naming.NamingException;
 
 import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
 
+import org.djunits.unit.TimeUnit;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Abs;
 import org.djunits.value.vdouble.scalar.Length.Rel;
 import org.djunits.value.vdouble.scalar.Speed;
+import org.djunits.value.vdouble.scalar.Time;
 import org.junit.Test;
 import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
 import org.opentrafficsim.core.gtu.RelativePosition.TYPE;
 import org.opentrafficsim.core.model.OTSModel;
 import org.opentrafficsim.core.network.NetworkException;
 import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.simulationengine.SimpleSimulator;
 
 /**
  * Test the AbstractGTU class.
@@ -28,7 +37,7 @@ import org.opentrafficsim.core.network.OTSNetwork;
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
-public class GTUTest
+public class GTUTest implements OTSModelInterface
 
 {
 
@@ -37,18 +46,21 @@ public class GTUTest
      * @throws GTUException
      * @throws NetworkException
      * @throws SimRuntimeException
+     * @throws NamingException 
      */
     @Test
-    public void testAbstractGTU() throws GTUException, SimRuntimeException, NetworkException
+    public void testAbstractGTU() throws GTUException, SimRuntimeException, NetworkException, NamingException
     {
         TestGTU firstGTU = null;
         TestGTU lastGTU = null;
         OTSNetwork model = new OTSNetwork("network");
+        OTSDEVSSimulatorInterface simulator = new SimpleSimulator(new Time.Abs(0, TimeUnit.SI), new Time.Rel(0, TimeUnit.SI), 
+                new Time.Rel(9999, TimeUnit.SI), this); 
         for (String id : new String[]{"id1", "id2"})
         {
             for (GTUType gtuType : new GTUType[]{GTUType.makeGTUType("gtu type 1"), GTUType.makeGTUType("gtu type 2")})
             {
-                TestGTU gtu = new TestGTU(id, gtuType, null, model);
+                TestGTU gtu = new TestGTU(id, gtuType, simulator, model);
                 assertEquals("new GTU has correct id", id, gtu.getId());
                 assertEquals("new GTU has correct GTUType", gtuType, gtu.getGTUType());
                 assertEquals("new GTU has correct reference position", RelativePosition.REFERENCE_POSITION, gtu
@@ -63,6 +75,22 @@ public class GTUTest
         assertFalse("first GTU and last GTU have different id", firstGTU.getId().equals(lastGTU.getId()));
         assertFalse("first GTU and last GTU have different GTUType", firstGTU.getGTUType().equals(lastGTU.getGTUType()));
 
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel(
+            SimulatorInterface<Abs<TimeUnit>, org.djunits.value.vdouble.scalar.DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> simulator)
+            throws SimRuntimeException, RemoteException
+    {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SimulatorInterface<Abs<TimeUnit>, org.djunits.value.vdouble.scalar.DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> getSimulator()
+            throws RemoteException
+    {
+        return null;
     }
 }
 
