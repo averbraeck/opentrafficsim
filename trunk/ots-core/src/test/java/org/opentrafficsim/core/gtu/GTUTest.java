d18b0a5785aeffb22c60aa916674f6904ca63751
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-12-23 09:06:20 +0000

    Expanded test coverage.

--- trunk/ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUTest.java
+++ trunk/ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUTest.java
@@ -12,6 +12,7 @@ import javax.naming.NamingException;
 
 import nl.tudelft.simulation.dsol.SimRuntimeException;
 import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.language.d3.DirectedPoint;
 
 import org.djunits.unit.TimeUnit;
 import org.djunits.value.vdouble.scalar.DoubleScalar.Abs;
@@ -50,29 +51,29 @@ public class GTUTest implements OTSModelInterface
      * @throws GTUException
      * @throws NetworkException
      * @throws SimRuntimeException
-     * @throws NamingException 
-     * @throws RemoteException 
+     * @throws NamingException
+     * @throws RemoteException
      */
     @Test
     public void testAbstractGTU() throws GTUException, SimRuntimeException, NetworkException, NamingException, RemoteException
     {
         TestGTU firstGTU = null;
         TestGTU lastGTU = null;
-        OTSNetwork model = new OTSNetwork("network");
-        OTSDEVSSimulatorInterface simulator = new SimpleSimulator(new Time.Abs(0, TimeUnit.SI), new Time.Rel(0, TimeUnit.SI), 
-                new Time.Rel(9999, TimeUnit.SI), this); 
-        for (String id : new String[]{"id1", "id2"})
+        OTSNetwork perceivableContext = new OTSNetwork("network");
+        OTSDEVSSimulatorInterface simulator =
+                new SimpleSimulator(new Time.Abs(0, TimeUnit.SI), new Time.Rel(0, TimeUnit.SI),
+                        new Time.Rel(9999, TimeUnit.SI), this);
+        for (String id : new String[] { "id1", "id2" })
         {
-            for (GTUType gtuType : new GTUType[]{GTUType.makeGTUType("gtu type 1"), GTUType.makeGTUType("gtu type 2")})
+            for (GTUType gtuType : new GTUType[] { GTUType.makeGTUType("gtu type 1"), GTUType.makeGTUType("gtu type 2") })
             {
-                TestGTU gtu = new TestGTU(id, gtuType, simulator, model);
+                TestGTU gtu = new TestGTU(id, gtuType, simulator, null, perceivableContext);
                 assertEquals("new GTU has correct id", id, gtu.getId());
                 assertEquals("new GTU has correct GTUType", gtuType, gtu.getGTUType());
-                assertEquals("new GTU has correct reference position", RelativePosition.REFERENCE_POSITION, gtu
-                    .getReference());
+                assertEquals("new GTU has correct reference position", RelativePosition.REFERENCE_POSITION, gtu.getReference());
                 assertEquals("new GTU has correct simulator", simulator, gtu.getSimulator());
                 assertEquals("new GTU has odometer value 0", 0, gtu.getOdometer().si, 0);
-                assertTrue("new GTU is stored in the model", model.getGTUs().contains(gtu));
+                assertTrue("new GTU is stored in the perceivable context", perceivableContext.getGTUs().contains(gtu));
                 lastGTU = gtu;
                 if (null == firstGTU)
                 {
@@ -82,7 +83,18 @@ public class GTUTest implements OTSModelInterface
         }
         assertFalse("first GTU and last GTU have different id", firstGTU.getId().equals(lastGTU.getId()));
         assertFalse("first GTU and last GTU have different GTUType", firstGTU.getGTUType().equals(lastGTU.getGTUType()));
-
+        DirectedPoint initialLocation =
+                new DirectedPoint(10, 20, 30, Math.toRadians(10), Math.toRadians(20), Math.toRadians(30));
+        TestGTU gtu = new TestGTU("id3", GTUType.makeGTUType("gtu type 1"), simulator, initialLocation, perceivableContext);
+        DirectedPoint actualLocation = gtu.getLocation();
+        assertEquals("initial location", 0, initialLocation.distance(actualLocation), 0.002);
+        // Only the Z-rotation is returned non-zero; the returned X and Y rotation are zero
+        assertEquals("initial rotation X", 0, actualLocation.getRotX(), 0.00001);
+        assertEquals("initial rotation Y", 0, actualLocation.getRotY(), 0.00001);
+        assertEquals("initial rotation Z", initialLocation.getRotZ(), actualLocation.getRotZ(), 0.00001);
+        assertEquals("perceivable context now contains 5 GTUs", 5, perceivableContext.getGTUs().size());
+        gtu.destroy();
+        assertFalse("perceivable context no longer contains the destroyed GTU", perceivableContext.containsGTU(gtu));
     }
 
     /** {@inheritDoc} */
@@ -110,17 +122,18 @@ class TestGTU extends AbstractGTU
     private static final long serialVersionUID = 20151111L;
 
     /**
-     * @param id
-     * @param gtuType
-     * @param simulator
-     * @param model
-     * @throws NetworkException
-     * @throws SimRuntimeException
+     * @param id String; id of the new GTU
+     * @param gtuType GTUType; type of the new GTU
+     * @param simulator OTSDEVSSimulatorInterface; simulator that controls the new GTU
+     * @param initialLocation DirectedPoint; initial location and direction of the new GTU
+     * @param perceivableContext PerceivableContext; the perceivable context of the new GTU
+     * @throws NetworkException when the network is inconsistent
+     * @throws SimRuntimeException when something goes wrong in the scheduling of the first move event
      */
-    public TestGTU(String id, GTUType gtuType, OTSDEVSSimulatorInterface simulator, PerceivableContext model) throws SimRuntimeException,
-        NetworkException
+    public TestGTU(String id, GTUType gtuType, OTSDEVSSimulatorInterface simulator, DirectedPoint initialLocation,
+            PerceivableContext perceivableContext) throws SimRuntimeException, NetworkException
     {
-        super(id, gtuType, simulator, null, null, null, model);
+        super(id, gtuType, simulator, null, null, initialLocation, perceivableContext);
     }
 
     /** {@inheritDoc} */
@@ -163,13 +176,6 @@ class TestGTU extends AbstractGTU
     public Map<TYPE, RelativePosition> getRelativePositions()
     {
         return null;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void destroy()
-    {
-        // Not used
     }
 
     /** {@inheritDoc} */
