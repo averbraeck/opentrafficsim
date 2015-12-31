4b92f614a0eb26767701213bec9f16fe83f84279
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2015-12-31 10:31:41 +0000

    Expanded test coverage of OperationalPlan.

--- trunk/ots-core/src/test/java/org/opentrafficsim/core/gtu/plan/operational/OperationalPlanTest.java
+++ trunk/ots-core/src/test/java/org/opentrafficsim/core/gtu/plan/operational/OperationalPlanTest.java
@@ -5,11 +5,15 @@ import static org.junit.Assert.fail;
 import nl.tudelft.simulation.language.d3.DirectedPoint;
 
 import org.djunits.unit.AccelerationUnit;
+import org.djunits.unit.LengthUnit;
 import org.djunits.unit.SpeedUnit;
 import org.djunits.unit.TimeUnit;
 import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Length;
 import org.djunits.value.vdouble.scalar.Speed;
 import org.djunits.value.vdouble.scalar.Time;
+import org.djunits.value.vdouble.scalar.Time.Abs;
+import org.djunits.value.vdouble.scalar.Time.Rel;
 import org.junit.Test;
 import org.opentrafficsim.core.geometry.OTSGeometryException;
 import org.opentrafficsim.core.geometry.OTSLine3D;
@@ -51,20 +55,29 @@ public class OperationalPlanTest
             // Use a tolerance that is larger than the z-offset (0.001)
             assertEquals("Distance from wait point at " + t + " is 0", 0, waitPoint.distance(locationAtT), 0.002);
         }
+        assertEquals("end location matches start location", 0,
+                new OTSPoint3D(op.getEndLocation()).distance(new OTSPoint3D(waitPoint)).si, 0.0001);
         OTSLine3D path = new OTSLine3D(new OTSPoint3D(12, 13, 14), new OTSPoint3D(123, 234, 345));
         Speed startSpeed = new Speed(20, SpeedUnit.KM_PER_HOUR);
         Speed endSpeed = new Speed(50, SpeedUnit.KM_PER_HOUR);
         Acceleration maxAcceleration = new Acceleration(1, AccelerationUnit.METER_PER_SECOND_2);
         Acceleration maxDeceleration = new Acceleration(6, AccelerationUnit.METER_PER_SECOND_2);
         op =
-            OperationalPlanBuilder.buildGradualAccelerationPlan(path, startTime, startSpeed, endSpeed, maxAcceleration,
-                maxDeceleration);
+                OperationalPlanBuilder.buildGradualAccelerationPlan(path, startTime, startSpeed, endSpeed, maxAcceleration,
+                        maxDeceleration);
         assertEquals("Start speed is " + startSpeed, startSpeed.si, op.getStartSpeed().si, 0.00001);
         assertEquals("Start time is " + startTime, startTime.si, op.getStartTime().si, 0.00001);
         assertEquals("End speed is " + endSpeed, endSpeed.si, op.getEndSpeed().si, 0.00001);
         // TODO assertEquals("getPath returns the path", path, op.getPath());
         // What acceleration is required to reach endSpeed at the end of the path?
         // (This mathematical derivation constructed independently from the OperationalPlanBuilder code.)
+        OTSLine3D returnedPath = op.getPath();
+        // This fails: assertEquals("returned path should match path", path, returnedPath);
+        assertEquals("size of path should match", path.size(), returnedPath.size());
+        for (int i = 0; i < path.size(); i++)
+        {
+            assertEquals("position of point " + i, 0, path.get(i).distance(returnedPath.get(i)).si, 0.0001);
+        }
         double pathLength = path.getLengthSI();
         // Solve eq 1: startSpeed * t + 0.5 * a * t * t == pathLength
         // And eq 2: startSpeed + t * a == endSpeed
@@ -106,12 +119,33 @@ public class OperationalPlanTest
         for (int i = 0; i <= steps; i++)
         {
             double stepTime = startTime.si + t * i / steps * 0.9999;// sometimes fails for endTime
-            DirectedPoint actualPosition = op.getLocation(new Time.Abs(stepTime, TimeUnit.SI));
+            Time.Abs absTime = new Time.Abs(stepTime, TimeUnit.SI);
             double deltaT = stepTime - startTime.si;
-            double fraction = (startSpeed.si * deltaT + 0.5 * a.si * deltaT * deltaT) / path.getLength().si;
+            Time.Rel relTime = new Time.Rel(deltaT, TimeUnit.SI);
+            double expectedDistance = startSpeed.si * deltaT + 0.5 * a.si * deltaT * deltaT;
+            double fraction = expectedDistance / path.getLength().si;
             OTSPoint3D expectedPosition = new OTSPoint3D(path.getLocationFraction(fraction));
-            // TODO actualPosition appears to be bogus in the current "implementation".
-            // assertEquals("Position at time " + deltaT, expectedPosition.distance(new OTSPoint3D(actualPosition)), 0.0001);
+            DirectedPoint actualPosition = op.getLocation(absTime);
+            assertEquals("Position at abs time " + deltaT, 0, expectedPosition.distance(new OTSPoint3D(actualPosition)).si,
+                    0.002);
+            actualPosition = op.getLocation(relTime);
+            assertEquals("Position at rel time " + deltaT, 0, expectedPosition.distance(new OTSPoint3D(actualPosition)).si,
+                    0.002);
+            double expectedVelocity = startSpeed.si + a.si * deltaT;
+            Speed actualSpeed = op.getVelocity(absTime);
+            assertEquals("Velocity at abs time " + deltaT, expectedVelocity, actualSpeed.si, 0.0001);
+            actualSpeed = op.getVelocity(relTime);
+            assertEquals("Velocity at rel time " + deltaT, expectedVelocity, actualSpeed.si, 0.0001);
+            Time.Abs actualTimeAtPosition = op.timeAtDistance(new Length.Rel(fraction * path.getLength().si, LengthUnit.SI));
+            assertEquals("TimeAtDistance matches time", startTime.si + deltaT, actualTimeAtPosition.si, 0.0001);
+            double actualAcceleration = op.getAcceleration(absTime).si;
+            assertEquals("acceleration at abs time", a.si, actualAcceleration, 0.00001);
+            actualAcceleration = op.getAcceleration(relTime).si;
+            assertEquals("acceleration at rel time", a.si, actualAcceleration, 0.00001);
+            assertEquals("actual traveled distance at abs time", expectedDistance, op.getTraveledDistance(absTime).si, 0.0001);
+            assertEquals("actual traveled distance at rel time", expectedDistance, op.getTraveledDistance(relTime).si, 0.0001);
+            assertEquals("actual traveled distanceSI at abs time", expectedDistance, op.getTraveledDistanceSI(absTime), 0.0001);
+            assertEquals("actual traveled distanceSI at rel time", expectedDistance, op.getTraveledDistanceSI(relTime), 0.0001);
         }
     }
 }
