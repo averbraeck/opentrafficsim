0188f628b1c81f25f1ac5b2597dd06e252390de4
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-02-23 15:39:35 +0000

    0.06.00.

---
+++ release/0.06.00/ots-core/src/main/java/org/opentrafficsim/core/network/LateralDirectionality.java
@@ -0,0 +1,40 @@
+package org.opentrafficsim.core.network;
+
+/**
+ * Directionality in lateral direction. LEFT is the direction to the left of the longitudinal orientation of the GTU, relative
+ * to the forward driving direction. RIGHT is the direction to the right of the longitudinal orientation of the GTU, relative to
+ * the forward driving direction.
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Oct 15, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public enum LateralDirectionality
+{
+    /** Direction to the left of the longitudinal orientation of the GTU, relative to the forward driving direction. */
+    LEFT,
+    /** Direction to the right of the longitudinal orientation of the GTU, relative to the forward driving direction. */
+    RIGHT;
+
+    /**
+     * Determine whether the direction is the left direction.
+     * @return whether the direction is the left direction
+     */
+    public boolean isLeft()
+    {
+        return this.equals(LEFT);
+    }
+
+    /**
+     * Determine whether the direction is the right direction.
+     * @return whether the direction is the right direction
+     */
+    public boolean isRight()
+    {
+        return this.equals(LEFT);
+    }
+}
---
+++ release/0.06.00/ots-core/src/main/java/org/opentrafficsim/simulationengine/properties/PropertyException.java
@@ -0,0 +1,67 @@
+package org.opentrafficsim.simulationengine.properties;
+
+/**
+ * Exception thrown when an operation is attempted that is not compatible with the indicated property.
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version 18 dec. 2014 <br>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class PropertyException extends Exception
+{
+    /**  */
+    private static final long serialVersionUID = 20141023L;
+
+    /**
+     * Construct a new IncompatiblePropertyException.
+     */
+    public PropertyException()
+    {
+        super();
+    }
+
+    /**
+     * Construct a new IncompatiblePropertyException.
+     * @param message String; description of the problem
+     */
+    public PropertyException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Construct a new IncompatiblePropertyException.
+     * @param cause Throwable; the cause of this ValueException
+     */
+    public PropertyException(final Throwable cause)
+    {
+        super(cause);
+    }
+
+    /**
+     * Construct a new IncompatiblePropertyException.
+     * @param message String; description of the problem
+     * @param cause Throwable; the cause of this ValueException
+     */
+    public PropertyException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * Construct a new IncompatiblePropertyException.
+     * @param message String; description of the problem
+     * @param cause Throwable; the cause of this ValueException
+     * @param enableSuppression boolean; whether or not suppression is enabled or disabled
+     * @param writableStackTrace boolean; whether or not the stack trace should be writable
+     */
+    public PropertyException(final String message, final Throwable cause, final boolean enableSuppression,
+        final boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
