2bfe46ba68cb1b22083c131e78618b387767d97b
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-03-01 13:00:13 +0000

    -

--- release/0.07.00/ots-build-tools/development/mvn-cmd.bat
+++
@@ -1,8 +0,0 @@
-REM $HeadURL: https://svn.tbm.tudelft.nl/TBM-SK/DSOL-AOS/dsol-aos/development/mvn-cmd.bat $
-REM $Id$
-
-REM Changes to directory %1 and runs Maven command %2, assuming mvn is in %PATH%
-
-CD /d "%1"
-CALL mvn -DdownloadSources=true -DdownloadJavadocs=true %2
-if not "%ERRORLEVEL%" == "0" PAUSE  
