0fe77b8a35662ac931a7251d3c7b485a1cd5b63a
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-03-01 13:07:58 +0000

    Release 0.07.00

---
+++ branches/0.07/ots-demo/src/main/java/org/opentrafficsim/demo/geometry/TestGeometry.java
@@ -0,0 +1,78 @@
+package org.opentrafficsim.demo.geometry;
+
+import java.awt.Dimension;
+import java.awt.geom.Rectangle2D;
+import java.rmi.RemoteException;
+
+import javax.naming.NamingException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.animation.D2.AnimationPanel;
+import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
+import nl.tudelft.simulation.dsol.gui.swing.DSOLApplication;
+import nl.tudelft.simulation.dsol.gui.swing.DSOLPanel;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.event.Event;
+
+import org.djunits.unit.TimeUnit;
+import org.djunits.unit.UNITS;
+import org.djunits.value.vdouble.scalar.DoubleScalar;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.core.dsol.OTSDEVSAnimator;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSReplication;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Oct 16, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class TestGeometry extends DSOLApplication implements UNITS
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @param title t
+     * @param panel p
+     */
+    public TestGeometry(final String title,
+        final DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> panel)
+    {
+        super(title, panel);
+    }
+
+    /**
+     * @param args args
+     * @throws RemoteException if error
+     * @throws SimRuntimeException if error
+     * @throws NamingException if error
+     */
+    public static void main(final String[] args) throws SimRuntimeException, NamingException, RemoteException
+    {
+        OTSModelInterface model = new TestModel();
+        OTSDEVSAnimator simulator = new OTSDEVSAnimator();
+        OTSReplication replication =
+            new OTSReplication("rep1", new OTSSimTimeDouble(new DoubleScalar.Abs<TimeUnit>(0.0, SECOND)), new Time.Rel(
+                0.0, SECOND), new Time.Rel(1800.0, SECOND), model);
+        simulator.initialize(replication, ReplicationMode.TERMINATING);
+        DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> panel =
+            new DSOLPanel<DoubleScalar.Abs<TimeUnit>, DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble>(model, simulator);
+
+        Rectangle2D extent = new Rectangle2D.Double(-50, -50, 300, 100);
+        Dimension size = new Dimension(1024, 768);
+        AnimationPanel animationPanel = new AnimationPanel(extent, size, simulator);
+        panel.getTabbedPane().addTab(0, "animation", animationPanel);
+
+        // tell the animation panel to update its statistics
+        animationPanel.notify(new Event(SimulatorInterface.START_REPLICATION_EVENT, simulator, null));
+
+        new TestGeometry("TestGeometry", panel);
+    }
+
+}
