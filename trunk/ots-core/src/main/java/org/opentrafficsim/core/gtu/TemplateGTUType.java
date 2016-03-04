35e72c64887feaabfbe263ae24dfd61fd3788c6a
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-03-04 20:10:10 +0000

    OTS-85 Major redesign of template GTU related classes.

--- trunk/ots-core/src/main/java/org/opentrafficsim/core/gtu/TemplateGTUType.java
+++ trunk/ots-core/src/main/java/org/opentrafficsim/core/gtu/TemplateGTUType.java
@@ -2,12 +2,12 @@ package org.opentrafficsim.core.gtu;
 
 import java.io.Serializable;
 
-import org.djunits.unit.LengthUnit;
-import org.djunits.unit.SpeedUnit;
 import org.djunits.value.vdouble.scalar.Length;
 import org.djunits.value.vdouble.scalar.Speed;
+import org.opentrafficsim.core.distributions.Generator;
+import org.opentrafficsim.core.distributions.ProbabilityException;
 import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
-import org.opentrafficsim.core.units.distributions.ContinuousDistDoubleScalar;
+import org.opentrafficsim.core.idgenerator.IdGenerator;
 
 /**
  * TemplateGTUType stores most of the information that is needed to generate a GTU.
@@ -27,44 +27,95 @@ public class TemplateGTUType implements Serializable
     /** The type of the GTU. */
     private final GTUType gtuType;
 
-    /** distribution of the length of the GTU. */
-    private final ContinuousDistDoubleScalar.Rel<Length.Rel, LengthUnit> lengthDist;
+    /** The IdGenerator for constructed GTUs. */
+    private final IdGenerator idGenerator;
 
-    /** distribution of the width of the GTU. */
-    private final ContinuousDistDoubleScalar.Rel<Length.Rel, LengthUnit> widthDist;
+    /** Generator for the length of the GTU. */
+    private final Generator<Length.Rel> lengthGenerator;
 
-    /** distribution of the maximum speed of the GTU. */
-    private final ContinuousDistDoubleScalar.Rel<Speed, SpeedUnit> maximumSpeedDist;
+    /** Generator for the width of the GTU. */
+    private final Generator<Length.Rel> widthGenerator;
 
-    /** the simulator. */
+    /** Generator for the maximum speed of the GTU. */
+    private final Generator<Speed> maximumVelocityGenerator;
+
+    /** The simulator. */
     private final OTSDEVSSimulatorInterface simulator;
 
+    /** Length of the next GTU. */
+    private Length.Rel length = null;
+
+    /** Width of the next GTU. */
+    private Length.Rel width = null;
+
+    /** Maximum velocity of the next GTU. */
+    private Speed maximumVelocity = null;
+
     /**
-     * @param id The id of the GTUType to make it identifiable.
-     * @param lengthDist the length of the GTU type (parallel with driving direction).
-     * @param widthDist the width of the GTU type (perpendicular to driving direction).
-     * @param maximumSpeedDist the maximum speed of the GTU type (in the driving direction).
+     * @param typeId The id of the GTUType to make it identifiable.
+     * @param idGenerator IdGenerator; the id generator used to generate names for GTUs constructed using this TemplateGTUType.
+     *            Provide null to use the default id generator of AbstractGTU.
+     * @param lengthGenerator Generator&lt;Length.Rel&gt;; generator for the length of the GTU type (parallel with driving
+     *            direction).
+     * @param widthGenerator Generator&lt;Length.Rel&gt;; generator for the width of the GTU type (perpendicular to driving
+     *            direction).
+     * @param maximumVelocityGenerator Generator&lt;Speed&gt;; generator for the maximum velocity of the GTU type (in the
+     *            driving direction).
      * @param simulator the simulator.
-     * @throws GTUException when GTUType defined more than once
      */
-    public TemplateGTUType(final String id, final ContinuousDistDoubleScalar.Rel<Length.Rel, LengthUnit> lengthDist,
-        final ContinuousDistDoubleScalar.Rel<Length.Rel, LengthUnit> widthDist,
-        final ContinuousDistDoubleScalar.Rel<Speed, SpeedUnit> maximumSpeedDist,
-        final OTSDEVSSimulatorInterface simulator) throws GTUException
+    public TemplateGTUType(final String typeId, IdGenerator idGenerator, final Generator<Length.Rel> lengthGenerator,
+            final Generator<Length.Rel> widthGenerator, final Generator<Speed> maximumVelocityGenerator,
+            final OTSDEVSSimulatorInterface simulator)
     {
-        this.gtuType = GTUType.makeGTUType(id);
-        this.lengthDist = lengthDist;
-        this.widthDist = widthDist;
-        this.maximumSpeedDist = maximumSpeedDist;
+        this.gtuType = GTUType.makeGTUType(typeId);
+        this.idGenerator = idGenerator;
+        this.lengthGenerator = lengthGenerator;
+        this.widthGenerator = widthGenerator;
+        this.maximumVelocityGenerator = maximumVelocityGenerator;
         this.simulator = simulator;
     }
 
     /**
-     * @return length.
+     * Generate the properties of the next GTU.
+     * @throws ProbabilityException when a generator is improperly configured
+     */
+    public void generateCharacteristics() throws ProbabilityException
+    {
+        if (null != this.length)
+        {
+            System.err.println("generateCharacteristics called without intevening call to clearCharacteristics");
+        }
+        this.length = this.lengthGenerator.draw();
+        this.width = this.widthGenerator.draw();
+        this.maximumVelocity = this.maximumVelocityGenerator.draw();
+    }
+
+    /**
+     * Clear all generated values to ensure null pointer exceptions when a getter is called before the generateCharacteristics
+     * method is called.
+     */
+    public void clearCharacteristics()
+    {
+        this.length = null;
+        this.width = null;
+        this.maximumVelocity = null;
+    }
+
+    /**
+     * Retrieve the id generator.
+     * @return IdGenerator
+     */
+    public final IdGenerator getIdGenerator()
+    {
+        return this.idGenerator;
+    }
+
+    /**
+     * @return Length.Rel
      */
     public final Length.Rel getLength()
     {
-        return this.lengthDist.draw();
+        return this.length;
     }
 
     /**
@@ -72,7 +123,7 @@ public class TemplateGTUType implements Serializable
      */
     public final Length.Rel getWidth()
     {
-        return this.widthDist.draw();
+        return this.width;
     }
 
     /**
@@ -80,7 +131,7 @@ public class TemplateGTUType implements Serializable
      */
     public final Speed getMaximumVelocity()
     {
-        return this.maximumSpeedDist.draw();
+        return this.maximumVelocity;
     }
 
     /**
