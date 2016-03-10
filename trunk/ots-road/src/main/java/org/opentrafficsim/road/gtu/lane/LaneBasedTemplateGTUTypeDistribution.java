5d2b9f0e6a665fcc3aa6ba5bb4e4a7eaca2e1604
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-03-10 10:48:53 +0000

    OTS-85 New class LaneBasedGTUGenrator uses Characteristics (generated in turn by Templates) to check for sufficient room and then construct new GTUs.

---
+++ trunk/ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/LaneBasedTemplateGTUTypeDistribution.java
@@ -0,0 +1,47 @@
+package org.opentrafficsim.road.gtu.lane;
+
+import org.opentrafficsim.core.distributions.Distribution;
+import org.opentrafficsim.core.distributions.ProbabilityException;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+
+/**
+ * Distribution of LaneBasedTemplateGTUType.
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Mar 9, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class LaneBasedTemplateGTUTypeDistribution implements LaneBasedGTUCharacteristicsGenerator
+{
+
+    /** The Distribution of lane based template GTU types. */
+    private final Distribution<LaneBasedTemplateGTUType> distribution;
+
+    /**
+     * Construct a new LaneBasedTemplateGTUTypeDistribution.
+     * @param distributionOfLanebasedTemplateGTUType
+     */
+    public LaneBasedTemplateGTUTypeDistribution(
+            final Distribution<LaneBasedTemplateGTUType> distributionOfLanebasedTemplateGTUType)
+    {
+        this.distribution = distributionOfLanebasedTemplateGTUType;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public LaneBasedGTUCharacteristics draw() throws ProbabilityException
+    {
+        return this.distribution.draw().draw();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public OTSDEVSSimulatorInterface getSimulator()
+    {
+        return this.distribution.get(0).getObject().getSimulator();
+    }
+
+}
