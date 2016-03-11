e08878f55b30927fcfa987f6d8c43007fb5ae966
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-03-11 10:50:08 +0000

    Removed unused import.

--- trunk/ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUExceptionTest.java
+++ trunk/ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUExceptionTest.java
@@ -1,7 +1,6 @@
 package org.opentrafficsim.core.gtu;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
