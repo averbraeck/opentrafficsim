6376c5a5212669d275db5a5138b26e9f437acbb5
OTSim <P.Knoppers@tudelft.nl>
2016-03-11 09:33:57 +0000

    OTS-85 Wrote unit test for the GTUCharacteristics class. Test coverage was 100% on the first run!

---
+++ ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUCharacteristicsTest.java
@@ -0,0 +1,108 @@
+package org.opentrafficsim.core.gtu;
+
+import static org.junit.Assert.assertEquals;
+
+import java.rmi.RemoteException;
+
+import javax.naming.NamingException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.unit.SpeedUnit;
+import org.djunits.unit.TimeUnit;
+import org.djunits.value.vdouble.scalar.DoubleScalar.Abs;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Length.Rel;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.djunits.value.vdouble.scalar.Time;
+import org.junit.Test;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
+import org.opentrafficsim.core.idgenerator.IdGenerator;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.simulationengine.SimpleSimulator;
+
+/**
+ * Test the GTUCharacteristics class
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Mar 11, 2016 <br>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class GTUCharacteristicsTest implements OTSModelInterface
+{
+    /** */
+    private static final long serialVersionUID = 20160311L;
+
+    /**
+     * Test the GTUCharacteristics class.
+     * @throws SimRuntimeException should never happen
+     * @throws NamingException should never happen
+     */
+    @Test
+    public void testGTUCharacteristics() throws SimRuntimeException, NamingException
+    {
+        // Make two sets of values so we can prove that the constructed GTUCharacteristics sets are really distinct.
+        GTUType gtuTypeA = GTUType.makeGTUType("Type A");
+        GTUType gtuTypeB = GTUType.makeGTUType("Type B");
+        IdGenerator idGeneratorA = new IdGenerator("A");
+        IdGenerator idGeneratorB = new IdGenerator("B");
+        Length.Rel lengthA = new Length.Rel(5, LengthUnit.METER);
+        Length.Rel lengthB = new Length.Rel(10, LengthUnit.METER);
+        Length.Rel widthA = new Length.Rel(2, LengthUnit.METER);
+        Length.Rel widthB = new Length.Rel(2.5, LengthUnit.METER);
+        Speed maximumSpeedA = new Speed(180, SpeedUnit.KM_PER_HOUR);
+        Speed maximumSpeedB = new Speed(130, SpeedUnit.KM_PER_HOUR);
+        OTSDEVSSimulatorInterface simulatorA =
+                new SimpleSimulator(new Time.Abs(0, TimeUnit.SI), new Time.Rel(0, TimeUnit.SI), new Time.Rel(1000,
+                        TimeUnit.SECOND), this);
+        OTSDEVSSimulatorInterface simulatorB =
+                new SimpleSimulator(new Time.Abs(0, TimeUnit.SI), new Time.Rel(0, TimeUnit.SI), new Time.Rel(1000,
+                        TimeUnit.SECOND), this);
+        OTSNetwork networkA = new OTSNetwork("testGTUCharacteristics A");
+        OTSNetwork networkB = new OTSNetwork("testGTUCharacteristics B");
+        GTUCharacteristics gtucA =
+                new GTUCharacteristics(gtuTypeA, idGeneratorA, lengthA, widthA, maximumSpeedA, simulatorA, networkA);
+        GTUCharacteristics gtucB =
+                new GTUCharacteristics(gtuTypeB, idGeneratorB, lengthB, widthB, maximumSpeedB, simulatorB, networkB);
+        assertEquals("gtuTypeA", gtuTypeA, gtucA.getGTUType());
+        assertEquals("gtuTypeB", gtuTypeB, gtucB.getGTUType());
+        assertEquals("idGeneratorA", idGeneratorA, gtucA.getIdGenerator());
+        assertEquals("idGeneratorB", idGeneratorB, gtucB.getIdGenerator());
+        assertEquals("lengthA", lengthA, gtucA.getLength());
+        assertEquals("lengthB", lengthB, gtucB.getLength());
+        assertEquals("widthA", widthA, gtucA.getWidth());
+        assertEquals("widthB", widthB, gtucB.getWidth());
+        assertEquals("maximumVelocityA", maximumSpeedA, gtucA.getMaximumVelocity());
+        assertEquals("maximumVelocityB", maximumSpeedB, gtucB.getMaximumVelocity());
+        assertEquals("simulatorA", simulatorA, gtucA.getSimulator());
+        assertEquals("simulatorB", simulatorB, gtucB.getSimulator());
+        assertEquals("networkA", networkA, gtucA.getNetwork());
+        assertEquals("networkB", networkB, gtucB.getNetwork());
+    }
+
+    /** ... */
+    private SimulatorInterface<Abs<TimeUnit>, org.djunits.value.vdouble.scalar.DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> simulator;
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel(
+            SimulatorInterface<Abs<TimeUnit>, org.djunits.value.vdouble.scalar.DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> theSimulator)
+            throws SimRuntimeException, RemoteException
+    {
+        this.simulator = theSimulator;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SimulatorInterface<Abs<TimeUnit>, org.djunits.value.vdouble.scalar.DoubleScalar.Rel<TimeUnit>, OTSSimTimeDouble> getSimulator()
+            throws RemoteException
+    {
+        return this.simulator;
+    }
+}
