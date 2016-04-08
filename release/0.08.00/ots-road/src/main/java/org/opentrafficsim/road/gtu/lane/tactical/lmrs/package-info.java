60e742f4c55cf4d72dc244270960553d1609d89d
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-04-08 01:49:13 +0000

    Tagged release 0.08.00

--- release/0.08.00/ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/lmrs/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes that implement the LMRS lane change model integrated with a car-following model forming a tactical planner.
- * @author Wouter Schakel
- */
-package org.opentrafficsim.road.gtu.lane.tactical.lmrs;
