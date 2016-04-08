fd6a2f09930b6810ed3560b903013fec115697e8
wjschakel <89968231+WJSchakel@users.noreply.github.com>
2016-04-08 04:30:33 +0000

    Behavioral characteristics added

--- ots-core/src/main/java/org/opentrafficsim/core/gtu/TemplateGTUType.java
+++ ots-core/src/main/java/org/opentrafficsim/core/gtu/TemplateGTUType.java
@@ -103,7 +103,7 @@ public class TemplateGTUType implements Serializable, Generator<GTUCharacteristi
     }
 
     /** {@inheritDoc} */
-    public final GTUCharacteristics draw() throws ProbabilityException, ParameterException
+    public GTUCharacteristics draw() throws ProbabilityException, ParameterException
     {
         return new GTUCharacteristics(this.gtuType, this.idGenerator, this.lengthGenerator.draw(), this.widthGenerator.draw(),
                 this.maximumVelocityGenerator.draw(), this.simulator, this.network);
