40f9edebe0f44038658401099078e8fbd81daa48
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-06-23 12:14:57 +0000

    Better javadoc. Added method to print the quad tree as ASCII art. Implemented toArray and toArray(T[]) Code cleanup and bug fixes in many places. Added intersectingShapes(Rectangle2D) and intersectingShapes(OTSShape).

--- trunk/ots-core/src/main/java/org/opentrafficsim/core/geometry/OTS2DSet.java
+++ trunk/ots-core/src/main/java/org/opentrafficsim/core/geometry/OTS2DSet.java
@@ -12,7 +12,16 @@ import org.opentrafficsim.core.Throw;
 import com.vividsolutions.jts.geom.Envelope;
 
 /**
- * Set of OTSShape3D objects, organized by 2D projection.
+ * Set of OTSShape objects and provides methods for fast selection of those objects that intersect an OTSShape. <br>
+ * An OTS2DSet internally stores the OTSShapes in a quad tree. At time of construction the minimum cell size is defined. Node
+ * expansion is never performed on nodes that are smaller than this limit. <br>
+ * Each node (even the non-leaf nodes) store a set of OTSShape. Non-leaf nodes locally store those shapes that completely cover
+ * the rectangular area of the node. Such shapes are <b>not</b> also stored in leaf nodes below that node. OTSShapes that
+ * partially cover a non-leaf node are stored in each of the leaf nodes below that node that those OTSShapes (partially) cover.
+ * Leaf nodes that cannot be expanded (because they are too small) also store all OTSShapes that partially cover the area of the
+ * node. <br>
+ * If removal of an OTSShape objects results in a leaf becoming empty, that leaf is removed from its parent (which may then
+ * itself become empty and removed in turn).
  * <p>
  * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
@@ -42,8 +51,13 @@ public class OTS2DSet implements Set<OTSShape>
     /** How fine will this quad tree divide. */
     private final double minimumCellSize;
 
+    /** Can this node contain sub nodes? */
+    private final boolean mayHaveSubNodes;
+
     /**
-     * Construct an empty OTS2DSet for a region.
+     * Construct an empty OTS2DSet for a rectangular region. Objects that do not intersect this region will never be stored in
+     * this OTS2DSet. (Trying to add such an OTSShape is <b>not</b> an error; the <code>add</code> method will return false,
+     * indicating that the set has not been modified.)
      * @param boundingBox Rectangle2D; the region
      * @param minimumCellSize double; resolution of the underlying quad tree
      * @throws OTSGeometryException when the bounding box covers no surface
@@ -58,6 +72,8 @@ public class OTS2DSet implements Set<OTSShape>
         this.boundingBox = boundingBox;
         this.boundingShape = rectangleShape(boundingBox);
         this.minimumCellSize = minimumCellSize;
+        this.mayHaveSubNodes =
+                this.boundingBox.getWidth() > this.minimumCellSize || this.boundingBox.getHeight() > this.minimumCellSize;
     }
 
     /** {@inheritDoc} */
@@ -88,11 +104,13 @@ public class OTS2DSet implements Set<OTSShape>
                 || shapeEnvelope.getMinY() >= this.boundingBox.getMaxY()
                 || shapeEnvelope.getMaxY() <= this.boundingBox.getMinY())
         {
-            return false;
+            return false; // shape does not intersect the bounding box of this node
         }
-        if (shape.contains(this.boundingBox)) // This contains operation is the geo operation contains!
+        // This contains operation is the spatial operation with that name!
+        if ((!this.mayHaveSubNodes) || shape.contains(this.boundingBox))
         {
-            if (this.shapes.contains(shape)) // This contains operation is the set operation contains!
+            // This contains operation is the set operation with that name!
+            if (this.shapes.contains(shape))
             {
                 return true;
             }
@@ -100,11 +118,7 @@ public class OTS2DSet implements Set<OTSShape>
         // Recursively check the sub nodes
         for (OTS2DSet subSet : this.leaves)
         {
-            if (null == subSet)
-            {
-                continue;
-            }
-            if (subSet.contains(shape))
+            if (null != subSet && subSet.contains(shape))
             {
                 return true;
             }
@@ -119,18 +133,46 @@ public class OTS2DSet implements Set<OTSShape>
         return new QuadTreeIterator();
     }
 
+    /** Returned value of toArray() when size of this OTS2DSet is 0. */
+    private static final Object[] EMPTY_ARRAY_OF_OBJECT = new Object[0];
+
     /** {@inheritDoc} */
     @Override
     public final Object[] toArray()
     {
-        return null;
+        if (0 == this.size())
+        {
+            return EMPTY_ARRAY_OF_OBJECT;
+        }
+        Object[] result = new Object[this.size()];
+        int nextIndex = 0;
+        for (OTSShape shape : this)
+        {
+            result[nextIndex++] = shape;
+        }
+        return result;
     }
 
     /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
     @Override
     public final <T> T[] toArray(final T[] a)
     {
-        return null;
+        T[] result = a;
+        if (result.length < this.size())
+        {
+            result = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), this.size());
+        }
+        else if (result.length > this.size())
+        {
+            result[this.size()] = null;
+        }
+        int nextIndex = 0;
+        for (OTSShape shape : this)
+        {
+            result[nextIndex++] = (T) shape;
+        }
+        return result;
     }
 
     /**
@@ -164,9 +206,9 @@ public class OTS2DSet implements Set<OTSShape>
         {
             return false;
         }
-        if (e.contains(this.boundingBox) || this.boundingBox.getWidth() < this.minimumCellSize
-                && this.boundingBox.getHeight() < this.minimumCellSize)
+        if ((!this.mayHaveSubNodes) || e.contains(this.boundingBox))
         {
+            // e belongs in the set of shapes of this node.
             boolean result = this.shapes.add(e);
             if (result)
             {
@@ -174,7 +216,7 @@ public class OTS2DSet implements Set<OTSShape>
             }
             return result;
         }
-        // Add it to the applicable leaf nodes.
+        // This node may have sub nodes and e does not entirely contain this node. Add e to all applicable sub nodes.
         boolean result = false;
         for (int index = 0; index < this.leaves.length; index++)
         {
@@ -184,9 +226,9 @@ public class OTS2DSet implements Set<OTSShape>
                         new Rectangle2D.Double(index / 2 == 0 ? this.boundingBox.getMinX() : this.boundingBox.getCenterX(),
                                 index % 2 == 0 ? this.boundingBox.getMinY() : this.boundingBox.getCenterY(),
                                 this.boundingBox.getWidth() / 2, this.boundingBox.getHeight() / 2);
-                // OTSShape subBoxShape = new OTSShape(subBox);
                 if (rectangleShape(subBox).intersects(e))
                 {
+                    // Expand this node by adding a sub node.
                     try
                     {
                         this.leaves[index] = new OTS2DSet(subBox, this.minimumCellSize);
@@ -194,15 +236,20 @@ public class OTS2DSet implements Set<OTSShape>
                         {
                             result = true;
                         }
+                        else
+                        {
+                            throw new Error("Cannot happen: new node refused to add shape that intersects it");
+                        }
                     }
                     catch (OTSGeometryException exception)
                     {
-                        exception.printStackTrace();
+                        exception.printStackTrace(); // Should not be possible
                     }
                 }
             }
             else
             {
+                // Leaf node already exists. Let the leaf determine if e should be stored (somewhere) in it.
                 if (this.leaves[index].add(e))
                 {
                     result = true;
@@ -213,7 +260,7 @@ public class OTS2DSet implements Set<OTSShape>
         {
             this.size++;
         }
-        return true;
+        return result;
     }
 
     /** {@inheritDoc} */
@@ -249,7 +296,7 @@ public class OTS2DSet implements Set<OTSShape>
                     result = true;
                     if (0 == set.size())
                     {
-                        this.leaves[index] = null;
+                        this.leaves[index] = null; // Cut off empty leaf node
                     }
                 }
             }
@@ -333,78 +380,54 @@ public class OTS2DSet implements Set<OTSShape>
     }
 
     /**
-     * Iterator for quad tree. Shall iterate over the local set of shapes and the (up to four) non-null leave nodes.
+     * Return the set of all shapes in this OTS2DSet that intersect the given rectangle.
+     * @param rectangle Rectangle2D; the rectangle
+     * @return Set&lt;OTSShape&gt;; the shapes that intersect the rectangle
      */
-    class QuadTreeIterator implements Iterator<OTSShape>
+    public final Set<OTSShape> intersectingShapes(final Rectangle2D rectangle)
     {
-        /**
-         * Position in the current node. Phase values 0..3 index the four leaves; phase value 4 selects the shapes set the
-         * current node.
-         */
-        private int phase = 4;
-
-        /** Iterator of the current (sub-)node. */
-        private Iterator<OTSShape> subIterator = null;
-
-        /** {@inheritDoc} */
-        @SuppressWarnings("synthetic-access")
-        @Override
-        public final boolean hasNext()
+        Set<OTSShape> result = new HashSet<OTSShape>();
+        if (!this.boundingBox.intersects(rectangle))
         {
-            while (this.phase >= 0)
-            {
-                if (null == this.subIterator)
-                {
-                    if (4 == this.phase)
-                    {
-                        this.subIterator = OTS2DSet.this.shapes.iterator();
-                    }
-                    else
-                    {
-                        // The second part of this if statement is a (minor) performance improvement
-                        if (null != OTS2DSet.this.leaves[this.phase] || 0 == OTS2DSet.this.leaves[this.phase].size())
-                        {
-                            this.phase--; // move to the next phase
-                            continue;
-                        }
-                        this.subIterator = OTS2DSet.this.leaves[this.phase].iterator();
-                    }
-                }
-                // If execution gets here; we have a valid subIterator.
-                if (this.subIterator.hasNext())
-                {
-                    return true;
-                }
-                // Our subIterator has run out of things to return. Get rid of it, then see if there is another set to iterate
-                // over.
-                this.subIterator = null;
-                this.phase--; // move to the next phase.
-            }
-            return false;
+            return result;
         }
-
-        /** {@inheritDoc} */
-        @Override
-        public final OTSShape next()
+        for (OTS2DSet leaf : this.leaves)
         {
-            if (hasNext())
+            if (null != leaf && leaf.boundingBox.intersects(rectangle))
             {
-                return this.subIterator.next();
+                result.addAll(leaf.intersectingShapes(rectangle));
             }
-            throw new NoSuchElementException();
         }
-
-        /** {@inheritDoc} */
-        @Override
-        public final void remove()
+        for (OTSShape shape : this.shapes)
         {
-            if (null == this.subIterator)
+            OTSShape rectangleShape = rectangleShape(rectangle);
+            if (rectangleShape.intersects(shape))
             {
-                throw new IllegalStateException();
+                result.add(shape);
             }
-            this.subIterator.remove();
         }
+        return result;
+    }
 
+    /**
+     * Return all OTSShapes in this OTS2DSet that intersect a given OTSShape.
+     * @param shape OTSShape; the given OTSShape
+     * @return Set&lt;OTSShape&gt;; all OTSShapes in this OTS2DSet that intersect <code>shape</code>
+     */
+    public final Set<OTSShape> intersectingShapes(final OTSShape shape)
+    {
+        Envelope envelope = shape.getEnvelope();
+        Set<OTSShape> result =
+                intersectingShapes(new Rectangle2D.Double(envelope.getMinX(), envelope.getMinY(), envelope.getWidth(),
+                        envelope.getHeight()));
+        for (Iterator<OTSShape> it = result.iterator(); it.hasNext();)
+        {
+            if (!it.next().intersects(shape))
+            {
+                it.remove();
+            }
+        }
+        return result;
     }
 
     /** {@inheritDoc} */
@@ -432,7 +455,7 @@ public class OTS2DSet implements Set<OTSShape>
             return leaf.toString(recursionDepth - 1);
         }
         int leafSize = this.leaves[index].size();
-        return leafSize + "shape" + (1 == leafSize ? "" : "s");
+        return leafSize + " shape" + (1 == leafSize ? "" : "s");
     }
 
     /**
@@ -451,7 +474,7 @@ public class OTS2DSet implements Set<OTSShape>
     }
 
     /**
-     * Return a number of concatenated copies of a string.
+     * Return concatenation of a number of copies of a string.
      * @param count int; number of copies to concatenate
      * @param string String; the string to repeat
      * @return String
@@ -479,7 +502,8 @@ public class OTS2DSet implements Set<OTSShape>
     private static final int NUMBERSIZE = 6;
 
     /**
-     * Similar to toStringGraphic, but with OTS2DSet argument which can be null.
+     * Similar to toStringGraphic, but with OTS2DSet argument which can be null. <br>
+     * This code is <b>not</b> optimized for performance; the repeated use of String.split is probably expensive.
      * @param set OTS2DSet; the OTS2DSet to render. Can be null.
      * @param recursionDepth int; levels to recurse
      * @return String
@@ -560,13 +584,105 @@ public class OTS2DSet implements Set<OTSShape>
     }
 
     /**
-     * Return a String array depicting a OTS2DSet.
+     * Return a String depicting an OTS2DSet.
      * @param recursionDepth int; levels to recurse
      * @return String
      */
     public final String toStringGraphic(final int recursionDepth)
     {
         return subStringGraphic(this, recursionDepth);
+    }
+
+    /**
+     * Iterator for quad tree. Shall iterate over the local set of shapes and the (up to four) non-null leave nodes.
+     */
+    class QuadTreeIterator implements Iterator<OTSShape>
+    {
+        /**
+         * Position in the current node. Phase values 0..3 index the four leaves; phase value 4 selects the shapes set the
+         * current node.
+         */
+        private int phase = 4;
+
+        /** Set of OTSShapes that were already returned by this iterator. This is probably dreadfully expensive. */
+        private Set<OTSShape> seen = new HashSet<OTSShape>();
+
+        /** Iterator of the current (sub-)node. */
+        private Iterator<OTSShape> subIterator = null;
+
+        /** The next OTSShape. Must be stored because hasNext has to inspect it to ensure it has not been seen before. */
+        private OTSShape nextResult;
+
+        /** {@inheritDoc} */
+        @SuppressWarnings("synthetic-access")
+        @Override
+        public final boolean hasNext()
+        {
+            while (this.phase >= 0)
+            {
+                if (null == this.subIterator)
+                {
+                    if (4 == this.phase)
+                    {
+                        this.subIterator = OTS2DSet.this.shapes.iterator();
+                    }
+                    else
+                    {
+                        // The second part of this if statement is a (minor) performance improvement
+                        if (null == OTS2DSet.this.leaves[this.phase] || 0 == OTS2DSet.this.leaves[this.phase].size())
+                        {
+                            this.phase--; // move to the next phase
+                            continue;
+                        }
+                        this.subIterator = OTS2DSet.this.leaves[this.phase].iterator();
+                    }
+                }
+                // If execution gets here; we have a valid subIterator.
+                while (this.subIterator.hasNext())
+                {
+                    this.nextResult = this.subIterator.next();
+                    if (this.seen.contains(this.nextResult))
+                    {
+                        continue;
+                    }
+                    this.seen.add(this.nextResult);
+                    return true;
+                }
+                // Our subIterator has run out of things to return. Get rid of it, then see if there is another set to iterate
+                // over.
+                this.subIterator = null;
+                this.phase--; // move to the next phase.
+            }
+            return false;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final OTSShape next()
+        {
+            if (null == this.nextResult)
+            {
+                if (!hasNext())
+                {
+                    throw new NoSuchElementException();
+                }
+            }
+            OTSShape result = this.nextResult;
+            this.nextResult = null;
+            return result;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final void remove()
+        {
+            if (null == this.subIterator)
+            {
+                throw new IllegalStateException();
+            }
+            this.subIterator.remove();
+        }
+
     }
 
 }
