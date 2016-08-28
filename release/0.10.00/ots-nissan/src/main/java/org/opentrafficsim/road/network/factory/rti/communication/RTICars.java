e4822bb90d9e30495333af985c2d2418c4b6ca97
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-08-28 21:19:05 +0000

    Version 0.10.00
    Changes implemented:
    OTS-84	Demo simulations don't check for sufficient headway
    OTS-163 Save network with xstream
    OTS-164 POLYLINE element for LINK in XML
    OTS-165 Tidy up XML definitions
    OTS-169 Split ots-road project in ots-road and separate projects for the different parsers
    OTS-171 Create separate ots-imb project
    OTS-172 Pub/sub between GTU, Lane and Graphs

---
+++ release/0.10.00/ots-nissan/src/main/java/org/opentrafficsim/road/network/factory/rti/communication/RTICars.java
@@ -0,0 +1,59 @@
+package org.opentrafficsim.road.network.factory.rti.communication;
+
+import javax.naming.NamingException;
+
+import nl.tudelft.simulation.language.d3.DirectedPoint;
+
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.gtu.GTUException;
+import org.opentrafficsim.core.gtu.GTUType;
+import org.opentrafficsim.core.gtu.plan.operational.OperationalPlanException;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.road.gtu.lane.LaneBasedIndividualGTU;
+
+/** */
+public class RTICars extends LaneBasedIndividualGTU
+{
+
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /** */
+    private DirectedPoint current = new DirectedPoint(0, 0, 0, 0, 0, 0);
+
+    /**
+     * @param valueOf car id
+     * @param carType GTU type
+     * @param carLength car length
+     * @param width car width
+     * @param maxSpeed maximum speed of the car
+     * @param simulator the simulator
+     * @param network the network on which the car will be registered
+     * @throws GTUException on GTU inconsistency
+     * @throws NamingException when animation cannot be registered
+     * @throws OperationalPlanException when plan retrieval fails
+     */
+    public RTICars(final String valueOf, final GTUType carType, final Length carLength, final Length width, final Speed maxSpeed,
+            final OTSDEVSSimulatorInterface simulator, final OTSNetwork network)
+            throws NamingException, GTUException, OperationalPlanException
+    {
+        super(valueOf, carType, carLength, width, maxSpeed, simulator, network);
+        this.current = this.getOperationalPlan().getLocation(simulator.getSimulatorTime().getTime());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public DirectedPoint getLocation()
+    {
+        double x = this.current.x + (0.01 * (Math.cos(this.current.getRotZ())));
+        double y = this.current.y + (0.01 * (Math.sin(this.current.getRotZ())));
+
+        this.current.setX(x);
+        this.current.setY(y);
+
+        return this.current;
+    }
+}
