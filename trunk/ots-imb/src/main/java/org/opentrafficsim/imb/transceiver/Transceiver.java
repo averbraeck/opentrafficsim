7c326a3eaa6dfaefe4a1c603898a93a4b06b79ac
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-10-18 17:42:25 +0000

    Start with the ots-imb-kpi project. Data is not received 100% correctly from the bus yet.

--- trunk/ots-imb/src/main/java/org/opentrafficsim/imb/transceiver/Transceiver.java
+++ trunk/ots-imb/src/main/java/org/opentrafficsim/imb/transceiver/Transceiver.java
@@ -1,24 +1,23 @@
-package org.opentrafficsim.imb.connector;
+package org.opentrafficsim.imb.transceiver;
 
 import org.opentrafficsim.imb.IMBException;
+import org.opentrafficsim.imb.connector.Connector;
 
 import nl.tno.imb.TByteBuffer;
-import nl.tudelft.simulation.event.EventListenerInterface;
-import nl.tudelft.simulation.event.EventProducerInterface;
 
 /**
- * Relay events between IMB domain and DSOL domain.
+ * Relay events between IMB domain and OTS domain.
  * <p>
  * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
- * <p>
- * @version $Revision$, $LastChangedDate$, by $Author$,
- *          initial version Sep 9, 2016 <br>
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Oct 18, 2016 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
  */
-public interface Transceiver extends EventListenerInterface, EventProducerInterface
+public interface Transceiver
 {
     /**
      * Retrieve the id for the Transceiver channel, e.g. "GTU" or "Simulator Control".
@@ -39,5 +38,5 @@ public interface Transceiver extends EventListenerInterface, EventProducerInterf
      * @throws IMBException in case the IMB event cannot be handled by this Transceiver
      */
     void handleMessageFromIMB(String imbEventName, TByteBuffer imbPayload) throws IMBException;
-    
 }
+
