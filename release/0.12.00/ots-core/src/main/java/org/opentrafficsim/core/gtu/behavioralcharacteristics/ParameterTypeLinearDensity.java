d4d04259eeb08025cc64cfb718fab0961752a77f
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-11-02 11:59:33 +0000

    OTS version 0.12.

---
+++ release/0.12.00/ots-core/src/main/java/org/opentrafficsim/core/gtu/behavioralcharacteristics/ParameterTypeLinearDensity.java
@@ -0,0 +1,76 @@
+package org.opentrafficsim.core.gtu.behavioralcharacteristics;
+
+import java.io.Serializable;
+
+import org.djunits.value.vdouble.scalar.LinearDensity;
+
+/**
+ * Wrapper class for LinearDensity parameters.
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, 
+ *          initial version Apr 13, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class ParameterTypeLinearDensity extends ParameterType<LinearDensity> implements Serializable
+{
+
+    /** */
+    private static final long serialVersionUID = 20150000L;
+
+    /**
+     * Constructor without default value and check.
+     * @param id Short name of parameter.
+     * @param description Parameter description or full name.
+     */
+    public ParameterTypeLinearDensity(final String id, final String description)
+    {
+        super(id, description, LinearDensity.class);
+    }
+
+    /**
+     * Constructor with default value, without check.
+     * @param id Short name of parameter.
+     * @param description Parameter description or full name.
+     * @param defaultValue Default value.
+     */
+    public ParameterTypeLinearDensity(final String id, final String description, final LinearDensity defaultValue)
+    {
+        super(id, description, LinearDensity.class, defaultValue);
+    }
+
+    /**
+     * Constructor without default value, with check.
+     * @param id Short name of parameter.
+     * @param description Parameter description or full name.
+     * @param check Check for parameter values.
+     */
+    public ParameterTypeLinearDensity(final String id, final String description, final Check check)
+    {
+        super(id, description, LinearDensity.class, check);
+    }
+
+    /**
+     * Constructor with default value and check.
+     * @param id Short name of parameter.
+     * @param description Parameter description or full name.
+     * @param defaultValue Default value.
+     * @param check Check for parameter values.
+     */
+    public ParameterTypeLinearDensity(final String id, final String description, final LinearDensity defaultValue,
+            final Check check)
+    {
+        super(id, description, LinearDensity.class, defaultValue, check);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return "ParameterTypeLinearDensity [id=" + getId() + ", description=" + getDescription() + "]";
+    }
+
+}
