315cd4d6629a83708d0a9af80489dd73db802c59
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-11-05 16:13:34 +0000

    Removed unused packages.

--- trunk/ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/fourwaystop/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes that implement a behavioural model for 4-way stops, forming a tactical planner.
- * @author Wouter Schakel
- */
-package org.opentrafficsim.road.gtu.lane.tactical.fourwaystop;
