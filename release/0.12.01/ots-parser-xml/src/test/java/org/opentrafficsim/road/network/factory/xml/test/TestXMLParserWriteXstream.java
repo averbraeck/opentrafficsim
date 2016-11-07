d7052b901125059fa4c8ccbbbb84bcae0b351524
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-11-07 21:13:07 +0000

    Intermediate version 0.12.01 -- traffic lights working with IDM, MOBIL, LMRS.

---
+++ release/0.12.01/ots-parser-xml/src/test/java/org/opentrafficsim/road/network/factory/xml/test/TestXMLParserWriteXstream.java
@@ -0,0 +1,219 @@
+package org.opentrafficsim.road.network.factory.xml.test;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javax.naming.NamingException;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.djunits.unit.FrequencyUnit;
+import org.djunits.unit.SpeedUnit;
+import org.djunits.unit.TimeUnit;
+import org.djunits.value.vdouble.scalar.Dimensionless;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Frequency;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.modelproperties.Property;
+import org.opentrafficsim.base.modelproperties.PropertyException;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
+import org.opentrafficsim.core.geometry.OTSGeometryException;
+import org.opentrafficsim.core.gis.CoordinateTransformWGS84toRDNew;
+import org.opentrafficsim.core.gtu.GTUException;
+import org.opentrafficsim.core.gtu.animation.GTUColorer;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.kpi.sampling.Query;
+import org.opentrafficsim.kpi.sampling.data.SpeedLimit;
+import org.opentrafficsim.kpi.sampling.indicator.MeanSpeed;
+import org.opentrafficsim.kpi.sampling.indicator.MeanTravelTime;
+import org.opentrafficsim.kpi.sampling.indicator.MeanTripLength;
+import org.opentrafficsim.kpi.sampling.indicator.TotalDelay;
+import org.opentrafficsim.kpi.sampling.indicator.TotalNumberOfStops;
+import org.opentrafficsim.kpi.sampling.indicator.TotalTravelDistance;
+import org.opentrafficsim.kpi.sampling.indicator.TotalTravelTime;
+import org.opentrafficsim.road.network.factory.OTSNetworkUtils;
+import org.opentrafficsim.road.network.factory.xml.XmlNetworkLaneParser;
+import org.opentrafficsim.road.network.sampling.RoadSampler;
+import org.opentrafficsim.simulationengine.AbstractWrappableAnimation;
+import org.opentrafficsim.simulationengine.OTSSimulationException;
+import org.opentrafficsim.simulationengine.SimpleSimulatorInterface;
+import org.xml.sax.SAXException;
+
+import nl.javel.gisbeans.io.esri.CoordinateTransform;
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.animation.D2.GisRenderable2D;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.language.io.URLResource;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate: 2015-09-14 01:33:02 +0200 (Mon, 14 Sep 2015) $, @version $Revision: 1401 $, by $Author: averbraeck $,
+ * initial version Oct 17, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class TestXMLParserWriteXstream extends AbstractWrappableAnimation
+{
+    /**
+     * Main program.
+     * @param args String[]; the command line arguments (not used)
+     * @throws SimRuntimeException should never happen
+     */
+    public static void main(final String[] args) throws SimRuntimeException
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    TestXMLParserWriteXstream xmlParserWriteXStream = new TestXMLParserWriteXstream();
+                    // 1 hour simulation run for testing
+                    xmlParserWriteXStream.buildAnimator(new Time(0.0, TimeUnit.SECOND), new Duration(0.0, TimeUnit.SECOND),
+                            new Duration(60.0, TimeUnit.MINUTE), new ArrayList<Property<?>>(), null, true);
+                }
+                catch (SimRuntimeException | NamingException | OTSSimulationException | PropertyException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String shortName()
+    {
+        return "TestXMLModel";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String description()
+    {
+        return "TestXMLModel";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void stopTimersThreads()
+    {
+        super.stopTimersThreads();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final JPanel makeCharts(final SimpleSimulatorInterface simulator)
+    {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final OTSModelInterface makeModel(final GTUColorer colorer)
+    {
+        return new TestXMLModelWriteXStream();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Double makeAnimationRectangle()
+    {
+        // return new Rectangle2D.Double(-1000, -1000, 2000, 2000);
+        return new Rectangle2D.Double(104000, 482000, 5000, 5000);
+        // return new Rectangle2D.Double(0, 0, 5000, 5000);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return "TestXMLParser []";
+    }
+
+    /**
+     * Model to test the XML parser.
+     * <p>
+     * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. <br>
+     * All rights reserved. BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim
+     * License</a>.
+     * <p>
+     * $LastChangedDate: 2015-09-14 01:33:02 +0200 (Mon, 14 Sep 2015) $, @version $Revision: 1401 $, by $Author: averbraeck $,
+     * initial version un 27, 2015 <br>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+     */
+    class TestXMLModelWriteXStream implements OTSModelInterface
+    {
+        /** */
+        private static final long serialVersionUID = 20141121L;
+
+        /** The simulator. */
+        private OTSDEVSSimulatorInterface simulator;
+
+        /** {@inheritDoc} */
+        @Override
+        public final void constructModel(
+                final SimulatorInterface<Time, Duration, OTSSimTimeDouble> pSimulator)
+                throws SimRuntimeException
+        {
+            this.simulator = (OTSDEVSSimulatorInterface) pSimulator;
+            long millis = System.currentTimeMillis();
+            URL url = URLResource.getResource("/N201v8.xml");
+            XmlNetworkLaneParser nlp = new XmlNetworkLaneParser(this.simulator);
+            OTSNetwork network;
+            try
+            {
+                network = nlp.build(url);
+                System.out.println("parsing took : " + (System.currentTimeMillis() - millis) + " ms");
+
+                millis = System.currentTimeMillis();
+                String xml = OTSNetworkUtils.toXml(network);
+                System.out.println("making XML took : " + (System.currentTimeMillis() - millis) + " ms");
+                millis = System.currentTimeMillis();
+                Files.write(Paths.get("e://temp/network.txt"), xml.getBytes());
+                System.out.println("writing took : " + (System.currentTimeMillis() - millis) + " ms");
+            }
+            catch (NetworkException | ParserConfigurationException | SAXException | IOException | NamingException | GTUException
+                    | OTSGeometryException exception)
+            {
+                exception.printStackTrace();
+            }
+
+            URL gisURL = URLResource.getResource("/N201/map.xml");
+            System.err.println("GIS-map file: " + gisURL.toString());
+            CoordinateTransform rdto0 = new CoordinateTransformWGS84toRDNew(0, 0);
+            new GisRenderable2D(this.simulator, gisURL, rdto0);
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public SimulatorInterface<Time, Duration, OTSSimTimeDouble> getSimulator()
+        {
+            return this.simulator;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final String toString()
+        {
+            return "TestXMLModel [simulator=" + this.simulator + "]";
+        }
+
+    }
+
+}
