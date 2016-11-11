017c5da750dfe18db7be52723ae8e9d8571f7e11
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-11-11 14:29:34 +0000

    Initial version of ModelControl KPI module.

---
+++ trunk/ots-imb-kpi/src/main/java/org/opentrafficsim/imb/kpi/demo/ModelControlKPI.java
@@ -0,0 +1,105 @@
+package org.opentrafficsim.imb.kpi.demo;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import nl.tno.imb.TConnection;
+import nl.tno.imb.mc.ModelParameters;
+
+import org.opentrafficsim.imb.IMBException;
+
+
+
+/**
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$,
+ *          initial version Nov 11, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class ModelControlKPI extends nl.tno.imb.mc.ModelStarter
+{
+    /** Thread that handles the KPI process. */
+    private Thread kpiThread;
+
+    /**
+     * @param args
+     * @param providedModelName
+     * @param providedModelId
+     * @throws IMBException
+     */
+    public ModelControlKPI(String[] args, String providedModelName, int providedModelId) throws IMBException
+    {
+        super(args, providedModelName, providedModelId);
+    }
+
+    /**
+     * Entry point
+     * @param args
+     * @throws InvocationTargetException
+     * @throws InterruptedException
+     */
+    public static void main(final String[] args) throws InvocationTargetException, InterruptedException
+    {
+        SwingUtilities.invokeAndWait(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                try
+                {
+                    new ModelControlKPI(args, "KPI Model", 3456);
+                }
+                catch (IMBException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void startModel(final ModelParameters parameters, final TConnection imbConnection)
+    {
+        System.out.println("startModel called");
+        System.out.println("parameters: " + parameters);
+        
+        this.kpiThread = new Thread(new Runnable() {
+            public void run() {
+                // TODO VissimQueryKPI.doTheWork(imbConnection);
+            }
+        });
+        this.kpiThread.start();
+        System.out.println("Started the KPI thread");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void stopModel()
+    {
+        System.out.println("stopModel called");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void quitApplication()
+    {
+        System.out.println("Received quit application request");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void parameterRequest(ModelParameters parameters)
+    {
+        System.out.println("Received parameter request");
+    }
+
+
+}
