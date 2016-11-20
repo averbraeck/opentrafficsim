b20cd09d750597742bb514ff02e899c9582123e0
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-11-20 15:09:50 +0000

    deployed version 0.12.02

---
+++ release/0.12.02/ots-core/ots-core/src/main/java/org/opentrafficsim/core/gtu/GTUCharacteristics.java
@@ -0,0 +1,142 @@
+package org.opentrafficsim.core.gtu;
+
+import java.io.Serializable;
+
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.idgenerator.IdGenerator;
+import org.opentrafficsim.core.network.OTSNetwork;
+
+/**
+ * Characteristics of a GTU. This class is used to store all characteristics of a (not-yet constructed) GTU.
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Mar 8, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class GTUCharacteristics implements Serializable
+{
+
+    /** */
+    private static final long serialVersionUID = 20160000L;
+
+    /** The type of the GTU. */
+    private final GTUType gtuType;
+
+    /** The id generator that will generate the id of the GTU. */
+    private final IdGenerator idGenerator;
+
+    /** Length of the GTU. */
+    private final Length length;
+
+    /** Width of the GTU. */
+    private final Length width;
+
+    /** Maximum speed of the GTU. */
+    private final Speed maximumSpeed;
+
+    /** The simulator that controls the GTU. */
+    private final OTSDEVSSimulatorInterface simulator;
+
+    /** The OTSNetwork that all generated GTUs will be registered in. */
+    private final OTSNetwork network;
+
+    /**
+     * Construct a new set of GTUCharacteristics.
+     * @param gtuType GTUType; type of the (not yet constructed) GTU
+     * @param idGenerator IdGenerator; the id generator for the (not yet constructed) GTU
+     * @param length Length; the length of the (non yet constructed) GTU
+     * @param width Length; the width of the (non yet constructed) GTU
+     * @param maximumSpeed Length; the maximum speed of the (non yet constructed) GTU
+     * @param simulator OTSDEVSSimulatorInterface; the simulator that controls the (not yet constructed) GTU
+     * @param network OTSNetwork; the network that will contain the GTU
+     */
+    public GTUCharacteristics(final GTUType gtuType, final IdGenerator idGenerator, final Length length,
+            final Length width, final Speed maximumSpeed, final OTSDEVSSimulatorInterface simulator,
+            final OTSNetwork network)
+    {
+        this.gtuType = gtuType;
+        this.idGenerator = idGenerator;
+        this.length = length;
+        this.width = width;
+        this.maximumSpeed = maximumSpeed;
+        this.simulator = simulator;
+        this.network = network;
+    }
+
+    /**
+     * Retrieve the GTU type.
+     * @return GTUType.
+     */
+    public final GTUType getGTUType()
+    {
+        return this.gtuType;
+    }
+
+    /**
+     * Retrieve the id generator.
+     * @return IdGenerator
+     */
+    public final IdGenerator getIdGenerator()
+    {
+        return this.idGenerator;
+    }
+
+    /**
+     * Retrieve the length.
+     * @return Length
+     */
+    public final Length getLength()
+    {
+        return this.length;
+    }
+
+    /**
+     * Retrieve the width.
+     * @return Width.Rel
+     */
+    public final Length getWidth()
+    {
+        return this.width;
+    }
+
+    /**
+     * Retrieve the maximum speed.
+     * @return Speed
+     */
+    public final Speed getMaximumSpeed()
+    {
+        return this.maximumSpeed;
+    }
+
+    /**
+     * Retrieve the simulator.
+     * @return OTSDEVSSimulatorInterface
+     */
+    public final OTSDEVSSimulatorInterface getSimulator()
+    {
+        return this.simulator;
+    }
+
+    /**
+     * Retrieve the network.
+     * @return OTSNetwork
+     */
+    public final OTSNetwork getNetwork()
+    {
+        return this.network;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return "GTUCharacteristics [gtuType=" + this.gtuType + ", length=" + this.length + ", width=" + this.width
+                + ", maximumSpeed=" + this.maximumSpeed + "]";
+    }
+
+}
---
+++ release/0.12.02/ots-core/ots-core/src/main/java/org/opentrafficsim/core/gtu/animation/IDGTUColorer.java
@@ -0,0 +1,68 @@
+package org.opentrafficsim.core.gtu.animation;
+
+import java.awt.Color;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opentrafficsim.core.gtu.GTU;
+
+/**
+ * Color GTUs based on their id. If the id ends on one or more digits, the value that those digits constitute is used.
+ * Otherwise, the hash code of the string representation of the id is used.
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$,
+ *          initial version 28 mei 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class IDGTUColorer implements GTUColorer, Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20150000L;
+
+    /** The legend. */
+    public static final ArrayList<LegendEntry> LEGEND;
+
+    /**
+     * Construct a new IDGTUColorer.
+     */
+    static
+    {
+        LEGEND = new ArrayList<LegendEntry>();
+        LEGEND.add(new LegendEntry(Color.BLACK, "black", "black"));
+        LEGEND.add(new LegendEntry(new Color(0xa5, 0x2a, 0x2a), "brown", "brown"));
+        LEGEND.add(new LegendEntry(Color.RED, "red", "red"));
+        LEGEND.add(new LegendEntry(Color.ORANGE, "orange", "orange"));
+        LEGEND.add(new LegendEntry(Color.YELLOW, "yellow", "yellow"));
+        LEGEND.add(new LegendEntry(Color.GREEN, "green", "green"));
+        LEGEND.add(new LegendEntry(Color.BLUE, "blue", "blue"));
+        LEGEND.add(new LegendEntry(Color.MAGENTA, "magenta", "magenta"));
+        LEGEND.add(new LegendEntry(Color.GRAY, "gray", "gray"));
+        LEGEND.add(new LegendEntry(Color.WHITE, "white", "white"));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Color getColor(final GTU gtu)
+    {
+        return gtu.getBaseColor();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final List<LegendEntry> getLegend()
+    {
+        return LEGEND;
+    }
+
+    /** {@inheritDoc} */
+    public final String toString()
+    {
+        return "ID";
+    }
+
+}
