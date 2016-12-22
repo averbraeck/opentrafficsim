58227b69c27d2942271d33c6a61c6185b1f05ba2
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-12-22 06:55:41 +0000

    deployed version 0.13.01

---
+++ release/0.13.01/ots-demo/src/main/java/org/opentrafficsim/demo/conflict/TestNetworkDemo.java
@@ -0,0 +1,158 @@
+package org.opentrafficsim.demo.conflict;
+
+import static org.opentrafficsim.core.gtu.GTUType.VEHICLE;
+
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.naming.NamingException;
+import javax.swing.SwingUtilities;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.unit.TimeUnit;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.modelproperties.Property;
+import org.opentrafficsim.base.modelproperties.PropertyException;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
+import org.opentrafficsim.core.gtu.animation.GTUColorer;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.road.animation.AnimationToggles;
+import org.opentrafficsim.road.network.factory.xml.XmlNetworkLaneParser;
+import org.opentrafficsim.road.network.lane.conflict.ConflictBuilder;
+import org.opentrafficsim.simulationengine.AbstractWrappableAnimation;
+import org.opentrafficsim.simulationengine.OTSSimulationException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.language.io.URLResource;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 11 dec. 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class TestNetworkDemo extends AbstractWrappableAnimation
+{
+
+    /** */
+    private static final long serialVersionUID = 20161211L;
+
+    /** {@inheritDoc} */
+    @Override
+    protected final OTSModelInterface makeModel(final GTUColorer colorer) throws OTSSimulationException
+    {
+        return new TestNetworkModel();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final void addAnimationToggles()
+    {
+        AnimationToggles.setTextAnimationTogglesStandard(this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String shortName()
+    {
+        return "Test network demonstration";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String description()
+    {
+        return "Test network demonstration";
+    }
+
+    /**
+     * The simulation model.
+     */
+    class TestNetworkModel implements OTSModelInterface
+    {
+
+        /** */
+        private static final long serialVersionUID = 20161211L;
+
+        /** The network. */
+        private OTSNetwork network;
+
+        /** Simulator. */
+        private OTSDEVSSimulatorInterface simulator;
+
+        /** {@inheritDoc} */
+        @Override
+        public void constructModel(final SimulatorInterface<Time, Duration, OTSSimTimeDouble> arg0)
+                throws SimRuntimeException, RemoteException
+        {
+            this.simulator = (OTSDEVSSimulatorInterface) arg0;
+            try
+            {
+                URL url = URLResource.getResource("/conflict/Test-Network-14.xml");
+                XmlNetworkLaneParser nlp = new XmlNetworkLaneParser(this.simulator);
+                this.network = nlp.build(url);
+
+                ConflictBuilder.buildConflicts(this.network, VEHICLE, this.simulator,
+                        new ConflictBuilder.FixedWidthGenerator(new Length(2.0, LengthUnit.SI)));
+
+            }
+            catch (Exception exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public SimulatorInterface<Time, Duration, OTSSimTimeDouble> getSimulator() throws RemoteException
+        {
+            return this.simulator;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public OTSNetwork getNetwork()
+        {
+            return this.network;
+        }
+
+    }
+
+    /**
+     * Main program.
+     * @param args String[]; the command line arguments (not used)
+     * @throws SimRuntimeException should never happen
+     */
+    public static void main(final String[] args) throws SimRuntimeException
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    TestNetworkDemo animation = new TestNetworkDemo();
+                    // 1 hour simulation run for testing
+                    animation.buildAnimator(new Time(0.0, TimeUnit.SECOND), new Duration(0.0, TimeUnit.SECOND),
+                            new Duration(60.0, TimeUnit.MINUTE), new ArrayList<Property<?>>(), null, true);
+                }
+                catch (SimRuntimeException | NamingException | OTSSimulationException | PropertyException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
