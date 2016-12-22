f566e61d90dca792522377b6a41f26390e9fad52
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-12-22 13:18:05 +0000

    ots-water first batch of additions based on the IDVV project.

--- trunk/ots-water/src/main/java/org/opentrafficsim/water/network/Waterway.java
+++ trunk/ots-water/src/main/java/org/opentrafficsim/water/network/Waterway.java
@@ -13,14 +13,22 @@ import org.opentrafficsim.core.network.Network;
 import org.opentrafficsim.core.network.NetworkException;
 import org.opentrafficsim.core.network.OTSLink;
 import org.opentrafficsim.core.network.OTSNode;
+import org.opentrafficsim.water.network.infra.Obstacle;
+import org.opentrafficsim.water.transfer.Terminal;
 
 /**
+ * A waterway, i.e. a river, canal or sailable route on a lake or sea.
  * <p>
  * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * <p>
+ * Based on software from the IDVV project, which is Copyright (c) 2013 Rijkswaterstaat - Dienst Water, Verkeer en Leefomgeving
+ * and licensed without restrictions to Delft University of Technology, including the right to sub-license sources and derived
+ * products to third parties.
  * </p>
  * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
- * initial version Sep 27, 2015 <br>
+ * initial version Nov 6, 2016 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
 public class Waterway extends OTSLink
@@ -28,8 +36,8 @@ public class Waterway extends OTSLink
     /** */
     private static final long serialVersionUID = 20150927L;
 
-    /** description. */
-    // TODO private final String description;
+    /** name. */
+    private final String name;
 
     /** current; positive direction is along the design line. */
     private double current;
@@ -38,13 +46,13 @@ public class Waterway extends OTSLink
     private SortedMap<Length, Obstacle> obstacles = new TreeMap<>();
 
     /** list of terminals, sorted on distance along the design line. */
-    // TODO private SortedMap<Length, Terminal> terminals = new TreeMap<>();
-
+    private SortedMap<Length, Terminal> terminals = new TreeMap<>();
 
     /**
      * Construct a new waterway.
      * @param network the network.
-     * @param id the link id
+     * @param id the waterway id
+     * @param name the name
      * @param startNode start node (directional)
      * @param endNode end node (directional)
      * @param linkType Link type to indicate compatibility with GTU types
@@ -54,17 +62,20 @@ public class Waterway extends OTSLink
      *            waterway that cannot be used for sailing.
      * @throws NetworkException when waterway with this id already exists
      */
-    public Waterway(final Network network, final String id, final OTSNode startNode, final OTSNode endNode,
+    @SuppressWarnings("checkstyle:parameternumber")
+    public Waterway(final Network network, final String id, final String name, final OTSNode startNode, final OTSNode endNode,
             final LinkType linkType, final OTSLine3D designLine, final LongitudinalDirectionality directionality)
             throws NetworkException
     {
         super(network, id, startNode, endNode, linkType, designLine, directionality);
+        this.name = name;
     }
 
     /**
      * Construct a new waterway.
      * @param network the network.
-     * @param id the link id
+     * @param id the waterway id
+     * @param name the name
      * @param startNode start node (directional)
      * @param endNode end node (directional)
      * @param linkType Link type to indicate compatibility with GTU types
@@ -75,11 +86,21 @@ public class Waterway extends OTSLink
      *            waterway.
      * @throws NetworkException when waterway with this id already exists
      */
-    public Waterway(final Network network, final String id, final OTSNode startNode, final OTSNode endNode,
+    @SuppressWarnings("checkstyle:parameternumber")
+    public Waterway(final Network network, final String id, final String name, final OTSNode startNode, final OTSNode endNode,
             final LinkType linkType, final OTSLine3D designLine,
             final Map<GTUType, LongitudinalDirectionality> directionalityMap) throws NetworkException
     {
         super(network, id, startNode, endNode, linkType, designLine, directionalityMap);
+        this.name = name;
+    }
+
+    /**
+     * @return name
+     */
+    public final String getName()
+    {
+        return this.name;
     }
 
 }
