539354dd1ed7d68373c1e3d95c1ebb60672a927b
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2016-12-22 06:54:35 +0000

    deployed version 0.13.01

---
+++ release/0.13.01/ots-road/src/main/java/org/opentrafficsim/road/gtu/animation/DefaultCarAnimation.java
@@ -0,0 +1,270 @@
+package org.opentrafficsim.road.gtu.animation;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.ImageObserver;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+import javax.naming.NamingException;
+import javax.swing.SwingUtilities;
+
+import org.opentrafficsim.core.animation.TextAlignment;
+import org.opentrafficsim.core.animation.TextAnimation;
+import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
+import org.opentrafficsim.core.gtu.animation.GTUColorer;
+import org.opentrafficsim.core.gtu.animation.IDGTUColorer;
+import org.opentrafficsim.road.gtu.lane.LaneBasedIndividualGTU;
+
+import nl.tudelft.simulation.dsol.animation.Locatable;
+import nl.tudelft.simulation.dsol.animation.D2.Renderable2D;
+import nl.tudelft.simulation.language.d2.Angle;
+import nl.tudelft.simulation.language.d3.DirectedPoint;
+
+/**
+ * Draw a car.
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision: 1401 $, $LastChangedDate: 2015-09-14 01:33:02 +0200 (Mon, 14 Sep 2015) $, by $Author: averbraeck $,
+ *          initial version 29 dec. 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class DefaultCarAnimation extends Renderable2D implements Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20150000L;
+
+    /** The GTUColorer that determines the fill color for the car. */
+    private GTUColorer gtuColorer;
+
+    /**
+     * Construct the DefaultCarAnimation for a LaneBasedIndividualCar.
+     * @param gtu the Car to draw
+     * @param simulator the simulator to schedule on
+     * @throws NamingException in case of registration failure of the animation
+     * @throws RemoteException on communication failure
+     */
+    public DefaultCarAnimation(final LaneBasedIndividualGTU gtu, final OTSSimulatorInterface simulator)
+            throws NamingException, RemoteException
+    {
+        this(gtu, simulator, null);
+    }
+
+    /**
+     * Construct the DefaultCarAnimation for a LaneBasedIndividualCar.
+     * @param gtu the Car to draw
+     * @param simulator the simulator to schedule on
+     * @param gtuColorer GTUColorer; the GTUColorer that determines what fill color to use
+     * @throws NamingException in case of registration failure of the animation
+     * @throws RemoteException on communication failure
+     */
+    public DefaultCarAnimation(final LaneBasedIndividualGTU gtu, final OTSSimulatorInterface simulator,
+            final GTUColorer gtuColorer) throws NamingException, RemoteException
+    {
+        super(gtu, simulator);
+        if (null == gtuColorer)
+        {
+            this.gtuColorer = new IDGTUColorer();
+        }
+        else
+        {
+            this.gtuColorer = gtuColorer;
+        }
+
+        new Text(gtu, gtu.getId(), 0.0f, 0.0f, TextAlignment.CENTER, Color.BLACK, simulator);
+    }
+
+    /**
+     * Replace the GTUColorer.
+     * @param newGTUColorer GTUColorer; the GTUColorer to use from now on
+     */
+    public final void setGTUColorer(final GTUColorer newGTUColorer)
+    {
+        this.gtuColorer = newGTUColorer;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void paint(final Graphics2D graphics, final ImageObserver observer)
+    {
+        final LaneBasedIndividualGTU car = (LaneBasedIndividualGTU) getSource();
+
+        if (car.isDestroyed())
+        {
+            try
+            {
+                SwingUtilities.invokeLater(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        try
+                        {
+                            destroy();
+                        }
+                        catch (NamingException | NullPointerException e)
+                        {
+                            // ignore
+                            // TODO Solve in DSOL
+                        }
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                // ignore
+            }
+            return;
+        }
+
+        final double length = car.getLength().getSI();
+        final double l2 = length / 2;
+        final double width = car.getWidth().getSI();
+        final double w2 = width / 2;
+        final double w4 = width / 4;
+        graphics.setColor(this.gtuColorer.getColor(car));
+        BasicStroke saveStroke = (BasicStroke) graphics.getStroke();
+        graphics.setStroke(new BasicStroke(0));
+        Rectangle2D rectangle = new Rectangle2D.Double(-l2, -w2, length, width);
+        graphics.draw(rectangle);
+        graphics.fill(rectangle);
+        // Draw a white disk at the front to indicate which side faces forward
+        graphics.setColor(Color.WHITE);
+        Ellipse2D.Double frontIndicator = new Ellipse2D.Double(l2 - w2 - w4, -w4, w2, w2);
+        graphics.draw(frontIndicator);
+        graphics.fill(frontIndicator);
+
+        // turn indicator lights
+        graphics.setColor(Color.YELLOW);
+        if (car.getTurnIndicatorStatus() != null && car.getTurnIndicatorStatus().isLeftOrBoth())
+        {
+            Rectangle2D.Double leftIndicator = new Rectangle2D.Double(l2 - w4, -w2, w4, w4);
+            graphics.fill(leftIndicator);
+        }
+        if (car.getTurnIndicatorStatus() != null && car.getTurnIndicatorStatus().isRightOrBoth())
+        {
+            Rectangle2D.Double rightIndicator = new Rectangle2D.Double(l2 - w4, w2 - w4, w4, w4);
+            graphics.fill(rightIndicator);
+        }
+
+        // braking lights
+        graphics.setColor(Color.RED);
+        if (car.getAcceleration().si < -0.5) // TODO this could be a property of a GTU
+        {
+            Rectangle2D.Double leftBrake = new Rectangle2D.Double(-l2, w2 - w4, w4, w4);
+            Rectangle2D.Double rightBrake = new Rectangle2D.Double(-l2, -w2, w4, w4);
+            graphics.setColor(Color.RED);
+            graphics.fill(leftBrake);
+            graphics.fill(rightBrake);
+        }
+        graphics.setStroke(saveStroke);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return super.toString(); // this.getSource().toString();
+    }
+
+    /**
+     * Text animation for the Car. Separate class to be able to turn it on and off...
+     * <p>
+     * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.
+     * <br>
+     * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+     * </p>
+     * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+     * initial version Dec 11, 2016 <br>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+     * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+     */
+    public class Text extends TextAnimation
+    {
+        /** */
+        private static final long serialVersionUID = 20161211L;
+
+        /**
+         * @param source the object for which the text is displayed
+         * @param text the text to display
+         * @param dx the horizontal movement of the text, in meters
+         * @param dy the vertical movement of the text, in meters
+         * @param textPlacement where to place the text
+         * @param color the color of the text
+         * @param simulator the simulator
+         * @throws NamingException when animation context cannot be created or retrieved
+         * @throws RemoteException - when remote context cannot be found
+         */
+        public Text(final Locatable source, final String text, final float dx, final float dy,
+                final TextAlignment textPlacement, final Color color, final OTSSimulatorInterface simulator)
+                throws RemoteException, NamingException
+        {
+            super(source, text, dx, dy, textPlacement, color, 1.0f, simulator);
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final void paint(final Graphics2D graphics, final ImageObserver observer) throws RemoteException
+        {
+            if (((LaneBasedIndividualGTU) getSource()).isDestroyed())
+            {
+                try
+                {
+                    SwingUtilities.invokeLater(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            destroy();
+                        }
+                    });
+                }
+                catch (Exception e)
+                {
+                    // ignore
+                }
+                return;
+            }
+
+            super.paint(graphics, observer);
+        }
+
+        /**
+         * Try to destroy the animation.
+         */
+        protected final void destroy()
+        {
+            try
+            {
+                this.animationImpl.destroy();
+            }
+            catch (NamingException exception)
+            {
+                System.err.println("Tried to destroy Text for GTU animation of GTU " + getSource().toString());
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        @SuppressWarnings("checkstyle:designforextension")
+        public DirectedPoint getLocation() throws RemoteException
+        {
+            // draw always on top, and not upside down.
+            DirectedPoint p = ((LaneBasedIndividualGTU) this.source).getLocation();
+            double a = Angle.normalizePi(p.getRotZ());
+            if (a > Math.PI / 2.0 || a < -0.99 * Math.PI / 2.0)
+            {
+                a += Math.PI;
+            }
+            return new DirectedPoint(p.x, p.y, Double.MAX_VALUE, 0.0, 0.0, a);
+        }
+    }
+
+}
---
+++ release/0.13.01/ots-road/src/main/java/org/opentrafficsim/road/network/lane/conflict/Conflict.java
@@ -0,0 +1,333 @@
+package org.opentrafficsim.road.network.lane.conflict;
+
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+import javax.naming.NamingException;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.value.vdouble.scalar.Length;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
+import org.opentrafficsim.core.geometry.OTSGeometryException;
+import org.opentrafficsim.core.geometry.OTSLine3D;
+import org.opentrafficsim.core.geometry.OTSPoint3D;
+import org.opentrafficsim.core.gtu.GTUDirectionality;
+import org.opentrafficsim.core.gtu.GTUType;
+import org.opentrafficsim.core.network.LongitudinalDirectionality;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.road.network.animation.ConflictAnimation;
+import org.opentrafficsim.road.network.lane.CrossSectionElement;
+import org.opentrafficsim.road.network.lane.Lane;
+import org.opentrafficsim.road.network.lane.object.AbstractLaneBasedObject;
+
+import nl.tudelft.simulation.language.Throw;
+
+/**
+ * Conflicts deal with traffic on different links/roads that need to consider each other as their paths may be in conflict
+ * spatially. A single {@code Conflict} represents the one-sided consideration of a conflicting situation. I.e., what is
+ * considered <i>a single conflict in traffic theory, is represented by two {@code Conflict}s</i>, one on each of the
+ * conflicting {@code Lane}s.
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Sep 7, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public final class Conflict extends AbstractLaneBasedObject
+{
+
+    /** */
+    private static final long serialVersionUID = 20160915L;
+
+    /** Conflict type, i.e. crossing, merge or split. */
+    private final ConflictType conflictType;
+
+    /** Conflict rule, i.e. priority, give way, stop or all-stop. */
+    private final ConflictRule conflictRule;
+
+    /** Accompanying other conflict. */
+    private Conflict otherConflict;
+
+    /** The length of the conflict along the lane centerline. */
+    private final Length length;
+
+    /** GTU direction. */
+    private final GTUDirectionality direction;
+
+    /** Simulator for animation and timed events. */
+    private final OTSDEVSSimulatorInterface simulator;
+
+    /** GTU type. */
+    private final GTUType gtuType;
+
+    /** Lock object for cloning a pair of conflicts. */
+    private final Object cloneLock;
+
+    /**
+     * @param lane lane where this conflict starts
+     * @param longitudinalPosition position of start of conflict on lane
+     * @param length length of the conflict along the lane centerline
+     * @param direction GTU direction
+     * @param geometry geometry of conflict
+     * @param conflictRule conflict rule, i.e. priority, give way, stop or all-stop
+     * @param conflictType conflict type, i.e. crossing, merge or split
+     * @param simulator the simulator for animation and timed events
+     * @param cloneLock lock object for cloning a pair of conflicts
+     * @param gtuType gtu type
+     * @throws NetworkException when the position on the lane is out of bounds
+     */
+    @SuppressWarnings("checkstyle:parameternumber")
+    private Conflict(final Lane lane, final Length longitudinalPosition, final Length length, final GTUDirectionality direction,
+            final OTSLine3D geometry, final ConflictType conflictType, final ConflictRule conflictRule,
+            final OTSDEVSSimulatorInterface simulator, final GTUType gtuType, final Object cloneLock) throws NetworkException
+    {
+        super(UUID.randomUUID().toString(), lane, Throw.whenNull(direction, "Direction may not be null.").isPlus()
+                ? LongitudinalDirectionality.DIR_PLUS : LongitudinalDirectionality.DIR_MINUS, longitudinalPosition, geometry);
+        this.length = length;
+        this.direction = direction;
+        this.conflictType = conflictType;
+        this.conflictRule = conflictRule;
+        this.simulator = simulator;
+        this.gtuType = gtuType;
+        this.cloneLock = cloneLock;
+
+        try
+        {
+            new ConflictAnimation(this, simulator);
+        }
+        catch (RemoteException | NamingException exception)
+        {
+            throw new NetworkException(exception);
+        }
+
+        // Create conflict end
+        if (conflictType.equals(ConflictType.SPLIT) || conflictType.equals(ConflictType.MERGE))
+        {
+            Length position =
+                    conflictType.equals(ConflictType.SPLIT) ? (direction.isPlus() ? length : lane.getLength().minus(length))
+                            : (direction.isPlus() ? lane.getLength() : Length.ZERO);
+            try
+            {
+                new ConflictEnd(this, lane,
+                        direction.isPlus() ? LongitudinalDirectionality.DIR_PLUS : LongitudinalDirectionality.DIR_MINUS,
+                        position);
+            }
+            catch (OTSGeometryException exception)
+            {
+                // does not happen
+                throw new RuntimeException("Could not create dummy geometry for ConflictEnd.", exception);
+            }
+        }
+    }
+
+    /**
+     * @return conflictType.
+     */
+    public ConflictType getConflictType()
+    {
+        return this.conflictType;
+    }
+
+    /**
+     * @return conflictRule.
+     */
+    public ConflictRule getConflictRule()
+    {
+        return this.conflictRule;
+    }
+
+    /**
+     * @return length.
+     */
+    public Length getLength()
+    {
+        return this.length;
+    }
+
+    /**
+     * @return otherConflict.
+     */
+    public Conflict getOtherConflict()
+    {
+        return this.otherConflict;
+    }
+
+    /**
+     * @return gtuType.
+     */
+    public GTUType getGtuType()
+    {
+        return this.gtuType;
+    }
+
+    /**
+     * Creates a pair of conflicts.
+     * @param conflictType conflict type, i.e. crossing, merge or split
+     * @param lane1 lane of conflict 1
+     * @param longitudinalPosition1 longitudinal position of conflict 1
+     * @param length1 {@code Length} of conflict 1
+     * @param direction1 GTU direction of conflict 1
+     * @param geometry1 geometry of conflict 1
+     * @param conflictRule1 conflict rule of conflict 1
+     * @param gtuType1 gtu type of conflict 1
+     * @param lane2 lane of conflict 2
+     * @param longitudinalPosition2 longitudinal position of conflict 2
+     * @param length2 {@code Length} of conflict 2
+     * @param direction2 GTU direction of conflict 2
+     * @param geometry2 geometry of conflict 2
+     * @param conflictRule2 conflict rule of conflict 2
+     * @param gtuType2 gtu type of conflict 2
+     * @param simulator the simulator for animation and timed events
+     * @throws NetworkException if the combination of conflict type and both conflict rules is not correct
+     */
+    @SuppressWarnings("checkstyle:parameternumber")
+    public static void generateConflictPair(final ConflictType conflictType, final Lane lane1,
+            final Length longitudinalPosition1, final Length length1, final GTUDirectionality direction1,
+            final OTSLine3D geometry1, final ConflictRule conflictRule1, final GTUType gtuType1, final Lane lane2,
+            final Length longitudinalPosition2, final Length length2, final GTUDirectionality direction2,
+            final OTSLine3D geometry2, final ConflictRule conflictRule2, final GTUType gtuType2,
+            final OTSDEVSSimulatorInterface simulator) throws NetworkException
+    {
+        // lane, longitudinalPosition, length and geometry are checked in AbstractLaneBasedObject
+        Throw.whenNull(conflictType, "Conflict type may not be null.");
+        Throw.whenNull(conflictRule1, "Conflict rule may not be null.");
+        Throw.whenNull(conflictRule2, "Conflict rule may not be null.");
+        if (conflictType.equals(ConflictType.SPLIT))
+        {
+            // Split with split (on split)
+            Throw.when(!conflictRule1.equals(ConflictRule.SPLIT) || !conflictRule2.equals(ConflictRule.SPLIT),
+                    NetworkException.class, "Both conflict rules should be split for conflict type split.");
+        }
+        else
+        {
+            // Priority with give-way/stop
+            boolean check1 = conflictRule1.equals(ConflictRule.PRIORITY) && !conflictRule2.equals(ConflictRule.GIVE_WAY)
+                    && !conflictRule2.equals(ConflictRule.STOP);
+            boolean check2 = conflictRule2.equals(ConflictRule.PRIORITY) && !conflictRule1.equals(ConflictRule.GIVE_WAY)
+                    && !conflictRule1.equals(ConflictRule.STOP);
+            boolean check3 = conflictRule1.equals(ConflictRule.GIVE_WAY) && !conflictRule2.equals(ConflictRule.PRIORITY);
+            boolean check4 = conflictRule2.equals(ConflictRule.GIVE_WAY) && !conflictRule1.equals(ConflictRule.PRIORITY);
+            boolean check5 = conflictRule1.equals(ConflictRule.STOP) && !conflictRule2.equals(ConflictRule.PRIORITY);
+            boolean check6 = conflictRule2.equals(ConflictRule.STOP) && !conflictRule1.equals(ConflictRule.PRIORITY);
+            Throw.when(check1 || check2 || check3 || check4 || check5 || check6, NetworkException.class,
+                    "Conflict rules need to be a combination of 'PRIORITY' and 'GIVE_WAY' or 'STOP', "
+                            + "if any of these types is used.");
+            // All-stop with all-stop
+            boolean check7 = conflictRule1.equals(ConflictRule.ALL_STOP) && !conflictRule2.equals(ConflictRule.ALL_STOP);
+            boolean check8 = conflictRule2.equals(ConflictRule.ALL_STOP) && !conflictRule1.equals(ConflictRule.ALL_STOP);
+            Throw.when(check7 || check8, NetworkException.class,
+                    "Conflict rule 'ALL_STOP' can only be combined with a conflict rule 'ALL_STOP'.");
+            // No split
+            Throw.when(conflictRule1.equals(ConflictRule.SPLIT) || conflictRule2.equals(ConflictRule.SPLIT),
+                    NetworkException.class, "Conflict rule 'SPLIT' may only be used on conflicts of type SPLIT.");
+        }
+        Object cloneLock = new Object();
+        Conflict conf1 = new Conflict(lane1, longitudinalPosition1, length1, direction1, geometry1, conflictType, conflictRule1,
+                simulator, gtuType1, cloneLock);
+        Conflict conf2 = new Conflict(lane2, longitudinalPosition2, length2, direction2, geometry2, conflictType, conflictRule2,
+                simulator, gtuType2, cloneLock);
+        conf1.otherConflict = conf2;
+        conf2.otherConflict = conf1;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "Conflict [conflictType=" + this.conflictType + ", conflictRule=" + this.conflictRule + "]";
+    }
+
+    /**
+     * Clone of other conflict.
+     */
+    private Conflict otherClone;
+
+    /** {@inheritDoc} */
+    @Override
+    public Conflict clone(final CrossSectionElement newCSE, final OTSSimulatorInterface newSimulator, final boolean animation)
+            throws NetworkException
+    {
+        Throw.when(!(newCSE instanceof Lane), NetworkException.class, "sensors can only be cloned for Lanes");
+        Throw.when(!(newSimulator instanceof OTSDEVSSimulatorInterface), NetworkException.class,
+                "simulator should be a DEVSSimulator");
+        Conflict out = new Conflict((Lane) newCSE, getLongitudinalPosition(), this.length, this.direction, getGeometry(),
+                this.conflictType, this.conflictRule, this.simulator, this.gtuType, this.cloneLock);
+        synchronized (this.cloneLock)
+        {
+            // couple both clones
+            if (this.otherClone == null || this.otherClone.simulator != newSimulator)
+            {
+                // other clone will do it
+                this.otherConflict.otherClone = out;
+            }
+            else
+            {
+                out.otherConflict = this.otherClone;
+                this.otherClone.otherConflict = out;
+            }
+            // reset successful clone of pair, or remove otherClone from other simulator (or was already null)
+            this.otherClone = null;
+        }
+        return out;
+    }
+
+    /**
+     * <p>
+     * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.
+     * <br>
+     * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+     * <p>
+     * @version $Revision$, $LastChangedDate$, by $Author$, initial version 14 dec. 2016 <br>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+     * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+     */
+    public class ConflictEnd extends AbstractLaneBasedObject
+    {
+
+        /** */
+        private static final long serialVersionUID = 20161214L;
+
+        /** Conflict. */
+        private final Conflict conflict;
+
+        /**
+         * @param conflict conflict
+         * @param lane lane
+         * @param direction valid direction
+         * @param longitudinalPosition position
+         * @throws NetworkException on network exception
+         * @throws OTSGeometryException does not happen
+         */
+        ConflictEnd(final Conflict conflict, final Lane lane, final LongitudinalDirectionality direction,
+                final Length longitudinalPosition) throws NetworkException, OTSGeometryException
+        {
+            super(conflict.getId() + "End", lane, direction, longitudinalPosition,
+                    new OTSLine3D(new OTSPoint3D(0, 0, 0), new OTSPoint3D(1, 0, 0)));
+            this.conflict = conflict;
+        }
+
+        /**
+         * @return conflict
+         */
+        public final Conflict getConflict()
+        {
+            return this.conflict;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final AbstractLaneBasedObject clone(final CrossSectionElement newCSE, final OTSSimulatorInterface newSimulator,
+                final boolean animation) throws NetworkException
+        {
+            // Constructor of Conflict creates these.
+            return null;
+        }
+
+    }
+
+}
