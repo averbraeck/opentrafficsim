2aa1431a0f1356541b7ccaa0eccb5fdb12856c49
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2017-04-22 23:30:39 +0000

    Sim0MQ test classes and default folder structure for Maven.

---
+++ sim0mq-demo/src/test/java/org/sim0mq/demo/test/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Test for ZeroMQ Demo classes.
+ * <p>
+ * (c) copyright 2002-2016 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs</a>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @version Oct 21, 2016
+ */
+package org.sim0mq.demo.test;
