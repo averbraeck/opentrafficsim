bf7f036600a58f1307f4cedd36663bdb3b9e842d
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2017-05-01 07:34:28 +0000

    deploy version 0.15.00

---
+++ release/0.15.00/ots-demo/src/main/java/loadfromxml/LoadXML.java
@@ -0,0 +1,196 @@
+package loadfromxml;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.naming.NamingException;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.djunits.unit.DurationUnit;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.modelproperties.Property;
+import org.opentrafficsim.base.modelproperties.PropertyException;
+import org.opentrafficsim.core.dsol.OTSDEVSSimulatorInterface;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSSimTimeDouble;
+import org.opentrafficsim.core.geometry.OTSGeometryException;
+import org.opentrafficsim.core.gtu.GTUException;
+import org.opentrafficsim.core.gtu.animation.GTUColorer;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.road.network.factory.xml.XmlNetworkLaneParser;
+import org.opentrafficsim.simulationengine.AbstractWrappableAnimation;
+import org.opentrafficsim.simulationengine.OTSSimulationException;
+import org.xml.sax.SAXException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.event.EventProducer;
+
+/**
+ * Select a OTS-network XML file, load it and run it.
+ * <p>
+ * Copyright (c) 2013-2017 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Apr 21, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class LoadXML extends AbstractWrappableAnimation
+{
+
+    /** */
+    private static final long serialVersionUID = 20170421L;
+
+    /** Name of the XML file. */
+    private String fileName = null;
+
+    /** The XML code. */
+    private String xml = null;
+
+    /**
+     * Load a network from an XML file; program entry point.
+     * @param args String[]; the command line arguments (currently not used)
+     * @throws IOException when the file could not be read
+     * @throws PropertyException should never happen
+     * @throws OTSSimulationException when an error occurs during simulation
+     * @throws NamingException when a name collision is detected
+     * @throws SimRuntimeException should never happen
+     */
+    public static void main(final String[] args) throws IOException, SimRuntimeException, NamingException,
+            OTSSimulationException, PropertyException
+    {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.addChoosableFileFilter(new FileFilter()
+        {
+
+            @Override
+            public boolean accept(final File f)
+            {
+                String name = f.getName();
+                int length = name.length();
+                if (length < 5)
+                {
+                    return false;
+                }
+                String type = name.substring(length - 4);
+                return type.equalsIgnoreCase(".xml");
+            }
+
+            @Override
+            public String getDescription()
+            {
+                return "XML files";
+            }
+        });
+        fileChooser.removeChoosableFileFilter(fileChooser.getAcceptAllFileFilter());
+        if (JFileChooser.APPROVE_OPTION != fileChooser.showOpenDialog(null))
+        {
+            System.out.println("No file chosen; exiting");
+            System.exit(0);
+        }
+        LoadXML loadXML = new LoadXML();
+        loadXML.fileName = fileChooser.getSelectedFile().getAbsolutePath();
+        loadXML.xml = new String(Files.readAllBytes(Paths.get(loadXML.fileName)));
+        try
+        {
+            loadXML.buildAnimator(Time.ZERO, Duration.ZERO, new Duration(3600, DurationUnit.SI), new ArrayList<Property<?>>(),
+                    null, true);
+        }
+        catch (SimRuntimeException sre)
+        {
+            JOptionPane.showMessageDialog(null, sre.getMessage(), "Exception occured", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String shortName()
+    {
+        return this.fileName;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String description()
+    {
+        return "OTS network from " + this.fileName;
+    }
+
+    /** Currently active XML model. */
+    private XMLModel model = null;
+
+    /** {@inheritDoc} */
+    @Override
+    protected final OTSModelInterface makeModel(final GTUColorer colorer) throws OTSSimulationException
+    {
+        this.model = new XMLModel();
+        return this.model;
+    }
+
+    /**
+     * The network.
+     */
+    class XMLModel extends EventProducer implements OTSModelInterface
+    {
+
+        /** */
+        private static final long serialVersionUID = 20170421L;
+
+        /** The network. */
+        private OTSNetwork network;
+
+        /** The simulator. */
+        private SimulatorInterface<Time, Duration, OTSSimTimeDouble> simulator;
+
+        /** {@inheritDoc} */
+        @SuppressWarnings("synthetic-access")
+        @Override
+        public void constructModel(final SimulatorInterface<Time, Duration, OTSSimTimeDouble> theSimulator)
+                throws SimRuntimeException, RemoteException
+        {
+            this.simulator = theSimulator;
+            XmlNetworkLaneParser nlp = new XmlNetworkLaneParser((OTSDEVSSimulatorInterface) theSimulator);
+            try
+            {
+                this.network = nlp.build(new ByteArrayInputStream(LoadXML.this.xml.getBytes(StandardCharsets.UTF_8)));
+            }
+            catch (NetworkException | ParserConfigurationException | SAXException | IOException | NamingException
+                    | GTUException | OTSGeometryException exception)
+            {
+                exception.printStackTrace();
+                // Abusing the SimRuntimeException to propagate the message to the main method (the problem could actually be a
+                // SAXException)
+                throw new SimRuntimeException(exception.getMessage());
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public SimulatorInterface<Time, Duration, OTSSimTimeDouble> getSimulator() throws RemoteException
+        {
+            return this.simulator;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public OTSNetwork getNetwork()
+        {
+            return this.network;
+        }
+
+    }
+}
---
+++ release/0.15.00/ots-demo/src/main/java/org/opentrafficsim/demo/timing/TimeCircularRoadSimulation.java
@@ -0,0 +1,147 @@
+package org.opentrafficsim.demo.timing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingException;
+import javax.swing.SwingUtilities;
+
+import org.djunits.unit.UNITS;
+import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.modelproperties.CompoundProperty;
+import org.opentrafficsim.base.modelproperties.ProbabilityDistributionProperty;
+import org.opentrafficsim.base.modelproperties.Property;
+import org.opentrafficsim.base.modelproperties.PropertyException;
+import org.opentrafficsim.base.modelproperties.SelectionProperty;
+import org.opentrafficsim.core.gtu.AbstractGTU;
+import org.opentrafficsim.demo.carFollowing.CircularRoad;
+import org.opentrafficsim.road.gtu.lane.AbstractLaneBasedGTU;
+import org.opentrafficsim.road.modelproperties.IDMPropertySet;
+import org.opentrafficsim.simulationengine.AbstractWrappableAnimation;
+import org.opentrafficsim.simulationengine.OTSSimulationException;
+import org.opentrafficsim.simulationengine.SimpleAnimator;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2017 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Apr 14, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class TimeCircularRoadSimulation implements UNITS
+{
+    /** */
+    private static final boolean GRAPHS = true;
+    
+    /** */
+    public TimeCircularRoadSimulation()
+    {
+        try
+        {
+            AbstractWrappableAnimation simulation = new CircularRoad();
+            List<Property<?>> activeProperties = new ArrayList<>();
+            activeProperties.addAll(simulation.getProperties());
+            for (Property<?> ap : activeProperties)
+            {
+                if (ap instanceof SelectionProperty)
+                {
+                    SelectionProperty sp = (SelectionProperty) ap;
+                    if ("TacticalPlanner".equals(sp.getKey()))
+                    {
+                        sp.setValue("DIRECTED/IDM");
+                    }
+                }
+            }
+            if (!GRAPHS)
+            {
+                for (Property<?> ap : activeProperties)
+                {
+                    if (ap instanceof CompoundProperty)
+                    {
+                        CompoundProperty cp = (CompoundProperty) ap;
+                        cp.setReadOnly(false);
+                        if ("OutputGraphs".equals(cp.getKey()))
+                        {
+                            while (cp.size() > 0)
+                            {
+                                cp.remove(0);
+                            }
+                        }
+                    }
+                }
+            }
+            activeProperties.add(new ProbabilityDistributionProperty("TrafficComposition", "Traffic composition",
+                    "<html>Mix of passenger cars and trucks</html>", new String[] { "passenger car", "truck" },
+                    new Double[] { 0.8, 0.2 }, false, 5));
+            CompoundProperty modelSelection =
+                    new CompoundProperty("ModelSelection", "Model selection", "Modeling specific settings", null, false, 300);
+            modelSelection.add(new SelectionProperty("SimulationScale", "Simulation scale", "Level of detail of the simulation",
+                    new String[] { "Micro", "Macro", "Meta" }, 0, true, 0));
+            modelSelection.add(new SelectionProperty("CarFollowingModel", "Car following model", "",
+                    new String[] { "IDM", "IDM+" }, 1, false, 1));
+            modelSelection.add(IDMPropertySet.makeIDMPropertySet("IDMCar", "Car", new Acceleration(1.56, METER_PER_SECOND_2),
+                    new Acceleration(2.09, METER_PER_SECOND_2), new Length(3.0, METER), new Duration(1.2, SECOND), 2));
+            modelSelection
+                    .add(IDMPropertySet.makeIDMPropertySet("IDMTruck", "Truck", new Acceleration(0.75, METER_PER_SECOND_2),
+                            new Acceleration(1.25, METER_PER_SECOND_2), new Length(3.0, METER), new Duration(1.2, SECOND), 3));
+            activeProperties.add(modelSelection);
+            SimpleAnimator sim = simulation.buildAnimator(Time.ZERO, Duration.ZERO, new Duration(3600.0, SECOND),
+                    activeProperties, null, false);
+
+            SwingUtilities.invokeLater(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    simulation.getPanel().getTabbedPane().remove(0);
+                    simulation.getPanel().getTabbedPane().remove(2);
+                }
+            });
+            sim.setSpeedFactor(1.0E9);
+
+            Thread.sleep(5000);
+
+            sim.scheduleEventRel(new Duration(59.999, MINUTE), this, this, "stop", new Object[] { System.currentTimeMillis() });
+            sim.start();
+        }
+        catch (SimRuntimeException | PropertyException | NamingException | OTSSimulationException
+                | InterruptedException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Timer.
+     * @param startTime start time of simulation
+     */
+    protected void stop(final long startTime)
+    {
+        System.out.println("Time of simulation was : " + (System.currentTimeMillis() - startTime) / 1000 + " seconds");
+        System.out.println("Aligned GTU moves      = " + AbstractGTU.ALIGN_COUNT);
+        System.out.println("Cached positions       = " + AbstractLaneBasedGTU.CACHED_POSITION);
+        System.out.println("Non-cached positions   = " + AbstractLaneBasedGTU.NON_CACHED_POSITION);
+        System.exit(0);
+    }
+
+    /**
+     * @param args args should be empty
+     */
+    public static void main(final String[] args)
+    {
+        AbstractGTU.ALIGNED = true;
+        AbstractLaneBasedGTU.CACHING = true;
+
+        new TimeCircularRoadSimulation();
+    }
+
+}
