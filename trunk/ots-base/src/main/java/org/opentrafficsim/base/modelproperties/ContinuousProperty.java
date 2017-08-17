0eeedca435a6cbf8a10731482e3145b5f3121f13
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2017-08-17 09:41:14 +0000

    Minor change to ContinuousProperty to make it more compatible with a ParameterTypeDouble with DoubleBound constraint.

--- trunk/ots-base/src/main/java/org/opentrafficsim/base/modelproperties/ContinuousProperty.java
+++ trunk/ots-base/src/main/java/org/opentrafficsim/base/modelproperties/ContinuousProperty.java
@@ -29,12 +29,18 @@ public class ContinuousProperty extends AbstractProperty<Double> implements Seri
     /** The maximum value of the property. */
     private Double maximumValue;
 
+    /** The minimum value is valid. */
+    private boolean includesLowerBound;
+
+    /** The maximum value is valid. */
+    private boolean includesUpperBound;
+
     /**
-     * Construct a ContinousProperty.
+     * Construct a ContinousProperty that is inclusive of lowerBound and upperBound.
      * @param key String; the unique key of the new property
      * @param shortName String; the short name of the new ContinuousProperty
      * @param description String; description of the new ContinuousProperty (may use HTML mark up)
-     * @param initialValue Double; the initial value of the new ContinuousProperty
+     * @param initialValue Double; the initial value of the new ContinuousProperty (may be out of range)
      * @param minimumValue Double; the minimum value of the new ContinuousProperty
      * @param maximumValue Double; the maximumValue of the new ContinuousProperty
      * @param formatString String; format string to display the value
@@ -46,10 +52,35 @@ public class ContinuousProperty extends AbstractProperty<Double> implements Seri
             final Double minimumValue, final Double maximumValue, final String formatString, final boolean readOnly,
             final int displayPriority)
     {
+        this(key, shortName, description, initialValue, minimumValue, maximumValue, true, true, formatString, readOnly,
+                displayPriority);
+    }
+
+    /**
+     * Construct a ContinousProperty.
+     * @param key String; the unique key of the new property
+     * @param shortName String; the short name of the new ContinuousProperty
+     * @param description String; description of the new ContinuousProperty (may use HTML mark up)
+     * @param initialValue Double; the initial value of the new ContinuousProperty (may be out of range)
+     * @param minimumValue Double; the minimum value of the new ContinuousProperty
+     * @param maximumValue Double; the maximumValue of the new ContinuousProperty
+     * @param includesLowerBound boolean; if true, the minimumValue is valid; if false, the minimumValue is not valid
+     * @param includesUpperBound boolean; if true, the maximumValue is valid; if false, the maximumValue is not valid
+     * @param formatString String; format string to display the value
+     * @param readOnly boolean; if true this ContinuousProperty can not be altered
+     * @param displayPriority int; the displayPriority of the new ContinuousProperty
+     */
+    @SuppressWarnings("checkstyle:parameternumber")
+    public ContinuousProperty(final String key, final String shortName, final String description, final Double initialValue,
+            final Double minimumValue, final Double maximumValue, final boolean includesLowerBound,
+            final boolean includesUpperBound, final String formatString, final boolean readOnly, final int displayPriority)
+    {
         super(key, displayPriority, shortName, description);
         this.value = initialValue;
         this.minimumValue = minimumValue;
         this.maximumValue = maximumValue;
+        this.includesLowerBound = includesLowerBound;
+        this.includesUpperBound = includesUpperBound;
         this.format = formatString;
         setReadOnly(readOnly);
     }
@@ -85,7 +116,13 @@ public class ContinuousProperty extends AbstractProperty<Double> implements Seri
         {
             throw new PropertyException("This property is read-only");
         }
-        if (this.minimumValue > newValue || this.maximumValue < newValue)
+        // @formatter:off
+        if (this.minimumValue > newValue || this.maximumValue < newValue
+                || this.minimumValue == newValue && (!this.includesLowerBound) && this.minimumValue != this.maximumValue 
+                || this.maximumValue == newValue && (!this.includesUpperBound) && this.minimumValue != this.maximumValue 
+                || this.minimumValue == newValue && this.maximumValue == newValue && (!this.includesLowerBound) 
+                && (! this.includesUpperBound))
+        // @formatter:on
         {
             throw new PropertyException("new value " + newValue + " is out of valid range (" + this.minimumValue + ".."
                     + this.maximumValue + ")");
@@ -114,6 +151,24 @@ public class ContinuousProperty extends AbstractProperty<Double> implements Seri
     {
         return new ContinuousProperty(getKey(), getShortName(), getDescription(), this.value, this.minimumValue,
                 this.maximumValue, this.format, isReadOnly(), getDisplayPriority());
+    }
+
+    /**
+     * Is the range inclusive of the minimumValue?
+     * @return boolean; true if the range is inclusive of the minimumValue
+     */
+    public final boolean includesLowerBound()
+    {
+        return this.includesLowerBound;
+    }
+
+    /**
+     * Is the range inclusive of the maximumValue?
+     * @return boolean; true if the range is inclusive of the maximumValue
+     */
+    public final boolean includesUpperBound()
+    {
+        return this.includesUpperBound;
     }
 
 }
