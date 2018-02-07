ccac7ea6eadffc6a3f95176af2cd5f5b6c514b05
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2018-02-07 11:11:12 +0000

    Updated to use size prefixed proto buf messages

--- trunk/ots-aimsun-demo/src/main/java/org/opentrafficsim/demo/TestController.java
+++ trunk/ots-aimsun-demo/src/main/java/org/opentrafficsim/demo/TestController.java
@@ -6,13 +6,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
-import nl.tudelft.simulation.language.io.URLResource;
-
 import org.opentrafficsim.aimsun.proto.AimsunControlProtoBuf;
+
+import nl.tudelft.simulation.language.io.URLResource;
 
 /**
  * Test client for AimsunController.
@@ -86,54 +87,47 @@ public final class TestController
             System.err.println("Missing required argument(s) ip=<ip-number_or_hostname> port=<port-number>");
             System.exit(1);
         }
-        // Socket to talk to server
-        System.out.println("Connecting to server...");
-        Socket socket = new Socket(ip, port);
-        System.out.println("Connected");
-        OutputStream outputStream = socket.getOutputStream();
-        InputStream inputStream = socket.getInputStream();
-        // Send a build network command
-        System.out.println("Sending 1st test message");
+        // Construct the create simulation command (inclusing the network description in XML
         AimsunControlProtoBuf.CreateSimulation.Builder createSimulationBuilder =
                 AimsunControlProtoBuf.CreateSimulation.newBuilder();
         createSimulationBuilder.setRunTime(3600d);
         createSimulationBuilder.setWarmUpTime(0d);
         // String network = URLResource.getResource("/aimsun/singleRoad.xml").toString(); // wrong; fix later
-        String network = null; // IOUtils.toString(URLResource.getResource("/aimsun/singleRoad.xml"));
-        URLConnection conn = URLResource.getResource("/aimsun/singleRoad.xml").openConnection();
+        String networkResource = "/aimsun/singleRoad.xml";
+        String network = null; // IOUtils.toString(URLResource.getResource(networkResource));
+        URL networkURL = URLResource.getResource(networkResource);
+        if (null == networkURL)
+        {
+            throw new Error("Could not load network from resource " + networkResource);
+        }
+        URLConnection conn = networkURL.openConnection();
         try (BufferedReader reader =
                 new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8)))
         {
             network = reader.lines().collect(Collectors.joining("\n"));
         }
 
+        // Socket to talk to server
+        System.out.println("Connecting to server...");
+        Socket socket = new Socket(ip, port);
+        System.out.println("Connected");
+        OutputStream outputStream = socket.getOutputStream();
+        InputStream inputStream = socket.getInputStream();
+        // Send a create simulation command
         createSimulationBuilder.setNetworkXML(network);
-        AimsunControlProtoBuf.OTSMessage command =
-                AimsunControlProtoBuf.OTSMessage.newBuilder().setCreateSimulation(createSimulationBuilder.build()).build();
-        // for (byte b : command.toByteArray())
-        // {
-        // System.out.print(String.format("%02x ", b));
-        // }
-        // System.out.println("");
-        command.writeDelimitedTo(outputStream);
-        // Send a simulate until command with time 0 to retrieve the initial gtu positions
-        System.out.println("Sending simulate until 0 command to retrieve the initial GTU positions");
-        AimsunControlProtoBuf.SimulateUntil simulateUntil =
-                AimsunControlProtoBuf.SimulateUntil.newBuilder().setTime(0d).build();
-        command = AimsunControlProtoBuf.OTSMessage.newBuilder().setSimulateUntil(simulateUntil).build();
-        command.writeDelimitedTo(outputStream);
-        // Receive a reply
-        System.out.println("Waiting for / reading reply");
-        AimsunControlProtoBuf.OTSMessage reply = AimsunControlProtoBuf.OTSMessage.parseDelimitedFrom(inputStream);
-        System.out.println("Received " + reply);
-        for (int step = 1; step <= 20; step++)
+        System.out.println("Sending CREATESIMULATION message");
+        sendProtoMessage(outputStream,
+                AimsunControlProtoBuf.OTSMessage.newBuilder().setCreateSimulation(createSimulationBuilder.build()).build());
+        // Simulate 10 seconds in 0.5 second steps
+        for (int step = 0; step <= 20; step++)
         {
-            simulateUntil = AimsunControlProtoBuf.SimulateUntil.newBuilder().setTime(0.5d * step).build();
-            System.out.println("Simulate step " + step);
-            command = AimsunControlProtoBuf.OTSMessage.newBuilder().setSimulateUntil(simulateUntil).build();
-            command.writeDelimitedTo(outputStream);
-            System.out.println("Waiting for / reading reply");
-            reply = AimsunControlProtoBuf.OTSMessage.parseDelimitedFrom(inputStream);
+            AimsunControlProtoBuf.SimulateUntil simulateUntil =
+                    AimsunControlProtoBuf.SimulateUntil.newBuilder().setTime(0.5d * step).build();
+            System.out.println("Sending simulate up to step " + step + " command");
+            sendProtoMessage(outputStream, AimsunControlProtoBuf.OTSMessage.newBuilder().setSimulateUntil(simulateUntil)
+                    .build());
+            System.out.println("Receive reply");
+            AimsunControlProtoBuf.OTSMessage reply = receiveProtoMessage(inputStream);
             System.out.println("Received " + reply);
         }
         try
@@ -146,4 +140,88 @@ public final class TestController
         }
         socket.close();
     }
+
+    /**
+     * Transit one message to the OTS server.
+     * @param outputStream OutputStream; output stream to the OTS server
+     * @param message AimsunControlProtobuf.OTSMessage; the message
+     * @throws IOException when communication fails in any way
+     */
+    public static void sendProtoMessage(final OutputStream outputStream, final AimsunControlProtoBuf.OTSMessage message)
+            throws IOException
+    {
+        int size = message.getSerializedSize();
+        // System.out.println("About to transmit message of " + size + " bytes");
+        byte[] sizeBytes = new byte[4];
+        sizeBytes[0] = (byte) ((size >> 24) & 0xff);
+        sizeBytes[1] = (byte) ((size >> 16) & 0xff);
+        sizeBytes[2] = (byte) ((size >> 8) & 0xff);
+        sizeBytes[3] = (byte) (size & 0xff);
+        outputStream.write(sizeBytes);
+        byte[] buffer = new byte[size];
+        buffer = message.toByteArray();
+        outputStream.write(buffer);
+        // System.out.println("Done");
+    }
+
+    /**
+     * Read one OTSMessage.
+     * @param inputStream InputStream; input stream to read from
+     * @return OTSMessage; the OTSMessage that was constructed from the read bytes
+     * @throws IOException when communication fails
+     */
+    public static AimsunControlProtoBuf.OTSMessage receiveProtoMessage(final InputStream inputStream) throws IOException
+    {
+        byte[] sizeBytes = receiveBytes(inputStream, 4);
+        // for (int i = 0; i < 4; i++)
+        // {
+        // System.out.print(String.format("%d ", sizeBytes[i]));
+        // }
+        int size =
+                ((sizeBytes[0] & 0xff) << 24) + ((sizeBytes[1] & 0xff) << 16) + ((sizeBytes[2] & 0xff) << 8)
+                        + (sizeBytes[3] & 0xff);
+        // System.out.println(String.format("-> %d", size));
+        byte[] messageBytes = receiveBytes(inputStream, size);
+        return AimsunControlProtoBuf.OTSMessage.parseFrom(messageBytes);
+    }
+
+    /**
+     * Read a specified number of bytes.
+     * @param inputStream InputStream; input stream to read from
+     * @param size int; number of bytes to read
+     * @return byte[]; byte array filled with the read bytes
+     * @throws IOException when communication fails
+     */
+    public static byte[] receiveBytes(final InputStream inputStream, final int size) throws IOException
+    {
+        System.out.print("Need to read " + size + " bytes ... ");
+        int offset = 0;
+        byte[] buffer = new byte[size];
+        while (true)
+        {
+            int bytesRead = inputStream.read(buffer, offset, buffer.length - offset);
+            if (-1 == bytesRead)
+            {
+                break;
+            }
+            offset += bytesRead;
+            if (buffer.length == offset)
+            {
+                System.out.println("Got all " + buffer.length + " requested bytes");
+                break;
+            }
+            if (buffer.length < offset)
+            {
+                System.out.println("Oops: Got more than " + buffer.length + " requested bytes");
+                break;
+            }
+            System.out.print("Now got " + offset + " bytes; need to read " + (buffer.length - offset) + " more bytes ");
+        }
+        if (offset != buffer.length)
+        {
+            throw new IOException("Got only " + offset + " of expected " + buffer.length + " bytes");
+        }
+        return buffer;
+    }
+
 }
