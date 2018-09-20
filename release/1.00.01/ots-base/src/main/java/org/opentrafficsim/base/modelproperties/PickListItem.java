14f76fa8d1966b81a4d03e6cfcb68b92e15174b9
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2018-09-20 00:41:51 +0000

    deployed version 1.00.01

---
+++ release/1.00.01/ots-base/src/main/java/org/opentrafficsim/base/modelproperties/PickListItem.java
@@ -0,0 +1,84 @@
+package org.opentrafficsim.base.modelproperties;
+
+/**
+ * Value that can be used in a PickList.
+ * <p>
+ * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Aug 14, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ * @param <T> type of the pick ids (usually String or Integer)
+ */
+public class PickListItem<T>
+{
+    /** Id of this pick list item. */
+    private final T id;
+
+    /** Display text of this pick list item. */
+    private final String displayText;
+
+    /** Description (may use HTML formatting tags). */
+    private final String description;
+
+    /**
+     * Construct a new PickListItem.
+     * @param id T; the id of the new PickListItem
+     * @param displayText String; the text to show to the user in the pick list
+     * @param description String; the description of this pick list item (may use HTML formatting)
+     */
+    public PickListItem(final T id, final String displayText, final String description)
+    {
+        this.id = id;
+        this.displayText = displayText;
+        this.description = description;
+    }
+
+    /**
+     * Construct a new PickListItem.
+     * @param id T; the id of the new PickListItem
+     * @param displayText String; the text to show to the user in the pick list; also used for the description
+     */
+    public PickListItem(final T id, final String displayText)
+    {
+        this(id, displayText, displayText);
+    }
+
+    /**
+     * Retrieve the id of this PickListItem.
+     * @return T; the id of this PickListItem
+     */
+    public final T getId()
+    {
+        return this.id;
+    }
+
+    /**
+     * Retrieve the display text of this PickListItem.
+     * @return String; the display text of this PickListItem
+     */
+    public final String getDisplayText()
+    {
+        return this.displayText;
+    }
+
+    /**
+     * Retrieve the description of this PickListItem.
+     * @return String; the description of this PickListItem
+     */
+    public final String getDescription()
+    {
+        return this.description;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public String toString()
+    {
+        return "PickListItem [id=" + this.id + ", displayText=" + this.displayText + ", description=" + this.description + "]";
+    }
+
+}
---
+++ release/1.00.01/ots-base/src/test/java/org/opentrafficsim/base/parameters/PickTest.java
@@ -0,0 +1,78 @@
+package org.opentrafficsim.base.parameters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opentrafficsim.base.modelproperties.PickList;
+import org.opentrafficsim.base.modelproperties.PickListItem;
+
+/**
+ * Test the PickList class and closely related classes.
+ * <p>
+ * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Aug 16, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class PickTest
+{
+
+    /**
+     * Test the PickList class.
+     * @throws ParameterException if this happens uncaught; this test has failed
+     */
+    // TODO: The testPickList unit test giver errors!!
+    // @Test
+    public final void testPickList() throws ParameterException
+    {
+        PickListItem<String> item1 = new PickListItem<>("id1", "description1", "long description 1");
+        assertEquals("can retrieve id", "id1", item1.getId());
+        assertEquals("can retrieve display text", "description1", item1.getDisplayText());
+        assertEquals("can retrieve description", "long description 1", item1.getDescription());
+        assertTrue("toString returns something with PickListItem in it", item1.toString().indexOf("PickListItem") >= 0);
+        PickList<String> pl = new PickList<>("id", "description", item1);
+        assertEquals("can retrieve id", "id", pl.getId());
+        assertEquals("can retrieve description", "description", pl.getDescription());
+        assertTrue("toString returns something with PickList in it", pl.toString().indexOf("PickList") >= 0);
+        PickListItem<String> item2 = new PickListItem<>("id1", "description2"); // Same id!
+        assertEquals("using 2 arg constructor sets description to display text", "description2", item2.getDisplayText());
+        assertEquals("using 2 arg constructor sets description to display text", "description2", item2.getDescription());
+        try
+        {
+            pl.addItem(item2);
+        }
+        catch (ParameterException pe)
+        {
+            // Ignore expected exception
+        }
+        item2 = new PickListItem<>("id2", "description2", "long description 2");
+        pl.addItem(item2); // should not throw an exception
+        String constraintDescription = pl.getConstraint().toString();
+        assertNotNull("constraint toString may not be null", constraintDescription);
+        assertTrue("constraint toString contains id1", constraintDescription.indexOf("id1") >= 0);
+        assertTrue("constraint toString contains id2", constraintDescription.indexOf("id2") >= 0);
+
+        pl = new PickList<>("id", "description", item1, item2);
+        List<PickListItem<String>> list = new ArrayList<>();
+        try
+        {
+            new PickList<>("idXX", "descriptionXX", list);
+        }
+        catch (ParameterException pe)
+        {
+            // Ignore expected exception
+        }
+        list.add(item1);
+        pl = new PickList<>("idXX", "descriptionXX", list);
+        assertEquals("can retrieve id", "idXX", pl.getId());
+        assertEquals("can retrieve description", "descriptionXX", pl.getDescription());
+        assertTrue("toString returns something with PickList in it", pl.toString().indexOf("PickList") >= 0);
+    }
+}
