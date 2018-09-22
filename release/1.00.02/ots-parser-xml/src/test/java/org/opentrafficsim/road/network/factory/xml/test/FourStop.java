e8890444794f07633d2eb368e0f83b401ef7862c
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2018-09-22 21:39:48 +0000

    deployed version 1.00.02

---
+++ release/1.00.02/ots-parser-xml/src/test/java/org/opentrafficsim/road/network/factory/xml/test/FourStop.java
@@ -0,0 +1,198 @@
+package org.opentrafficsim.road.network.factory.xml.test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.naming.NamingException;
+import javax.swing.SwingUtilities;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.djunits.unit.DurationUnit;
+import org.djunits.value.ValueException;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.modelproperties.Property;
+import org.opentrafficsim.base.modelproperties.PropertyException;
+import org.opentrafficsim.base.parameters.ParameterException;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.geometry.OTSGeometryException;
+import org.opentrafficsim.core.gtu.GTUException;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.road.animation.AnimationToggles;
+import org.opentrafficsim.road.network.factory.xml.XmlNetworkLaneParser;
+import org.opentrafficsim.simulationengine.AbstractWrappableAnimation;
+import org.opentrafficsim.simulationengine.OTSSimulationException;
+import org.opentrafficsim.simulationengine.SimpleSimulatorInterface;
+import org.xml.sax.SAXException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDoubleUnit;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.language.io.URLResource;
+
+/**
+ * Four stop demo
+ * <p>
+ * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Dec 1, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author Wouter Schakel
+ */
+public class FourStop extends AbstractWrappableAnimation
+{
+    /**
+     * Main program.
+     * @param args String[]; the command line arguments (not used)
+     * @throws SimRuntimeException should never happen
+     */
+    public static void main(final String[] args) throws SimRuntimeException
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    FourStop xmlModel = new FourStop();
+                    // 1 hour simulation run for testing
+                    xmlModel.buildAnimator(Time.ZERO, Duration.ZERO, new Duration(60.0, DurationUnit.MINUTE),
+                            new ArrayList<Property<?>>(), null, true);
+                }
+                catch (SimRuntimeException | NamingException | OTSSimulationException | PropertyException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void addTabs(SimpleSimulatorInterface simulator) throws OTSSimulationException, PropertyException
+    {
+        super.addTabs(simulator);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void addAnimationToggles()
+    {
+        // hideAnimationClass(OTSLink.class);
+        // hideAnimationClass(Shoulder.class);
+
+        // addToggleAnimationButton("Link", OTSLink.class, "Show/hide Links", false);
+        // addToggleAnimationButton("Shoulder", Shoulder.class, "Show/hide Shoulders", true);
+        // addToggleAnimationButton("Lane", Lane.class, "Show/hide Lanes", true);
+
+        AnimationToggles.setTextAnimationTogglesStandard(this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String shortName()
+    {
+        return "TestXMLModel";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String description()
+    {
+        return "TestXMLModel";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void stopTimersThreads()
+    {
+        super.stopTimersThreads();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final OTSModelInterface makeModel()
+    {
+        return new TestXMLModel();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "FourStop []";
+    }
+
+    /**
+     * Model to test the XML parser.
+     * <p>
+     * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. <br>
+     * All rights reserved. BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim
+     * License</a>.
+     * <p>
+     * $LastChangedDate: 2015-09-14 01:33:02 +0200 (Mon, 14 Sep 2015) $, @version $Revision: 1401 $, by $Author: averbraeck $,
+     * initial version un 27, 2015 <br>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+     */
+    class TestXMLModel implements OTSModelInterface
+    {
+        /** */
+        private static final long serialVersionUID = 20141121L;
+
+        /** The simulator. */
+        private DEVSSimulatorInterface.TimeDoubleUnit simulator;
+
+        /** the network. */
+        private OTSNetwork network = null;
+
+        /** {@inheritDoc} */
+        @Override
+        public final void constructModel(final SimulatorInterface<Time, Duration, SimTimeDoubleUnit> pSimulator)
+                throws SimRuntimeException
+        {
+            this.simulator = (DEVSSimulatorInterface.TimeDoubleUnit) pSimulator;
+            URL url = URLResource.getResource("/4-stop-3-1.xml");
+            XmlNetworkLaneParser nlp = new XmlNetworkLaneParser(this.simulator);
+            try
+            {
+                this.network = nlp.build(url, true);
+            }
+            catch (NetworkException | ParserConfigurationException | SAXException | IOException | NamingException | GTUException
+                    | OTSGeometryException | ValueException | ParameterException exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public SimulatorInterface<Time, Duration, SimTimeDoubleUnit> getSimulator()
+
+        {
+            return this.simulator;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public OTSNetwork getNetwork()
+        {
+            return this.network;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final String toString()
+        {
+            return "TestXMLModel [simulator=" + this.simulator + "]";
+        }
+
+    }
+
+}
---
+++ release/1.00.02/ots-parser-xml/src/test/java/org/opentrafficsim/road/network/factory/xml/test/LMRSTests.java
@@ -0,0 +1,198 @@
+package org.opentrafficsim.road.network.factory.xml.test;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.naming.NamingException;
+import javax.swing.SwingUtilities;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.djunits.unit.DurationUnit;
+import org.djunits.value.ValueException;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.modelproperties.Property;
+import org.opentrafficsim.base.modelproperties.PropertyException;
+import org.opentrafficsim.base.parameters.ParameterException;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.geometry.OTSGeometryException;
+import org.opentrafficsim.core.gtu.GTUException;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.opentrafficsim.road.animation.AnimationToggles;
+import org.opentrafficsim.road.network.factory.xml.XmlNetworkLaneParser;
+import org.opentrafficsim.simulationengine.AbstractWrappableAnimation;
+import org.opentrafficsim.simulationengine.OTSSimulationException;
+import org.xml.sax.SAXException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDoubleUnit;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
+import nl.tudelft.simulation.language.io.URLResource;
+
+/**
+ * Four stop demo
+ * <p>
+ * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Dec 1, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author Wouter Schakel
+ */
+public class LMRSTests extends AbstractWrappableAnimation
+{
+    /**
+     * Main program.
+     * @param args String[]; the command line arguments (not used)
+     * @throws SimRuntimeException should never happen
+     */
+    public static void main(final String[] args) throws SimRuntimeException
+    {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    LMRSTests xmlModel = new LMRSTests();
+                    // 1 hour simulation run for testing
+                    xmlModel.buildAnimator(Time.ZERO, Duration.ZERO, new Duration(60.0, DurationUnit.MINUTE),
+                            new ArrayList<Property<?>>(), null, true);
+                }
+                catch (SimRuntimeException | NamingException | OTSSimulationException | PropertyException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String shortName()
+    {
+        return "TestXMLModel";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String description()
+    {
+        return "TestXMLModel";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void stopTimersThreads()
+    {
+        super.stopTimersThreads();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final OTSModelInterface makeModel()
+    {
+        return new TestXMLModel();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void addAnimationToggles()
+    {
+        AnimationToggles.setTextAnimationTogglesStandard(this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Double makeAnimationRectangle()
+    {
+        return new Rectangle2D.Double(162000, 384500, 2000, 2000);
+        // return new Rectangle2D.Double(-100, -100, 3200, 200);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "LMRSTests []";
+    }
+
+    /**
+     * Model to test the XML parser.
+     * <p>
+     * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. <br>
+     * All rights reserved. BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim
+     * License</a>.
+     * <p>
+     * $LastChangedDate: 2015-09-14 01:33:02 +0200 (Mon, 14 Sep 2015) $, @version $Revision: 1401 $, by $Author: averbraeck $,
+     * initial version un 27, 2015 <br>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+     */
+    class TestXMLModel implements OTSModelInterface
+    {
+        /** */
+        private static final long serialVersionUID = 20141121L;
+
+        /** The simulator. */
+        private DEVSSimulatorInterface.TimeDoubleUnit simulator;
+
+        /** the network. */
+        private OTSNetwork network;
+
+        /** {@inheritDoc} */
+        @Override
+        public final void constructModel(final SimulatorInterface<Time, Duration, SimTimeDoubleUnit> pSimulator)
+                throws SimRuntimeException
+        {
+            this.simulator = (DEVSSimulatorInterface.TimeDoubleUnit) pSimulator;
+            // URL url = URLResource.getResource("/LMRSStraightTest.xml");
+            // URL url = URLResource.getResource("/LMRSLaneDropTest.xml");
+            // URL url = URLResource.getResource("/LMRSOnRampTest.xml");
+            // URL url = URLResource.getResource("/LMRSOnRampTaperTest.xml");
+            // URL url = URLResource.getResource("/networkv2_90km_V5.xml");
+            URL url = URLResource.getResource("/testEindhovenGenerator4.xml");
+            XmlNetworkLaneParser nlp = new XmlNetworkLaneParser(this.simulator);
+            try
+            {
+                this.network = nlp.build(url, true);
+
+            }
+            catch (NetworkException | ParserConfigurationException | SAXException | IOException | NamingException | GTUException
+                    | OTSGeometryException | ValueException | ParameterException exception)
+            {
+                exception.printStackTrace();
+            }
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public SimulatorInterface<Time, Duration, SimTimeDoubleUnit> getSimulator()
+        {
+            return this.simulator;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public OTSNetwork getNetwork()
+        {
+            return this.network;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public final String toString()
+        {
+            return "TestXMLModel [simulator=" + this.simulator + "]";
+        }
+
+    }
+
+}
