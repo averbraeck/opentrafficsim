a5dde769b38e32cc8936cfe9c86f8826be35b9f6
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2018-10-11 21:11:02 +0000

    A program called ParamComments has been added in ots-build-tools to help enter/repair the correct types in the @param entries in the method comments. All classes have been repaired using this program. Now the constructors have also been repaired.

--- ots-build-tools/src/main/java/org/opentrafficsim/package-info.java
+++ ots-build-tools/src/main/java/org/opentrafficsim/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Utility to clean the .classfile files for all projects; the excludes=** will be removed. <br>
+ * Utility to clean the .classfile files for all projects; the excludes=** will be removed, and utilities to include types in
+ * the javadoc for methods and constructors. <br>
  * <br>
  * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
