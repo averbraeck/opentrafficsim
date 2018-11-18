f6971e99c1437e438379392dc6e56bd45352916d
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2018-11-18 20:51:10 +0000

    DrawingInfo for animation, but not specific to Swing or any other drawing scheme.

--- ots-core/src/main/java/org/opentrafficsim/core/animation/DrawingInfoLine.java
+++ ots-core/src/main/java/org/opentrafficsim/core/animation/DrawingInfoLine.java
@@ -11,14 +11,64 @@ import java.awt.Color;
  * source code and binary code of this software is proprietary information of Delft University of Technology.
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
  */
-public class DrawingInfoLine
+public class DrawingInfoLine implements DrawingInfo
 {
     /** lineColor. */
-    @SuppressWarnings("checkstyle:visibilitymodifier")
-    public Color lineColor = Color.BLACK;
-    
-    /** lineWidth, to be interpreted by the animation package; could be relative. */
-    @SuppressWarnings("checkstyle:visibilitymodifier")
-    public float lineWidth = 1.0f;
-}
+    private Color lineColor = Color.BLACK;
 
+    /** lineWidth, to be interpreted by the animation package; could be relative. */
+    private float lineWidth = 1.0f;
+
+    /**
+     * Create an empty DrawingInfo object for a line.
+     */
+    public DrawingInfoLine()
+    {
+        super();
+    }
+    
+    /**
+     * Create a DrawingInfo object for a line.
+     * @param lineColor Color; the line color
+     * @param lineWidth float; the line width, which could be relative and is open for interpretation by the visualization or
+     *            animation class
+     */
+    public DrawingInfoLine(final Color lineColor, final float lineWidth)
+    {
+        super();
+        this.lineColor = lineColor;
+        this.lineWidth = lineWidth;
+    }
+
+    /**
+     * @return lineColor
+     */
+    public final Color getLineColor()
+    {
+        return this.lineColor;
+    }
+
+    /**
+     * @param lineColor set lineColor
+     */
+    public final void setLineColor(final Color lineColor)
+    {
+        this.lineColor = lineColor;
+    }
+
+    /**
+     * @return lineWidth
+     */
+    public final float getLineWidth()
+    {
+        return this.lineWidth;
+    }
+
+    /**
+     * @param lineWidth set lineWidth
+     */
+    public final void setLineWidth(final float lineWidth)
+    {
+        this.lineWidth = lineWidth;
+    }
+}
--- ots-core/src/main/java/org/opentrafficsim/core/animation/DrawingInfoShape.java
+++ ots-core/src/main/java/org/opentrafficsim/core/animation/DrawingInfoShape.java
@@ -11,21 +11,153 @@ import java.awt.Color;
  * source code and binary code of this software is proprietary information of Delft University of Technology.
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
  */
-public class DrawingInfoShape
+public class DrawingInfoShape implements DrawingInfo
 {
     /** fillColor. */
-    @SuppressWarnings("checkstyle:visibilitymodifier")
-    public Color fillColor = Color.GRAY;
+    private Color fillColor = Color.GRAY;
 
     /** lineColor. */
-    @SuppressWarnings("checkstyle:visibilitymodifier")
-    public Color lineColor = Color.BLACK;
+    private Color lineColor = Color.BLACK;
 
     /** lineWidth, to be interpreted by the animation package; could be relative. */
-    @SuppressWarnings("checkstyle:visibilitymodifier")
-    public float lineWidth = 1.0f;
+    private float lineWidth = 1.0f;
 
     /** fill? */
-    @SuppressWarnings("checkstyle:visibilitymodifier")
-    public boolean fill = true;
+    private boolean filled = true;
+
+    /** draw contour line? */
+    private boolean stroked = true;
+
+    /**
+     * Create an empty DrawingInfo object for a shape.
+     */
+    public DrawingInfoShape()
+    {
+        super();
+    }
+
+    /**
+     * Create a DrawingInfo object for a shape that is not filled.
+     * @param lineColor Color; the line color
+     * @param lineWidth float; the line width, which could be relative and is open for interpretation by the visualization or
+     *            animation class
+     */
+    public DrawingInfoShape(final Color lineColor, final float lineWidth)
+    {
+        super();
+        this.lineColor = lineColor;
+        this.lineWidth = lineWidth;
+        this.filled = false;
+        this.stroked = true;
+    }
+
+    /**
+     * Create a DrawingInfo object for a shape that is filled, with a contour line.
+     * @param lineColor Color; the line color
+     * @param lineWidth float; the line width, which could be relative and is open for interpretation by the visualization or
+     *            animation class
+     * @param fillColor Color; the fill color
+     */
+    public DrawingInfoShape(final Color lineColor, final float lineWidth, final Color fillColor)
+    {
+        super();
+        this.lineColor = lineColor;
+        this.lineWidth = lineWidth;
+        this.fillColor = fillColor;
+        this.filled = true;
+        this.stroked = true;
+    }
+
+    /**
+     * Create a DrawingInfo object for a shape that is filled, without a contour line.
+     * @param fillColor Color; the fill color
+     */
+    public DrawingInfoShape(final Color fillColor)
+    {
+        super();
+        this.fillColor = fillColor;
+        this.filled = true;
+        this.stroked = false;
+    }
+
+    /**
+     * @return fillColor
+     */
+    public final Color getFillColor()
+    {
+        return this.fillColor;
+    }
+
+    /**
+     * @param fillColor set fillColor
+     */
+    public final void setFillColor(final Color fillColor)
+    {
+        this.fillColor = fillColor;
+    }
+
+    /**
+     * @return lineColor
+     */
+    public final Color getLineColor()
+    {
+        return this.lineColor;
+    }
+
+    /**
+     * @param lineColor set lineColor
+     */
+    public final void setLineColor(final Color lineColor)
+    {
+        this.lineColor = lineColor;
+    }
+
+    /**
+     * @return lineWidth
+     */
+    public final float getLineWidth()
+    {
+        return this.lineWidth;
+    }
+
+    /**
+     * @param lineWidth set lineWidth
+     */
+    public final void setLineWidth(final float lineWidth)
+    {
+        this.lineWidth = lineWidth;
+    }
+
+    /**
+     * @return filled
+     */
+    public final boolean isFilled()
+    {
+        return this.filled;
+    }
+
+    /**
+     * @param filled set filled
+     */
+    public final void setFilled(final boolean filled)
+    {
+        this.filled = filled;
+    }
+
+    /**
+     * @return stroked
+     */
+    public final boolean isStroked()
+    {
+        return this.stroked;
+    }
+
+    /**
+     * @param stroked set stroked
+     */
+    public final void setStroked(final boolean stroked)
+    {
+        this.stroked = stroked;
+    }
+
 }
