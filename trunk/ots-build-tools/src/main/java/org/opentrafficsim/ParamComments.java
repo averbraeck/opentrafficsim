ab2527e6df399b1a74baca8dc6033702a220fb8c
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-01-06 00:32:52 +0000

    (c) 2019 instead of 2018.

--- trunk/ots-build-tools/src/main/java/org/opentrafficsim/ParamComments.java
+++ trunk/ots-build-tools/src/main/java/org/opentrafficsim/ParamComments.java
@@ -22,7 +22,7 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
  * Utility to add or update the type foe each parameter in the javadoc of all java files in /src/main/java in all or in selected
  * projects in the workspace. Run this utility only from Eclipse!<br>
  * <br>
- * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2003-2019 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
  * source code and binary code of this software is proprietary information of Delft University of Technology.
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
