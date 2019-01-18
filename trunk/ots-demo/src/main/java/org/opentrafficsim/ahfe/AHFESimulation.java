96dda69c62f58c0d59022ba6f89ca4576e61ab2f
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-01-18 01:30:14 +0000

    Repaired AHFESimulation and AHFEAnimation w.r.t. DSOL 3.03.

--- trunk/ots-demo/src/main/java/org/opentrafficsim/ahfe/AHFESimulation.java
+++ trunk/ots-demo/src/main/java/org/opentrafficsim/ahfe/AHFESimulation.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -20,7 +19,9 @@ import org.djunits.value.vdouble.scalar.Time;
 import org.djutils.exceptions.Throw;
 import org.djutils.io.URLResource;
 import org.opentrafficsim.core.dsol.AbstractOTSModel;
-import org.opentrafficsim.core.dsol.OTSSimulationException;
+import org.opentrafficsim.core.dsol.AbstractOTSSimulationApplication;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
+import org.opentrafficsim.core.dsol.OTSSimulator;
 import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
 import org.opentrafficsim.core.gtu.AbstractGTU;
 import org.opentrafficsim.core.network.OTSNetwork;
@@ -35,11 +36,8 @@ import org.opentrafficsim.road.network.sampling.GtuData;
 import org.opentrafficsim.road.network.sampling.LinkData;
 import org.opentrafficsim.road.network.sampling.RoadSampler;
 import org.opentrafficsim.road.network.sampling.data.TimeToCollision;
-import org.opentrafficsim.simulationengine.AbstractWrappableSimulation;
 
 import nl.tudelft.simulation.dsol.SimRuntimeException;
-import nl.tudelft.simulation.dsol.model.inputparameters.InputParameter;
-import nl.tudelft.simulation.dsol.model.inputparameters.InputParameterException;
 
 /**
  * Simulation for AHFE congress.
@@ -52,7 +50,7 @@ import nl.tudelft.simulation.dsol.model.inputparameters.InputParameterException;
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
  */
-public class AHFESimulation extends AbstractWrappableSimulation
+public class AHFESimulation extends AbstractOTSSimulationApplication
 {
     /** Warm-up time. */
     static final Time WARMUP = Time.createSI(360);
@@ -69,160 +67,12 @@ public class AHFESimulation extends AbstractWrappableSimulation
     /** */
     private static final long serialVersionUID = 20170228L;
 
-    /** Replication. */
-    private final Integer replication;
-
-    /** Anticipation strategy. */
-    private final String anticipationStrategy;
-
-    /** Reaction time. */
-    private final Duration reactionTime;
-
-    /** Future anticipation time. */
-    private final Duration anticipationTime;
-
-    /** Truck fraction. */
-    private final double truckFraction;
-
-    /** Distance error. */
-    private final double distanceError;
-
-    /** Speed error. */
-    private final double speedError;
-
-    /** Acceleration error. */
-    private final double accelerationError;
-
-    /** Left demand. */
-    private final Frequency leftDemand;
-
-    /** Right demand. */
-    private final Frequency rightDemand;
-
-    /** Left fraction, per road. */
-    private final double leftFraction;
-
-    /** Sampler. */
-    Sampler<GtuData> sampler;
-
     /**
-     * @param replication Integer; replication
-     * @param anticipationStrategy String; anticipation strategy
-     * @param reactionTime Duration; reaction time
-     * @param anticipationTime Duration; anticipation time
-     * @param truckFraction double; truck fraction
-     * @param distanceError double; distance error
-     * @param speedError double; speed error
-     * @param accelerationError double; acceleration error
-     * @param leftFraction double; left demand
-     * @param rightDemand Frequency; right demand
-     * @param leftDemand Frequency; left fraction, per road
+     * @param model OTSModelInterface; the model
      */
-    @SuppressWarnings("checkstyle:parameternumber")
-    public AHFESimulation(final Integer replication, final String anticipationStrategy, final Duration reactionTime,
-            final Duration anticipationTime, final double truckFraction, final double distanceError, final double speedError,
-            final double accelerationError, final Frequency leftDemand, final Frequency rightDemand, final double leftFraction)
+    public AHFESimulation(final OTSModelInterface model)
     {
-        super();
-        this.replication = replication;
-        this.anticipationStrategy = anticipationStrategy;
-        this.reactionTime = reactionTime;
-        this.anticipationTime = anticipationTime;
-        this.truckFraction = truckFraction;
-        this.distanceError = distanceError;
-        this.speedError = speedError;
-        this.accelerationError = accelerationError;
-        this.leftDemand = leftDemand;
-        this.rightDemand = rightDemand;
-        this.leftFraction = leftFraction;
-    }
-
-    /**
-     * @return replication.
-     */
-    public Integer getReplication()
-    {
-        return this.replication;
-    }
-
-    /**
-     * @return anticipationStrategy.
-     */
-    public String getAnticipationStrategy()
-    {
-        return this.anticipationStrategy;
-    }
-
-    /**
-     * @return reactionTime.
-     */
-    public Duration getReactionTime()
-    {
-        return this.reactionTime;
-    }
-
-    /**
-     * @return anticipationTime.
-     */
-    public Duration getAnticipationTime()
-    {
-        return this.anticipationTime;
-    }
-
-    /**
-     * @return truckFraction.
-     */
-    public double getTruckFraction()
-    {
-        return this.truckFraction;
-    }
-
-    /**
-     * @return distanceError.
-     */
-    public double getDistanceError()
-    {
-        return this.distanceError;
-    }
-
-    /**
-     * @return speedError.
-     */
-    public double getSpeedError()
-    {
-        return this.speedError;
-    }
-
-    /**
-     * @return accelerationError.
-     */
-    public double getAccelerationError()
-    {
-        return this.accelerationError;
-    }
-
-    /**
-     * @return leftDemand.
-     */
-    public Frequency getLeftDemand()
-    {
-        return this.leftDemand;
-    }
-
-    /**
-     * @return rightDemand.
-     */
-    public Frequency getRightDemand()
-    {
-        return this.rightDemand;
-    }
-
-    /**
-     * @return leftFraction.
-     */
-    public double getLeftFraction()
-    {
-        return this.leftFraction;
+        super(model);
     }
 
     /**
@@ -230,6 +80,7 @@ public class AHFESimulation extends AbstractWrappableSimulation
      * @param args String[]; the command line arguments
      * @throws SimRuntimeException should never happen
      */
+    @SuppressWarnings("checkstyle:methodlength")
     public static void main(final String[] args) throws SimRuntimeException
     {
         AbstractGTU.ALIGNED = false;
@@ -430,29 +281,29 @@ public class AHFESimulation extends AbstractWrappableSimulation
             {
                 try
                 {
-                    AHFESimulation model = new AHFESimulation(finalReplication, finalAnticipationStrategy, finalReactionTime,
-                            finalAnticipationTime, finalTruckFraction, finalDistanceError, finalSpeedError,
+                    OTSSimulator simulator = new OTSSimulator();
+                    final AHFEModel ahfeModel = new AHFEModel(simulator, finalReplication, finalAnticipationStrategy,
+                            finalReactionTime, finalAnticipationTime, finalTruckFraction, finalDistanceError, finalSpeedError,
                             finalAccelerationError, finalLeftDemand, finalRightDemand, finalLeftFraction);
                     System.out.println("Setting up replication " + finalReplication);
-                    model.setNextReplication(finalReplication);
+                    simulator.initialize(Time.ZERO, Duration.ZERO, Duration.createSI(SIMEND.si), ahfeModel, finalReplication);
+                    new AHFESimulation(ahfeModel);
                     // 1 hour simulation run for testing
-                    model.buildSimulator(Time.ZERO, Duration.ZERO, Duration.createSI(SIMEND.si),
-                            new ArrayList<InputParameter<?, ?>>());
                     if (finalAutoRun)
                     {
                         int lastReportedTime = -60;
                         int reportTimeClick = 60;
                         while (true)
                         {
-                            int currentTime = (int) model.getSimulator().getSimulatorTime().si;
+                            int currentTime = (int) simulator.getSimulatorTime().si;
                             if (currentTime >= lastReportedTime + reportTimeClick)
                             {
                                 lastReportedTime = currentTime / reportTimeClick * reportTimeClick;
-                                System.out.println("time is " + model.getSimulator().getSimulatorTime());
+                                System.out.println("time is " + simulator.getSimulatorTime());
                             }
                             try
                             {
-                                model.getSimulator().step();
+                                simulator.step();
                             }
                             catch (SimRuntimeException sre)
                             {
@@ -474,7 +325,7 @@ public class AHFESimulation extends AbstractWrappableSimulation
                                         osw = new OutputStreamWriter(zos);
                                         bw = new BufferedWriter(osw);
                                         bw.write("Collision");
-                                        bw.write(model.getSimulator().getSimulatorTime().toString());
+                                        bw.write(simulator.getSimulatorTime().toString());
                                     }
                                     catch (IOException exception2)
                                     {
@@ -510,10 +361,10 @@ public class AHFESimulation extends AbstractWrappableSimulation
                                 }
                                 else
                                 {
-                                    System.out.println("Simulation ends; time is " + model.getSimulator().getSimulatorTime());
-                                    if (model.sampler != null)
+                                    System.out.println("Simulation ends; time is " + simulator.getSimulatorTime());
+                                    if (ahfeModel.getSampler() != null)
                                     {
-                                        model.sampler.writeToFile(finalScenario + ".csv");
+                                        ahfeModel.getSampler().writeToFile(finalScenario + ".csv");
                                     }
                                 }
                                 long t2 = System.currentTimeMillis();
@@ -525,7 +376,7 @@ public class AHFESimulation extends AbstractWrappableSimulation
 
                     }
                 }
-                catch (SimRuntimeException | NamingException | OTSSimulationException | InputParameterException exception)
+                catch (SimRuntimeException | NamingException exception)
                 {
                     exception.printStackTrace();
                 }
@@ -536,32 +387,91 @@ public class AHFESimulation extends AbstractWrappableSimulation
     /**
      * The AHFE simulation model.
      */
-    class AHFEModel extends AbstractOTSModel
+    static class AHFEModel extends AbstractOTSModel
     {
-
-        /**
-         * @param simulator the simulator
-         */
-        AHFEModel(final OTSSimulatorInterface simulator)
-        {
-            super(simulator);
-        }
-
         /** */
         private static final long serialVersionUID = 20170228L;
 
         /** The network. */
         private OTSNetwork network;
 
+        /** Replication. */
+        private final Integer replication;
+
+        /** Anticipation strategy. */
+        private final String anticipationStrategy;
+
+        /** Reaction time. */
+        private final Duration reactionTime;
+
+        /** Future anticipation time. */
+        private final Duration anticipationTime;
+
+        /** Truck fraction. */
+        private final double truckFraction;
+
+        /** Distance error. */
+        private final double distanceError;
+
+        /** Speed error. */
+        private final double speedError;
+
+        /** Acceleration error. */
+        private final double accelerationError;
+
+        /** Left demand. */
+        private final Frequency leftDemand;
+
+        /** Right demand. */
+        private final Frequency rightDemand;
+
+        /** Left fraction, per road. */
+        private final double leftFraction;
+
+        /** Sampler. */
+        private Sampler<GtuData> sampler;
+
+        /**
+         * @param simulator the simulator
+         * @param replication Integer; replication
+         * @param anticipationStrategy String; anticipation strategy
+         * @param reactionTime Duration; reaction time
+         * @param anticipationTime Duration; anticipation time
+         * @param truckFraction double; truck fraction
+         * @param distanceError double; distance error
+         * @param speedError double; speed error
+         * @param accelerationError double; acceleration error
+         * @param leftFraction double; left demand
+         * @param rightDemand Frequency; right demand
+         * @param leftDemand Frequency; left fraction, per road
+         */
+        @SuppressWarnings("checkstyle:parameternumber")
+        AHFEModel(final OTSSimulatorInterface simulator, final Integer replication, final String anticipationStrategy,
+                final Duration reactionTime, final Duration anticipationTime, final double truckFraction,
+                final double distanceError, final double speedError, final double accelerationError, final Frequency leftDemand,
+                final Frequency rightDemand, final double leftFraction)
+        {
+            super(simulator);
+            this.replication = replication;
+            this.anticipationStrategy = anticipationStrategy;
+            this.reactionTime = reactionTime;
+            this.anticipationTime = anticipationTime;
+            this.truckFraction = truckFraction;
+            this.distanceError = distanceError;
+            this.speedError = speedError;
+            this.accelerationError = accelerationError;
+            this.leftDemand = leftDemand;
+            this.rightDemand = rightDemand;
+            this.leftFraction = leftFraction;
+        }
 
         /** {@inheritDoc} */
         @SuppressWarnings("synthetic-access")
         @Override
-        public void constructModel()
-                throws SimRuntimeException
+        public void constructModel() throws SimRuntimeException
         {
-            AHFESimulation.this.sampler = new RoadSampler(this.simulator);
-            AHFESimulation.this.sampler.registerExtendedDataType(new TimeToCollision());
+            this.sampler = new RoadSampler(this.simulator);
+            this.sampler.registerExtendedDataType(new TimeToCollision());
             try
             {
                 // InputStream stream = URLResource.getResourceAsStream("/AHFE/Network.xml"); // Running from eclipse
@@ -583,10 +493,9 @@ public class AHFESimulation extends AbstractWrappableSimulation
                 registerLinkToSampler(linkData, Length.ZERO, linkData.getLength().minus(ignoreEnd));
 
                 // Generator
-                AHFEUtil.createDemand(this.network, null, this.simulator, getReplication(),
-                        getAnticipationStrategy(), getReactionTime(), getAnticipationTime(), getTruckFraction(), SIMEND,
-                        getLeftDemand(), getRightDemand(), getLeftFraction(), getDistanceError(), getSpeedError(),
-                        getAccelerationError());
+                AHFEUtil.createDemand(this.network, null, this.simulator, getReplication(), getAnticipationStrategy(),
+                        getReactionTime(), getAnticipationTime(), getTruckFraction(), SIMEND, getLeftDemand(), getRightDemand(),
+                        getLeftFraction(), getDistanceError(), getSpeedError(), getAccelerationError());
 
             }
             catch (Exception exception)
@@ -607,7 +516,7 @@ public class AHFESimulation extends AbstractWrappableSimulation
             {
                 Length start = laneData.getLength().multiplyBy(startDistance.si / linkData.getLength().si);
                 Length end = laneData.getLength().multiplyBy(endDistance.si / linkData.getLength().si);
-                AHFESimulation.this.sampler.registerSpaceTimeRegion(new SpaceTimeRegion(
+                this.sampler.registerSpaceTimeRegion(new SpaceTimeRegion(
                         new KpiLaneDirection(laneData, KpiGtuDirectionality.DIR_PLUS), start, end, WARMUP, SIMEND));
             }
         }
@@ -617,6 +526,102 @@ public class AHFESimulation extends AbstractWrappableSimulation
         public OTSNetwork getNetwork()
         {
             return this.network;
+        }
+
+        /**
+         * @return replication.
+         */
+        public Integer getReplication()
+        {
+            return this.replication;
+        }
+
+        /**
+         * @return anticipationStrategy.
+         */
+        public String getAnticipationStrategy()
+        {
+            return this.anticipationStrategy;
+        }
+
+        /**
+         * @return reactionTime.
+         */
+        public Duration getReactionTime()
+        {
+            return this.reactionTime;
+        }
+
+        /**
+         * @return anticipationTime.
+         */
+        public Duration getAnticipationTime()
+        {
+            return this.anticipationTime;
+        }
+
+        /**
+         * @return truckFraction.
+         */
+        public double getTruckFraction()
+        {
+            return this.truckFraction;
+        }
+
+        /**
+         * @return distanceError.
+         */
+        public double getDistanceError()
+        {
+            return this.distanceError;
+        }
+
+        /**
+         * @return speedError.
+         */
+        public double getSpeedError()
+        {
+            return this.speedError;
+        }
+
+        /**
+         * @return accelerationError.
+         */
+        public double getAccelerationError()
+        {
+            return this.accelerationError;
+        }
+
+        /**
+         * @return leftDemand.
+         */
+        public Frequency getLeftDemand()
+        {
+            return this.leftDemand;
+        }
+
+        /**
+         * @return rightDemand.
+         */
+        public Frequency getRightDemand()
+        {
+            return this.rightDemand;
+        }
+
+        /**
+         * @return leftFraction.
+         */
+        public double getLeftFraction()
+        {
+            return this.leftFraction;
+        }
+
+        /**
+         * @return sampler
+         */
+        public final Sampler<GtuData> getSampler()
+        {
+            return this.sampler;
         }
 
     }
