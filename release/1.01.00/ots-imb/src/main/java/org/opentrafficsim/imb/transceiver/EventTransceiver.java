ece9cd96732eccbadb793b894f2df255b899fe36
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-01-19 17:49:29 +0000

    deployed vresion 1.01.00

---
+++ release/1.01.00/ots-imb/src/main/java/org/opentrafficsim/imb/transceiver/EventTransceiver.java
@@ -0,0 +1,20 @@
+package org.opentrafficsim.imb.transceiver;
+
+import nl.tudelft.simulation.event.EventListenerInterface;
+import nl.tudelft.simulation.event.EventProducerInterface;
+
+/**
+ * Relay events between IMB domain and OTS domain, enabled by pub/sub.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Sep 9, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public interface EventTransceiver extends Transceiver, EventListenerInterface, EventProducerInterface
+{
+    // no methods added; combines Transceiver with an EventListener and EventProducer
+}
