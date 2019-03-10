801760cc9edb1a68c174b81dacf12597fb3f8f5d
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2019-03-10 02:32:21 +0000

    XSDs versioned. New folder for xsds on the Web: https: https://opentrafficsim.org/docs/xsd/ All demos now reference these xsds (and the old parser). All xmls and xsds (including pom.xml, site.xml, and others) are error free with validation. ots-xsd: bindings.xsd renamed to bindings.xml (it is an xml...) ots-xsd: pom.xml updated with latest changes.

--- ots-xsd/src/main/java/org/opentrafficsim/xml/generated/LANETYPES.java
+++ ots-xsd/src/main/java/org/opentrafficsim/xml/generated/LANETYPES.java
@@ -13,24 +13,23 @@ import java.util.List;
 import javax.annotation.Generated;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for SIGNALGROUPTYPE complex type.
+ * <p>Java class for anonymous complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="SIGNALGROUPTYPE"&gt;
+ * &lt;complexType&gt;
  *   &lt;complexContent&gt;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
  *       &lt;sequence&gt;
- *         &lt;element name="TRAFFICLIGHT" type="{http://www.opentrafficsim.org/ots}SIGNALGROUPTRAFFICLIGHTTYPE" maxOccurs="unbounded"/&gt;
+ *         &lt;element ref="{http://www.opentrafficsim.org/ots}LANETYPE" maxOccurs="unbounded" minOccurs="0"/&gt;
  *       &lt;/sequence&gt;
- *       &lt;attribute name="ID" use="required" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
  * &lt;/complexType&gt;
@@ -39,73 +38,45 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "SIGNALGROUPTYPE", propOrder = {
-    "trafficlight"
+@XmlType(name = "", propOrder = {
+    "lanetype"
 })
+@XmlRootElement(name = "LANETYPES")
 @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-10T02:38:16+01:00", comments = "JAXB RI v2.3.0")
-public class SIGNALGROUPTYPE {
+public class LANETYPES {
 
-    @XmlElement(name = "TRAFFICLIGHT", required = true)
+    @XmlElement(name = "LANETYPE")
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-10T02:38:16+01:00", comments = "JAXB RI v2.3.0")
-    protected List<SIGNALGROUPTRAFFICLIGHTTYPE> trafficlight;
-    @XmlAttribute(name = "ID", required = true)
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-10T02:38:16+01:00", comments = "JAXB RI v2.3.0")
-    protected String id;
+    protected List<LANETYPE> lanetype;
 
     /**
-     * Gets the value of the trafficlight property.
+     * Gets the value of the lanetype property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the trafficlight property.
+     * This is why there is not a <CODE>set</CODE> method for the lanetype property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getTRAFFICLIGHT().add(newItem);
+     *    getLANETYPE().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link SIGNALGROUPTRAFFICLIGHTTYPE }
+     * {@link LANETYPE }
      * 
      * 
      */
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-10T02:38:16+01:00", comments = "JAXB RI v2.3.0")
-    public List<SIGNALGROUPTRAFFICLIGHTTYPE> getTRAFFICLIGHT() {
-        if (trafficlight == null) {
-            trafficlight = new ArrayList<SIGNALGROUPTRAFFICLIGHTTYPE>();
+    public List<LANETYPE> getLANETYPE() {
+        if (lanetype == null) {
+            lanetype = new ArrayList<LANETYPE>();
         }
-        return this.trafficlight;
-    }
-
-    /**
-     * Gets the value of the id property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-10T02:38:16+01:00", comments = "JAXB RI v2.3.0")
-    public String getID() {
-        return id;
-    }
-
-    /**
-     * Sets the value of the id property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-10T02:38:16+01:00", comments = "JAXB RI v2.3.0")
-    public void setID(String value) {
-        this.id = value;
+        return this.lanetype;
     }
 
 }
