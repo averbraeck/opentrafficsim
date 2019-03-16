3220c9b38676aa00608164d8f0018ab87e5af85f
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-03-16 17:58:54 +0000

    deployed version 1.01.01

---
+++ release/1.01.01/ots-kpi/src/main/java/org/opentrafficsim/kpi/sampling/data/ExtendedDataTypeDuration.java
@@ -0,0 +1,59 @@
+package org.opentrafficsim.kpi.sampling.data;
+
+import org.djunits.unit.AbsoluteLinearUnit;
+import org.djunits.unit.DurationUnit;
+import org.djunits.unit.Unit;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vfloat.scalar.FloatDuration;
+import org.djunits.value.vfloat.vector.FloatDurationVector;
+import org.opentrafficsim.kpi.interfaces.GtuDataInterface;
+
+/**
+ * Extended data type for duration values.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 21 mrt. 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ * @param <G> gtu data type
+ */
+public abstract class ExtendedDataTypeDuration<G extends GtuDataInterface>
+        extends ExtendedDataTypeFloat<DurationUnit, FloatDuration, FloatDurationVector, G>
+{
+
+    /**
+     * Constructor setting the id.
+     * @param id String; id
+     */
+    public ExtendedDataTypeDuration(String id)
+    {
+        super(id);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final FloatDuration convertValue(final float value)
+    {
+        return FloatDuration.createSI(value);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final FloatDurationVector convert(final float[] storage) throws ValueException
+    {
+        return new FloatDurationVector(storage, DurationUnit.SI, StorageType.DENSE);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <AU extends AbsoluteLinearUnit<AU, RU>, RU extends Unit<RU>> FloatDuration interpolate(final FloatDuration value0,
+            final FloatDuration value1, final double f)
+    {
+        return FloatDuration.interpolate(value0, value1, (float) f);
+    }
+
+}
