af5eaecc421005a838e6941b7ce04bfaa00c3394
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2019-03-16 13:13:55 +0000

    DefinitionsParser updated.

--- ots-parser-xml/src/main/java/org/opentrafficsim/road/network/factory/xml/network/DefinitionsParser.java
+++ ots-parser-xml/src/main/java/org/opentrafficsim/road/network/factory/xml/network/DefinitionsParser.java
@@ -1,21 +1,22 @@
 package org.opentrafficsim.road.network.factory.xml.network;
 
 /**
- * DefinitionParser parses the XML nodes od the DEFINITIONS tag: GTUTYPE, GTU, ROADTYPE, LANETYPE and ROADLAYOUT. <br>
+ * DefinitionParser parses the XML nodes od the DEFINITIONS tag: GTUTYPE, GTUTEMPLATE, ROADTYPE, LANETYPE and ROADLAYOUT. <br>
  * <br>
  * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
  * source code and binary code of this software is proprietary information of Delft University of Technology.
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
  */
-public final class DefinitionParser
+public final class DefinitionsParser
 {
     /** */
-    private DefinitionParser()
+    private DefinitionsParser()
     {
         // utility class
     }
 
+    
 //    for (Node definitionNode : definitionNodes)
 //        GTUTypeTag.parseGTUTypes(definitionNode.getChildNodes(), this);
 //    for (Node definitionNode : definitionNodes)
