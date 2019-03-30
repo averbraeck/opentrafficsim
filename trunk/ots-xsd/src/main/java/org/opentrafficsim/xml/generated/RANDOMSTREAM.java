25a76c4c4ee0e3c1faa9b890d85abdb79315cf8d
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-03-30 10:24:42 +0000

    Next set of updates for ots-xsd.

--- trunk/ots-xsd/src/main/java/org/opentrafficsim/xml/generated/RANDOMSTREAM.java
+++ trunk/ots-xsd/src/main/java/org/opentrafficsim/xml/generated/RANDOMSTREAM.java
@@ -9,6 +9,7 @@
 package org.opentrafficsim.xml.generated;
 
 import java.io.Serializable;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Generated;
@@ -16,32 +17,33 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import org.djunits.value.vdouble.scalar.Time;
-import org.opentrafficsim.xml.bindings.TimeAdapter;
 
 
 /**
- * <p>Java class for GLOBALTIME complex type.
+ * <p>Java class for anonymous complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="GLOBALTIME"&gt;
+ * &lt;complexType&gt;
  *   &lt;complexContent&gt;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="TIME" maxOccurs="unbounded" minOccurs="2"&gt;
+ *       &lt;sequence maxOccurs="unbounded"&gt;
+ *         &lt;element name="REPLICATION"&gt;
  *           &lt;complexType&gt;
  *             &lt;complexContent&gt;
  *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *                 &lt;attribute name="VALUE" use="required" type="{http://www.opentrafficsim.org/ots}TIMETYPE" /&gt;
+ *                 &lt;attribute name="ID" use="required" type="{http://www.w3.org/2001/XMLSchema}positiveInteger" /&gt;
+ *                 &lt;attribute name="SEED" use="required" type="{http://www.w3.org/2001/XMLSchema}integer" /&gt;
  *               &lt;/restriction&gt;
  *             &lt;/complexContent&gt;
  *           &lt;/complexType&gt;
  *         &lt;/element&gt;
  *       &lt;/sequence&gt;
+ *       &lt;attribute name="ID" use="required" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
  * &lt;/complexType&gt;
@@ -50,48 +52,78 @@ import org.opentrafficsim.xml.bindings.TimeAdapter;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "GLOBALTIME", propOrder = {
-    "time"
+@XmlType(name = "", propOrder = {
+    "replication"
 })
+@XmlRootElement(name = "RANDOMSTREAM")
 @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-public class GLOBALTIME
+public class RANDOMSTREAM
     implements Serializable
 {
 
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
     private final static long serialVersionUID = 10102L;
-    @XmlElement(name = "TIME", required = true)
+    @XmlElement(name = "REPLICATION", required = true)
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-    protected List<GLOBALTIME.TIME> time;
+    protected List<RANDOMSTREAM.REPLICATION> replication;
+    @XmlAttribute(name = "ID", required = true)
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
+    protected String id;
 
     /**
-     * Gets the value of the time property.
+     * Gets the value of the replication property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the time property.
+     * This is why there is not a <CODE>set</CODE> method for the replication property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getTIME().add(newItem);
+     *    getREPLICATION().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link GLOBALTIME.TIME }
+     * {@link RANDOMSTREAM.REPLICATION }
      * 
      * 
      */
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-    public List<GLOBALTIME.TIME> getTIME() {
-        if (time == null) {
-            time = new ArrayList<GLOBALTIME.TIME>();
+    public List<RANDOMSTREAM.REPLICATION> getREPLICATION() {
+        if (replication == null) {
+            replication = new ArrayList<RANDOMSTREAM.REPLICATION>();
         }
-        return this.time;
+        return this.replication;
+    }
+
+    /**
+     * Gets the value of the id property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
+    public String getID() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
+    public void setID(String value) {
+        this.id = value;
     }
 
 
@@ -104,7 +136,8 @@ public class GLOBALTIME
      * &lt;complexType&gt;
      *   &lt;complexContent&gt;
      *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
-     *       &lt;attribute name="VALUE" use="required" type="{http://www.opentrafficsim.org/ots}TIMETYPE" /&gt;
+     *       &lt;attribute name="ID" use="required" type="{http://www.w3.org/2001/XMLSchema}positiveInteger" /&gt;
+     *       &lt;attribute name="SEED" use="required" type="{http://www.w3.org/2001/XMLSchema}integer" /&gt;
      *     &lt;/restriction&gt;
      *   &lt;/complexContent&gt;
      * &lt;/complexType&gt;
@@ -115,41 +148,70 @@ public class GLOBALTIME
     @XmlAccessorType(XmlAccessType.FIELD)
     @XmlType(name = "")
     @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-    public static class TIME
+    public static class REPLICATION
         implements Serializable
     {
 
         @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
         private final static long serialVersionUID = 10102L;
-        @XmlAttribute(name = "VALUE", required = true)
-        @XmlJavaTypeAdapter(TimeAdapter.class)
+        @XmlAttribute(name = "ID", required = true)
+        @XmlSchemaType(name = "positiveInteger")
         @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-        protected Time value;
+        protected BigInteger id;
+        @XmlAttribute(name = "SEED", required = true)
+        @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
+        protected BigInteger seed;
 
         /**
-         * Gets the value of the value property.
+         * Gets the value of the id property.
          * 
          * @return
          *     possible object is
-         *     {@link String }
+         *     {@link BigInteger }
          *     
          */
         @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-        public Time getVALUE() {
-            return value;
+        public BigInteger getID() {
+            return id;
         }
 
         /**
-         * Sets the value of the value property.
+         * Sets the value of the id property.
          * 
          * @param value
          *     allowed object is
-         *     {@link String }
+         *     {@link BigInteger }
          *     
          */
         @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
-        public void setVALUE(Time value) {
-            this.value = value;
+        public void setID(BigInteger value) {
+            this.id = value;
+        }
+
+        /**
+         * Gets the value of the seed property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link BigInteger }
+         *     
+         */
+        @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
+        public BigInteger getSEED() {
+            return seed;
+        }
+
+        /**
+         * Sets the value of the seed property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link BigInteger }
+         *     
+         */
+        @Generated(value = "com.sun.tools.xjc.Driver", date = "2019-03-30T11:23:29+01:00", comments = "JAXB RI v2.3.0")
+        public void setSEED(BigInteger value) {
+            this.seed = value;
         }
 
     }
