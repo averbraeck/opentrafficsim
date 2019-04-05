8d25169433b84eaaa3e7a50aa3cfe93041cb0584
wjschakel <wjschakel@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-04-05 12:12:41 +0000

    StaticFieldNameAdapter added. Changed "generator" stream to "generation" in some places.

--- trunk/ots-xsd/src/main/java/org/opentrafficsim/xml/bindings/ClassNameAdapter.java
+++ trunk/ots-xsd/src/main/java/org/opentrafficsim/xml/bindings/ClassNameAdapter.java
@@ -3,7 +3,7 @@ package org.opentrafficsim.xml.bindings;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
- * AngleAdapter converts between the XML String for a class name and the Class object. <br>
+ * ClassNameAdapter converts between the XML String for a class name and the Class object. <br>
  * <br>
  * Copyright (c) 2003-2018 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
