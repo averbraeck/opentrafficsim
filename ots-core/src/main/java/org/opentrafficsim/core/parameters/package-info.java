a5ce36e57f0cafde72ad3442f50ab67be82b8361
wjschakel <89968231+WJSchakel@users.noreply.github.com>
2019-04-05 09:37:40 +0000

    Added InputParameters and moved package.

---
+++ ots-core/src/main/java/org/opentrafficsim/core/parameters/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Objects involving parameters.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Apr 13, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+package org.opentrafficsim.core.parameters;
