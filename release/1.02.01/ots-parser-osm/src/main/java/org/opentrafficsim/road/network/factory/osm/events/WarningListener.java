5d5b03e7b8db03f27ecc6b63ccb7dc70f946c570
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-04-20 00:39:43 +0000

    Committed version 1.02.01

---
+++ release/1.02.01/ots-parser-osm/src/main/java/org/opentrafficsim/road/network/factory/osm/events/WarningListener.java
@@ -0,0 +1,21 @@
+package org.opentrafficsim.road.network.factory.osm.events;
+
+import java.util.EventListener;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate: 2015-07-26 01:01:13 +0200 (Sun, 26 Jul 2015) $, @version $Revision: 1155 $, by $Author: averbraeck $,
+ * initial version 20.03.2015 <br>
+ * @author <a>Moritz Bergmann</a>
+ */
+public interface WarningListener extends EventListener
+{
+    /**
+     * Process one WarningEvent.
+     * @param warningEvent WarningEvent; the warning event
+     */
+    void warning(WarningEvent warningEvent);
+}
---
+++ release/1.02.01/ots-parser-osm/src/main/java/org/opentrafficsim/road/network/factory/osm/output/GTUTypes.java
@@ -0,0 +1,39 @@
+package org.opentrafficsim.road.network.factory.osm.output;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>. <br>
+ * This is a preliminary Enumeration for possible GTU Types for usage during development and testing.
+ * <p>
+ * $LastChangedDate: 2015-08-27 01:29:47 +0200 (Thu, 27 Aug 2015) $, @version $Revision: 1303 $, by $Author: averbraeck $,
+ * initial version 19.12.2014 <br>
+ * @author <a>Moritz Bergmann</a>
+ */
+public enum GTUTypes
+{
+    /** */
+    CAR,
+    /** */
+    BIKE,
+    /** */
+    PEDESTRIAN,
+    /** */
+    SCOOTER,
+    /** */
+    TRAM,
+    /** */
+    TRAIN,
+    /** */
+    METRO,
+    /** */
+    BOAT,
+    /** */
+    PLANE,
+    /** */
+    HORSE,
+    /** */
+    SNOWMOBILE,
+    /** */
+    SKI;
+}
