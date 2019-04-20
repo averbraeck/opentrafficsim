5b24dbdcb5a91c61507d6e1155133dab7d59b746
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-04-20 00:39:03 +0000

    Committed version 1.02.01

---
+++ release/1.02.01/ots-trafficcontrol/src/main/java/org/opentrafficsim/trafficcontrol/ActuatedTrafficController.java
@@ -0,0 +1,23 @@
+package org.opentrafficsim.trafficcontrol;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 21 feb. 2019 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public interface ActuatedTrafficController extends TrafficController
+{
+
+    /**
+     * Tell the traffic controller that the state of a detector has changed.
+     * @param detectorId String; id of the detector
+     * @param detectingGTU boolean;
+     */
+    public void updateDetector(String detectorId, boolean detectingGTU);
+
+}
