08642a819a8c3f7d8e7131494e808f633ee02016
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-04-20 00:40:44 +0000

    Committed version 1.02.01

---
+++ release/1.02.01/ots-sim0mq-kpi/src/main/java/org/opentrafficsim/sim0mq/kpi/demo/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Demo for KPI module via sim0mq bus.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 4 nov. 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+package org.opentrafficsim.sim0mq.kpi.demo;
