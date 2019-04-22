a99443c08ae1c9818b8bcc353e094f3f1663b690
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2019-04-22 21:06:40 +0000

    Old Ainsun parser model removed.

--- ots-parser-xml/src/test/java/org/opentrafficsim/road/network/factory/xml/test/TestXMLParserAimsun.java
+++ ots-parser-xml/src/test/java/org/opentrafficsim/road/network/factory/xml/test/TestXMLParserAimsun.java
@@ -1,23 +1,23 @@
 package org.opentrafficsim.road.network.factory.xml.test;
 
 import java.awt.Dimension;
-import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.rmi.RemoteException;
 
 import javax.naming.NamingException;
 import javax.swing.SwingUtilities;
+import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.djunits.unit.LengthUnit;
-import org.djunits.value.ValueException;
 import org.djunits.value.vdouble.scalar.Duration;
 import org.djunits.value.vdouble.scalar.Length;
 import org.djunits.value.vdouble.scalar.Time;
 import org.djutils.io.URLResource;
-import org.opentrafficsim.base.parameters.ParameterException;
 import org.opentrafficsim.core.dsol.AbstractOTSModel;
 import org.opentrafficsim.core.dsol.OTSAnimator;
+import org.opentrafficsim.core.dsol.OTSModelInterface;
 import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
 import org.opentrafficsim.core.geometry.OTSGeometryException;
 import org.opentrafficsim.core.gtu.GTUException;
@@ -25,26 +25,27 @@ import org.opentrafficsim.core.gtu.GTUType;
 import org.opentrafficsim.core.network.NetworkException;
 import org.opentrafficsim.draw.core.OTSDrawingException;
 import org.opentrafficsim.road.network.OTSRoadNetwork;
-import org.opentrafficsim.road.network.factory.xml.old.XmlNetworkLaneParserOld;
-import org.opentrafficsim.road.network.factory.xml.test.TestXMLParserAimsun.TestXMLModelAimsun;
+import org.opentrafficsim.road.network.factory.xml.XmlParserException;
+import org.opentrafficsim.road.network.factory.xml.parser.XmlNetworkLaneParser;
 import org.opentrafficsim.road.network.lane.conflict.ConflictBuilder;
 import org.opentrafficsim.swing.gui.OTSAnimationPanel;
 import org.opentrafficsim.swing.gui.OTSSimulationApplication;
 import org.xml.sax.SAXException;
 
 import nl.tudelft.simulation.dsol.SimRuntimeException;
-import nl.tudelft.simulation.dsol.animation.D2.GisRenderable2D;
 
 /**
+ * New parser test.
  * <p>
  * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
  * <p>
- * $LastChangedDate: 2015-09-14 01:33:02 +0200 (Mon, 14 Sep 2015) $, @version $Revision: 1401 $, by $Author: averbraeck $,
- * initial version Oct 17, 2014 <br>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Dec 1, 2015 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author Wouter Schakel
  */
-public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAimsun>
+public class TestXMLParserAimsun extends OTSSimulationApplication<OTSModelInterface>
 {
     /** */
     private static final long serialVersionUID = 1L;
@@ -54,8 +55,7 @@ public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAi
      * @param animationPanel the animation panel
      * @throws OTSDrawingException on drawing error
      */
-    public TestXMLParserAimsun(final TestXMLModelAimsun model, final OTSAnimationPanel animationPanel)
-            throws OTSDrawingException
+    public TestXMLParserAimsun(final OTSModelInterface model, final OTSAnimationPanel animationPanel) throws OTSDrawingException
     {
         super(model, animationPanel);
     }
@@ -75,7 +75,7 @@ public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAi
                 try
                 {
                     OTSAnimator simulator = new OTSAnimator();
-                    TestXMLModelAimsun xmlModel = new TestXMLModelAimsun(simulator);
+                    TestAimsunModel xmlModel = new TestAimsunModel(simulator);
                     simulator.initialize(Time.ZERO, Duration.ZERO, Duration.createSI(3600.0), xmlModel);
                     OTSAnimationPanel animationPanel = new OTSAnimationPanel(xmlModel.getNetwork().getExtent(),
                             new Dimension(800, 600), simulator, xmlModel, DEFAULT_COLORER, xmlModel.getNetwork());
@@ -91,9 +91,9 @@ public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAi
 
     /** {@inheritDoc} */
     @Override
-    public final String toString()
+    public String toString()
     {
-        return "TestXMLParserAimsun []";
+        return "TestXMLParserAimsunNew []";
     }
 
     /**
@@ -108,21 +108,18 @@ public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAi
      * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
      * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
      */
-    static class TestXMLModelAimsun extends AbstractOTSModel
+    static class TestAimsunModel extends AbstractOTSModel
     {
         /** */
         private static final long serialVersionUID = 20141121L;
 
         /** the network. */
-        private OTSRoadNetwork network;
-
-        /** the GIS map. */
-        private GisRenderable2D gisMap;
+        private OTSRoadNetwork network = null;
 
         /**
          * @param simulator the simulator
          */
-        TestXMLModelAimsun(final OTSSimulatorInterface simulator)
+        TestAimsunModel(final OTSSimulatorInterface simulator)
         {
             super(simulator);
         }
@@ -132,26 +129,18 @@ public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAi
         public final void constructModel() throws SimRuntimeException
         {
             URL url = URLResource.getResource("/AimsunOtsNetwork.xml");
-            XmlNetworkLaneParserOld nlp = new XmlNetworkLaneParserOld(this.simulator);
+            this.network = new OTSRoadNetwork("Aimsun network", true);
             try
             {
-                this.network = nlp.build(url, true);
+                XmlNetworkLaneParser.build(url.getPath(), this.network, getSimulator());
                 ConflictBuilder.buildConflicts(this.network, this.network.getGtuType(GTUType.DEFAULTS.VEHICLE), this.simulator,
                         new ConflictBuilder.FixedWidthGenerator(new Length(2.0, LengthUnit.SI)));
             }
-            catch (NetworkException | ParserConfigurationException | SAXException | IOException | NamingException | GTUException
-                    | OTSGeometryException | ValueException | ParameterException exception)
+            catch (NetworkException | ParserConfigurationException | SAXException | OTSGeometryException | JAXBException
+                    | URISyntaxException | XmlParserException | GTUException exception)
             {
                 exception.printStackTrace();
             }
-        }
-
-        /**
-         * @return gisMap
-         */
-        public final GisRenderable2D getGisMap()
-        {
-            return this.gisMap;
         }
 
         /** {@inheritDoc} */
@@ -165,8 +154,9 @@ public class TestXMLParserAimsun extends OTSSimulationApplication<TestXMLModelAi
         @Override
         public final String toString()
         {
-            return "TestXMLModelAimsun [simulator=" + this.simulator + "]";
+            return "TestXMLParserAimsunNew [simulator=" + this.simulator + "]";
         }
 
     }
+
 }
