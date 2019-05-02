73dd9ec0be6e42efcfd15b166ab76ee10b541564
OTSim <P.Knoppers@tudelft.nl>
2019-05-02 12:36:17 +0000

    fixed javadoc.

--- ots-draw/src/main/java/org/opentrafficsim/draw/road/ConflictAnimation.java
+++ ots-draw/src/main/java/org/opentrafficsim/draw/road/ConflictAnimation.java
@@ -19,6 +19,7 @@ import org.opentrafficsim.road.network.lane.conflict.ConflictType;
 import nl.tudelft.simulation.dsol.simulators.SimulatorInterface;
 
 /**
+ * Animate a conflict.
  * <p>
  * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
