209860b590c28fc82759a879c596db206ad5de84
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-05-27 09:36:22 +0000

    Now collects data from all trajectory shape files. (Not yet the point data)

--- trunk/ots-parser-nwb/src/main/java/org/opentrafficsim/road/network/factory/nwb/Importer.java
+++ trunk/ots-parser-nwb/src/main/java/org/opentrafficsim/road/network/factory/nwb/Importer.java
@@ -3,7 +3,9 @@ package org.opentrafficsim.road.network.factory.nwb;
 import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.naming.NamingException;
@@ -13,7 +15,6 @@ import org.djunits.value.vdouble.scalar.Length;
 import org.djunits.value.vdouble.scalar.Speed;
 import org.djunits.value.vdouble.scalar.Time;
 import org.opengis.feature.Feature;
-import org.opentrafficsim.core.compatibility.GTUCompatibility;
 import org.opentrafficsim.core.dsol.AbstractOTSModel;
 import org.opentrafficsim.core.dsol.OTSAnimator;
 import org.opentrafficsim.core.dsol.OTSModelInterface;
@@ -30,7 +31,6 @@ import org.opentrafficsim.road.network.OTSRoadNetwork;
 import org.opentrafficsim.road.network.factory.nwb.ShapeFileReader.FeatureQualifier;
 import org.opentrafficsim.road.network.lane.Lane;
 import org.opentrafficsim.road.network.lane.LaneType;
-import org.opentrafficsim.road.network.lane.LaneType.DEFAULTS;
 import org.opentrafficsim.road.network.lane.changing.LaneKeepingPolicy;
 import org.opentrafficsim.swing.gui.OTSAnimationPanel;
 import org.opentrafficsim.swing.gui.OTSSimulationApplication;
@@ -74,9 +74,14 @@ public class Importer extends OTSSimulationApplication<OTSModelInterface>
      * @throws IOException When a file could not be read
      * @throws OTSGeometryException When the geometry of a design line contains duplicate points
      * @throws NetworkException Should not happen
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws SecurityException
+     * @throws NoSuchFieldException
      */
-    public static void main(final String[] args) throws OTSDrawingException, SimRuntimeException, NamingException, IOException,
-            OTSGeometryException, NetworkException
+    public static void main(final String[] args)
+            throws OTSDrawingException, SimRuntimeException, NamingException, IOException, OTSGeometryException,
+            NetworkException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException
     {
         System.out.println("Parsing road sections");
         String baseDir = "c:\\NWB";
@@ -103,7 +108,6 @@ public class Importer extends OTSSimulationApplication<OTSModelInterface>
                     {
                         if (p.x < 80000 || p.y < 444000 || p.x > 90000 || p.y > 455000)
                         {
-                            collected++;
                             return (Long) null;
                         }
                     }
@@ -112,45 +116,60 @@ public class Importer extends OTSSimulationApplication<OTSModelInterface>
                 {
                     e.printStackTrace();
                 }
+                collected++;
                 return (Long) feature.getProperty("WVK_ID").getValue();
             }
         });
         long end = System.nanoTime();
         System.out.println(String.format("Data collection time %.3fs", (end - start) / 1e9));
         System.out.println("Retrieved " + roadData.size() + " records");
-
-        start = System.nanoTime();
-        ShapeFileReader lanes = new ShapeFileReader(baseDir + File.separator + "wegvakken", date,
-                "Rijstroken" + File.separator + "Rijstroken.shp");
-        Map<Long, Feature> laneData = lanes.readShapeFile(new FeatureQualifier()
+        Map<String, Map<Long, Feature>> additionalData = new HashMap<>();
+        List<String> databases = Arrays.asList(new String[] { "adv_snelheden", "beb_kommen", "convergenties", "divergenties",
+                "doelgroepstroken", "gel_beperkingen", "inhaalverboden", "kantstroken", "kruispunten", "kunstinweg",
+                "kunstoverweg", "lichtmasten", "max_snelheden", "mengstroken", "portalen", "rijbanen", "rijstroken",
+                "signaleringen", "spoorovergangen", "strksignaleringn", "verhardingen", "verlichtingen", "voorrangswegen",
+                "wegbermen", "wegcat_beleving", "wegcat_formeel" });
+        for (String database : databases)
         {
-
-            @Override
-            public Long qualify(Feature feature)
+            start = System.nanoTime();
+            ShapeFileReader lanes = new ShapeFileReader(baseDir + File.separator + "wegvakken", date,
+                    database + File.separator + database + ".shp");
+            Map<Long, Feature> laneData = lanes.readShapeFile(new FeatureQualifier()
             {
-                // System.out.println(feature);
-                Long wvkId = (Long) feature.getProperty("WVK_ID").getValue();
-                return roadData.containsKey(wvkId) ? wvkId : null;
-                // if (((String) feature.getProperty("WEGNUMMER").getValue()).contains("013"))
-                // {
-                // System.out.println(feature);
-                // }
-                // return ((String) feature.getProperty("WEGNUMMER").getValue()).contains("013");
-            }
-        });
-        end = System.nanoTime();
-        System.out.println(String.format("Data collection time %.3fs", (end - start) / 1e9));
-        System.out.println("Retrieved " + laneData.size() + " records");
+
+                @Override
+                public Long qualify(Feature feature)
+                {
+                    Long wvkId = (Long) feature.getProperty("WVK_ID").getValue();
+                    return roadData.containsKey(wvkId) ? wvkId : null;
+                }
+            });
+            end = System.nanoTime();
+            System.out.println(String.format("Data collection time for %s %.3fs", database, (end - start) / 1e9));
+            System.out.println("Retrieved " + laneData.size() + " records");
+            additionalData.put(database, laneData);
+        }
+
         OTSRoadNetwork network = new OTSRoadNetwork("NWB import", true);
+
         int nextNodeNumber = 0;
+
         int nextLinkNumber = 0;
-        GTUCompatibility<LinkType> compatibility = new GTUCompatibility<>((LinkType) null);
-        LinkType linkType = new LinkType("Road", null, compatibility, network);
+
+        LinkType freeWayLinkType = network.getLinkType(LinkType.DEFAULTS.FREEWAY);
+        LinkType roadLinkType = network.getLinkType(LinkType.DEFAULTS.ROAD);
+
         OTSAnimator simulator = new OTSAnimator();
+
         NWBModel nwbModel = new NWBModel(simulator, network, "NWB network", "NWB network");
         simulator.initialize(Time.ZERO, Duration.ZERO, Duration.createSI(3600.0), nwbModel);
+
         Length standardLaneWidth = Length.createSI(3.5);
-        LaneType standardLaneType = network.getLaneType(DEFAULTS.HIGHWAY);
+
+        LaneType freeWay = network.getLaneType(LaneType.DEFAULTS.FREEWAY);
+        LaneType highWay = network.getLaneType(LaneType.DEFAULTS.HIGHWAY);
+        LaneType urbanWay = network.getLaneType(LaneType.DEFAULTS.URBAN_ROAD_LANE);
+
         Map<GTUType, Speed> speedLimitMap = new HashMap<>();
 
         for (Long wvkId : roadData.keySet())
@@ -171,31 +190,52 @@ public class Importer extends OTSSimulationApplication<OTSModelInterface>
             }
             ExtendedCrossSectionLink link = new ExtendedCrossSectionLink(network,
                     String.format("%s.%d", feature.getProperty("STT_NAAM").getValue(), ++nextLinkNumber), startNode, endNode,
-                    linkType, designLine, simulator, LaneKeepingPolicy.KEEPRIGHT);
+                    "R".equals(feature.getProperty("WEGBEHSRT").getValue()) ? freeWayLinkType : roadLinkType, designLine,
+                    simulator, LaneKeepingPolicy.KEEPRIGHT);
             // System.out.println("Adding feature for " + wvkId + " which contains " + roadData.get(wvkId));
             link.addFeature("Wegvakken", feature);
-            Feature f = laneData.get(wvkId);
-            if (null != f)
+            for (String database : databases)
             {
-                link.addFeature("Basic roads database Rijstroken", f);
-                // We don't know lateral positions of the CrossSectionElements; this leads to odd problems...
-                String laneFormat = (String) f.getProperty("OMSCHR").getValue();
-                int lanesAtStart = Integer.parseInt(laneFormat.substring(0, 1));
-                int lanesAtEnd = Integer.parseInt(laneFormat.substring(5, 6));
-                int maxLaneNo = Math.max(lanesAtStart, lanesAtEnd);
-                Length cumulativeOffsetAtStart = Length.ZERO;
-                Length cumulativeOffsetAtEnd = Length.ZERO;
-                for (int laneNo = 0; laneNo < maxLaneNo; laneNo++)
+                Feature f = additionalData.get(database).get(wvkId);
+                if (null != f)
                 {
-                    Length widthAtStart = laneNo < lanesAtStart - lanesAtEnd ? Length.ZERO : standardLaneWidth;
-                    Length widthAtEnd = laneNo < lanesAtEnd - lanesAtStart ? Length.ZERO : standardLaneWidth;
-                    new Lane(link, String.format("Lane %d", laneNo + 1), cumulativeOffsetAtStart, cumulativeOffsetAtEnd,
-                            widthAtStart, widthAtEnd, standardLaneType, speedLimitMap, false);
-                    cumulativeOffsetAtStart = cumulativeOffsetAtStart.minus(widthAtStart);
-                    cumulativeOffsetAtEnd = cumulativeOffsetAtEnd.minus(widthAtEnd);
+                    link.addFeature(database, f);
+                    if ("rijstroken".equals(database))
+                    {
+                        LaneType laneType = null;
+                        switch ((String) feature.getProperty("WEGBEHSRT").getValue())
+                        {
+                            case "R":
+                                laneType = freeWay;
+                                break;
+                            case "P":
+                                laneType = highWay;
+                                break;
+                            default: // G, T, W
+                                laneType = urbanWay;
+                                break;
+                        }
+                        // We don't know lateral positions of the CrossSectionElements; this leads to odd problems...
+                        String laneFormat = (String) f.getProperty("OMSCHR").getValue();
+                        int lanesAtStart = Integer.parseInt(laneFormat.substring(0, 1));
+                        int lanesAtEnd = Integer.parseInt(laneFormat.substring(5, 6));
+                        int maxLaneNo = Math.max(lanesAtStart, lanesAtEnd);
+                        Length cumulativeOffsetAtStart = Length.ZERO;
+                        Length cumulativeOffsetAtEnd = Length.ZERO;
+                        for (int laneNo = 0; laneNo < maxLaneNo; laneNo++)
+                        {
+                            Length widthAtStart = laneNo < lanesAtStart - lanesAtEnd ? Length.ZERO : standardLaneWidth;
+                            Length widthAtEnd = laneNo < lanesAtEnd - lanesAtStart ? Length.ZERO : standardLaneWidth;
+                            new Lane(link, String.format("Lane %d", laneNo + 1), cumulativeOffsetAtStart, cumulativeOffsetAtEnd,
+                                    widthAtStart, widthAtEnd, laneType, speedLimitMap, false);
+                            cumulativeOffsetAtStart = cumulativeOffsetAtStart.minus(widthAtStart);
+                            cumulativeOffsetAtEnd = cumulativeOffsetAtEnd.minus(widthAtEnd);
+                        }
+                    }
                 }
             }
         }
+
         OTSAnimationPanel animationPanel = new OTSAnimationPanel(network.getExtent(), new Dimension(800, 600), simulator,
                 nwbModel, DEFAULT_COLORER, nwbModel.getNetwork());
         new Importer(nwbModel, animationPanel);
