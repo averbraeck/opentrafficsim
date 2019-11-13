7ef52e2ecf2fc4c2a40c09fed1ff78caf1b942d0
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-11-13 01:14:29 +0000

    deployed version 1.02.02

---
+++ release/1.02.02/ots-animation/src/main/java/org/opentrafficsim/core/animation/gtu/colorer/DefaultSwitchableGTUColorer.java
@@ -0,0 +1,45 @@
+package org.opentrafficsim.core.animation.gtu.colorer;
+
+import java.io.Serializable;
+
+import org.djunits.unit.SpeedUnit;
+import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Speed;
+
+/**
+ * A simple way to construct a SwitchableGTUColorer set up with the "standard" set of GTUColorers. <br>
+ * Do not assume that the set of GTUColorers in the result will never change.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision: 4742 $, $LastChangedDate: 2018-11-18 20:48:30 +0100 (Sun, 18 Nov 2018) $, by $Author: averbraeck $,
+ *          initial version Jun 18, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class DefaultSwitchableGTUColorer extends SwitchableGTUColorer implements Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20150000L;
+
+    /** The initial set of GTU colorers in the default list. */
+    private static final GTUColorer[] COLORERS;
+
+    static
+    {
+        COLORERS = new GTUColorer[3];
+        COLORERS[0] = new IDGTUColorer();
+        COLORERS[1] = new SpeedGTUColorer(new Speed(150, SpeedUnit.KM_PER_HOUR));
+        COLORERS[2] = new AccelerationGTUColorer(Acceleration.createSI(-6.0), Acceleration.createSI(2));
+    }
+
+    /**
+     * create a default list of GTU colorers.
+     */
+    public DefaultSwitchableGTUColorer()
+    {
+        super(0, DefaultSwitchableGTUColorer.COLORERS);
+    }
+
+}
---
+++ release/1.02.02/ots-animation/src/main/java/org/opentrafficsim/road/gtu/colorer/LmrsSwitchableColorer.java
@@ -0,0 +1,55 @@
+package org.opentrafficsim.road.gtu.colorer;
+
+import java.awt.Color;
+
+import org.djunits.unit.SpeedUnit;
+import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.opentrafficsim.core.animation.gtu.colorer.AccelerationGTUColorer;
+import org.opentrafficsim.core.animation.gtu.colorer.IDGTUColorer;
+import org.opentrafficsim.core.animation.gtu.colorer.SpeedGTUColorer;
+import org.opentrafficsim.core.animation.gtu.colorer.SwitchableGTUColorer;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveBusStop;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveCourtesy;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveGetInLane;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveKeep;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveRoute;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveSocioSpeed;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveSpeed;
+import org.opentrafficsim.road.gtu.lane.tactical.lmrs.IncentiveSpeedWithCourtesy;
+
+/**
+ * Colorer for LMRS.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 14 apr. 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class LmrsSwitchableColorer extends SwitchableGTUColorer
+{
+
+    /** */
+    private static final long serialVersionUID = 20170414L;
+
+    /**
+     * Constructor.
+     * @throws IndexOutOfBoundsException initial index out of bounds
+     */
+    public LmrsSwitchableColorer() throws IndexOutOfBoundsException
+    {
+        super(0, new FixedColor(Color.BLUE, "Blue"), GTUTypeColorer.DEFAULT, new IDGTUColorer(),
+                new SpeedGTUColorer(new Speed(150, SpeedUnit.KM_PER_HOUR)),
+                new DesiredSpeedColorer(new Speed(50, SpeedUnit.KM_PER_HOUR), new Speed(150, SpeedUnit.KM_PER_HOUR)),
+                new AccelerationGTUColorer(Acceleration.createSI(-6.0), Acceleration.createSI(2)), new SplitColorer(),
+                new SynchronizationColorer(), new DesiredHeadwayColorer(), new TotalDesireColorer(),
+                new IncentiveColorer(IncentiveRoute.class), new IncentiveColorer(IncentiveSpeedWithCourtesy.class),
+                new IncentiveColorer(IncentiveSpeed.class), new IncentiveColorer(IncentiveKeep.class),
+                new IncentiveColorer(IncentiveGetInLane.class), new IncentiveColorer(IncentiveCourtesy.class),
+                new IncentiveColorer(IncentiveSocioSpeed.class), new IncentiveColorer(IncentiveBusStop.class));
+    }
+
+}
