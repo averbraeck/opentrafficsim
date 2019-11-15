d6d2b7c457c7982c5da4e17c3623f2a7ca1a4214
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-11-15 16:10:05 +0000

    deployed version 1.03.00

---
+++ release/1.03.00/ots-animation/src/main/java/org/opentrafficsim/core/animation/gtu/colorer/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * GTU animations.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision: 4742 $, $LastChangedDate: 2018-11-18 20:48:30 +0100 (Sun, 18 Nov 2018) $, by $Author: averbraeck $,
+ *          initial 27 mei 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://Hansvanlint.weblog.tudelft.nl">Hans van Lint</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
+ * @author <a href="http://www.citg.tudelft.nl">Yufei Yuan</a>
+ */
+package org.opentrafficsim.core.animation.gtu.colorer;
---
+++ release/1.03.00/ots-animation/src/main/java/org/opentrafficsim/core/animation/network/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Persistent storage of the drawing information for animated / visualized objects. <br>
+ * <br>
+ * Copyright (c) 2003-2019 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
+ * source code and binary code of this software is proprietary information of Delft University of Technology.
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+package org.opentrafficsim.core.animation.network;
