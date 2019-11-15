6d9cff381048cb9e24ab23a50465ec6477705c40
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-11-15 15:45:14 +0000

    delete incomplete release 1.03.00

--- release/1.03.00/ots-build-tools/development/mvn-all-clean-install.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM $HeadURL: https://svn.tbm.tudelft.nl/TBM-SK/DSOL-AOS/dsol-aos/development/mvn-all-clean-install.bat $
-REM $Id$
-
-REM Runs Maven commands 'clean' and 'install' for all Maven artifacts
-
-CALL "%~dp0mvn-all.bat" clean install
-PAUSE
