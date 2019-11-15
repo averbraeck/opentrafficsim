e1fade6bd788c2a756c908d6bde47d028c4869af
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-11-15 15:16:50 +0000

    deployed version 1.03.00

---
+++ release/1.03.00/ots-web/src/main/java/org/opentrafficsim/web/test/DefaultsFactory.java
@@ -0,0 +1,56 @@
+package org.opentrafficsim.web.test;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.value.vdouble.scalar.Length;
+import org.opentrafficsim.base.parameters.ParameterException;
+import org.opentrafficsim.base.parameters.ParameterSet;
+import org.opentrafficsim.base.parameters.ParameterTypes;
+import org.opentrafficsim.base.parameters.Parameters;
+
+/**
+ * Factory for defaults in demos.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Apr 8, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public final class DefaultsFactory
+{
+
+    /**
+     * Do not create instance.
+     */
+    private DefaultsFactory()
+    {
+        //
+    }
+
+    /**
+     * Returns a default set of parameters.
+     * @return Default set of parameters.
+     */
+    public static Parameters getDefaultParameters()
+    {
+
+        Parameters params = new ParameterSet().setDefaultParameters(ParameterTypes.class);
+
+        // demos use different value from default LMRS value
+        try
+        {
+            params.setParameter(ParameterTypes.LOOKAHEAD, new Length(250, LengthUnit.SI));
+        }
+        catch (ParameterException pe)
+        {
+            throw new RuntimeException("Parameter type 'LOOKAHEAD' could not be set.", pe);
+        }
+
+        return params;
+
+    }
+
+}
