aa53742439562c27439c3b6cda78210cf4dfd14f
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-11-15 15:45:32 +0000

    deployed version 1.03.00

---
+++ release/1.03.00/ots-kpi/src/main/java/org/opentrafficsim/kpi/sampling/data/ExtendedDataTypeSpeed.java
@@ -0,0 +1,57 @@
+package org.opentrafficsim.kpi.sampling.data;
+
+import org.djunits.unit.SpeedUnit;
+import org.djunits.value.ValueRuntimeException;
+import org.djunits.value.storage.StorageType;
+import org.djunits.value.vfloat.scalar.FloatSpeed;
+import org.djunits.value.vfloat.vector.FloatSpeedVector;
+import org.djunits.value.vfloat.vector.base.FloatVector;
+import org.opentrafficsim.kpi.interfaces.GtuDataInterface;
+
+/**
+ * Extended data type for speed values.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 21 mrt. 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ * @param <G> gtu data type
+ */
+public abstract class ExtendedDataTypeSpeed<G extends GtuDataInterface>
+        extends ExtendedDataTypeFloat<SpeedUnit, FloatSpeed, FloatSpeedVector, G>
+{
+
+    /**
+     * Constructor setting the id.
+     * @param id String; id
+     */
+    public ExtendedDataTypeSpeed(final String id)
+    {
+        super(id);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final FloatSpeed convertValue(final float value)
+    {
+        return FloatSpeed.instantiateSI(value);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final FloatSpeedVector convert(final float[] storage) throws ValueRuntimeException
+    {
+        return FloatVector.instantiate(storage, SpeedUnit.SI, StorageType.DENSE);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public FloatSpeed interpolate(final FloatSpeed value0, final FloatSpeed value1, final double f)
+    {
+        return FloatSpeed.interpolate(value0, value1, (float) f);
+    }
+
+}
---
+++ release/1.03.00/ots-kpi/src/main/java/org/opentrafficsim/kpi/sampling/indicator/MeanTripLength.java
@@ -0,0 +1,54 @@
+package org.opentrafficsim.kpi.sampling.indicator;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.kpi.interfaces.GtuDataInterface;
+import org.opentrafficsim.kpi.sampling.Query;
+import org.opentrafficsim.kpi.sampling.Trajectory;
+import org.opentrafficsim.kpi.sampling.TrajectoryGroup;
+
+/**
+ * Sum of trajectory lengths divided by number of GTU's.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 1 okt. 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class MeanTripLength extends AbstractIndicator<Length>
+{
+
+    /** {@inheritDoc} */
+    @Override
+    protected <G extends GtuDataInterface> Length calculate(final Query<G> query, final Time startTime, final Time endTime,
+            final List<TrajectoryGroup<G>> trajectoryGroups)
+    {
+        Length sum = Length.ZERO;
+        Set<String> gtuIds = new LinkedHashSet<>();
+        for (TrajectoryGroup<?> trajectoryGroup : trajectoryGroups)
+        {
+            for (Trajectory<?> trajectory : trajectoryGroup.getTrajectories())
+            {
+                sum = sum.plus(trajectory.getTotalLength());
+                gtuIds.add(trajectory.getGtuId());
+            }
+        }
+        return sum.divide(gtuIds.size());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public String toString()
+    {
+        return "MeanTripLength []";
+    }
+
+}
