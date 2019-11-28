9295d1044cb8a34ab5a638ffe9f93c604ded77ef
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2019-11-28 10:29:48 +0000

    Delete old parser classes

--- ots-parser-xml/src/main/java/org/opentrafficsim/road/network/factory/xml/old/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * <p>
- * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
- * <p>
- * @version Jul 23, 2015 <br>
- * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- */
-package org.opentrafficsim.road.network.factory.xml.old;
