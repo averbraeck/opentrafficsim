839100b1117c14c3703b6bc2a9730ec74eb6dafd
pknoppers <pknoppers@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2019-12-02 14:33:16 +0000

    Fixed javadoc. Renamed some method parameters to be more descriptive.

--- trunk/ots-core/src/main/java/org/opentrafficsim/core/gtu/plan/operational/OperationalPlanBuilder.java
+++ trunk/ots-core/src/main/java/org/opentrafficsim/core/gtu/plan/operational/OperationalPlanBuilder.java
@@ -69,23 +69,24 @@ public final class OperationalPlanBuilder
     }
 
     /**
-     * Build a plan with a path and a given start speed to try to reach a provided end speed, exactly at the end of the curve.
-     * The acceleration (and deceleration) are capped by maxAcceleration and maxDeceleration. Therefore, there is no guarantee
-     * that the end speed is actually reached by this plan.
+     * Build a plan with a path and a given start speed to try to reach a provided end speed, exactly at the provided
+     * <code>endTime</code>. The acceleration (and deceleration) are capped by maxAcceleration and maxDeceleration. Therefore,
+     * there is no guarantee that the end speed is actually reached by this plan. <p>
+     * TODO: rename this method buildConstantAccelerationPlan.
      * @param gtu GTU; the GTU for debugging purposes
      * @param path OTSLine3D; the path to drive (provides the length)
      * @param startTime Time; the current time or a time in the future when the plan should start
      * @param startSpeed Speed; the speed at the start of the path
      * @param endSpeed Speed; the required end speed
-     * @param maxAcceleration Acceleration; the maximum acceleration that can be applied, provided as a POSITIVE number
-     * @param maxDeceleration Acceleration; the maximum deceleration that can be applied, provided as a NEGATIVE number
+     * @param maximumAcceleration Acceleration; the maximum acceleration that can be applied, provided as a POSITIVE number
+     * @param maximumDeceleration Acceleration; the maximum deceleration that can be applied, provided as a NEGATIVE number
      * @return the operational plan to accomplish the given end speed
      * @throws OperationalPlanException when the length of the path and the calculated driven distance implied by the
      *             constructed segment list differ more than a given threshold
      */
     public static OperationalPlan buildGradualAccelerationPlan(final GTU gtu, final OTSLine3D path, final Time startTime,
-            final Speed startSpeed, final Speed endSpeed, final Acceleration maxAcceleration,
-            final Acceleration maxDeceleration) throws OperationalPlanException
+            final Speed startSpeed, final Speed endSpeed, final Acceleration maximumAcceleration,
+            final Acceleration maximumDeceleration) throws OperationalPlanException
     {
         Length length = path.getLength();
         OperationalPlan.Segment segment;
@@ -100,16 +101,16 @@ public final class OperationalPlanBuilder
             Acceleration acceleration = endSpeed.minus(startSpeed).divide(duration);
             try
             {
-                if (acceleration.si < 0.0 && acceleration.lt(maxDeceleration))
+                if (acceleration.si < 0.0 && acceleration.lt(maximumDeceleration))
                 {
-                    acceleration = maxDeceleration;
+                    acceleration = maximumDeceleration;
                     // duration = new Duration(abc(acceleration.si / 2, startSpeed.si, -length.si), DurationUnit.SI);
                     duration = new Duration(Solver.firstSolutionAfter(0, acceleration.si / 2, startSpeed.si, -length.si),
                             DurationUnit.SI);
                 }
-                if (acceleration.si > 0.0 && acceleration.gt(maxAcceleration))
+                if (acceleration.si > 0.0 && acceleration.gt(maximumAcceleration))
                 {
-                    acceleration = maxAcceleration;
+                    acceleration = maximumAcceleration;
                     // duration = new Duration(abc(acceleration.si / 2, startSpeed.si, -length.si), DurationUnit.SI);
                     duration = new Duration(Solver.firstSolutionAfter(0, acceleration.si / 2, startSpeed.si, -length.si),
                             DurationUnit.SI);
@@ -149,21 +150,25 @@ public final class OperationalPlanBuilder
      * Build a plan with a path and a given start speed to try to reach a provided end speed. Acceleration or deceleration is as
      * provided, until the end speed is reached. After this, constant end speed is used to reach the end point of the path.
      * There is no guarantee that the end speed is actually reached by this plan. If the end speed is zero, and it is reached
-     * before completing the path, a truncated path that ends where the GTU stops is used instead.
+     * before completing the path, a truncated path that ends where the GTU stops is used instead. The maximum acceleration and
+     * deceleration is limited by the provided values. If these prevent the <code>endSpeed</code> from being reached, the
+     * generated plan is a constant acceleration plan using the limiting value.
      * @param gtu GTU; the GTU for debugging purposes
      * @param path OTSLine3D; the path to drive (provides the length)
      * @param startTime Time; the current time or a time in the future when the plan should start
      * @param startSpeed Speed; the speed at the start of the path
      * @param endSpeed Speed; the required end speed
-     * @param acceleration Acceleration; the acceleration to use if endSpeed &gt; startSpeed, provided as a POSITIVE number
-     * @param deceleration Acceleration; the deceleration to use if endSpeed &lt; startSpeed, provided as a NEGATIVE number
+     * @param maximumAcceleration Acceleration; the acceleration to use if endSpeed &gt; startSpeed, provided as a POSITIVE
+     *            number
+     * @param maximumDeceleration Acceleration; the deceleration to use if endSpeed &lt; startSpeed, provided as a NEGATIVE
+     *            number
      * @return the operational plan to accomplish the given end speed
      * @throws OperationalPlanException when the length of the path and the calculated driven distance implied by the
      *             constructed segment list differ more than a given threshold
      */
     public static OperationalPlan buildMaximumAccelerationPlan(final GTU gtu, final OTSLine3D path, final Time startTime,
-            final Speed startSpeed, final Speed endSpeed, final Acceleration acceleration, final Acceleration deceleration)
-            throws OperationalPlanException
+            final Speed startSpeed, final Speed endSpeed, final Acceleration maximumAcceleration,
+            final Acceleration maximumDeceleration) throws OperationalPlanException
     {
         Length length = path.getLength();
         ArrayList<OperationalPlan.Segment> segmentList = new ArrayList<>();
@@ -177,37 +182,39 @@ public final class OperationalPlanBuilder
             {
                 if (endSpeed.gt(startSpeed))
                 {
-                    Duration t = endSpeed.minus(startSpeed).divide(acceleration);
-                    Length x = startSpeed.times(t).plus(acceleration.times(0.5).times(t).times(t));
+                    Duration t = endSpeed.minus(startSpeed).divide(maximumAcceleration);
+                    Length x = startSpeed.times(t).plus(maximumAcceleration.times(0.5).times(t).times(t));
                     if (x.ge(length))
                     {
                         // we cannot reach the end speed in the given distance with the given acceleration
                         // Duration duration = new Duration(abc(acceleration.si / 2, startSpeed.si, -length.si),
                         // DurationUnit.SI);
                         Duration duration = new Duration(
-                                Solver.firstSolutionAfter(0, acceleration.si / 2, startSpeed.si, -length.si), DurationUnit.SI);
-                        segmentList.add(new OperationalPlan.AccelerationSegment(duration, acceleration));
+                                Solver.firstSolutionAfter(0, maximumAcceleration.si / 2, startSpeed.si, -length.si),
+                                DurationUnit.SI);
+                        segmentList.add(new OperationalPlan.AccelerationSegment(duration, maximumAcceleration));
                     }
                     else
                     {
                         // we reach the (higher) end speed before the end of the segment. Make two segments.
-                        segmentList.add(new OperationalPlan.AccelerationSegment(t, acceleration));
+                        segmentList.add(new OperationalPlan.AccelerationSegment(t, maximumAcceleration));
                         Duration duration = length.minus(x).divide(endSpeed);
                         segmentList.add(new OperationalPlan.SpeedSegment(duration));
                     }
                 }
                 else
                 {
-                    Duration t = endSpeed.minus(startSpeed).divide(deceleration);
-                    Length x = startSpeed.times(t).plus(deceleration.times(0.5).times(t).times(t));
+                    Duration t = endSpeed.minus(startSpeed).divide(maximumDeceleration);
+                    Length x = startSpeed.times(t).plus(maximumDeceleration.times(0.5).times(t).times(t));
                     if (x.ge(length))
                     {
                         // we cannot reach the end speed in the given distance with the given deceleration
                         // Duration duration = new Duration(abc(deceleration.si / 2, startSpeed.si, -length.si),
                         // DurationUnit.SI);
-                        Duration duration = new Duration(
-                                Solver.firstSolutionAfter(0, deceleration.si / 2, startSpeed.si, -length.si), DurationUnit.SI);
-                        segmentList.add(new OperationalPlan.AccelerationSegment(duration, deceleration));
+                        Duration duration =
+                                new Duration(Solver.firstSolutionAfter(0, maximumDeceleration.si / 2, startSpeed.si, -length.si),
+                                        DurationUnit.SI);
+                        segmentList.add(new OperationalPlan.AccelerationSegment(duration, maximumDeceleration));
                     }
                     else
                     {
@@ -215,11 +222,11 @@ public final class OperationalPlanBuilder
                         {
                             // if endSpeed == 0, we cannot reach the end of the path. Therefore, build a partial path.
                             OTSLine3D partialPath = path.truncate(x.si);
-                            segmentList.add(new OperationalPlan.AccelerationSegment(t, deceleration));
+                            segmentList.add(new OperationalPlan.AccelerationSegment(t, maximumDeceleration));
                             return new OperationalPlan(gtu, partialPath, startTime, startSpeed, segmentList);
                         }
                         // we reach the (lower) end speed, larger than zero, before the end of the segment. Make two segments.
-                        segmentList.add(new OperationalPlan.AccelerationSegment(t, deceleration));
+                        segmentList.add(new OperationalPlan.AccelerationSegment(t, maximumDeceleration));
                         Duration duration = length.minus(x).divide(endSpeed);
                         segmentList.add(new OperationalPlan.SpeedSegment(duration));
                     }
@@ -261,7 +268,7 @@ public final class OperationalPlanBuilder
      */
     public static void main(final String[] args) throws OperationalPlanException, OTSGeometryException
     {
-        OTSLine3D path1 = new OTSLine3D(new OTSPoint3D[] {new OTSPoint3D(0.0, 0.0), new OTSPoint3D(100.0, 0.0)});
+        OTSLine3D path1 = new OTSLine3D(new OTSPoint3D[] { new OTSPoint3D(0.0, 0.0), new OTSPoint3D(100.0, 0.0) });
 
         // go from 0 to 10 m/s over entire distance. This should take 20 sec with a=0.5 m/s2.
         OperationalPlan plan1 =
