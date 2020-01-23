3d56b5939762e70a404d90693fabd2fc19c94c70
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-01-23 12:16:31 +0000

    Deployed OTS version 1.03.01

---
+++ release/1.03.01/ots-kpi/src/main/java/org/opentrafficsim/kpi/interfaces/NodeDataInterface.java
@@ -0,0 +1,24 @@
+package org.opentrafficsim.kpi.interfaces;
+
+import org.opentrafficsim.base.Identifiable;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 12 okt. 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public interface NodeDataInterface extends Identifiable
+{
+
+    /**
+     * @return id
+     */
+    @Override
+    String getId();
+
+}
---
+++ release/1.03.01/ots-kpi/src/main/java/org/opentrafficsim/kpi/sampling/meta/MetaDataCrossSections.java
@@ -0,0 +1,117 @@
+package org.opentrafficsim.kpi.sampling.meta;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.djutils.exceptions.Throw;
+import org.opentrafficsim.kpi.interfaces.GtuDataInterface;
+import org.opentrafficsim.kpi.sampling.CrossSection;
+import org.opentrafficsim.kpi.sampling.KpiDirectedLanePosition;
+import org.opentrafficsim.kpi.sampling.TrajectoryAcceptList;
+import org.opentrafficsim.kpi.sampling.TrajectoryGroup;
+
+/**
+ * Accepts trajectories that have passed all cross sections as defined in a query.
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Sep 29, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class MetaDataCrossSections extends MetaDataType<CrossSection>
+{
+
+    /**
+     * 
+     */
+    public MetaDataCrossSections()
+    {
+        super("crossSection");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final CrossSection getValue(final GtuDataInterface gtu)
+    {
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String formatValue(String format, CrossSection value)
+    {
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        String delimiter = "";
+        for (KpiDirectedLanePosition kpiDirectedLanePosition : value.getDirectedLanePositions())
+        {
+            str.append(delimiter);
+            delimiter = "|";
+            str.append(kpiDirectedLanePosition.getLaneData().getId());
+            str.append(kpiDirectedLanePosition.getKpiGtuDirection().isPlus() ? "+" : "-");
+            str.append(String.format(format, kpiDirectedLanePosition.getPosition().si));
+        }
+        str.append("]");
+        return str.toString();
+    }
+
+    /**
+     * Accepts all trajectory's or rejects all trajectory's depending on whether all cross sections have been crossed.
+     */
+    @Override
+    public final void accept(final TrajectoryAcceptList trajectoryAcceptList, final Set<CrossSection> querySet)
+    {
+        Throw.whenNull(trajectoryAcceptList, "Trajectory accept list may not be null.");
+        Throw.whenNull(querySet, "Qeury set may not be null.");
+        Set<CrossSection> crossedCrossSections = new LinkedHashSet<>();
+        // Loop over trajectoryList/trajectoryGroupList combo
+        for (int i = 0; i < trajectoryAcceptList.size(); i++)
+        {
+            TrajectoryGroup trajectoryGroup = trajectoryAcceptList.getTrajectoryGroup(i);
+            // Loop over cross sections
+            Iterator<CrossSection> crossSectionIterator = querySet.iterator();
+            while (crossSectionIterator.hasNext())
+            {
+                CrossSection crossSection = crossSectionIterator.next();
+                // Loop over lanes in cross section
+                Iterator<KpiDirectedLanePosition> directedLanePositionIterator = crossSection.getIterator();
+                while (directedLanePositionIterator.hasNext())
+                {
+                    KpiDirectedLanePosition directedLanePosition = directedLanePositionIterator.next();
+                    // If Trajectories is of same lane and direction, check position
+                    if (trajectoryGroup.getLaneDirection().getLaneData().equals(directedLanePosition.getLaneData())
+                            && trajectoryGroup.getLaneDirection().getKpiDirection()
+                                    .equals(directedLanePosition.getKpiGtuDirection()))
+                    {
+                        double position = directedLanePosition.getPosition().si;
+                        float[] x = trajectoryAcceptList.getTrajectory(i).getX();
+                        double xStart = x[0];
+                        double xEnd = x[x.length - 1];
+                        if ((xStart < position && position < xEnd) || (xEnd < position && position < xStart))
+                        {
+                            // Trajectory was up- and downstream of the location, so the location was crossed
+                            crossedCrossSections.add(crossSection);
+                        }
+                    }
+                }
+            }
+        }
+        if (querySet.equals(crossedCrossSections))
+        {
+            trajectoryAcceptList.acceptAll();
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public String toString()
+    {
+        return "MetaDataCrossSections: [id=" + getId() + "]";
+    }
+
+}
