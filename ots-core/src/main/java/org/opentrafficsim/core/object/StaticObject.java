16380f30ea508967a7f7ece965cf0f3c8f85469a
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2020-01-23 10:27:59 +0000

    Javadoc

--- ots-core/src/main/java/org/opentrafficsim/core/object/StaticObject.java
+++ ots-core/src/main/java/org/opentrafficsim/core/object/StaticObject.java
@@ -60,7 +60,7 @@ public class StaticObject extends EventProducer implements ObjectInterface, Seri
 
     /**
      * Initialize the object after it has been fully created.
-     * @throws NetworkException, e.g. on error registering the object in the network
+     * @throws NetworkException e.g. on error registering the object in the network
      */
     @SuppressWarnings("checkstyle:designforextension")
     protected void init() throws NetworkException
@@ -76,7 +76,7 @@ public class StaticObject extends EventProducer implements ObjectInterface, Seri
      * @param geometry OTSLine3D; the top-level 2D outline of the object
      * @param height Length; the height of the object
      * @return the static object
-     * @throws NetworkException, e.g. on error registering the object in the network
+     * @throws NetworkException e.g. on error registering the object in the network
      */
     public static StaticObject create(final String id, final OTSLine3D geometry, final Length height) throws NetworkException
     {
@@ -90,7 +90,7 @@ public class StaticObject extends EventProducer implements ObjectInterface, Seri
      * @param id String; the id
      * @param geometry OTSLine3D; the top-level 2D outline of the object
      * @return the static object
-     * @throws NetworkException, e.g. on error registering the object in the network
+     * @throws NetworkException e.g. on error registering the object in the network
      */
     public static StaticObject create(final String id, final OTSLine3D geometry) throws NetworkException
     {
