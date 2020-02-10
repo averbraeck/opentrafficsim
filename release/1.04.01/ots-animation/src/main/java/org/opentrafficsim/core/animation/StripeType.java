5ef34439791928fd17888a6084fdb8d226564a96
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-02-10 22:14:15 +0000

    deployed ots version 1.04.01

---
+++ release/1.04.01/ots-animation/src/main/java/org/opentrafficsim/core/animation/StripeType.java
@@ -0,0 +1,30 @@
+package org.opentrafficsim.core.animation;
+
+/**
+ * StripeType.java. <br>
+ * <br>
+ * Copyright (c) 2003-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://www.simulation.tudelft.nl/" target="_blank">www.simulation.tudelft.nl</a>. The
+ * source code and binary code of this software is proprietary information of Delft University of Technology.
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+public enum StripeType
+{
+    /** Single solid line. */
+    SOLID,
+
+    /** Line |¦ allow to go to left, but not to right. */
+    LEFTONLY,
+
+    /** Line ¦| allow to go to right, but not to left. */
+    RIGHTONLY,
+
+    /** Dashes ¦ allow to cross in both directions. */
+    DASHED,
+
+    /** Double solid line ||, don't cross. */
+    DOUBLE,
+
+    /** Block : allow to cross in both directions. */
+    BLOCK
+}
---
+++ release/1.04.01/ots-animation/src/main/java/org/opentrafficsim/road/gtu/colorer/ReactionTimeColorer.java
@@ -0,0 +1,97 @@
+package org.opentrafficsim.road.gtu.colorer;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.djunits.value.vdouble.scalar.Duration;
+import org.opentrafficsim.base.parameters.ParameterTypes;
+import org.opentrafficsim.core.animation.ColorInterpolator;
+import org.opentrafficsim.core.animation.gtu.colorer.GTUColorer;
+import org.opentrafficsim.core.gtu.GTU;
+
+/**
+ * Colors the reaction time.
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 31 jan. 2019 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class ReactionTimeColorer implements GTUColorer
+{
+
+    /** Full. */
+    static final Color HIGH = Color.RED;
+
+    /** Medium. */
+    static final Color MID = Color.YELLOW;
+
+    /** Zero. */
+    static final Color LOW = Color.GREEN;
+
+    /** Fully compensated by anticipation reliance. */
+    static final Color AR = Color.WHITE;
+
+    /** Not available. */
+    static final Color NA = Color.BLUE;
+
+    /** Legend. */
+    private final List<LegendEntry> legend;
+
+    /** Reaction time. */
+    private final Duration maxReactionTime;
+
+    /**
+     * Constructor.
+     * @param maxReactionTime Duration; maximum reaction time.
+     */
+    public ReactionTimeColorer(final Duration maxReactionTime)
+    {
+        this.maxReactionTime = maxReactionTime;
+        this.legend = new ArrayList<>();
+        this.legend.add(new LegendEntry(LOW, maxReactionTime.times(0.0).toString(), "small reaction time"));
+        this.legend.add(new LegendEntry(MID, maxReactionTime.times(0.5).toString(), "medium reaction time"));
+        this.legend.add(new LegendEntry(HIGH, maxReactionTime.toString(), "large reaction time"));
+        this.legend.add(new LegendEntry(NA, "N/A", "N/A"));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Color getColor(final GTU drawable)
+    {
+        Duration tr = drawable.getParameters().getParameterOrNull(ParameterTypes.TR);
+        if (tr == null)
+        {
+            return NA;
+        }
+        if (tr.si < .5 * this.maxReactionTime.si)
+        {
+            return ColorInterpolator.interpolateColor(LOW, MID, tr.si / (.5 * this.maxReactionTime.si));
+        }
+        else if (tr.si <= this.maxReactionTime.si)
+        {
+            return ColorInterpolator.interpolateColor(MID, HIGH,
+                    (tr.si - .5 * this.maxReactionTime.si) / (.5 * this.maxReactionTime.si));
+        }
+        return HIGH;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public List<LegendEntry> getLegend()
+    {
+        return this.legend;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "Reaction time";
+    }
+
+}
