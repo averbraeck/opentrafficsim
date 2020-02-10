d4dd9370963576d0e4d225b4b8602cca562bdedb
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-02-10 22:14:22 +0000

    deployed ots version 1.04.01

---
+++ release/1.04.01/ots-draw/src/main/java/org/opentrafficsim/draw/graphs/ContourPlotAcceleration.java
@@ -0,0 +1,170 @@
+package org.opentrafficsim.draw.graphs;
+
+import java.awt.Color;
+import java.util.List;
+
+import org.djunits.unit.AccelerationUnit;
+import org.djunits.value.ValueRuntimeException;
+import org.djunits.value.storage.StorageType;
+import org.djunits.value.vdouble.matrix.AccelerationMatrix;
+import org.djunits.value.vdouble.matrix.base.DoubleMatrix;
+import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.base.WeightedMeanAndSum;
+import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
+import org.opentrafficsim.core.egtf.Converter;
+import org.opentrafficsim.core.egtf.Quantity;
+import org.opentrafficsim.draw.core.BoundsPaintScale;
+import org.opentrafficsim.draw.graphs.ContourDataSource.ContourDataType;
+import org.opentrafficsim.kpi.sampling.Trajectory;
+import org.opentrafficsim.kpi.sampling.TrajectoryGroup;
+
+/**
+ * Contour plot for acceleration.
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 10 okt. 2018 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class ContourPlotAcceleration extends AbstractContourPlot<Acceleration>
+{
+
+    /** Quantity for the EGTF. */
+    private static final Quantity<Acceleration, AccelerationMatrix> QUANTITY =
+            new Quantity<>("acceleration", new Converter<AccelerationMatrix>()
+            {
+                /** {@inheritDoc} */
+                @Override
+                public AccelerationMatrix convert(final double[][] filteredData)
+                {
+                    try
+                    {
+                        return DoubleMatrix.instantiate(filteredData, AccelerationUnit.SI, StorageType.DENSE);
+                    }
+                    catch (ValueRuntimeException exception)
+                    {
+                        // should not happen as filtered data comes from the EGTF
+                        throw new RuntimeException("Unexpected exception while converting acceleration to output format.",
+                                exception);
+                    }
+                }
+            });
+
+    /** Contour data type. */
+    private static final ContourDataType<Acceleration, WeightedMeanAndSum<Double, Double>> CONTOUR_DATA_TYPE =
+            new ContourDataType<Acceleration, WeightedMeanAndSum<Double, Double>>()
+            {
+                /** {@inheritDoc} */
+                @Override
+                public WeightedMeanAndSum<Double, Double> identity()
+                {
+                    return new WeightedMeanAndSum<>();
+                }
+
+                /** {@inheritDoc} */
+                @Override
+                public WeightedMeanAndSum<Double, Double> processSeries(final WeightedMeanAndSum<Double, Double> intermediate,
+                        final List<TrajectoryGroup<?>> trajectories, final List<Length> xFrom, final List<Length> xTo,
+                        final Time tFrom, final Time tTo)
+                {
+                    for (int i = 0; i < trajectories.size(); i++)
+                    {
+                        TrajectoryGroup<?> trajectoryGroup = trajectories.get(i);
+                        for (Trajectory<?> trajectory : trajectoryGroup.getTrajectories())
+                        {
+                            if (GraphUtil.considerTrajectory(trajectory, tFrom, tTo))
+                            {
+                                trajectory = trajectory.subSet(xFrom.get(i), xTo.get(i), tFrom, tTo);
+                                float[] t = trajectory.getT();
+                                float[] a = trajectory.getA();
+                                for (int j = 0; j < t.length - 1; j++)
+                                {
+                                    intermediate.add((double) a[j], (double) (t[j + 1] - t[j]));
+                                }
+                            }
+                        }
+                    }
+                    return intermediate;
+                }
+
+                /** {@inheritDoc} */
+                @Override
+                public Acceleration finalize(final WeightedMeanAndSum<Double, Double> intermediate)
+                {
+                    return Acceleration.instantiateSI(intermediate.getMean());
+                }
+
+                /** {@inheritDoc} */
+                @Override
+                public Quantity<Acceleration, ?> getQuantity()
+                {
+                    return QUANTITY;
+                }
+
+            };
+
+    /**
+     * Constructor.
+     * @param caption String; caption
+     * @param simulator OTSSimulatorInterface; simulator
+     * @param dataPool ContourDataSource&lt;?&gt;; data pool
+     */
+    public ContourPlotAcceleration(final String caption, final OTSSimulatorInterface simulator,
+            final ContourDataSource<?> dataPool)
+    {
+        super(caption, simulator, dataPool, createPaintScale(), new Acceleration(1.0, AccelerationUnit.SI), "%.0fm/s\u00B2",
+                "acceleration %.2f m/s\u00B2");
+    }
+
+    /**
+     * Creates a paint scale from red, via yellow to green.
+     * @return ContinuousColorPaintScale; paint scale
+     */
+    private static BoundsPaintScale createPaintScale()
+    {
+        double[] boundaries = {-3.0, -1.5, 0.0, 1.0, 2.0};
+        Color[] colorValues = BoundsPaintScale.reverse(BoundsPaintScale.GREEN_RED_DARK);
+        return new BoundsPaintScale(boundaries, colorValues);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public GraphType getGraphType()
+    {
+        return GraphType.ACCELERATION_CONTOUR;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected double scale(final double si)
+    {
+        return si;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected double getValue(final int item, final double cellLength, final double cellSpan)
+    {
+        return getDataPool().get(item, CONTOUR_DATA_TYPE);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected ContourDataType<Acceleration, WeightedMeanAndSum<Double, Double>> getContourDataType()
+    {
+        return CONTOUR_DATA_TYPE;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "ContourPlotAcceleration []";
+    }
+
+}
---
+++ release/1.04.01/ots-draw/src/main/java/org/opentrafficsim/draw/graphs/GraphUtil.java
@@ -0,0 +1,192 @@
+package org.opentrafficsim.draw.graphs;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Time;
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
+import org.jfree.chart.LegendItem;
+import org.jfree.chart.LegendItemCollection;
+import org.jfree.chart.entity.LegendItemEntity;
+import org.opentrafficsim.kpi.sampling.SamplingException;
+import org.opentrafficsim.kpi.sampling.Trajectory;
+
+/**
+ * Contains some static utilities.
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 15 okt. 2018 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public final class GraphUtil
+{
+
+    /**
+     * Constructor.
+     */
+    private GraphUtil()
+    {
+        // no instances
+    }
+
+    /**
+     * Helper method for quick filtering of trajectories by checking if the time of the trajectory has overlap with the given
+     * time.
+     * @param trajectory Trajectory&lt;?&gt;; trajectory
+     * @param startTime Time; start time
+     * @param endTime Time; end time
+     * @return boolean; true if the trajectory should be considered for the given time
+     */
+    public static boolean considerTrajectory(final Trajectory<?> trajectory, final Time startTime, final Time endTime)
+    {
+        try
+        {
+            return trajectory.getT(0) < endTime.si && trajectory.getT(trajectory.size() - 1) > startTime.si;
+        }
+        catch (SamplingException exception)
+        {
+            throw new RuntimeException("Unexpected exception while checking whether the trajectory should be considered.",
+                    exception);
+        }
+    }
+
+    /**
+     * Helper method for quick filtering of trajectories by checking if the position of the trajectory has overlap with the
+     * given range.
+     * @param trajectory Trajectory&lt;?&gt;; trajectory
+     * @param startPosition Length; start position
+     * @param endPosition Length; end position
+     * @return boolean; true if the trajectory should be considered for the given time
+     */
+    public static boolean considerTrajectory(final Trajectory<?> trajectory, final Length startPosition,
+            final Length endPosition)
+    {
+        try
+        {
+            return trajectory.getX(0) < startPosition.si && trajectory.getX(trajectory.size() - 1) > endPosition.si;
+        }
+        catch (SamplingException exception)
+        {
+            throw new RuntimeException("Unexpected exception while checking whether the trajectory should be considered.",
+                    exception);
+        }
+    }
+
+    /**
+     * Ensures that the given capacity is available in the array. The array may become or may be longer than the required
+     * capacity. This method assumes that the array has non-zero length, and that the capacity required is at most 1 more than
+     * what the array can provide.
+     * @param data double[]; data array
+     * @param capacity int; required capacity
+     * @return double[]; array with at least the requested capacity
+     */
+    public static double[] ensureCapacity(final double[] data, final int capacity)
+    {
+        if (data.length < capacity)
+        {
+            double[] out = new double[data.length + (data.length >> 1)];
+            System.arraycopy(data, 0, out, 0, data.length);
+            return out;
+        }
+        return data;
+    }
+
+    /**
+     * Ensures that the given capacity is available in the array. The array may become or may be longer than the required
+     * capacity. This method assumes that the array has non-zero length, and that the capacity required is at most 1 more than
+     * what the array can provide.
+     * @param data float[]; data array
+     * @param capacity int; required capacity
+     * @return float[]; array with at least the requested capacity
+     */
+    public static float[] ensureCapacity(final float[] data, final int capacity)
+    {
+        if (data.length < capacity)
+        {
+            float[] out = new float[data.length + (data.length >> 1)];
+            System.arraycopy(data, 0, out, 0, data.length);
+            return out;
+        }
+        return data;
+    }
+
+    /**
+     * Ensures that the given capacity is available in the array. The array may become or may be longer than the required
+     * capacity. This method assumes that the array has non-zero length, and that the capacity required is at most 1 more than
+     * what the array can provide.
+     * @param data int[]; data array
+     * @param capacity int; required capacity
+     * @return int[]; array with at least the requested capacity
+     */
+    public static int[] ensureCapacity(final int[] data, final int capacity)
+    {
+        if (data.length < capacity)
+        {
+            int[] out = new int[data.length + (data.length >> 1)];
+            System.arraycopy(data, 0, out, 0, data.length);
+            return out;
+        }
+        return data;
+    }
+
+    /**
+     * Returns a chart listener that allows the series to be enabled and disabled by clicking on the respective legend item.
+     * @param legend LegendItemCollection; legend
+     * @param visibility List&lt;Boolean&gt;; visibility of each series; the listener will store visibility in this list, which
+     *            an {@code AbstractRenderer} can use in {@code isSeriesVisible(series)} to show or hide the series
+     * @param <K> underlying key type of the series
+     * @return ChartMouseListener; listener that will allow series to be enabled and disabled by clicking on the respective
+     *         legend item
+     */
+    @SuppressWarnings("unchecked")
+    public static <K> ChartMouseListener getToggleSeriesByLegendListener(final LegendItemCollection legend,
+            final List<Boolean> visibility)
+    {
+        Map<K, Paint> colors = new LinkedHashMap<>();
+        Map<K, Integer> series = new LinkedHashMap<>();
+        for (int i = 0; i < legend.getItemCount(); i++)
+        {
+            LegendItem legendItem = legend.get(i);
+            colors.put((K) legendItem.getSeriesKey(), legendItem.getFillPaint());
+            series.put((K) legendItem.getSeriesKey(), i);
+            legendItem.setToolTipText("Click to show/hide");
+        }
+        if (legend.getItemCount() < 2)
+        {
+            return null;
+        }
+        return new ChartMouseListener()
+        {
+            /** {@inheritDoc} */
+            @Override
+            public void chartMouseClicked(final ChartMouseEvent event)
+            {
+                if (event.getEntity() instanceof LegendItemEntity)
+                {
+                    K key = (K) ((LegendItemEntity) event.getEntity()).getSeriesKey();
+                    int s = series.get(key);
+                    boolean visible = !visibility.get(s);
+                    visibility.set(s, visible);
+                    legend.get(s).setLabelPaint(visible ? Color.BLACK : Color.LIGHT_GRAY);
+                    legend.get(s).setFillPaint(visible ? colors.get(key) : Color.LIGHT_GRAY);
+                }
+            }
+
+            /** {@inheritDoc} */
+            @Override
+            public void chartMouseMoved(final ChartMouseEvent event)
+            {
+                //
+            }
+        };
+    }
+}
