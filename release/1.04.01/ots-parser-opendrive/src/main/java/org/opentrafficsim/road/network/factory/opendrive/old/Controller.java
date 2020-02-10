5d89a4def981d3fb8a06ab1b4de721a93b65da0f
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-02-10 22:14:58 +0000

    deployed ots version 1.04.01

---
+++ release/1.04.01/ots-parser-opendrive/src/main/java/org/opentrafficsim/road/network/factory/opendrive/old/Controller.java
@@ -0,0 +1,274 @@
+package org.opentrafficsim.road.network.factory.opendrive.old;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.naming.NamingException;
+
+import org.djunits.unit.DurationUnit;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.opentrafficsim.core.gtu.GTUException;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.road.network.lane.object.trafficlight.SimpleTrafficLight;
+import org.opentrafficsim.road.network.lane.object.trafficlight.TrafficLightColor;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.simulators.AnimatorInterface;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+
+/**
+ * Traffic Light Controller
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision: 1155 $, $LastChangedDate: 2015-07-26 01:01:13 +0200 (Sun, 26 Jul 2015) $, by $Author: averbraeck $,
+ *          initial version 15 jul. 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class Controller implements Serializable
+{
+
+    /** */
+    private static final long serialVersionUID = 20150715L;
+
+    /** */
+    private DEVSSimulatorInterface.TimeDoubleUnit simulator;
+
+    /** */
+    private String id;
+
+    /** */
+    private Map<Integer, Set<SimpleTrafficLight>> trafficLights = new LinkedHashMap<Integer, Set<SimpleTrafficLight>>();
+
+    /**
+     * @param name String; the name of the OnOffTrafficLight
+     * @param simulator DEVSSimulatorInterface.TimeDoubleUnit; the simulator to avoid NullPointerExceptions
+     * @throws GTUException when GTU cannot be created.
+     * @throws NamingException if an error occurs when adding the animation handler
+     * @throws NetworkException when the GTU cannot be placed on the given lane
+     */
+    public Controller(final String name, final DEVSSimulatorInterface.TimeDoubleUnit simulator)
+            throws GTUException, NetworkException, NamingException
+    {
+        this.id = name;
+        this.simulator = simulator;
+
+        try
+        {
+            // new DefaultBlockOnOffAnimation(this, getSimulator());
+            // animation
+            if (simulator instanceof AnimatorInterface)
+            {
+                // TODO
+            }
+            simulator.scheduleEventRel(Duration.ZERO, this, this, "unBlock", null);
+        }
+        catch (SimRuntimeException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * 
+     */
+    protected void unBlock()
+    {
+        try
+        {
+            this.simulator.scheduleEventRel(new Duration(96.0, DurationUnit.SECOND), this, this, "unBlock", null);
+        }
+        catch (SimRuntimeException exception)
+        {
+            exception.printStackTrace();
+        }
+
+        if (this.trafficLights.containsKey(3))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(3))
+            {
+                try
+                {
+                    // System.out.println("traffic light 3 at time " + this.simulator.getSimulatorTime() + " is " +
+                    // ((TrafficLight) light).getTrafficLightColor().toString());
+                    this.simulator.scheduleEventRel(Duration.ZERO, this, light, "setTrafficLightColor",
+                            new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(15.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(24.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(4))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(4))
+            {
+                try
+                {
+                    this.simulator.scheduleEventRel(Duration.ZERO, this, light, "setTrafficLightColor",
+                            new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(15.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(24.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(6))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(6))
+            {
+                try
+                {
+                    // System.out.println("traffic light 6 at time " + this.simulator.getSimulatorTime());
+
+                    this.simulator.scheduleEventRel(new Duration(24.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(39.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(48.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(7))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(7))
+            {
+                try
+                {
+                    this.simulator.scheduleEventRel(new Duration(24.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(39.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(48.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(9))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(9))
+            {
+                try
+                {
+                    // System.out.println("traffic light 9 at time " + this.simulator.getSimulatorTime());
+
+                    this.simulator.scheduleEventRel(new Duration(48.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(63.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(72.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(10))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(10))
+            {
+                try
+                {
+                    this.simulator.scheduleEventRel(new Duration(48.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(63.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(72.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(12))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(12))
+            {
+                try
+                {
+                    // System.out.println("traffic light 12 at time " + this.simulator.getSimulatorTime());
+
+                    this.simulator.scheduleEventRel(new Duration(72.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(87.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(96.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+        if (this.trafficLights.containsKey(13))
+        {
+            for (SimpleTrafficLight light : this.trafficLights.get(13))
+            {
+                try
+                {
+                    this.simulator.scheduleEventRel(new Duration(72.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.GREEN});
+                    this.simulator.scheduleEventRel(new Duration(87.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.YELLOW});
+                    this.simulator.scheduleEventRel(new Duration(96.0, DurationUnit.SECOND), this, light,
+                            "setTrafficLightColor", new Object[] {TrafficLightColor.RED});
+                }
+                catch (SimRuntimeException exception)
+                {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * @param sequence2 int; sequence2
+     * @param trafficLight SimpleTrafficLight; trafficLight
+     */
+    public void addTrafficLight(int sequence2, SimpleTrafficLight trafficLight)
+    {
+        if (!this.trafficLights.containsKey(sequence2))
+        {
+            Set<SimpleTrafficLight> trafficLightsSet = new LinkedHashSet<SimpleTrafficLight>();
+            this.trafficLights.put(sequence2, trafficLightsSet);
+        }
+        this.trafficLights.get(sequence2).add(trafficLight);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return "Controller [id=" + this.id + ", trafficLights=" + this.trafficLights + "]";
+    }
+
+}
---
+++ release/1.04.01/ots-parser-opendrive/src/main/java/org/opentrafficsim/road/network/factory/opendrive/old/SignalsTag.java
@@ -0,0 +1,71 @@
+package org.opentrafficsim.road.network.factory.opendrive.old;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opentrafficsim.core.network.NetworkException;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Jul 23, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+class SignalsTag implements Serializable
+{
+
+    /** */
+    private static final long serialVersionUID = 20150723L;
+
+    /** Signal tags */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    List<SignalTag> signalTags = new ArrayList<SignalTag>();
+
+    /** Signal reference tag */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    List<SignalReferenceTag> signalReferenceTag = new ArrayList<SignalReferenceTag>();
+
+    /**
+     * Parse the attributes of the road tag. The sub-elements are parsed in separate classes.
+     * @param nodeList NodeList; the list of subnodes of the road node
+     * @param parser OpenDriveNetworkLaneParser; the parser with the lists of information
+     * @param roadTag RoadTag; the RoadTag to which this element belongs
+     * @throws SAXException when parsing of the tag fails
+     * @throws NetworkException when parsing of the tag fails
+     */
+    @SuppressWarnings("checkstyle:needbraces")
+    static void parseSignals(final NodeList nodeList, final OpenDriveNetworkLaneParserOld parser, final RoadTag roadTag)
+            throws SAXException, NetworkException
+    {
+        SignalsTag signalsTag = new SignalsTag();
+        for (Node node0 : XMLParser.getNodes(nodeList, "signals"))
+        {
+            for (Node node : XMLParser.getNodes(node0.getChildNodes(), "signal"))
+            {
+                SignalTag signalTag = SignalTag.parseSignal(node, parser);
+                signalsTag.signalTags.add(signalTag);
+                parser.signalTags.put(signalTag.id, signalTag);
+            }
+            for (Node node : XMLParser.getNodes(node0.getChildNodes(), "signalReference"))
+            {
+                SignalReferenceTag signalReferenceTag = SignalReferenceTag.parseSignalReference(node, parser);
+                signalsTag.signalReferenceTag.add(signalReferenceTag);
+            }
+        }
+        roadTag.signalsTag = signalsTag;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "SignalsTag [signalTags=" + this.signalTags + ", signalReferenceTag=" + this.signalReferenceTag + "]";
+    }
+}
