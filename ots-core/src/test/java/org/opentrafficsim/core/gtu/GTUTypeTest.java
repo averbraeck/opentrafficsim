ca61298fd9d60479aa7a0e39d773fe2abd41fa46
wjschakel <89968231+WJSchakel@users.noreply.github.com>
2020-02-13 10:30:49 +0000

    GTUType.defaultCharacteristics() now no longer creates duplicate templates. Test for this method is added.

--- ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUTypeTest.java
+++ ots-core/src/test/java/org/opentrafficsim/core/gtu/GTUTypeTest.java
@@ -3,8 +3,13 @@ package org.opentrafficsim.core.gtu;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.djutils.exceptions.Try;
 import org.junit.Test;
+import org.opentrafficsim.core.gtu.GTUType.DEFAULTS;
 import org.opentrafficsim.core.network.OTSNetwork;
+
+import nl.tudelft.simulation.jstats.streams.MersenneTwister;
+import nl.tudelft.simulation.jstats.streams.StreamInterface;
 
 /**
  * Test the methods and fields in the GTUType class.
@@ -33,5 +38,34 @@ public class GTUTypeTest
         // prove that the two are really distinct (do not use the same storage for the type string
         assertTrue("Id is stored in the newly created GTUType", "abc".equals(t.getId()));
         assertEquals("parent can be retrieved", network.getGtuType(GTUType.DEFAULTS.VEHICLE), t.getParent());
+    }
+
+    /**
+     * Check default GTU characteristics.
+     */
+    @Test
+    public final void defaultsTest()
+    {
+        OTSNetwork network = new OTSNetwork("network", true);
+        StreamInterface randomStream = new MersenneTwister();
+        GTUType car = network.getGtuType(DEFAULTS.CAR);
+        GTUCharacteristics characteristicsCar1 = Try.assign(() -> GTUType.defaultCharacteristics(car, network, randomStream),
+            "Exception while deriving default GTU characteristics");
+        GTUCharacteristics characteristicsCar2 = Try.assign(() -> GTUType.defaultCharacteristics(car, network, randomStream),
+            "Exception while deriving default GTU characteristics");
+        GTUType spaceCar = new GTUType("spaceCar", car);
+        GTUCharacteristics characteristicsSpaceCar1 = Try.assign(() -> GTUType.defaultCharacteristics(spaceCar, network,
+            randomStream), "Exception while deriving default GTU characteristics");
+        GTUCharacteristics characteristicsSpaceCar2 = Try.assign(() -> GTUType.defaultCharacteristics(spaceCar, network,
+            randomStream), "Exception while deriving default GTU characteristics");
+
+        // Note: we can only compare characteristics that we know are not distributed for the used GTU type CAR.
+        String message = "Default characteristics of DEFAULTS and derived GTUType should be equal.";
+        assertEquals(message, characteristicsCar1.getLength(), characteristicsCar2.getLength());
+        assertEquals(message, characteristicsCar1.getLength(), characteristicsSpaceCar1.getLength());
+        assertEquals(message, characteristicsCar1.getLength(), characteristicsSpaceCar2.getLength());
+        assertEquals(message, characteristicsCar1.getWidth(), characteristicsCar2.getWidth());
+        assertEquals(message, characteristicsCar1.getWidth(), characteristicsSpaceCar1.getWidth());
+        assertEquals(message, characteristicsCar1.getWidth(), characteristicsSpaceCar2.getWidth());
     }
 }
