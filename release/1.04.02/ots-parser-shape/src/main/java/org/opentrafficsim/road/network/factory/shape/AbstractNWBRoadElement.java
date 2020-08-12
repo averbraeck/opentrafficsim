a09cdc2eee03027d00814d85a6d70bb6c4ef1f04
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-08-12 03:17:20 +0000

    Deployed version 1.04.02

---
+++ release/1.04.02/ots-parser-shape/src/main/java/org/opentrafficsim/road/network/factory/shape/AbstractNWBRoadElement.java
@@ -0,0 +1,80 @@
+/**
+ *
+ */
+package org.opentrafficsim.road.network.factory.shape;
+
+import org.locationtech.jts.geom.Geometry;
+import org.opentrafficsim.core.network.OTSNode;
+
+/**
+ * @author P070518
+ */
+public abstract class AbstractNWBRoadElement
+{
+
+    // the geometry
+    private Geometry myGeom;
+
+    private OTSNode startNode;
+
+    private OTSNode endNode;
+
+    // the unique NWB wegvak ID
+    private String roadId;
+
+    // relative distance at begin of link
+    private Double beginDistance;
+
+    // relative distance at end of link
+    private Double endDistance;
+
+    /**
+     * @param myGeom Geometry;
+     * @param startNode OTSNode;
+     * @param endNode OTSNode;
+     * @param roadId String;
+     * @param beginDistance Double;
+     * @param endDistance Double;
+     */
+    public AbstractNWBRoadElement(final Geometry myGeom, final OTSNode startNode, final OTSNode endNode, final String roadId,
+            final Double beginDistance, final Double endDistance)
+    {
+        this.myGeom = myGeom;
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.roadId = roadId;
+        this.beginDistance = beginDistance;
+        this.endDistance = endDistance;
+    }
+
+    public Geometry getMyGeom()
+    {
+        return myGeom;
+    }
+
+    public OTSNode getStartNode()
+    {
+        return startNode;
+    }
+
+    public OTSNode getEndNode()
+    {
+        return endNode;
+    }
+
+    public String getRoadId()
+    {
+        return roadId;
+    }
+
+    public Double getBeginDistance()
+    {
+        return beginDistance;
+    }
+
+    public Double getEndDistance()
+    {
+        return endDistance;
+    }
+
+}
