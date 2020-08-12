87c114bde5a62d6a5e0c0531953285dbd2942d85
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-08-12 03:18:05 +0000

    Deployed version 1.04.02

---
+++ release/1.04.02/ots-sim0mq/src/main/java/org/opentrafficsim/sim0mq/publisher/NodeIdTransceiver.java
@@ -0,0 +1,43 @@
+package org.opentrafficsim.sim0mq.publisher;
+
+import org.djutils.immutablecollections.ImmutableSet;
+import org.opentrafficsim.core.network.Node;
+import org.opentrafficsim.core.network.OTSNetwork;
+
+/**
+ * Transceiver for Node ids.
+ * <p>
+ * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+
+public class NodeIdTransceiver extends AbstractIdTransceiver
+{
+    /**
+     * Construct a new LinkIdTransceiver.
+     * @param network OTSNetwork; the OTS network
+     */
+    public NodeIdTransceiver(final OTSNetwork network)
+    {
+        super(network, "Node id transceiver");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "NodeIdTransceiver [network=" + ", super=" + super.toString() + "]";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    ImmutableSet<Node> getSet()
+    {
+        return (ImmutableSet<Node>) getNetwork().getNodeMap().values();
+    }
+
+}
