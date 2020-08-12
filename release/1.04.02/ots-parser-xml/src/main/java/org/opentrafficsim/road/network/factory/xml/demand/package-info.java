315fb8e8fadd5a73c9f0c0c30c45152ac336399e
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-08-12 03:17:01 +0000

    Deployed version 1.04.02

---
+++ release/1.04.02/ots-parser-xml/src/main/java/org/opentrafficsim/road/network/factory/xml/demand/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 25 mei 2018 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+
+package org.opentrafficsim.road.network.factory.xml.demand;
