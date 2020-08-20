e158f1fc5e4bcdc81f396422a1df5576b2819d71
OTSim <P.Knoppers@tudelft.nl>
2020-08-20 13:48:46 +0000

    Initial commit of the CACC controlled GTU code.

---
+++ ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/cacc/LongitudinalControllerFactory.java
@@ -0,0 +1,24 @@
+package org.opentrafficsim.road.gtu.lane.tactical.cacc;
+
+/**
+ * Factory for controller.
+ * <p>
+ * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 31 okt. 2018 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ * @param <T> type of controller
+ */
+public interface LongitudinalControllerFactory<T extends LongitudinalController>
+{
+
+    /**
+     * Creates the next longitudinal controller.
+     * @return T; next longitudinal controller
+     */
+    T create();
+
+}
