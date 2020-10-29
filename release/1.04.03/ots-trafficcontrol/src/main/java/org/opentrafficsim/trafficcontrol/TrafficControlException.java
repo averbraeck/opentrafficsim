ca0be0b08d5de9a7ccbfcbc466e387cf265b4356
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-10-29 13:58:58 +0000

    Deployed version 1.04.03

---
+++ release/1.04.03/ots-trafficcontrol/src/main/java/org/opentrafficsim/trafficcontrol/TrafficControlException.java
@@ -0,0 +1,64 @@
+package org.opentrafficsim.trafficcontrol;
+
+/**
+ * Exceptions thrown by traffic control programs.
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version Nov 16, 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class TrafficControlException extends Exception
+{
+
+    /** */
+    private static final long serialVersionUID = 20161116;
+
+    /**
+     * 
+     */
+    public TrafficControlException()
+    {
+    }
+
+    /**
+     * @param message String; String
+     */
+    public TrafficControlException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * @param cause Throwable; Throwable
+     */
+    public TrafficControlException(final Throwable cause)
+    {
+        super(cause);
+    }
+
+    /**
+     * @param message String; String
+     * @param cause Throwable; Throwable
+     */
+    public TrafficControlException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * @param message String; description of the problem
+     * @param cause Throwable; the cause of this Exception
+     * @param enableSuppression boolean; whether or not suppression is enabled or disabled
+     * @param writableStackTrace boolean; whether or not the stack trace should be writable
+     */
+    public TrafficControlException(final String message, final Throwable cause, final boolean enableSuppression,
+            final boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
