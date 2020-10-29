fd3907c590621c5c4c2abb8cb2ca3b8bde7d03c2
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-10-29 14:00:36 +0000

    Deployed version 1.04.03

---
+++ release/1.04.03/ots-sim0mq/src/main/java/org/opentrafficsim/sim0mq/publisher/GTUIdTransceiver.java
@@ -0,0 +1,44 @@
+package org.opentrafficsim.sim0mq.publisher;
+
+import org.djutils.immutablecollections.Immutable;
+import org.djutils.immutablecollections.ImmutableLinkedHashSet;
+import org.djutils.immutablecollections.ImmutableSet;
+import org.opentrafficsim.core.gtu.GTU;
+import org.opentrafficsim.core.network.OTSNetwork;
+
+/**
+ * Transceiver for GTU ids.
+ * <p>
+ * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class GTUIdTransceiver extends AbstractIdTransceiver
+{
+    /**
+     * Construct a GTUIdTransceiver.
+     * @param network OTSNetwork; the OTS network
+     */
+    public GTUIdTransceiver(final OTSNetwork network)
+    {
+        super(network, "GTU id transceiver");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    ImmutableSet<GTU> getSet()
+    {
+        return new ImmutableLinkedHashSet<GTU>(getNetwork().getGTUs(), Immutable.WRAP);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "GTUIdTransceiver [network=" + getNetwork().getId() + ", super=" + super.toString() + "]";
+    }
+
+}
