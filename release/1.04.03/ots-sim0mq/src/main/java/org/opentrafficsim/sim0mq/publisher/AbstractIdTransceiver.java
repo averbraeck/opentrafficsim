92a3c12167f8e33414c59779f153171a1331bf23
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-10-31 05:13:07 +0000

    Re-deployed version 1.04.03

---
+++ release/1.04.03/ots-sim0mq/src/main/java/org/opentrafficsim/sim0mq/publisher/AbstractIdTransceiver.java
@@ -0,0 +1,78 @@
+package org.opentrafficsim.sim0mq.publisher;
+
+import org.djunits.Throw;
+import org.djutils.immutablecollections.ImmutableSet;
+import org.djutils.metadata.MetaData;
+import org.djutils.metadata.ObjectDescriptor;
+import org.djutils.serialization.SerializationException;
+import org.opentrafficsim.base.Identifiable;
+import org.opentrafficsim.core.network.OTSNetwork;
+import org.sim0mq.Sim0MQException;
+
+/**
+ * Common code for id transceivers that use an empty address.
+ * <p>
+ * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public abstract class AbstractIdTransceiver extends AbstractTransceiver
+{
+    /** The network. */
+    private final OTSNetwork network;
+
+    /**
+     * Construct a GTUIdTransceiver.
+     * @param network OTSNetwork; the OTS network
+     * @param id String; name of the IdTransceiver
+     */
+    public AbstractIdTransceiver(final OTSNetwork network, final String id)
+    {
+        super(id, new MetaData("No address", "empty address", new ObjectDescriptor[0]),
+                new MetaData("No address", "empty address", new ObjectDescriptor[] { new ObjectDescriptor("String array",
+                        "String array filled with all currently valid GTU ids", String[].class) }));
+        Throw.whenNull(network, "Network may not be null");
+        this.network = network;
+    }
+
+    /**
+     * Retrieve the set of names of objects that can be individually subscribed to.
+     * @return Set&lt;?&gt;; the set of names of objects whose that can be subscribed to. Each object in this set should
+     *         implement <code>Identifiable</code>
+     */
+    abstract ImmutableSet<?> getSet();
+
+    /** {@inheritDoc} */
+    @Override
+    public final Object[] get(final Object[] address, final ReturnWrapper returnWrapper)
+            throws Sim0MQException, SerializationException
+    {
+        String bad = verifyMetaData(getAddressFields(), address);
+        if (bad != null)
+        {
+            returnWrapper.nack(bad);
+            return null;
+        }
+        ImmutableSet<?> set = getSet();
+        Object[] result = new Object[set.size()];
+        int nextIndex = 0;
+        for (Object object : set)
+        {
+            result[nextIndex++] = ((Identifiable) object).getId();
+        }
+        return result;
+    }
+
+    /**
+     * Retrieve the network.
+     * @return OTSNetwork; the network
+     */
+    final OTSNetwork getNetwork()
+    {
+        return this.network;
+    }
+
+}
---
+++ release/1.04.03/ots-sim0mq/src/main/java/org/opentrafficsim/sim0mq/publisher/EventSerializer.java
@@ -0,0 +1,110 @@
+package org.opentrafficsim.sim0mq.publisher;
+
+import org.djunits.Throw;
+import org.djutils.event.Event;
+import org.djutils.event.EventType;
+import org.djutils.event.EventTypeInterface;
+import org.djutils.metadata.MetaData;
+import org.djutils.serialization.EndianUtil;
+import org.djutils.serialization.SerializationException;
+import org.djutils.serialization.serializers.Pointer;
+import org.djutils.serialization.serializers.Serializer;
+
+/**
+ * Convert one Event into an equivalent Sim0MQ Message, or back.
+ * <p>
+ * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ * @param <E> event type
+ */
+public class EventSerializer<E extends Event> implements Serializer<Event>
+{
+    /** The type of event that this EventSerializer handles. */
+    private final EventType eventType;
+
+    /**
+     * Construct a new EventSerializer for the specified <code>EventType</code>.
+     * @param eventType EventType; the event type
+     */
+    EventSerializer(final EventType eventType)
+    {
+        this.eventType = eventType;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int size(final Event event) throws SerializationException
+    {
+        EventTypeInterface et = event.getType();
+        Throw.when(et.equals(this.eventType), ClassCastException.class,
+                "EventSerializer for " + this.eventType.getName() + " cannot handle event " + event);
+        int result = et.getName().length();
+        MetaData metaData = this.eventType.getMetaData();
+        for (int index = 0; index < metaData.size(); index++)
+        {
+            // TODO: There has to be sme content here?
+        }
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int sizeWithPrefix(final Event event) throws SerializationException
+    {
+        return 1 + size(event);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public byte fieldType()
+    {
+        return 33;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void serialize(final Event object, final byte[] buffer, final Pointer pointer, final EndianUtil endianUtil)
+            throws SerializationException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void serializeWithPrefix(final Event object, final byte[] buffer, final Pointer pointer, final EndianUtil endianUtil)
+            throws SerializationException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Event deSerialize(final byte[] buffer, final Pointer pointer, final EndianUtil endianUtil)
+            throws SerializationException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String dataClassName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int getNumberOfDimensions()
+    {
+        return 0;
+    }
+
+}
