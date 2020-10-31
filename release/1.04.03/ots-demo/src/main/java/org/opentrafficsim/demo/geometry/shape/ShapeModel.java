1049db7e9d9b6ffb43d5a0443908dc4e74acca22
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-10-31 05:13:24 +0000

    Re-deployed version 1.04.03

---
+++ release/1.04.03/ots-demo/src/main/java/org/opentrafficsim/demo/geometry/shape/ShapeModel.java
@@ -0,0 +1,81 @@
+package org.opentrafficsim.demo.geometry.shape;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.djunits.value.vdouble.scalar.Time;
+import org.opentrafficsim.core.dsol.AbstractOTSModel;
+import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
+import org.opentrafficsim.core.network.Link;
+import org.opentrafficsim.road.network.OTSRoadNetwork;
+import org.opentrafficsim.road.network.lane.OTSRoadNode;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Sep 9, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.citg.tudelft.nl">Guus Tamminga</a>
+ */
+public class ShapeModel extends AbstractOTSModel
+{
+    /** */
+    private static final long serialVersionUID = 20140815L;
+
+    /** Nodes from shape file. */
+    private Map<String, OTSRoadNode> nodes;
+
+    /** Links from shape file. */
+    private Map<String, Link> shpLinks;
+
+    /** the network. */
+    private OTSRoadNetwork network = new OTSRoadNetwork("shape model network", true, getSimulator());
+
+    /**
+     * @param simulator OTSSimulatorInterface; the simulator
+     */
+    public ShapeModel(final OTSSimulatorInterface simulator)
+    {
+        super(simulator);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void constructModel() throws SimRuntimeException
+    {
+        try
+        {
+            // Read the shape files with the function:
+            this.nodes = ShapeFileReader.readNodes(network, "/gis/TESTcordonnodes.shp", "NODENR", true, true);
+            this.shpLinks = new LinkedHashMap<>();
+            ShapeFileReader.readLinks(network, "/gis/TESTcordonlinks_aangevuld.shp", this.shpLinks, this.nodes, this.simulator);
+
+            this.simulator.scheduleEventAbs(Time.ZERO, this, this, "ntmFlowTimestep", null);
+        }
+        catch (Throwable exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final OTSRoadNetwork getNetwork()
+    {
+        return this.network;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Serializable getSourceId()
+    {
+        return "ShapeModel";
+    }
+
+}
---
+++ release/1.04.03/ots-demo/src/main/java/org/opentrafficsim/demo/geometry/shape/ShapeTestApplication.java
@@ -0,0 +1,90 @@
+package org.opentrafficsim.demo.geometry.shape;
+
+import java.awt.Dimension;
+import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+
+import javax.naming.NamingException;
+
+import org.djunits.unit.util.UNITS;
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Time;
+import org.djutils.event.TimedEvent;
+import org.opentrafficsim.core.animation.gtu.colorer.DefaultSwitchableGTUColorer;
+import org.opentrafficsim.core.dsol.OTSAnimator;
+import org.opentrafficsim.core.dsol.OTSReplication;
+import org.opentrafficsim.draw.core.OTSDrawingException;
+import org.opentrafficsim.draw.factory.DefaultAnimationFactory;
+import org.opentrafficsim.swing.gui.AnimationToggles;
+import org.opentrafficsim.swing.gui.OTSAnimationPanel;
+import org.opentrafficsim.swing.gui.OTSSwingApplication;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.experiment.Replication;
+import nl.tudelft.simulation.dsol.experiment.ReplicationMode;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDoubleUnit;
+import nl.tudelft.simulation.dsol.swing.gui.DSOLApplication;
+import nl.tudelft.simulation.dsol.swing.gui.DSOLPanel;
+import nl.tudelft.simulation.language.DSOLException;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Aug 15, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class ShapeTestApplication extends DSOLApplication implements UNITS
+{
+    /**
+     * @param title String; String title of the application window
+     * @param panel DSOLPanel&lt;Time,Duration,SimTimeDoubleUnit&gt;; DSOLPanel
+     */
+    public ShapeTestApplication(final String title, final DSOLPanel<Time, Duration, SimTimeDoubleUnit> panel)
+    {
+        super(title, panel);
+    }
+
+    /** */
+    private static final long serialVersionUID = 20140819L;
+
+    /**
+     * @param args String[]; command line arguments
+     * @throws SimRuntimeException on ???
+     * @throws NamingException on ???
+     * @throws IOException on ???
+     * @throws OTSDrawingException on drawing error
+     * @throws DSOLException when the simulator does not implement the AnimatorInterface
+     */
+    public static void main(final String[] args)
+            throws SimRuntimeException, NamingException, IOException, OTSDrawingException, DSOLException
+    {
+        OTSAnimator simulator = new OTSAnimator("ShapeTestApplication");
+        ShapeModel model = new ShapeModel(simulator);
+        OTSReplication replication =
+                OTSReplication.create("rep1", Time.ZERO, Duration.ZERO, new Duration(7200.0, SECOND), model);
+        simulator.initialize(replication, ReplicationMode.TERMINATING);
+
+        DSOLPanel<Time, Duration, SimTimeDoubleUnit> panel = new DSOLPanel<Time, Duration, SimTimeDoubleUnit>(model, simulator);
+
+        Rectangle2D extent = new Rectangle2D.Double(65000.0, 440000.0, 55000.0, 30000.0);
+        Dimension size = new Dimension(1024, 768);
+        OTSAnimationPanel animationPanel =
+                new OTSAnimationPanel(extent, size, simulator, model, new DefaultSwitchableGTUColorer(), model.getNetwork());
+        panel.getTabbedPane().addTab(0, "animation", animationPanel);
+
+        DefaultAnimationFactory.animateNetwork(model.getNetwork(), model.getSimulator(), OTSSwingApplication.DEFAULT_COLORER);
+        AnimationToggles.setTextAnimationTogglesStandard(animationPanel);
+
+        // tell the animation panel to update its statistics
+        // TODO should be done automatically in DSOL!
+        animationPanel.notify(
+                new TimedEvent(Replication.START_REPLICATION_EVENT, simulator, null, simulator.getSimulatorTime()));
+
+        new ShapeTestApplication("Network Transmission Model", panel);
+        animationPanel.enableSimulationControlButtons();
+    }
+
+}
