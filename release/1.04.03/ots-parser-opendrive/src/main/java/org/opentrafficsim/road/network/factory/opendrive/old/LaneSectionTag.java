51dd415b6bd5f7e53b5896f06e83e256b186651b
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-10-31 05:12:15 +0000

    Re-deployed version 1.04.03

---
+++ release/1.04.03/ots-parser-opendrive/src/main/java/org/opentrafficsim/road/network/factory/opendrive/old/LaneSectionTag.java
@@ -0,0 +1,134 @@
+package org.opentrafficsim.road.network.factory.opendrive.old;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.value.vdouble.scalar.Length;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.road.network.lane.Lane;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Jul 23, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+class LaneSectionTag implements Serializable
+{
+
+    /** */
+    private static final long serialVersionUID = 20150723L;
+
+    /** Sequence of the laneSection. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    int id = 0;
+
+    /** Start position (s-coordinate). */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Length s = null;
+
+    /** Left lanes. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Map<Integer, LaneTag> leftLaneTags = new LinkedHashMap<Integer, LaneTag>();
+
+    /** Center lanes. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Map<Integer, LaneTag> centerLaneTags = new LinkedHashMap<Integer, LaneTag>();
+
+    /** Right lanes. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Map<Integer, LaneTag> rightLaneTags = new LinkedHashMap<Integer, LaneTag>();
+
+    /** All lanes. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Map<Integer, Lane> lanes = new LinkedHashMap<Integer, Lane>();
+
+    /**
+     * Parse the attributes of the road tag. The sub-elements are parsed in separate classes.
+     * @param node Node; the top-level road node
+     * @param parser OpenDriveNetworkLaneParser; the parser with the lists of information
+     * @return the generated RoadTag for further reference
+     * @throws SAXException when parsing of the tag fails
+     * @throws NetworkException when parsing of the tag fails
+     */
+    @SuppressWarnings("checkstyle:needbraces")
+    static LaneSectionTag parseLaneSection(final Node node, final OpenDriveNetworkLaneParserOld parser)
+            throws SAXException, NetworkException
+    {
+        NamedNodeMap attributes = node.getAttributes();
+        LaneSectionTag laneSectionTag = new LaneSectionTag();
+
+        Node s = attributes.getNamedItem("s");
+        if (s == null)
+            throw new SAXException("LaneSection: missing attribute s");
+        laneSectionTag.s = new Length(Double.parseDouble(s.getNodeValue().trim()), LengthUnit.METER);
+
+        for (Node leftNode : XMLParser.getNodes(node.getChildNodes(), "left"))
+            for (Node laneNode : XMLParser.getNodes(leftNode.getChildNodes(), "lane"))
+            {
+                LaneTag laneTag = LaneTag.parseLane(laneNode, parser);
+                laneSectionTag.leftLaneTags.put(laneTag.id, laneTag);
+            }
+
+        for (Node centerNode : XMLParser.getNodes(node.getChildNodes(), "center"))
+            for (Node laneNode : XMLParser.getNodes(centerNode.getChildNodes(), "lane"))
+            {
+                LaneTag laneTag = LaneTag.parseLane(laneNode, parser);
+                laneSectionTag.centerLaneTags.put(laneTag.id, laneTag);
+            }
+
+        for (Node rightNode : XMLParser.getNodes(node.getChildNodes(), "right"))
+            for (Node laneNode : XMLParser.getNodes(rightNode.getChildNodes(), "lane"))
+            {
+                LaneTag laneTag = LaneTag.parseLane(laneNode, parser);
+                laneSectionTag.rightLaneTags.put(laneTag.id, laneTag);
+            }
+
+        return laneSectionTag;
+    }
+
+    /**
+     * @param orientation String; Plus or minus orientation, indicated by '+' or '-'
+     * @return lanes a list of lanes in the given orientation
+     */
+    public List<Lane> findLanes(String orientation)
+    {
+        List<Lane> lanes1 = new ArrayList<Lane>();
+        for (int key : this.lanes.keySet())
+        {
+            Lane lane = this.lanes.get(key);
+            if (key < 0)
+            {
+                if (orientation.equals("+") && this.rightLaneTags.get(key).type.equals("driving"))
+                    lanes1.add(lane);
+            }
+            else if (key > 0)
+            {
+                if (orientation.equals("-") && this.leftLaneTags.get(key).type.equals("driving"))
+                    lanes1.add(lane);
+            }
+
+        }
+        if (lanes1.size() != 1)
+            System.err.println("Exception in finding lanes");
+        return lanes1;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return "LaneSectionTag [id=" + this.id + ", s=" + this.s + ", leftLaneTags=" + this.leftLaneTags + ", centerLaneTags="
+                + this.centerLaneTags + ", rightLaneTags=" + this.rightLaneTags + ", lanes=" + this.lanes + "]";
+    }
+}
---
+++ release/1.04.03/ots-parser-opendrive/src/main/java/org/opentrafficsim/road/network/factory/opendrive/parser/RoadParser.java
@@ -0,0 +1,160 @@
+package org.opentrafficsim.road.network.factory.opendrive.parser;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.naming.NamingException;
+
+import org.djunits.unit.DirectionUnit;
+import org.djunits.value.vdouble.scalar.Direction;
+import org.djutils.logger.CategoryLogger;
+import org.opentrafficsim.core.dsol.OTSSimulatorInterface;
+import org.opentrafficsim.core.geometry.OTSGeometryException;
+import org.opentrafficsim.core.geometry.OTSLine3D;
+import org.opentrafficsim.core.geometry.OTSPoint3D;
+import org.opentrafficsim.core.network.LinkType;
+import org.opentrafficsim.core.network.NetworkException;
+import org.opentrafficsim.draw.network.LinkAnimation;
+import org.opentrafficsim.draw.network.NodeAnimation;
+import org.opentrafficsim.opendrive.xml.generated.OpenDRIVE;
+import org.opentrafficsim.opendrive.xml.generated.OpenDRIVE.Junction;
+import org.opentrafficsim.opendrive.xml.generated.OpenDRIVE.Road;
+import org.opentrafficsim.opendrive.xml.generated.OpenDRIVE.Road.PlanView.Geometry;
+import org.opentrafficsim.road.network.OTSRoadNetwork;
+import org.opentrafficsim.road.network.factory.opendrive.OpenDriveParserException;
+import org.opentrafficsim.road.network.lane.CrossSectionLink;
+import org.opentrafficsim.road.network.lane.OTSRoadNode;
+import org.opentrafficsim.road.network.lane.changing.LaneKeepingPolicy;
+
+import nl.tudelft.simulation.language.d3.DirectedPoint;
+
+/**
+ * RoadParser.java.
+ * <p>
+ * Copyright (c) 2019-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
+ * <p>
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+public class RoadParser
+{
+    /** the XML structure for the xodr file. */
+    private final OpenDRIVE openDrive;
+
+    /** the network in which to write the information. */
+    private final OTSRoadNetwork otsNetwork;
+
+    /** the simulator. */
+    private final OTSSimulatorInterface simulator;
+
+    /** List of roads by id. */
+    private Map<String, Road> roadMap = new LinkedHashMap<>();
+
+    /** List of junctions by id. */
+    private Map<String, Junction> junctionMap = new LinkedHashMap<>();
+
+    /** OTSNodeMap. */
+    private Map<DirectedPoint, OTSRoadNode> otsNodeMap = new LinkedHashMap<>();
+
+    /**
+     * @param openDrive the XML structure for the xodr file.
+     * @param otsNetwork the network in which to write the information
+     * @param simulator the simulator
+     */
+    public RoadParser(final OpenDRIVE openDrive, final OTSRoadNetwork otsNetwork, final OTSSimulatorInterface simulator)
+    {
+        this.openDrive = openDrive;
+        this.otsNetwork = otsNetwork;
+        this.simulator = simulator;
+    }
+
+    /**
+     * Parse the roads in the OpenDrive XML structure.
+     * @throws OpenDriveParserException on parser error
+     * @throws NetworkException on network build error
+     * @throws OTSGeometryException on invalid line
+     */
+    public void parseRoads() throws OpenDriveParserException, NetworkException, OTSGeometryException
+    {
+        for (Road road : this.openDrive.getRoad())
+        {
+            this.roadMap.put(road.getId(), road);
+        }
+        for (Junction junction : this.openDrive.getJunction())
+        {
+            this.junctionMap.put(junction.getId(), junction);
+        }
+
+        buildNodesLinks();
+
+    }
+
+    /**
+     * Build the nodes and links in the network.
+     * @throws NetworkException on error
+     * @throws OTSGeometryException on error
+     */
+    private void buildNodesLinks() throws NetworkException, OTSGeometryException
+    {
+        for (Entry<String, Road> roadEntry : this.roadMap.entrySet())
+        {
+            Road road = roadEntry.getValue();
+            List<Geometry> geometryList = road.getPlanView().getGeometry();
+            OTSRoadNode startNode =
+                    addNode(geometryList.get(0).getX(), geometryList.get(0).getY(), geometryList.get(0).getHdg());
+            OTSRoadNode endNode = addNode(geometryList.get(geometryList.size() - 1).getX(),
+                    geometryList.get(geometryList.size() - 1).getY(), geometryList.get(geometryList.size() - 1).getHdg());
+            List<OTSPoint3D> pointList = new ArrayList<>();
+            for (Geometry geometry : geometryList)
+            {
+                pointList.add(new OTSPoint3D(geometry.getX(), geometry.getY()));
+            }
+            try
+            {
+                OTSLine3D designLine = new OTSLine3D(pointList);
+                CrossSectionLink otsLink = new CrossSectionLink(this.otsNetwork, roadEntry.getKey(), startNode, endNode,
+                        this.otsNetwork.getLinkType(LinkType.DEFAULTS.ROAD), designLine, LaneKeepingPolicy.KEEPLANE);
+                this.otsNetwork.addLink(otsLink);
+                new LinkAnimation(otsLink, this.simulator, 0.01f);
+            }
+            catch (NetworkException | RemoteException | NamingException | OTSGeometryException e)
+            {
+                CategoryLogger.always().warn(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Add or retrieve a node.
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param hdg the direction (heading)
+     * @return an earlier stored node or a newly generated node
+     * @throws NetworkException on error
+     */
+    private OTSRoadNode addNode(final double x, final double y, final double hdg) throws NetworkException
+    {
+        DirectedPoint id = new DirectedPoint(x, y, 0.0, 0.0, 0.0, hdg);
+        OTSRoadNode node = this.otsNodeMap.get(id);
+        if (node == null)
+        {
+            node = new OTSRoadNode(this.otsNetwork, id.toString(), new OTSPoint3D(x, y),
+                    new Direction(hdg, DirectionUnit.EAST_DEGREE));
+            this.otsNodeMap.put(id, node);
+            try
+            {
+                this.otsNetwork.addNode(node);
+                new NodeAnimation(node, this.simulator);
+            }
+            catch (NetworkException | RemoteException | NamingException e)
+            {
+                CategoryLogger.always().warn(e.getMessage());
+            }
+        }
+        return node;
+    }
+}
