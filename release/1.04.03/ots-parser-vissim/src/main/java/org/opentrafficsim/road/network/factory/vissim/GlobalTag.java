899e027d74d90ea0996e01c5afa217b08ae09194
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-10-31 05:12:32 +0000

    Re-deployed version 1.04.03

---
+++ release/1.04.03/ots-parser-vissim/src/main/java/org/opentrafficsim/road/network/factory/vissim/GlobalTag.java
@@ -0,0 +1,179 @@
+package org.opentrafficsim.road.network.factory.vissim;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.opentrafficsim.core.network.NetworkException;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+@SuppressWarnings("checkstyle:JavadocStyle")
+/**
+ * GlobalTag parser.
+ *
+ * <pre>
+ * {@code
+  <xsd:element name="GLOBAL">
+    <xsd:complexType>
+      <xsd:sequence>
+        <xsd:element name="SPEEDGTUCOLORER" minOccurs="0" maxOccurs="1">
+          <xsd:complexType>
+            <xsd:attribute name="MAXSPEED" type="SPEEDTYPE" use="required" />
+          </xsd:complexType>
+        </xsd:element>
+        <xsd:element name="ACCELERATIONGTUCOLORER" minOccurs="0" maxOccurs="1">
+          <xsd:complexType>
+            <xsd:attribute name="MAXDECELERATION" type="ACCELERATIONTYPE" use="required" />
+            <xsd:attribute name="MAXACCELERATION" type="ACCELERATIONTYPE" use="required" />
+          </xsd:complexType>
+        </xsd:element>
+        <xsd:element name="LANECHANGEURGEGTUCOLORER" minOccurs="0" maxOccurs="1">
+          <xsd:complexType>
+            <xsd:attribute name="MINLANECHANGEDISTANCE" type="LENGTHTYPE" use="required" />
+            <xsd:attribute name="HORIZON" type="LENGTHTYPE" use="required" />
+          </xsd:complexType>
+        </xsd:element>
+      </xsd:sequence>
+      <xsd:attribute ref="xml:base" />
+    </xsd:complexType>
+  </xsd:element>
+ * }
+ * </pre>
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Jul 23, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+class GlobalTag implements Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20150723L;
+
+    /** Default SpeedGTUColorer.maxSpeed. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Speed speedGTUColorerMaxSpeed = null;
+
+    /** Default AccelerationGTUColorer.maxDeceleration. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Acceleration accelerationGTUColorerMaxDeceleration = null;
+
+    /** Default AccelerationGTUColorer.maxAcceleration. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Acceleration accelerationGTUColorerMaxAcceleration = null;
+
+    /** Default LaneChangeUrgeGTUColorer.minLaneChangeDistance. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Length laneChangeUrgeGTUColorerMinLaneChangeDistance = null;
+
+    /** Default LaneChangeUrgeGTUColorer.horizon. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Length laneChangeUrgeGTUColorerHorizon = null;
+
+    /**
+     * @param nodeList NodeList; nodeList the top-level nodes of the XML-file
+     * @param parser VissimNetworkLaneParser; the parser with the lists of information
+     * @throws NetworkException when parsing of units fails
+     * @throws SAXException when parsing of GLOBAL tag fails
+     */
+    @SuppressWarnings("checkstyle:needbraces")
+    static void parseGlobal(final NodeList nodeList, final VissimNetworkLaneParser parser) throws NetworkException, SAXException
+    {
+        List<Node> nodes = XMLParser.getNodes(nodeList, "GLOBAL");
+        if (nodes.size() > 1)
+        {
+            throw new SAXException("GLOBAL: More than one tag GLOBAL in the XML-file");
+        }
+        if (nodes.size() == 1)
+        {
+            Node node = nodes.get(0);
+
+            parser.setGlobalTag(new GlobalTag());
+
+            // SPEEDGTUCOLORER attributes
+            List<Node> speedGTUColorerNodes = XMLParser.getNodes(node.getChildNodes(), "SPEEDGTUCOLORER");
+            if (speedGTUColorerNodes.size() > 1)
+            {
+                throw new SAXException("GLOBAL: More than one tag SPEEDGTUCOLORER in the XML-file");
+            }
+            if (speedGTUColorerNodes.size() == 1)
+            {
+                Node speedGTUColorerNode = nodes.get(0);
+                NamedNodeMap speedGTUColorerAttributes = speedGTUColorerNode.getAttributes();
+                if (speedGTUColorerAttributes.getNamedItem("MAXSPEED") == null)
+                {
+                    throw new SAXException("GLOBAL: No attribute MAXSPEED for the tag SPEEDGTUCOLORER");
+                }
+                parser.getGlobalTag().speedGTUColorerMaxSpeed =
+                        Speed.valueOf(speedGTUColorerAttributes.getNamedItem("MAXSPEED").getNodeValue());
+            }
+
+            // ACCELERATIONGTUCOLORER attributes
+            List<Node> accelerationGTUColorerNodes = XMLParser.getNodes(node.getChildNodes(), "ACCELERATIONGTUCOLORER");
+            if (accelerationGTUColorerNodes.size() > 1)
+            {
+                throw new SAXException("GLOBAL: More than one tag ACCELERATIONGTUCOLORER in the XML-file");
+            }
+            if (accelerationGTUColorerNodes.size() == 1)
+            {
+                Node accelerationGTUColorerNode = nodes.get(0);
+                NamedNodeMap accelerationGTUColorerAttributes = accelerationGTUColorerNode.getAttributes();
+                if (accelerationGTUColorerAttributes.getNamedItem("MAXDECELERATION") == null)
+                {
+                    throw new SAXException("GLOBAL: No attribute MAXDECELERATION for the tag ACCELERATIONGTUCOLORER");
+                }
+                parser.getGlobalTag().accelerationGTUColorerMaxDeceleration =
+                        Acceleration.valueOf(accelerationGTUColorerAttributes.getNamedItem("MAXDECELERATION").getNodeValue());
+                if (accelerationGTUColorerAttributes.getNamedItem("MAXACCELERATION") == null)
+                {
+                    throw new SAXException("GLOBAL: No attribute MAXACCELERATION for the tag ACCELERATIONGTUCOLORER");
+                }
+                parser.getGlobalTag().accelerationGTUColorerMaxAcceleration =
+                        Acceleration.valueOf(accelerationGTUColorerAttributes.getNamedItem("MAXACCELERATION").getNodeValue());
+            }
+
+            // LANECHANGEURGEGTUCOLORER attributes
+            List<Node> lcuGTUColorerNodes = XMLParser.getNodes(node.getChildNodes(), "LANECHANGEURGEGTUCOLORER");
+            if (lcuGTUColorerNodes.size() > 1)
+            {
+                throw new SAXException("GLOBAL: More than one tag LANECHANGEURGEGTUCOLORER in the XML-file");
+            }
+            if (lcuGTUColorerNodes.size() == 1)
+            {
+                Node lcuGTUColorerNode = nodes.get(0);
+                NamedNodeMap lcuGTUColorerAttributes = lcuGTUColorerNode.getAttributes();
+                if (lcuGTUColorerAttributes.getNamedItem("MINLANECHANGEDISTANCE") == null)
+                {
+                    throw new SAXException("GLOBAL: No attribute MINLANECHANGEDISTANCE for the tag LANECHANGEURGEGTUCOLORER");
+                }
+                parser.getGlobalTag().laneChangeUrgeGTUColorerMinLaneChangeDistance =
+                        Length.valueOf(lcuGTUColorerAttributes.getNamedItem("MINLANECHANGEDISTANCE").getNodeValue());
+                if (lcuGTUColorerAttributes.getNamedItem("HORIZON") == null)
+                {
+                    throw new SAXException("GLOBAL: No attribute HORIZON for the tag LANECHANGEURGEGTUCOLORER");
+                }
+                parser.getGlobalTag().laneChangeUrgeGTUColorerHorizon =
+                        Length.valueOf(lcuGTUColorerAttributes.getNamedItem("HORIZON").getNodeValue());
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "GlobalTag [speedGTUColorerMaxSpeed=" + this.speedGTUColorerMaxSpeed + ", accelerationGTUColorerMaxDeceleration="
+                + this.accelerationGTUColorerMaxDeceleration + ", accelerationGTUColorerMaxAcceleration="
+                + this.accelerationGTUColorerMaxAcceleration + ", laneChangeUrgeGTUColorerMinLaneChangeDistance="
+                + this.laneChangeUrgeGTUColorerMinLaneChangeDistance + ", laneChangeUrgeGTUColorerHorizon="
+                + this.laneChangeUrgeGTUColorerHorizon + "]";
+    }
+
+}
---
+++ release/1.04.03/ots-parser-vissim/src/main/java/org/opentrafficsim/road/network/factory/vissim/ShortestRouteTag.java
@@ -0,0 +1,132 @@
+package org.opentrafficsim.road.network.factory.vissim;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.djunits.value.vdouble.scalar.Frequency;
+import org.djunits.value.vdouble.scalar.LinearDensity;
+import org.opentrafficsim.core.network.NetworkException;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate$, @version $Revision$, by $Author$,
+ * initial version Jul 23, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+class ShortestRouteTag implements Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20150723L;
+
+    /** Name. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    String name = null;
+
+    /** From Node. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    NodeTag from = null;
+
+    /** Via Nodes. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    List<NodeTag> via = new ArrayList<NodeTag>();
+
+    /** To Node. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    NodeTag to = null;
+
+    /** Time unit for the "cost" per time. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    Frequency costPerTime = null;
+
+    /** Distance unit for the "cost" per time. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    LinearDensity costPerDistance = null;
+
+    /**
+     * Parse the SHORTESTROUTE tag.
+     * @param nodeList NodeList; nodeList the top-level nodes of the XML-file
+     * @param parser VissimNetworkLaneParser; the parser with the lists of information
+     * @throws SAXException when parsing of the tag fails
+     * @throws NetworkException when parsing of the tag fails
+     */
+    @SuppressWarnings("checkstyle:needbraces")
+    static void parseShortestRoutes(final NodeList nodeList, final VissimNetworkLaneParser parser)
+            throws SAXException, NetworkException
+    {
+        for (Node node : XMLParser.getNodes(nodeList, "SHORTESTROUTE"))
+        {
+            NamedNodeMap attributes = node.getAttributes();
+            ShortestRouteTag shortestRouteTag = new ShortestRouteTag();
+
+            if (attributes.getNamedItem("NAME") == null)
+            {
+                throw new SAXException("SHORTESTROUTE: missing attribute NAME");
+            }
+            shortestRouteTag.name = attributes.getNamedItem("NAME").getNodeValue().trim();
+            if (parser.getRouteTags().keySet().contains(shortestRouteTag.name))
+            {
+                throw new SAXException("SHORTESTROUTE: NAME " + shortestRouteTag.name + " defined twice");
+            }
+
+            if (attributes.getNamedItem("FROM") == null)
+            {
+                throw new SAXException("SHORTESTROUTE: missing attribute FROM");
+            }
+            String fromNode = attributes.getNamedItem("FROM").getNodeValue().trim();
+            if (!parser.getNodeTags().containsKey(fromNode))
+            {
+                throw new SAXException("SHORTESTROUTE " + shortestRouteTag.name + ": FROM node " + fromNode + " not found");
+            }
+            shortestRouteTag.from = parser.getNodeTags().get(fromNode);
+
+            if (attributes.getNamedItem("NODELIST") != null)
+            {
+                String viaNodes = attributes.getNamedItem("NODELIST").getNodeValue().trim();
+                shortestRouteTag.via = NodeTag.parseNodeList(viaNodes, parser);
+            }
+
+            if (attributes.getNamedItem("TO") == null)
+            {
+                throw new SAXException("SHORTESTROUTE: missing attribute TO");
+            }
+            String toNode = attributes.getNamedItem("TO").getNodeValue().trim();
+            if (!parser.getNodeTags().containsKey(toNode.trim()))
+            {
+                throw new SAXException("SHORTESTROUTE " + shortestRouteTag.name + ": TO node " + toNode + " not found");
+            }
+            shortestRouteTag.to = parser.getNodeTags().get(toNode);
+
+            Node distanceCost = attributes.getNamedItem("DISTANCECOST");
+            if (distanceCost == null)
+            {
+                throw new SAXException("SHORTESTROUTE: missing attribute DISTANCECOST");
+            }
+            shortestRouteTag.costPerDistance = LinearDensity.valueOf(distanceCost.getNodeValue().trim());
+
+            Node timeCost = attributes.getNamedItem("TIMECOST");
+            if (timeCost == null)
+            {
+                throw new SAXException("SHORTESTROUTE: missing attribute TIMECOST");
+            }
+            shortestRouteTag.costPerTime = Frequency.valueOf(timeCost.getNodeValue().trim());
+
+            parser.getShortestRouteTags().put(shortestRouteTag.name.trim(), shortestRouteTag);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return "ShortestRouteTag [name=" + this.name + ", from=" + this.from + ", via=" + this.via + ", to=" + this.to
+                + ", costPerTime=" + this.costPerTime + ", costPerDistance=" + this.costPerDistance + "]";
+    }
+}
