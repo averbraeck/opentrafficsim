b40db1a3db32700160e14fddc2a607ee0ec9028a
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2020-11-01 15:21:37 +0000

    Share project "ots-edit" into "https://svn.tbm.tudelft.nl/OTS-RESEARCH"

--- trunk/ots-edit/network/include.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  var includes = $('.include');
-  jQuery.each(includes, function(){
-    var file = $(this).data('include');
-    $(this).load(file);
-  });
-});
--- trunk/ots-edit/network/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start ();
-if (session_destroy ()) // Destroying All Sessions
-{
-	header ( "Location: index.php" ); // Redirecting To Home Page
-}
-?>
