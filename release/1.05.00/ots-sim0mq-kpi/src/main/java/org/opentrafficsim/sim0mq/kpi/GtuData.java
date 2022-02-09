70270b309d494fa5badf8c6ed573bc8ee6f29372
averbraeck <averbraeck@5f542327-97c0-4e2e-84b1-c6b983e3afb5>
2022-02-09 21:10:35 +0000

    Deployed version 1.05.00 of OTS-1

---
+++ release/1.05.00/ots-sim0mq-kpi/src/main/java/org/opentrafficsim/sim0mq/kpi/GtuData.java
@@ -0,0 +1,114 @@
+package org.opentrafficsim.sim0mq.kpi;
+
+import org.opentrafficsim.kpi.interfaces.GtuDataInterface;
+import org.opentrafficsim.kpi.interfaces.GtuTypeDataInterface;
+import org.opentrafficsim.kpi.interfaces.NodeDataInterface;
+import org.opentrafficsim.kpi.interfaces.RouteDataInterface;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 13 okt. 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class GtuData implements GtuDataInterface
+{
+    /** id. */
+    private final String id;
+
+    /** gtu type. */
+    private final GtuTypeData gtuType;
+
+    /** route. */
+    private final RouteData route;
+
+    /**
+     * @param id String; the id
+     * @param gtuType GtuTypeData; the gtu type
+     * @param route RouteData; the route
+     */
+    public GtuData(final String id, final GtuTypeData gtuType, final RouteData route)
+    {
+        this.id = id;
+        this.gtuType = gtuType;
+        this.route = route;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String getId()
+    {
+        return this.id;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final NodeDataInterface getOriginNodeData()
+    {
+        return this.route.getStartNode();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final NodeDataInterface getDestinationNodeData()
+    {
+        return this.route.getEndNode();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final GtuTypeDataInterface getGtuTypeData()
+    {
+        return this.gtuType;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final RouteDataInterface getRouteData()
+    {
+        return this.route;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GtuData other = (GtuData) obj;
+        if (this.id == null)
+        {
+            if (other.id != null)
+                return false;
+        }
+        else if (!this.id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "GtuData [id=" + this.id + ", gtuType=" + this.gtuType + ", route=" + this.route + "]";
+    }
+
+}
---
+++ release/1.05.00/ots-sim0mq-kpi/src/main/java/org/opentrafficsim/sim0mq/kpi/GtuTypeData.java
@@ -0,0 +1,81 @@
+package org.opentrafficsim.sim0mq.kpi;
+
+import org.opentrafficsim.kpi.interfaces.GtuTypeDataInterface;
+
+/**
+ * <p>
+ * Copyright (c) 2013-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, initial version 13 okt. 2016 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author <a href="http://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class GtuTypeData implements GtuTypeDataInterface
+{
+    /** type name. */
+    private final String gtuTypeName;
+
+    /**
+     * @param gtuTypeName String; gtu type name
+     */
+    public GtuTypeData(final String gtuTypeName)
+    {
+        this.gtuTypeName = gtuTypeName;
+    }
+
+    /**
+     * @return gtuTypeName
+     */
+    public final String getGtuTypeName()
+    {
+        return this.gtuTypeName;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
+    {
+        return this.gtuTypeName;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.gtuTypeName == null) ? 0 : this.gtuTypeName.hashCode());
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GtuTypeData other = (GtuTypeData) obj;
+        if (this.gtuTypeName == null)
+        {
+            if (other.gtuTypeName != null)
+                return false;
+        }
+        else if (!this.gtuTypeName.equals(other.gtuTypeName))
+            return false;
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "GtuTypeData [gtuTypeName=" + this.gtuTypeName + "]";
+    }
+
+}
