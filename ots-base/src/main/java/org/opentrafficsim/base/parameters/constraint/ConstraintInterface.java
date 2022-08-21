204ca2f602144063842a427539ee618410741a07
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-08-21 11:55:18 +0200

    turn formatter: on again

--- ots-base/src/main/java/org/opentrafficsim/base/parameters/constraint/ConstraintInterface.java
+++ ots-base/src/main/java/org/opentrafficsim/base/parameters/constraint/ConstraintInterface.java
@@ -1,10 +1,11 @@
 package org.opentrafficsim.base.parameters.constraint;
 
 /**
- * In order to define default constraints within a Parameter Type, an <code>enum</code> is available. This interface supplies easy
- * access to the values of this <code>enum</code>. To use this interface, simply implement it as below. The value <code>POSITIVE</code>
- * is a property of this interface pointing to the <code>enum</code> field <code>POSITIVE</code>. As a result, the value that is set for
- * <code>X</code> is checked to be above zero. Note that model and parameterType do not have to be defined in the same class.
+ * In order to define default constraints within a Parameter Type, an <code>enum</code> is available. This interface supplies
+ * easy access to the values of this <code>enum</code>. To use this interface, simply implement it as below. The value
+ * <code>POSITIVE</code> is a property of this interface pointing to the <code>enum</code> field <code>POSITIVE</code>. As a
+ * result, the value that is set for <code>X</code> is checked to be above zero. Note that model and parameterType do not have
+ * to be defined in the same class.
  * 
  * <pre>
  * public class myModel implements ConstraintInterface
@@ -24,12 +25,12 @@ package org.opentrafficsim.base.parameters.constraint;
  * </pre>
  * 
  * <br>
- * In order to implement <i>custom</i> checks, any Parameter Type must extend the <code>check</code> method of its super. An example
- * is given below. The method should throw a <code>ParameterException</code> whenever a constraint is not met. The static
- * <code>throwIf</code> method is used to do this. The first check is a simple check on the SI value being above 2. The second check
- * compares the value with the value of another parameter in the <code>Parameters</code>. These checks can only be performed if the
- * other parameter is present in the <code>Parameters</code>. <b>Checks with other parameter type values should always check whether
- * <code>Parameters</code> contains the other parameter type</b>. i.e. <code>params.contains()</code>.<br>
+ * In order to implement <i>custom</i> checks, any Parameter Type must extend the <code>check</code> method of its super. An
+ * example is given below. The method should throw a <code>ParameterException</code> whenever a constraint is not met. The
+ * static <code>throwIf</code> method is used to do this. The first check is a simple check on the SI value being above 2. The
+ * second check compares the value with the value of another parameter in the <code>Parameters</code>. These checks can only be
+ * performed if the other parameter is present in the <code>Parameters</code>. <b>Checks with other parameter type values should
+ * always check whether <code>Parameters</code> contains the other parameter type</b>. i.e. <code>params.contains()</code>.<br>
  * 
  * <pre>
  * public static final ParameterTypeLength X = new ParameterTypeLength(&quot;x&quot;, &quot;My x parameter.&quot;)
@@ -43,11 +44,11 @@ package org.opentrafficsim.base.parameters.constraint;
  * };
  * </pre>
  * 
- * Checks are invoked on default values (if given), in which case an empty <code>Parameters</code> is forwarded. At construction of
- * a Parameter Type, no <code>Parameters</code> is available. Checks are also invoked when value are set into <code>Parameters</code>,
- * in which case that <code>Parameters</code> forwards itself. Even still, if in the above case X is set before Y, the check is
- * never performed. Therefore, Y should also compare itself to X. <b>Two parameters that are checked with each other, should
- * both implement a check which is consistent with and mirrored to the the others' check.</b> <br>
+ * Checks are invoked on default values (if given), in which case an empty <code>Parameters</code> is forwarded. At construction
+ * of a Parameter Type, no <code>Parameters</code> is available. Checks are also invoked when value are set into
+ * <code>Parameters</code>, in which case that <code>Parameters</code> forwards itself. Even still, if in the above case X is
+ * set before Y, the check is never performed. Therefore, Y should also compare itself to X. <b>Two parameters that are checked
+ * with each other, should both implement a check which is consistent with and mirrored to the the others' check.</b> <br>
  * <br>
  * The type of the first argument in the <code>check()</code> method depends on the super Parameter Type. For example:<br>
  * <ul>
@@ -79,5 +80,5 @@ public interface ConstraintInterface
     Constraint<Number> NONZERO      = NumericConstraint.NONZERO;
     Constraint<Number> ATLEASTONE   = NumericConstraint.ATLEASTONE;
     Constraint<Number> UNITINTERVAL = DualBound.UNITINTERVAL;
-    
+    // @formatter:on
 }
