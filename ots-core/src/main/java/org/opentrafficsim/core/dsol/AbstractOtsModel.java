560c29bc47cceca69ac6531e1b42fae0fc18ef24
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-10-02 15:31:03 +0200

    Issue #11. Renamed OTS acronym to Ots in class names

--- ots-core/src/main/java/org/opentrafficsim/core/dsol/AbstractOtsModel.java
+++ ots-core/src/main/java/org/opentrafficsim/core/dsol/AbstractOtsModel.java
@@ -8,7 +8,7 @@ import nl.tudelft.simulation.dsol.model.AbstractDSOLModel;
 import nl.tudelft.simulation.jstats.streams.MersenneTwister;
 
 /**
- * AbstractOTSModel is the base class for a model that runs on an OTSSimulator.
+ * AbstractOtsModel is the base class for a model that runs on an OTSSimulator.
  * <p>
  * Copyright (c) 2013-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
