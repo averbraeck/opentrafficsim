e58dcc5bc56b4ce59a86d2346d5555b624743da0
wjschakel <wjschakel@TUD260143.tudelft.net>
2024-02-23 17:03:14 +0100

    Updated copyright in files from merged branch.

--- ots-draw/src/main/java/org/opentrafficsim/draw/road/OtsRenderable.java
+++ ots-draw/src/main/java/org/opentrafficsim/draw/road/OtsRenderable.java
@@ -17,7 +17,7 @@ import nl.tudelft.simulation.naming.context.Contextualized;
 /**
  * Extends {@code Renderable2d} to let the {@code contains} method look at the actual bounds shape, rather than only the box.
  * <p>
- * Copyright (c) 2023-2023 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://dittlab.tudelft.nl">Wouter Schakel</a>
