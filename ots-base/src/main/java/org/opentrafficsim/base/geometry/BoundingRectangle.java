1a0bfdc82b70b040c50df7b85a7889eb734c010b
wjschakel <wjschakel@TUD260143.tudelft.net>
2024-02-27 03:32:34 +0100

    Added midpoint and signed distance implementations to BoundedRectangle.

--- ots-base/src/main/java/org/opentrafficsim/base/geometry/BoundingRectangle.java
+++ ots-base/src/main/java/org/opentrafficsim/base/geometry/BoundingRectangle.java
@@ -1,6 +1,7 @@
 package org.opentrafficsim.base.geometry;
 
 import org.djutils.draw.line.Polygon2d;
+import org.djutils.draw.point.Point2d;
 
 /**
  * Bounding rectangle; this is a non-centered box.
@@ -25,6 +26,15 @@ public class BoundingRectangle implements OtsBounds2d
     /** Maximum y coordinate. */
     private final double maxY;
 
+    /** Half width. */
+    private final double dx;
+
+    /** Half height. */
+    private final double dy;
+
+    /** Middle point. */
+    private final Point2d midPoint;
+
     /** Resulting polygon. */
     private Polygon2d polygon;
 
@@ -41,6 +51,25 @@ public class BoundingRectangle implements OtsBounds2d
         this.maxX = maxX;
         this.minY = minY;
         this.maxY = maxY;
+        this.dx = (maxX - minX) / 2.0;
+        this.dy = (maxY - minY) / 2.0;
+        this.midPoint = new Point2d(minX + this.dx, minY + this.dy);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Point2d midPoint()
+    {
+        return this.midPoint;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double signedDistance(final Point2d point)
+    {
+        double qx = Math.abs(point.x - this.midPoint.x) - this.dx;
+        double qy = Math.abs(point.y - this.midPoint.y) - this.dy;
+        return Math.hypot(Math.max(qx, 0.0), Math.max(qy, 0.0)) + Math.min(Math.max(qx, qy), 0.0);
     }
 
     /** {@inheritDoc} */
