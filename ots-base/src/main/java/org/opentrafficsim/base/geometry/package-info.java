73fc14c982e80a0a23222b1fa01d285ad6c96986
wjschakel <wjschakel@TUD264483.tudelft.net>
2024-10-04 14:32:46 +0200

    Updated package-info files in ots-base. (Issue #143)

--- ots-base/src/main/java/org/opentrafficsim/base/geometry/package-info.java
+++ ots-base/src/main/java/org/opentrafficsim/base/geometry/package-info.java
@@ -1,4 +1,8 @@
 /**
- * Candidate classes for djutils-draw, used throughout OTS projects.
+ * OTS specific geometry functionality.
+ * <p>
+ * Copyright (c) 2013-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
  */
 package org.opentrafficsim.base.geometry;
--- ots-base/src/main/java/org/opentrafficsim/base/package-info.java
+++ ots-base/src/main/java/org/opentrafficsim/base/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Generic classes for entire OTS project such as Throw for preconditions and postcondition testing.
+ * Generic classes for entire OTS project.
  * <p>
  * Copyright (c) 2013-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
