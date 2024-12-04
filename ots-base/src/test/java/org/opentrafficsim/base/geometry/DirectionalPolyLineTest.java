398c7d4491d60d39bc56b278aa157b00499644d5
wjschakel <wjschakel@TUD264483.tudelft.net>
2024-12-04 16:38:17 +0100

    Added comments to test. (Issue #179)

--- ots-base/src/test/java/org/opentrafficsim/base/geometry/DirectionalPolyLineTest.java
+++ ots-base/src/test/java/org/opentrafficsim/base/geometry/DirectionalPolyLineTest.java
@@ -9,9 +9,20 @@ import org.djutils.exceptions.Try;
 import org.junit.jupiter.api.Test;
 import org.opentrafficsim.base.geometry.OtsLine2d.FractionalFallback;
 
+/**
+ * Test for DirectionalPolyLine.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
+ */
 public class DirectionalPolyLineTest
 {
 
+    /**
+     * Test method.
+     */
     @Test
     public void testLine()
     {
