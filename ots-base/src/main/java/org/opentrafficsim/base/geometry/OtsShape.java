d6628c5d96c55adcd42f877f5d55d8af6cec9470
wjschakel <wjschakel@TUD264483.tudelft.net>
2025-05-26 16:13:17 +0200

    Merge of spatial/locatable interfaces in to OtsShape. (Issue #204)

--- ots-base/src/main/java/org/opentrafficsim/base/geometry/OtsShape.java
+++ ots-base/src/main/java/org/opentrafficsim/base/geometry/OtsShape.java
@@ -1,130 +1,175 @@
 package org.opentrafficsim.base.geometry;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.djutils.draw.Directed2d;
+import org.djutils.draw.Directed3d;
+import org.djutils.draw.Transform2d;
+import org.djutils.draw.bounds.Bounds2d;
+import org.djutils.draw.line.PolyLine2d;
 import org.djutils.draw.line.Polygon2d;
+import org.djutils.draw.point.Point;
 import org.djutils.draw.point.Point2d;
 
+import nl.tudelft.simulation.dsol.animation.Locatable;
+
 /**
- * Shape representation of objects that is suitable for continuous spatial 2D algorithms. Default methods use the polygon
- * representation of the shape. For many simple shapes there may be faster methods to determine information.
+ * Locatable that specifies return types.
  * <p>
  * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
  */
-public interface OtsShape
+public interface OtsLocatable extends Locatable, SpatialObject
 {
 
-    /** Standard mid point. */
-    Point2d CENTER = new Point2d(0.0, 0.0);
-
-    /** Default number of segments in polygon. */
-    int DEFAULT_POLYGON_SEGMENTS = 128;
+    @Override
+    Point2d getLocation();
 
     /**
-     * Return the absolute lower bound for x.
-     * @return double; the absolute lower bound for x
+     * Returns the bounds relative to the location.
+     * @return bounds relative to the location.
      */
-    default double getMinX()
+    @Override
+    Bounds2d getBounds();
+
+    @Override
+    default double getDirZ()
     {
-        return asPolygon().getBounds().getMinX();
+        Point<?> p = getLocation();
+        return p == null ? 0.0 : p instanceof Directed2d ? ((Directed2d<?>) p).getDirZ()
+                : p instanceof Directed3d ? ((Directed3d<?>) p).getDirZ() : 0.0;
     }
 
     /**
-     * Return the absolute upper bound for x.
-     * @return double; the absolute upper bound for x
+     * Returns the shape relative to the location.
+     * @return the shape relative to the location.
      */
-    default double getMaxX()
+    default OtsShape getShape()
     {
-        return asPolygon().getBounds().getMaxX();
+        return new PolygonShape(relativeContour(this));
+    }
+
+    /*
+     * The following methods that take an OtsLocatable as input are defined not as default but as static, as these methods
+     * should not be exposed as functions of an OtsLocatable.
+     */
+
+    /**
+     * Generates a polygon as contour based on bounds and location of the locatable.
+     * @param locatable locatable
+     * @return contour
+     */
+    static Polygon2d boundsAsContour(final OtsLocatable locatable)
+    {
+        List<Point2d> points = new ArrayList<>();
+        Bounds2d bounds = locatable.getBounds();
+        points.add(new Point2d(bounds.getMaxX(), bounds.getMaxY()));
+        points.add(new Point2d(bounds.getMinX(), bounds.getMaxY()));
+        points.add(new Point2d(bounds.getMinX(), bounds.getMinY()));
+        points.add(new Point2d(bounds.getMaxX(), bounds.getMinY()));
+        return new Polygon2d(transform(toContourTransform(locatable.getLocation()), points.iterator()));
     }
 
     /**
-     * Return the absolute lower bound for y.
-     * @return double; the absolute lower bound for y
+     * Generates bounds based on polygon and location of the locatable.
+     * @param locatable locatable
+     * @return bounds
      */
-    default double getMinY()
+    static Bounds2d contourAsBounds(final OtsLocatable locatable)
     {
-        return asPolygon().getBounds().getMinY();
+        return relativeContour(locatable).getBounds();
     }
 
     /**
-     * Return the absolute upper bound for y.
-     * @return double; the absolute upper bound for y
+     * Returns the contour of the locatable translated and rotated such that it is defined relative to the location.
+     * @param locatable locatable
+     * @return contour relative to location
      */
-    default double getMaxY()
+    static Polygon2d relativeContour(final OtsLocatable locatable)
     {
-        return asPolygon().getBounds().getMaxY();
+        return transformContour(locatable.getContour(), locatable.getLocation());
     }
 
     /**
-     * Return the extent of this Bounds2d in the x-direction.
-     * @return double; the extent of this Bounds2d in the x-direction
+     * Transform the contour from the location to relative coordinates, which may also be an {@code OrientedPoint} for rotation.
+     * @param contour contour
+     * @param location location, which may also be an {@code OrientedPoint} for rotation
+     * @return transformed contour
      */
-    default double getDeltaX()
+    static Polygon2d transformContour(final Polygon2d contour, final Point2d location)
     {
-        return getMaxX() - getMinX();
+        return new Polygon2d(transform(toBoundsTransform(location), contour.iterator()));
     }
 
     /**
-     * Return the extent of this Bounds2d in the y-direction.
-     * @return double; the extent of this Bounds2d in the y-direction
+     * Transform the line by location, which may also be an {@code OrientedPoint} for rotation.
+     * @param line line
+     * @param location location, which may also be an {@code OrientedPoint} for rotation
+     * @return transformed line
      */
-    default double getDeltaY()
+    static PolyLine2d transformLine(final PolyLine2d line, final Point2d location)
     {
-        return getMaxY() - getMinY();
+        return new PolyLine2d(transform(toBoundsTransform(location), line.iterator()));
     }
 
     /**
-     * Return the mid point of this Bounds object.
-     * @return P; the mid point of this Bounds object
+     * Translates and possibly rotates points by location.
+     * @param transformation transformation
+     * @param itSource points
+     * @return translated and possibly rotated contour
      */
-    default Point2d midPoint()
+    private static List<Point2d> transform(final Transform2d transformation, final Iterator<Point2d> itSource)
     {
-        return CENTER;
+        Point2d prev = null;
+        List<Point2d> points = new ArrayList<>();
+        while (itSource.hasNext())
+        {
+            Point2d next = transformation.transform(itSource.next());
+            if (!next.equals(prev))
+            {
+                points.add(next);
+            }
+            prev = next;
+        }
+        return points;
     }
 
     /**
-     * Check if a point is contained in this OtsShape.
-     * @param point the point
-     * @return true if the point is within this OtsShape; false if the point is not within this OtsShape. Results may be
-     *         ill-defined for points on the edges of this Polygon.
+     * Returns a transformation by which absolute coordinates can be translated and rotated to the frame of the possibly
+     * oriented location around which bounds are defined.
+     * @param location location (can be an {@code Oriented}).
+     * @return transformation.
      */
-    default boolean contains(final Point2d point)
+    static Transform2d toBoundsTransform(final Point2d location)
     {
-        return contains(point.x, point.y);
+        Transform2d transformation = new Transform2d();
+        if (location instanceof Directed2d<?>)
+        {
+            transformation.rotation(-((Directed2d<?>) location).getDirZ());
+        }
+        transformation.translate(-location.getX(), -location.getY());
+        return transformation;
     }
 
     /**
-     * Check if a point is contained in this OtsShape.
-     * @param x x-coordinate
-     * @param y y-coordinate
-     * @return true if the point is within this OtsShape; false if the point is not within this OtsShape. Results may be
-     *         ill-defined for points on the edges of this Polygon.
+     * Returns a transformation by which relative coordinates can be translated and rotated to the frame of the possibly
+     * oriented location around which bounds are defined.
+     * @param location location (can be an {@code Oriented}).
+     * @return transformation.
      */
-    default boolean contains(final double x, final double y)
+    static Transform2d toContourTransform(final Point2d location)
     {
-        // do not use signedDistance(), it uses this function
-        return asPolygon().contains(x, y);
-    }
-
-    /**
-     * Returns a polygon representation of the bounds, such that an intersection can be derived.
-     * @return polygon representation of the bounds.
-     */
-    Polygon2d asPolygon();
-
-    /**
-     * Signed distance function. The coordinates must be transformed to this bound's space. Negative distances returned are
-     * inside the bounds, with the absolute value of the distance towards the edge. The default implementation is based on the
-     * polygon representation and is expensive.
-     * @param point point for which distance is returned.
-     * @return distance from point to these bounds.
-     */
-    default double signedDistance(final Point2d point)
-    {
-        double dist = asPolygon().closestPointOnPolyLine(point).distance(point);
-        return contains(point) ? -dist : dist;
+        Transform2d transformation = new Transform2d();
+        transformation.translate(location.getX(), location.getY());
+        if (location instanceof Directed2d<?>)
+        {
+            transformation.rotation(((Directed2d<?>) location).getDirZ());
+        }
+        return transformation;
     }
 
 }
