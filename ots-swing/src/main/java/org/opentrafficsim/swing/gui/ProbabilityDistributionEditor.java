9c6d68ddbdb6fd1eadb34ab4102d838bdeb3091b
wjschakel <wjschakel@6P0TG24.fritz.box>
2025-09-16 15:54:27 +0200

    Remove com.bric.multislider. (Issue #201)

--- ots-swing/src/main/java/org/opentrafficsim/swing/gui/ProbabilityDistributionEditor.java
+++ ots-swing/src/main/java/org/opentrafficsim/swing/gui/ProbabilityDistributionEditor.java
@@ -1,83 +1,194 @@
 package org.opentrafficsim.swing.gui;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.beans.PropertyChangeListener;
-import java.util.Arrays;
-import java.util.Locale;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
 
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicSliderUI;
 
-import com.bric.multislider.MultiThumbSlider;
-import com.bric.multislider.MultiThumbSliderUi;
-import com.bric.multislider.MultiThumbSliderUi.Thumb;
+import org.djutils.exceptions.Throw;
+import org.djutils.swing.multislider.LinearMultiSlider;
 
 /**
- * Wrapper for Jeremy Wood's MultiThumbSlider.
+ * Editor for a distribution of probabilities of all possible categories. The probabilities must sum to 1.0.
  * <p>
- * Copyright (c) 2013-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
  * </p>
+ * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
  * @author <a href="https://github.com/peter-knoppers">Peter Knoppers</a>
+ * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
+ * @param <T> category type
  */
-public class ProbabilityDistributionEditor extends JPanel
+public class ProbabilityDistributionEditor<T> extends LinearMultiSlider<Double>
 {
+
     /** */
-    private static final long serialVersionUID = 20141222L;
+    private static final long serialVersionUID = 20250916L;
 
-    /** The internal MultiThumbSlider. */
-    private MultiThumbSlider<String> slider;
+    /** Number of values the slider allows per percent. */
+    private final int valuesPerPercent;
 
-    /** The JLabels that indicate the current values. */
-    private JLabel[] labels;
+    /** Categories. */
+    private final List<T> categories;
+
+    /** Label function. */
+    private BiFunction<T, Double, String> labelFunction = (t, p) -> String.format("%s: %.1f%%", t, p * 100.0);
+
+    /** Category font size. */
+    private float categoryFontSize = 10.0f;
 
     /**
-     * Construct a graphical ProbabilityDistributioneEditor.
-     * @param elementNames the names of the elements of the probability distribution
-     * @param values the initial values of the probabilities (should add up to 1.0 and should have same length as
-     *            <cite>elementNames</cite>)
+     * Constructor.
+     * @param categories categories
+     * @param probabilities probabilities
+     * @param valuesPerPercent number of values the slider allows per percent
      */
-    public ProbabilityDistributionEditor(final String[] elementNames, final Double[] values)
+    public ProbabilityDistributionEditor(final List<T> categories, final double[] probabilities, final int valuesPerPercent)
     {
-        super(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        float[] initialValues = new float[values.length - 1];
-        double sum = 0;
-        String[] reducedNames = new String[elementNames.length - 1];
-        for (int i = 0; i < values.length - 1; i++)
+        super(0.0, 1.0, 100 * valuesPerPercent + 1, checkValues(probabilities));
+        Throw.whenNull(categories, "categories");
+        Throw.whenNull(probabilities, "probabilities");
+        Throw.when(categories.size() != new LinkedHashSet<>(categories).size(), IllegalArgumentException.class,
+                "The categories are not unique.");
+        Throw.when(valuesPerPercent < 1, IllegalArgumentException.class, "valuesPerPercent should be at least 1.");
+        this.categories = new ArrayList<>(categories);
+        this.valuesPerPercent = 100 * valuesPerPercent;
+        // create default %-labels, although not shown by default
+        setLabelTable(new Hashtable<Integer, JLabel>(IntStream.range(0, 11).collect(() -> new LinkedHashMap<>(),
+                (m, i) -> m.put(i * 10 * valuesPerPercent, new JLabel((i * 10) + "%")), (m1, m2) -> m1.putAll(m2))));
+        setMajorTickSpacing(5 * valuesPerPercent);
+        setPaintTicks(true);
+        setOverlap(true);
+        setPaintTrack(false);
+        this.addChangeListener(new ChangeListener()
         {
-            sum += values[i];
-            initialValues[i] = (float) sum;
-            reducedNames[i] = elementNames[i];
+            @Override
+            public void stateChanged(final ChangeEvent e)
+            {
+                // need to update the labels as we drag, otherwise the thumbs erase (part of) the labels
+                repaint();
+            }
+        });
+    }
+
+    /**
+     * Check values are positive and add up to one.
+     * @param probabilities probabilities
+     * @return cumulative result of probabilities
+     */
+    private static Double[] checkValues(final double[] probabilities)
+    {
+        Double[] out = new Double[probabilities.length - 1];
+        double cumul = 0.0;
+        for (int i = 0; i < probabilities.length - 1; i++)
+        {
+            Throw.when(probabilities[i] < 0.0, IllegalArgumentException.class, "Probabilities should not be negative.");
+            cumul += probabilities[i];
+            out[i] = cumul;
         }
-        this.slider = new MultiThumbSlider<String>(MultiThumbSlider.HORIZONTAL, initialValues, reducedNames);
-        this.slider.setThumbOverlap(true);
-        this.slider.putClientProperty(MultiThumbSliderUi.THUMB_SHAPE_PROPERTY, Thumb.Hourglass);
-        this.slider.setThumbOverlap(true);
-        this.slider.setAutoAdding(false);
-        this.slider.setPreferredSize(new Dimension(250, 50));
-        add(this.slider, gbc);
-        gbc.gridwidth = 1;
-        this.labels = new JLabel[values.length];
-        for (int i = 0; i < values.length; i++)
+        Throw.when(Math.abs(cumul + probabilities[probabilities.length - 1] - 1.0) > 1e-9, IllegalArgumentException.class,
+                "Probabilities do not add up to one.");
+        return out;
+    }
+
+    @Override
+    protected Double mapIndexToValue(final int index)
+    {
+        return ((double) index) / this.valuesPerPercent;
+    }
+
+    /**
+     * Sets the label function. This function receives the category object and the probability in the normalized [0...1] range.
+     * @param labelfunction label function receiving the category object and the probability in the normalized [0...1] range
+     */
+    public void setCategoryLabelFunction(final BiFunction<T, Double, String> labelfunction)
+    {
+        Throw.whenNull(labelfunction, "labelfunction");
+        this.labelFunction = labelfunction;
+    }
+
+    /**
+     * Set the font size for the category labels.
+     * @param categoryFontSize font size for the category labels
+     */
+    public void setCategoryFontSize(final float categoryFontSize)
+    {
+        Throw.when(categoryFontSize <= 0.0, IllegalArgumentException.class, "Category font size should be larger than 0.");
+        this.categoryFontSize = categoryFontSize;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void paint(final Graphics g)
+    {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        // similar code as in AbstractMultiSlider.calculateTrackSize(), but that''s all private
+        int trackSizeLoPx = 0;
+        JSlider js = getSlider(0);
+        BasicSliderUI ui = (BasicSliderUI) js.getUI();
+        int loValue = js.getMinimum();
+        int w = 0;
+        while (w < js.getWidth() && ui.valueForXPosition(w) == loValue)
         {
-            gbc.gridy++;
-            gbc.gridx = 0;
-            JLabel caption = new JLabel(elementNames[i] + ": ");
-            caption.setHorizontalAlignment(SwingConstants.TRAILING);
-            add(caption, gbc);
-            JLabel value = new JLabel("");
-            value.setHorizontalAlignment(SwingConstants.LEADING);
-            gbc.gridx = 1;
-            add(value, gbc);
-            this.labels[i] = value;
+            trackSizeLoPx = w++;
+        }
+
+        // edges between probability bands
+        int[] edges = new int[getNumberOfThumbs() + 2];
+        edges[0] = trackSizeLoPx;
+        for (int i = 0; i < getNumberOfThumbs(); i++)
+        {
+            edges[i + 1] = thumbPositionPx(i);
+        }
+        edges[edges.length - 1] = trackSizeLoPx + trackSize();
+
+        // draw labels
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setFont(g2.getFont().deriveFont(this.categoryFontSize));
+        FontMetrics fontMetric = g2.getFontMetrics();
+        for (int i = 0; i < edges.length - 1; i++)
+        {
+            int x = (edges[i] + edges[i + 1]) / 2;
+            String categoryLabel;
+            try
+            {
+                categoryLabel = this.labelFunction.apply(this.categories.get(i), getProbability(i));
+            }
+            catch (Exception e)
+            {
+                categoryLabel = this.categories.get(i).toString();
+            }
+            Rectangle2D d = fontMetric.getStringBounds(categoryLabel, g2);
+            int left = (int) (x - d.getWidth() / 2.0);
+            if (left < 0)
+            {
+                g2.drawString(categoryLabel, 0, fontMetric.getHeight());
+            }
+            else if (left + d.getWidth() > getWidth())
+            {
+                g2.drawString(categoryLabel, (int) (getWidth() - d.getWidth()), fontMetric.getHeight());
+            }
+            else
+            {
+                g2.drawString(categoryLabel, left, fontMetric.getHeight());
+            }
         }
     }
 
@@ -85,65 +196,47 @@ public class ProbabilityDistributionEditor extends JPanel
      * Retrieve the current probability values.
      * @return the probability values
      */
-    public final Double[] getProbabilities()
+    public double[] getProbabilities()
     {
-        float[] positions = this.slider.getThumbPositions();
-        Double[] result = new Double[positions.length + 1];
-        double previous = 0;
-        for (int i = 0; i < result.length - 1; i++)
+        double[] result = new double[this.categories.size()];
+        for (int i = 0; i < this.categories.size(); i++)
         {
-            double thisValue = positions[i];
-            result[i] = new Double(thisValue - previous);
-            previous = thisValue;
-        }
-        result[positions.length] = 1 - previous;
-        for (int i = 0; i < result.length; i++)
-        {
-            this.labels[i].setText(String.format(Locale.getDefault(), "%.3f", result[i]));
+            result[i] = getProbability(i);
         }
         return result;
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the probability of the given category.
+     * @param t category
+     * @return the probability of the given category
+     * @throws IllegalArgumentException if the category object is not part of the distribution
      */
-    @Override
-    public final void addPropertyChangeListener(final PropertyChangeListener pcl)
+    public double getProbability(final T t)
     {
-        this.slider.addPropertyChangeListener(pcl);
+        Throw.when(!this.categories.contains(t), IllegalArgumentException.class, "Category {} is not part of the distribution.",
+                t);
+        return getProbability(this.categories.indexOf(t));
     }
 
     /**
-     * {@inheritDoc}
+     * Returns the probability of category with given index.
+     * @param i category index
+     * @return the probability of category with given index
+     * @throws IndexOutOfBoundsException if the index is out of bounds
      */
-    @Override
-    public final void removePropertyChangeListener(final PropertyChangeListener pcl)
+    public double getProbability(final int i)
     {
-        this.slider.removePropertyChangeListener(pcl);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final void addPropertyChangeListener(final String key, final PropertyChangeListener pcl)
-    {
-        this.slider.addPropertyChangeListener(key, pcl);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final void removePropertyChangeListener(final String key, final PropertyChangeListener pcl)
-    {
-        this.slider.removePropertyChangeListener(key, pcl);
-    }
-
-    @Override
-    public final String toString()
-    {
-        return "ProbabilityDistributionEditor [slider=" + this.slider + ", labels=" + Arrays.toString(this.labels) + "]";
+        Objects.checkIndex(i, this.categories.size());
+        if (i == 0)
+        {
+            return getValue(0);
+        }
+        else if (i == this.categories.size() - 1)
+        {
+            return 1.0 - getValue(this.categories.size() - 2);
+        }
+        return getValue(i) - getValue(i - 1);
     }
 
 }
---
+++ ots-swing/src/test/java/org/opentrafficsim/swing/gui/ProbabilityDistributionTest.java
@@ -0,0 +1,141 @@
+package org.opentrafficsim.swing.gui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.event.ChangeEvent;
+
+import org.djutils.exceptions.Try;
+import org.djutils.swing.multislider.AbstractMultiSlider.FinalValueChangeListener;
+import org.junit.jupiter.api.Test;
+import org.opentrafficsim.core.definitions.DefaultsNl;
+import org.opentrafficsim.core.gtu.GtuType;
+
+/**
+ * Tests for and small demo of a {@code ProbabilityDistributionEditor}.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://github.com/peter-knoppers">Peter Knoppers</a>
+ * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
+ */
+public final class ProbabilityDistributionTest
+{
+
+    /**
+     * Constructor.
+     */
+    private ProbabilityDistributionTest()
+    {
+        //
+    }
+
+    /**
+     * Tests editor.
+     */
+    @Test
+    public void testProbabilityEditor()
+    {
+        List<String> cat = List.of("A", "B", "C");
+        double[] probs = new double[] {0.1, 0.3, 0.6};
+        Try.testFail(() -> new ProbabilityDistributionEditor<>(null, probs, 2), NullPointerException.class);
+        Try.testFail(() -> new ProbabilityDistributionEditor<>(cat, null, 2), NullPointerException.class);
+        Try.testFail(() -> new ProbabilityDistributionEditor<>(cat, probs, 0), IllegalArgumentException.class);
+        Try.testFail(() -> new ProbabilityDistributionEditor<>(cat, new double[] {0.1, 0.3, 0.5}, 2),
+                IllegalArgumentException.class);
+        Try.testFail(() -> new ProbabilityDistributionEditor<>(cat, new double[] {-0.1, 0.3, 0.7}, 2),
+                IllegalArgumentException.class);
+        Try.testFail(() -> new ProbabilityDistributionEditor<>(List.of("A", "B", "B"), probs, 2),
+                IllegalArgumentException.class);
+
+        ProbabilityDistributionEditor<String> edit = new ProbabilityDistributionEditor<>(cat, probs, 2);
+        edit.setCategoryFontSize(11.0f);
+        edit.setCategoryLabelFunction((t, p) -> "");
+        Try.testFail(() -> edit.setCategoryLabelFunction(null), NullPointerException.class);
+        Try.testFail(() -> edit.setCategoryFontSize(0.0f), IllegalArgumentException.class);
+        double[] probsOut = edit.getProbabilities();
+        for (int i = 0; i < probs.length; i++)
+        {
+            assertEquals(probs[i], probsOut[i], 1e-9, "Output probabilities not correct.");
+            assertEquals(probs[i], edit.getProbability(i), 1e-9, "Output probability not correct.");
+            assertEquals(probs[i], edit.getProbability(cat.get(i)), 1e-9, "Output probability not correct.");
+        }
+        Try.testFail(() -> edit.getProbability(-1), IndexOutOfBoundsException.class);
+        Try.testFail(() -> edit.getProbability(cat.size()), IndexOutOfBoundsException.class);
+        Try.testFail(() -> edit.getProbability("D"), IllegalArgumentException.class);
+
+        ProbabilityDistributionEditor<Integer> edit2 = new ProbabilityDistributionEditor<>(List.of(1, 2, 3), probs, 2);
+        edit2.getProbability(1);
+    }
+
+    /**
+     * Main.
+     * @param args arguments (ignored)
+     */
+    public static void main(final String[] args)
+    {
+        // frame
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(600, 200));
+
+        // distribution editor
+        JPanel panel = new JPanel(new BorderLayout());
+        frame.add(panel);
+        ProbabilityDistributionEditor<GtuType> slider =
+                new ProbabilityDistributionEditor<>(List.of(DefaultsNl.CAR, DefaultsNl.VAN, DefaultsNl.BUS, DefaultsNl.TRUCK),
+                        new double[] {0.4, 0.3, 0.25, 0.05}, 2);
+        slider.setCategoryFontSize(11.0f);
+        slider.setCategoryLabelFunction((g, p) -> String.format("%s %.1f%%", g.getId().replace("NL.", "").toLowerCase(), p * 100.0));
+        // slider.setPaintTicks(false);
+        // slider.setPaintLabels(true);
+        panel.add(slider, BorderLayout.PAGE_START);
+
+        // labels outside of slider
+        JPanel labelsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        labelsPanel.setPreferredSize(new Dimension(600, 60));
+        panel.add(labelsPanel, BorderLayout.PAGE_END);
+        JLabel label1 = new JLabel();
+        labelsPanel.add(label1);
+        JLabel label2 = new JLabel();
+        labelsPanel.add(label2);
+        JLabel label3 = new JLabel();
+        labelsPanel.add(label3);
+        JLabel label4 = new JLabel();
+        labelsPanel.add(label4);
+        JLabel label5 = new JLabel();
+        labelsPanel.add(label5);
+
+        // listener to update labels outside of slider
+        FinalValueChangeListener listener = new FinalValueChangeListener()
+        {
+            @Override
+            public void stateChanged(final ChangeEvent e)
+            {
+                label1.setText(String.format("Car: %.1f", slider.getProbability(DefaultsNl.CAR) * 100.0));
+                label2.setText(String.format("Van: %.1f", slider.getProbability(DefaultsNl.VAN) * 100.0));
+                label3.setText(String.format("Bus: %.1f", slider.getProbability(DefaultsNl.BUS) * 100.0));
+                label4.setText(String.format("Truck: %.1f", slider.getProbability(DefaultsNl.TRUCK) * 100.0));
+                double[] p = slider.getProbabilities();
+                label5.setText(
+                        String.format("All: [%.1f, %.1f, %.1f, %.1f]", p[0] * 100.0, p[1] * 100.0, p[2] * 100.0, p[3] * 100.0));
+            }
+        };
+        slider.addFinalValueChangeListener(listener);
+
+        // finalization
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        listener.stateChanged(null);
+        frame.setVisible(true);
+    }
+
+}
