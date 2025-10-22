037bd7c350ae874136baa79f0749440c889d1887
Marvin Baumann <marvin.baumann@kit.edu>
2025-10-22 08:23:54 +0200

    example concrete class for vehicle definition
    Signed-off-by: Marvin Baumann <marvin.baumann@kit.edu>

--- ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/mirova/core/VehicleTypes/HumanDriverVehicle.java
+++ ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/mirova/core/VehicleTypes/HumanDriverVehicle.java
@@ -5,28 +5,36 @@ import org.opentrafficsim.road.gtu.lane.LaneBasedGtu;
 import org.opentrafficsim.road.gtu.lane.perception.LanePerception;
 import org.opentrafficsim.road.gtu.lane.tactical.following.CarFollowingModel;
 import org.opentrafficsim.road.gtu.lane.tactical.mirova.MirovaTacticalPlanner;
-import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.DrivingTask.Cruise.BasicHuman.BasicHuman;
-import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.DrivingTask.MergeFromRamp.MergingHidas.MergingHidas;
-import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.DrivingTask.SupportMerge.SupportMergeHidas.SupportMergeHidas;
+import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.KnowledgeChunk.CongestionChunk.CongestionChunk;
+import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.KnowledgeChunk.DiscretionaryLaneChangeChunk.DiscretionaryLaneChangeChunk;
+import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.KnowledgeChunk.MandatoryLaneChangeChunk.MandatoryLaneChangeChunk;
+import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.KnowledgeChunk.MergeCooperationChunk.MergeCooperationChunk;
+import org.opentrafficsim.road.gtu.lane.tactical.mirova.core.KnowledgeChunk.SocialInteractionsChunk.SocialInteractionsChunk;
 
-public class HumanDrivenVehicle extends AbstractMirovaVehicle
-{
+/**
+ * HumanDriverVehicle.java.
+ *  */
+public class HumanDriverVehicle extends AbstractMirovaVehicle {
 
-    public HumanDrivenVehicle(final CarFollowingModel carFollowingModel, final LaneBasedGtu gtu,
-            final LanePerception lanePerception, final MirovaTacticalPlanner tacticalPlanner) throws OperationalPlanException
+    /**
+     * @param model
+     * @param gtu
+     * @param perception
+     * @param planner
+     * @throws OperationalPlanException
+     */
+    public HumanDriverVehicle(final CarFollowingModel model, final LaneBasedGtu gtu,
+                              final LanePerception perception, final MirovaTacticalPlanner planner)
+            throws OperationalPlanException
     {
-        super(carFollowingModel, gtu, lanePerception, tacticalPlanner);
+        super(model, gtu, perception, planner);
+
+        // knowledge configuration
+        addKnowledgeChunk(new DiscretionaryLaneChangeChunk(this));
+        addKnowledgeChunk(new MandatoryLaneChangeChunk(this));
+        addKnowledgeChunk(new SocialInteractionsChunk(this));
+        addKnowledgeChunk(new MergeCooperationChunk(this));
+        addKnowledgeChunk(new CongestionChunk(this));
     }
 
-    @Override
-    protected void initializeDrivingTasks() throws OperationalPlanException
-    {
-        this.listDrivingTasks.add(new MergingHidas(this));
-        this.listDrivingTasks.add(new SupportMergeHidas(this));
-        this.listDrivingTasks.add(new BasicHuman(this));
-        // Initialize driving tasks specific to human-driven vehicles here
-        // Example:
-        // this.dictDrivingTasks.put("CarFollowing", new CarFollowingTask());
-        // this.dictDrivingTasks.put("LaneKeeping", new LaneKeepingTask());
-    }
 }
