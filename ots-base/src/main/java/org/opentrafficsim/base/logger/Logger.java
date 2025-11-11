fcb4cd21d0ec7cac228be92495e106fd3991b1a3
WJSchakel <w.j.schakel@tudelft.nl>
2025-11-11 04:16:42 +0100

    Logger supports thread-specific time, PID, and Thread id. (Issue #240)

--- ots-base/src/main/java/org/opentrafficsim/base/logger/Logger.java
+++ ots-base/src/main/java/org/opentrafficsim/base/logger/Logger.java
@@ -8,15 +8,21 @@ import org.djutils.logger.CategoryLogger;
 import org.djutils.logger.CategoryLogger.DelegateLogger;
 import org.djutils.logger.LogCategory;
 
+import ch.qos.logback.classic.Level;
+
 /**
- * Logger for within OTS context. This logger uses category OTS and a format that may include simulation time. Every simulator
- * in OTS can attach itself as simulation time supplier through {@code Logger.setSimtimeSupplier()}. Example usage:
+ * Logger for within OTS context. This logger uses category OTS and a format that may include simulation time, PID and thread
+ * id. Every simulator in OTS can attach itself as simulation time supplier through {@code Logger.setSimtimeSupplier()}. The
+ * time supplier only applies to the {@link Thread} that creates it, and all threads created downstream of this thread. This
+ * means that when different simulations run within a single JVM, each log line will report the time of the relevant simulation.
+ * This does require that the simulator (or animator) is created in a dedicated thread for the parallel simulation. Example
+ * usage:
  *
  * <pre>
  * Logger.ots().trace("Logging example");
  * </pre>
  * <p>
- * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2025-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
@@ -28,22 +34,46 @@ public final class Logger
     public static final LogCategory OTS = new LogCategory("OTS");
 
     /** OTS logging pattern which includes the simulation time. */
-    private static final String PATTERN = "%date{HH:mm:ss}%X{time} %-5level %-3logger{0} %class{0}.%method:%line - %msg%n";
+    private static final String PATTERN =
+            "%date{HH:mm:ss}%X{time}%X{pid}%X{thread} %-5level %-3logger{0} %class{0}.%method:%line - %msg%n";
 
     /** Delegate logger with OTS category. */
     private static final DelegateLogger LOGGER;
 
+    /** Default time format. */
+    private static final String DEFAULT_TIME_FORMAT = "[%8.3fs]";
+
     /** Supplier of simulation time. */
-    private static Supplier<? extends Number> simTimeSupplier;
+    private static InheritableThreadLocal<Supplier<? extends Number>> simTimeSupplier = new InheritableThreadLocal<>();
 
     /** Format to display the time. */
-    private static String timeFormatString;
+    private static InheritableThreadLocal<String> timeFormatString = new InheritableThreadLocal<>();
+
+    /** Default PID format. */
+    private static final String DEFAULT_PID_FORMAT = "pid=%d";
+
+    /** Include PID. */
+    private static boolean includePid = false;
+
+    /** PID format. */
+    private static String pidFormat;
+
+    /** Default thread id format. */
+    private static final String DEFAULT_THREAD_ID_FORMAT = "thread=%d";
+
+    /** Include thread id. */
+    private static boolean includeThreadId = false;
+
+    /** Thread id format. */
+    private static String threadIdFormat;
 
     static
     {
         CategoryLogger.addLogCategory(OTS);
         CategoryLogger.setPattern(OTS, PATTERN);
         CategoryLogger.addFormatter(OTS, "time", Logger::getSimTimeString);
+        CategoryLogger.addFormatter(OTS, "pid", Logger::getPidString);
+        CategoryLogger.addFormatter(OTS, "thread", Logger::getThreadIdString);
         LOGGER = CategoryLogger.with(OTS);
     }
 
@@ -65,44 +95,35 @@ public final class Logger
     }
 
     /**
-     * Returns the string of the simulation time for the log message.
-     * @return the string of the simulation time for the log message
+     * Set log level of the OTS logger.
+     * @param level log level
      */
-    private static String getSimTimeString()
+    public static void setLogLevel(final Level level)
     {
-        if (simTimeSupplier == null)
-        {
-            return "";
-        }
-        try
-        {
-            return String.format(Locale.US, timeFormatString, simTimeSupplier.get().doubleValue());
-        }
-        catch (IllegalFormatException ex)
-        {
-            return " [FORMAT_ERR]";
-        }
+        CategoryLogger.setLogLevel(OTS, level);
     }
 
     /**
-     * Sets supplier of the simulation time. The value is formatted with {@code [%8.3fs]}.
+     * Sets supplier of the simulation time. The value is formatted with {@code [%8.3fs]}. The time supplier only applies to the
+     * {@code Thread} that creates it, and all threads created downstream of this thread.
      * @param timeSupplier supplier of the simulation time
      */
     public static void setSimTimeSupplier(final Supplier<? extends Number> timeSupplier)
     {
-        setSimTimeSupplier(timeSupplier, " [%8.3fs]");
+        setSimTimeSupplier(timeSupplier, DEFAULT_TIME_FORMAT);
     }
 
     /**
      * Sets supplier of the simulation time with dedicated format string. If the format string does not start with a blank, a
-     * blank is added at the beginning of the format string.
+     * blank is added at the beginning of the format string. The time supplier only applies to the {@code Thread} that creates
+     * it, and all threads created downstream of this thread.
      * @param timeSupplier supplier of the simulation time
      * @param timeFormat format string for the time value
      */
     public static void setSimTimeSupplier(final Supplier<? extends Number> timeSupplier, final String timeFormat)
     {
-        simTimeSupplier = timeSupplier;
-        timeFormatString = timeFormat.length() == 0 || timeFormat.charAt(0) == ' ' ? timeFormat : " " + timeFormat;
+        simTimeSupplier.set(timeSupplier);
+        timeFormatString.set(formatWithBlank(timeFormat));
     }
 
     /**
@@ -111,10 +132,120 @@ public final class Logger
      */
     public static void removeSimTimeSupplier(final Supplier<? extends Number> timeSupplier)
     {
-        if (timeSupplier != null && timeSupplier.equals(simTimeSupplier))
+        if (timeSupplier != null && timeSupplier.equals(simTimeSupplier.get()))
         {
-            simTimeSupplier = null;
+            simTimeSupplier.remove();
         }
+    }
+
+    /**
+     * Returns the string of the simulation time for the log message.
+     * @return the string of the simulation time for the log message
+     */
+    private static String getSimTimeString()
+    {
+        Supplier<? extends Number> timeSupplier = simTimeSupplier.get();
+        try
+        {
+            return timeSupplier == null ? ""
+                    : String.format(Locale.US, timeFormatString.get(), timeSupplier.get().doubleValue());
+        }
+        catch (IllegalFormatException ex)
+        {
+            return " T_FORMAT_ERR";
+        }
+    }
+
+    /**
+     * Sets the PID to be included in formatting or not. The default format is "pid=%d". To provide a format string use
+     * {@code includePid(String)}.
+     * @param include whether to include the PID in formatting
+     */
+    public static void includePid(final boolean include)
+    {
+        if (include)
+        {
+            includePid(DEFAULT_PID_FORMAT);
+            return;
+        }
+        includePid = false;
+    }
+
+    /**
+     * Sets the PID to be included in formatting with the given format.
+     * @param format format for PID
+     */
+    public static void includePid(final String format)
+    {
+        includePid = true;
+        pidFormat = formatWithBlank(format);
+    }
+
+    /**
+     * Returns the pid (process id).
+     * @return pid
+     */
+    private static String getPidString()
+    {
+        try
+        {
+            return includePid ? String.format(pidFormat, ProcessHandle.current().pid()) : "";
+        }
+        catch (IllegalFormatException ex)
+        {
+            return " PID_FORMAT_ERR";
+        }
+    }
+
+    /**
+     * Sets the thread id to be included in formatting or not. The default format is "thread=%d". To provide a format string use
+     * {@code includeThreadId(String)}.
+     * @param include whether to include the thread id in formatting
+     */
+    public static void includeThreadId(final boolean include)
+    {
+        if (include)
+        {
+            includeThreadId(DEFAULT_THREAD_ID_FORMAT);
+            return;
+        }
+        includeThreadId = false;
+    }
+
+    /**
+     * Sets the PID to be included in formatting with the given format.
+     * @param format format for PID
+     */
+    public static void includeThreadId(final String format)
+    {
+        includeThreadId = true;
+        threadIdFormat = formatWithBlank(format);
+    }
+
+    /**
+     * Returns the thread id.
+     * @return thread id
+     */
+    private static String getThreadIdString()
+    {
+        try
+        {
+            return includeThreadId ? String.format(threadIdFormat, Thread.currentThread().getId()) : "";
+        }
+        catch (IllegalFormatException ex)
+        {
+            return " THREAD_FORMAT_ERR";
+        }
+    }
+
+    /**
+     * Prepends a non-empty format with a blank space if it does not already starts with a blank.
+     * @param format format
+     * @return format that starts with a blank (if not empty)
+     */
+    private static String formatWithBlank(final String format)
+    {
+        return format.length() == 0 || format.charAt(0) == ' ' ? format : " " + format;
     }
 
 }
--- ots-base/src/test/java/org/opentrafficsim/base/logger/LoggerTester.java
+++ ots-base/src/test/java/org/opentrafficsim/base/logger/LoggerTester.java
@@ -1,13 +1,15 @@
 package org.opentrafficsim.base.logger;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.djutils.logger.CategoryLogger;
 import org.djutils.logger.CategoryLogger.CategoryAppenderFactory;
-import org.djutils.logger.CategoryLogger.ConsoleAppenderFactory;
 import org.djutils.logger.LogCategory;
 import org.junit.jupiter.api.Test;
 
@@ -32,22 +34,10 @@ public final class LoggerTester
     /** Test appender which stores log lines in a list so they can be checked. */
     private static StringListAppender<ILoggingEvent> appender = new StringListAppender<>();
 
-    /**
-     * Constructor.
-     */
-    private LoggerTester()
-    {
-        //
-    }
-
-    /**
-     * Tests OTS logger.
-     */
-    @Test
-    public void testLogger()
+    static
     {
         Logger.ots(); // adds the OTS log category by loading the class
-        CategoryLogger.setLogLevelAll(Level.TRACE);
+        Logger.setLogLevel(Level.TRACE);
         CategoryLogger.removeAppender("CONSOLE"); // disable console logging
         CategoryLogger.addAppender("TEST", new CategoryAppenderFactory()
         {
@@ -69,59 +59,223 @@ public final class LoggerTester
                 return appender;
             }
         });
+    }
 
+    /**
+     * Constructor.
+     */
+    private LoggerTester()
+    {
+        //
+    }
+
+    /**
+     * Tests OTS logger.
+     */
+    @Test
+    public void testTime()
+    {
+        // Setting log level
+        int preLength = appender.strList.size();
+        Logger.setLogLevel(Level.ERROR);
+        Logger.ots().warn("level_message");
+        assertEquals(preLength, appender.strList.size());
+        Logger.setLogLevel(Level.TRACE);
+
+        // Test a message
         Logger.ots().trace("test_message");
-        assertContains("test_message", "Message did not end up in log line.");
+        assertContains(-1, "test_message", "Message did not end up in log line.");
 
+        // Test removing no or not previously added supplier; should do nothing
         Supplier<Double> simulationTimeSupplier = () -> 3.14;
-        Logger.removeSimTimeSupplier(null); // should do nothing
-        Logger.removeSimTimeSupplier(simulationTimeSupplier); // should do nothing
+        Logger.removeSimTimeSupplier(null);
+        Logger.removeSimTimeSupplier(simulationTimeSupplier);
 
+        // Test time in log
         Logger.setSimTimeSupplier(simulationTimeSupplier);
         Logger.ots().trace("test_message");
-        assertContains("[   3.140s]", "Simulation time did not end up in log line, or not correctly formatted.");
+        assertContains(-1, "[   3.140s]", "Simulation time did not end up in log line, or not correctly formatted.");
 
+        // Test time from other supplier
         Supplier<Double> anotherSimulationTimeSupplier = () -> 2.14;
         Logger.setSimTimeSupplier(anotherSimulationTimeSupplier);
         Logger.ots().trace("test_message");
-        assertContains("[   2.140s]", "Setting another simulation time provider should change the simulation time.");
+        assertContains(-1, "[   2.140s]", "Setting another simulation time provider should change the simulation time.");
 
+        // Test time remains after removing old supplier (which should do nothing)
         Logger.removeSimTimeSupplier(simulationTimeSupplier);
         Logger.ots().trace("test_message");
-        assertContains("[   2.140s]", "Removing non-active simulation time provider should remove nothing.");
+        assertContains(-1, "[   2.140s]", "Removing non-active simulation time provider should remove nothing.");
 
+        // Test no time after removing new supplier
         Logger.removeSimTimeSupplier(anotherSimulationTimeSupplier);
         Logger.ots().trace("test_message");
         assertNotContains("[   2.140s]", "Removing simulation time provider should remove simulation time.");
 
-        Logger.setSimTimeSupplier(simulationTimeSupplier, "[%6.3fs]");
+        // Test custom format without blank
+        Logger.setSimTimeSupplier(simulationTimeSupplier, "[%6.2fs]");
         Logger.ots().trace("test_message");
-        assertContains(" [ 3.140s]", "Dedicated format without blank should format the simulation time.");
+        assertContains(-1, " [  3.14s]", "Dedicated format without blank should format the simulation time.");
 
-        Logger.setSimTimeSupplier(anotherSimulationTimeSupplier, " [%6.3fs]");
+        // Test custom format with blank
+        Logger.setSimTimeSupplier(anotherSimulationTimeSupplier, " [%6.2fs]");
         Logger.ots().trace("test_message");
-        assertContains(" [ 2.140s]", "Dedicated format with blank should format the simulation time.");
+        assertContains(-1, " [  2.14s]", "Dedicated format with blank should format the simulation time.");
 
+        // Test format problem
         Logger.setSimTimeSupplier(simulationTimeSupplier, "[%6.@#$3fs]");
         Logger.ots().trace("test_message");
-        assertContains(" [FORMAT_ERR]", "Bad format should end up as FORMAT_ERR in log.");
+        assertContains(-1, " T_FORMAT_ERR", "Bad format should end up as FORMAT_ERR in log.");
         assertNotContains(" [ 3.140s]", "Simulation time should not end up in log with a bad format.");
 
+        // Test empty format meaning no time (and no blank) is included
         Logger.setSimTimeSupplier(simulationTimeSupplier, "");
         Logger.ots().trace("test_message");
         assertNotContains("  ", "Empty format string should not result in double spaces.");
+    }
 
-        CategoryLogger.addAppender("CONSOLE", new ConsoleAppenderFactory("CONSOLE")); // reconstruct console logging
+    /**
+     * Tests that parallel simulations and the threads that run them report time specific to the simulation.
+     */
+    @Test
+    public void testParallel()
+    {
+        // Store results of threads (assertTrue in other than JUNIT thread does nothing for the test itself)
+        List<Integer> indexList = new ArrayList<>();
+        List<String> matchList = new ArrayList<>();
+        List<String> messageList = new ArrayList<>();
+
+        // Create simulation threads (parents) and for each a child thread, with logging in all these threads
+        List<Thread> threads = new ArrayList<>();
+        List<Double> time = new ArrayList<>(); // test time per thread (also for its child thread)
+        for (int i = 0; i < 5; i++)
+        {
+            time.add((double) i);
+            final int j = i; // final for in lambda
+            threads.add(new Thread(() ->
+            {
+                // Set time supplier and string to match for this parent thread
+                Logger.setSimTimeSupplier(() -> time.get(j));
+                String match = " [   " + j + ".000s]";
+                synchronized (appender)
+                {
+                    Logger.ots().info("test_message");
+                    indexList.add(appender.strList.size() - 1);
+                    matchList.add(match);
+                    messageList.add("Thread did not log its own time.");
+                }
+                // Child thread logging hopefully on the same time
+                Thread child = new Thread(() ->
+                {
+                    synchronized (appender)
+                    {
+                        Logger.ots().info("test_message");
+                        indexList.add(appender.strList.size() - 1);
+                        matchList.add(match);
+                        messageList.add("Thread child did not log its parent time.");
+                    }
+                });
+                child.run();
+                // Wait for child to finish
+                try
+                {
+                    child.join();
+                }
+                catch (InterruptedException exception)
+                {
+                    throw new RuntimeException(exception);
+                }
+            }));
+        }
+
+        // Start all threads
+        for (Thread thread : threads)
+        {
+            thread.start();
+        }
+
+        // The test thread, which created all simulation threads, should not have a time
+        synchronized (appender)
+        {
+            Logger.ots().trace("test_message");
+            assertNotContains("[", "Non-simulation thread should not contain time.");
+        }
+
+        // Join all threads (wait for them to finish)
+        for (Thread thread : threads)
+        {
+            try
+            {
+                thread.join();
+            }
+            catch (InterruptedException exception)
+            {
+                throw new RuntimeException(exception);
+            }
+        }
+
+        // Check results
+        for (int i = 0; i < indexList.size(); i++)
+        {
+            assertContains(indexList.get(i), matchList.get(i), messageList.get(i));
+        }
+    }
+
+    /**
+     * Tests that PID and Thread id are reported.
+     */
+    @Test
+    public void testIds()
+    {
+        // Differently formatted PID's
+        Logger.includePid(true);
+        Logger.ots().trace("test_message");
+        assertContains(-1, "pid=" + String.valueOf(ProcessHandle.current().pid()), "PID not logged.");
+        Logger.includePid("pid:%d");
+        Logger.ots().trace("test_message");
+        assertNotContains("pid=" + String.valueOf(ProcessHandle.current().pid()), "PID logged with old format.");
+        assertContains(-1, "pid:" + String.valueOf(ProcessHandle.current().pid()), "PID not logged with new format.");
+        Logger.includePid(false);
+        Logger.ots().trace("test_message");
+        assertNotContains("pid", "PID logged although logging it was disabled.");
+
+        // Bad PID format
+        Logger.includePid("pid:%@#$d");
+        Logger.ots().trace("test_message");
+        assertContains(-1, "PID_FORMAT_ERR", "PID format error not reported.");
+        assertNotContains("pid", "Bad format still results in a reported pid.");
+        Logger.includePid(false); // reset
+
+        // Differently formatted thread ids
+        Logger.includeThreadId(true);
+        Logger.ots().trace("test_message");
+        assertContains(-1, "thread=" + String.valueOf(Thread.currentThread().getId()), "Thread id not logged.");
+        Logger.includeThreadId("thread:%d");
+        Logger.ots().trace("test_message");
+        assertNotContains("thread=" + String.valueOf(Thread.currentThread().getId()), "Thread id logged with old format.");
+        assertContains(-1, "thread:" + String.valueOf(Thread.currentThread().getId()), "Thread id not logged with new format.");
+        Logger.includeThreadId(false);
+        Logger.ots().trace("test_message");
+        assertNotContains("thread", "Thread id logged although logging it was disabled.");
+
+        // Bad Thread id format
+        Logger.includeThreadId("thread:%@#$d");
+        Logger.ots().trace("test_message");
+        assertContains(-1, "THREAD_FORMAT_ERR", "Thread id format error not reported.");
+        assertNotContains("pid", "Bad format still results in a reported thread id.");
+        Logger.includeThreadId(false); // reset
     }
 
     /**
      * Asserts that the given contains string is contained in the last line of the log.
+     * @param index index of the log message, use {@code -1} for the last
      * @param contains contains string
      * @param message test fail message
      */
-    private void assertContains(final String contains, final String message)
+    private void assertContains(final int index, final String contains, final String message)
     {
-        assertTrue(appender.strList.get(appender.strList.size() - 1).contains(contains), message);
+        int ndx = index < 0 ? appender.strList.size() - 1 : index;
+        assertTrue(appender.strList.get(ndx).contains(contains), message);
     }
 
     /**
