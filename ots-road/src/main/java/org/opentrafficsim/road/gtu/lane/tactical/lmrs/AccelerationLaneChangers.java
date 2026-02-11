fa4dfaa3d0a4d7df5eb57fe7b6e7db024024efab
WJSchakel <w.j.schakel@tudelft.nl>
2026-02-11 17:30:26 +0100

    Fix: AccelerationLaneChangers should then return result of lanes.

--- ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/lmrs/AccelerationLaneChangers.java
+++ ots-road/src/main/java/org/opentrafficsim/road/gtu/lane/tactical/lmrs/AccelerationLaneChangers.java
@@ -53,7 +53,7 @@ public final class AccelerationLaneChangers implements AccelerationIncentive, St
                 }
             }
         }
-        return NO_REASON;
+        return a;
     }
 
     @Override
