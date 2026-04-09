2e8c5152d564290350a38694be24b2959d86de02
WJSchakel <w.j.schakel@tudelft.nl>
2026-04-09 23:49:21 +0200

    Make TimeStampedObject comparable. (Issue #293)

--- ots-base/src/main/java/org/opentrafficsim/base/TimeStampedObject.java
+++ ots-base/src/main/java/org/opentrafficsim/base/TimeStampedObject.java
@@ -1,6 +1,7 @@
 package org.opentrafficsim.base;
 
 import org.djunits.value.vdouble.scalar.Duration;
+import org.djutils.exceptions.Throw;
 
 /**
  * An object with a time stamp, where the object is of a specific class.
@@ -12,9 +13,27 @@ import org.djunits.value.vdouble.scalar.Duration;
  * @author <a href="https://github.com/peter-knoppers">Peter Knoppers</a>
  * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
  * @param <T> the time stamped object class.
- * @param object the object.
- * @param timestamp the simulation time stamp.
+ * @param object the object
+ * @param timestamp the simulation time stamp
  */
-public record TimeStampedObject<T>(T object, Duration timestamp)
+public record TimeStampedObject<T>(T object, Duration timestamp) implements Comparable<TimeStampedObject<?>>
 {
+
+    /**
+     * Constructor.
+     * @param object the object
+     * @param timestamp the simulation time stamp
+     */
+    public TimeStampedObject
+    {
+        Throw.whenNull(object, "object");
+        Throw.whenNull(timestamp, "timestamp");
+    }
+
+    @Override
+    public int compareTo(final TimeStampedObject<?> o)
+    {
+        return this.timestamp.compareTo(o.timestamp);
+    }
+
 }
