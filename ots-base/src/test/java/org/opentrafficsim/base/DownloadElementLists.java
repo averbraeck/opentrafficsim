8136fb3bfa97ac6fb75bcd9ff44a0d8f9601a8f9
WJSchakel <w.j.schakel@tudelft.nl>
2026-08-29 02:02:45 +0200

    Reconsider javadoc links for unreliable servers. (Issue #324)

---
+++ ots-base/src/test/java/org/opentrafficsim/base/DownloadElementLists.java
@@ -0,0 +1,107 @@
+package org.opentrafficsim.base;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.opentrafficsim.base.logger.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Downloads {@code element-list} files from each {@code <offlineLink>} tag in the main pom.xml. These are subsequently used as
+ * part of {@code maven-javadoc-plugin}.
+ * <p>
+ * Copyright (c) 2026-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.<br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * @author Wouter Schakel
+ */
+public final class DownloadElementLists
+{
+
+    /**
+     * Constructor.
+     */
+    private DownloadElementLists()
+    {
+        //
+    }
+
+    /**
+     * Download all element-list files referenced in offlineLink tags of the pom.
+     * @param args ignored
+     * @throws Exception on unrecoverable failure
+     */
+    public static void main(final String[] args) throws Exception
+    {
+        Path baseDirectory = Paths.get(".").toAbsolutePath().normalize();
+        Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                .parse(baseDirectory.getParent().resolve("pom.xml").toFile());
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        NodeList offlineLinks = (NodeList) xpath.evaluate("//offlineLink", document, XPathConstants.NODESET);
+        HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(30))
+                .followRedirects(HttpClient.Redirect.NORMAL).build();
+        Set<String> processed = new HashSet<>();
+        for (int i = 0; i < offlineLinks.getLength(); i++)
+        {
+            Node offlineLink = offlineLinks.item(i);
+            String url = xpath.evaluate("url", offlineLink).trim();
+            String location = xpath.evaluate("location", offlineLink).trim();
+            if (url.isEmpty() || location.isEmpty() | url.contains("opentrafficsim.org"))
+            {
+                continue;
+            }
+            String key = url + "|" + location;
+            if (!processed.add(key))
+            {
+                continue;
+            }
+            String elementListUrl = url.endsWith("/") ? url + "element-list" : url + "/element-list";
+            if (!location.startsWith("${basedir}/../"))
+            {
+                Logger.ots().warn("<location> \"{}\" of <offlineLink> does not start with \"${basedir}/../\" (skipping)",
+                        location);
+                continue;
+            }
+            location = location.replace("${basedir}/..", baseDirectory.getParent().toString());
+            Path targetDirectory = baseDirectory.resolve(location).normalize();
+            Files.createDirectories(targetDirectory);
+            Path targetFile = targetDirectory.resolve("element-list");
+            System.out.println(elementListUrl);
+            try
+            {
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(elementListUrl)).timeout(Duration.ofSeconds(60))
+                        .header("User-Agent", "OTS-ElementList-Downloader").GET().build();
+                HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+                if (response.statusCode() != 200)
+                {
+                    System.err.println("HTTP " + response.statusCode() + ": " + elementListUrl);
+                    continue;
+                }
+                try (InputStream stream = response.body())
+                {
+                    Files.copy(stream, targetFile, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+            catch (Exception exception)
+            {
+                System.err.println("Failed to download " + elementListUrl + ": " + exception.getMessage());
+            }
+        }
+    }
+
+}
