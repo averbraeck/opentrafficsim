d95c6e5440cae08aca2122306f4130c9e9434c8e
WJSchakel <w.j.schakel@tudelft.nl>
2026-09-05 01:42:31 +0200

    Enhanced with actual versions rather than latest. (Issue #324)

--- ots-base/src/test/java/org/opentrafficsim/base/DownloadElementLists.java
+++ ots-base/src/test/java/org/opentrafficsim/base/DownloadElementLists.java
@@ -10,11 +10,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.opentrafficsim.base.logger.Logger;
@@ -32,6 +37,9 @@ import org.w3c.dom.NodeList;
  */
 public final class DownloadElementLists
 {
+
+    /** Property pattern. */
+    private static final Pattern PROPERTY = Pattern.compile("\\$\\{([^}]+)\\}");
 
     /**
      * Constructor.
@@ -52,6 +60,9 @@ public final class DownloadElementLists
         Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder()
                 .parse(baseDirectory.getParent().resolve("pom.xml").toFile());
         XPath xpath = XPathFactory.newInstance().newXPath();
+
+        Map<String, String> properties = getProperties(document, xpath);
+
         NodeList offlineLinks = (NodeList) xpath.evaluate("//offlineLink", document, XPathConstants.NODESET);
         HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(30))
                 .followRedirects(HttpClient.Redirect.NORMAL).build();
@@ -59,9 +70,9 @@ public final class DownloadElementLists
         for (int i = 0; i < offlineLinks.getLength(); i++)
         {
             Node offlineLink = offlineLinks.item(i);
-            String url = xpath.evaluate("url", offlineLink).trim();
+            String url = resolveProperties(xpath.evaluate("url", offlineLink).trim(), properties);
             String location = xpath.evaluate("location", offlineLink).trim();
-            if (url.isEmpty() || location.isEmpty() | url.contains("opentrafficsim.org"))
+            if (url.isEmpty() || location.isEmpty() || url.contains("opentrafficsim.org"))
             {
                 continue;
             }
@@ -102,6 +113,49 @@ public final class DownloadElementLists
                 System.err.println("Failed to download " + elementListUrl + ": " + exception.getMessage());
             }
         }
+    }
+
+    /**
+     * Returns a map of all properties in the document. E.g. {@code djutils.version} to {@code 6.1.0}.
+     * @param document document of pom file
+     * @param xpath XPath evaluator
+     * @return map of all properties in the document
+     * @throws XPathExpressionException on wrong XPath
+     */
+    private static Map<String, String> getProperties(final Document document, final XPath xpath) throws XPathExpressionException
+    {
+        Map<String, String> out = new LinkedHashMap<String, String>();
+        Node properties = (Node) xpath.evaluate("/project/properties", document, XPathConstants.NODE);
+        Node property = properties.getFirstChild();
+        while (property != null)
+        {
+            if (property.hasChildNodes())
+            {
+                out.put(property.getNodeName(), property.getFirstChild().getNodeValue());
+            }
+            property = property.getNextSibling();
+        }
+        return out;
+    }
+
+    /**
+     * Resolve properties in URL.
+     * @param url URL
+     * @param properties properties
+     * @return URL with properties resolved.
+     */
+    public static String resolveProperties(final String url, final Map<String, String> properties)
+    {
+        Matcher matcher = PROPERTY.matcher(url);
+        StringBuffer result = new StringBuffer();
+        while (matcher.find())
+        {
+            String propertyName = matcher.group(1);
+            String replacement = properties.getOrDefault(propertyName, matcher.group());
+            matcher.appendReplacement(result, Matcher.quoteReplacement(replacement));
+        }
+        matcher.appendTail(result);
+        return result.toString();
     }
 
 }
